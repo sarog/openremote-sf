15f94aa47d1f810ec544c0738f8d2053857f1018
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2008-07-12 22:32:58 +0000

    testing the svn...

---
+++ Manager/Online/src/org/openremote/manager/online/controller/Registration.java
@@ -0,0 +1,37 @@
+/*
+ * OpenRemote, the Internet-enabled Home.
+ *
+ * This work is licensed under Creative Commons Attribution-Noncommercial-Share Alike 3.0
+ * United States, http://creativecommons.org/licenses/by-nc-sa/3.0/us/
+ */
+package org.openremote.manager.online.controller;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.security.cert.Certificate;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+/**
+ * Controller servlet which handles the REST API implementation for home controller registration.
+ *
+ * TODO : document the API
+ *
+ * @author <a href="mailto:juha@juhalindfors.com">Juha Lindfors</a>
+ */
+public class Registration extends HttpServlet
+{
+
+  @Override public void doGet(HttpServletRequest request, HttpServletResponse response)
+  {
+    try
+    {
+
+    }
+    catch (Throwable t)
+    {
+
+    }
+  }
+}
