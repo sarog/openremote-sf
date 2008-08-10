0066e10e649e0325c2b5242c107ccc1a6573b96b
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2008-08-10 06:16:54 +0000

    license as GPLv3, change of headers

--- Controller/JavaServices/build/build.bat
+++ Controller/JavaServices/build/build.bat
@@ -1,9 +1,26 @@
 @echo off
 
-REM   OpenRemote, the Internet-enabled Home.
+REM ###########################################################################
+REM OpenRemote, the Home of the Digital Home.
+REM Copyright 2008, OpenRemote Inc.
 REM
-REM   This work is licensed under Creative Commons Attribution-Noncommercial-Share Alike 3.0
-REM   United States, http://creativecommons.org/licenses/by-nc-sa/3.0/us/
+REM See the contributors.txt file in the distribution for a
+REM full listing of individual contributors.
+REM
+REM This is free software; you can redistribute it and/or modify it
+REM under the terms of the GNU General Public License as
+REM published by the Free Software Foundation; either version 3.0 of
+REM the License, or (at your option) any later version.
+REM
+REM This software is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+REM
+REM You should have received a copy of the GNU General Public
+REM License along with this software; if not, write to the Free
+REM Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+REM 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+REM ###########################################################################
 
 
 REM   Runs ANT build script.
