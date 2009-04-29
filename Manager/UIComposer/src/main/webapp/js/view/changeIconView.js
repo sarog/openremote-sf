99afbdd55629af759cee53e0648419313c006c1e
allenwei <allenwei@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-04-29 05:52:00 +0000

    commit missing files

---
+++ Manager/UIComposer/src/main/webapp/js/view/changeIconView.js
@@ -0,0 +1,29 @@
+ChangeIconView = function() {
+    return {
+        showFromBeehiveView: function(icons) {
+            $(".change_icon_from_container").hide();
+            $("#change_icon_from_beehive").show();
+			if ($("#change_icon_from_beehive :radio").length == 0 ) {
+				new EJS({url:"template/_beehiveIconList.ejs"}).update('change_icon_from_beehive',{icons:icons});
+			}
+        },
+        showFromUrlView: function() {
+            $(".change_icon_from_container").hide();
+            $("#change_icon_from_url").show();
+        },
+        showFromUploadView: function() {
+            $(".change_icon_from_container").hide();
+            $("#change_icon_from_upload").show();
+        },
+		getSelectIconSrc: function(){
+			return $("#change_icon_from_beehive input:checked").next("img").attr("src");
+		},
+		getInputUrl: function(){
+			return $("#icon_url_input").val();
+		},
+		getFileName: function(){
+			return $("#icon_file_name_input").val();
+		},
+		
+    };
+} ();
