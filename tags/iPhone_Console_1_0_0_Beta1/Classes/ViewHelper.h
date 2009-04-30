54eef82c30975c3e06c1257c1c3ea66c5b426ab9
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-04-30 12:16:11 +0000

    tag iPhone Console 1.0 Beta1

---
+++ tags/iPhone_Console_1_0_0_Beta1/Classes/ViewHelper.h
@@ -0,0 +1,18 @@
+//
+//  ViewHelper.h
+//  openremote
+//
+//  Created by finalist on 4/23/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ViewHelper : NSObject {
+
+}
+
++(void) showAlertViewWithTitle:(NSString *)title Message:(NSString *)message;
+
+@end
