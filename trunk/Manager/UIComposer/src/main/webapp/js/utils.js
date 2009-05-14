57d59ede22d95cd044837557acf6767455b04bcd
tomskywang <tomskywang@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-05-14 01:33:37 +0000

    modify svn version from java to javascript

--- trunk/Manager/UIComposer/src/main/webapp/js/utils.js
+++ trunk/Manager/UIComposer/src/main/webapp/js/utils.js
@@ -30,5 +30,37 @@ function isNumber () {
 	return (this.toString().match(/^[1-9]\d*$/) != null);
 }
 
-
+String.prototype.trim = trim;
+function trim(){
+	return this.replace(/(^\s*)|(\s*$)/g, "");
+} 
+function getVersionLabel(){
+	var headUrl = "$HeadURL: https://openremote.svn.sourceforge.net/svnroot/openremote/trunk/Manager/UIComposer/src/main/webapp/js/utils.js $";
+	var revision = "$Revision: 439 $";
+	var result = "Untagged";
+    var verStr = "";
+    var tagStart = -1;
+    if ((tagStart = headUrl.indexOf("tags")) >= 0) {
+       var tagsEnd = headUrl.indexOf("/", tagStart + 5);
+       if (tagsEnd >= 0) {
+          verStr = headUrl.substring(tagStart + 5, tagsEnd);
+       }
+    } else if ((tagStart = headUrl.indexOf("branches")) >= 0) {
+       var tagsEnd = headUrl.indexOf("/", tagStart + 9);
+       if (tagsEnd >= 0) {
+          verStr = headUrl.substring(tagStart + 9, tagsEnd);
+          verStr = "Branch: " + verStr;
+       }
+    } else if (revision != null) {
+       tagStart = revision.indexOf("$Revision:");
+       var tagsEnd = revision.indexOf("$", tagStart + 10);
+       if (tagsEnd >= 0) {
+          verStr = " r" + revision.substring(tagStart + 10, tagsEnd).trim();
+       }
+    }
+    if (verStr.length != 0) {
+       result = verStr.replace('_', '.');
+    }
+    return result;
+}
 
