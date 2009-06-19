518a884d5c1948cc0924ad4ecea19bea8e3d9be6
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-06-19 12:33:16 +0000

    Added license. IDEA also reformatted code to remove hard tabs

--- Manager/UIComposer/src/main/webapp/js/controller/protocolController.js
+++ Manager/UIComposer/src/main/webapp/js/controller/protocolController.js
@@ -1,4 +1,22 @@
-var KNXController = function() {
+/*
+ * OpenRemote, the Home of the Digital Home. Copyright 2008-2009, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site:
+ * http://www.fsf.org.
+ *
+ * Author: <a href="mailto:">Allen Wei</a>
+ * Author: <a href="mailto:juha@openremote.org">Juha Lindfors</a>
+ */
+ var KNXController = function() {
 	return {
          /**
          * Show create knx button dialog.
@@ -9,7 +27,7 @@ var KNXController = function() {
                     'Create': KNXController.confirmCreate
                 },
                 confirmButtonName:'Create',
-				width:350
+				        width:350
             });
         },
 	    /**
@@ -83,7 +101,7 @@ var X10Controller = function() {
                     'Create': X10Controller.confirmCreate
                 },
                 confirmButtonName:'Create',
-				width:350
+				        width:350
             });
         },
 		/**
