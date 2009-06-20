9c1846a81aca4ac299ceff100e7b86b022e7a611
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-06-20 01:58:04 +0000

    add missing KNX command to model

--- trunk/Manager/UIComposer/src/main/webapp/js/model/knx.js
+++ trunk/Manager/UIComposer/src/main/webapp/js/model/knx.js
@@ -1,33 +1,43 @@
 /*
- * OpenRemote, the Home of the Digital Home. Copyright 2008, OpenRemote Inc.
+ * OpenRemote, the Home of the Digital Home. Copyright 2008-2009, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3.0 of the License, or (at your option) any later version.
+ * This is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 3.0 of the
+ * License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * You should have received a copy of the GNU General Public License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site:
- * http://www.fsf.org.
+ * You should have received a copy of the GNU General Public License along with this software; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+/*
+ * TODO
+ *
+ * @author allen.wei@finalist.cn
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  */
 var KNX = function() {
 	function KNX () {
 		var self = this;
-		//inherit from Model
+
+    //inherit from Model
 		Model.call(self);
-		
-		
-        //text ui interface display
+
+    //text ui interface display
 		self.label = "";
 		self.groupAddress = "";
-		// convenient way to get the Class name.
+		self.command = "";
 
-        /**
-         * Get HTML getElementId
-         */
+    // convenient way to get the Class name.
+
+    /**
+     * Get HTML getElementId
+     */
 		self.getElementId = function() {
 			return "knx"+self.id;
 		};	
@@ -46,7 +56,8 @@ var KNX = function() {
 		knx.id           = model.id          ;
 		knx.label        = model.label       ;
 		knx.groupAddress = model.groupAddress;
-		return knx;
+    knx.command      = model.command     ;
+    return knx;
 	};
 
 	return KNX;
