d0fe25e1d2327e4feebf74af4a1617cae79c5397
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-06-27 14:42:58 +0000

    formatting, convert some hard tabs to soft tabs

--- branches/feature/TCP-Telnet-UIComposer-juha/src/main/webapp/js/model/model.js
+++ branches/feature/TCP-Telnet-UIComposer-juha/src/main/webapp/js/model/model.js
@@ -3,15 +3,22 @@
  *
  * See the contributors.txt file in the distribution for a full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3.0 of the License, or (at your option) any later version.
+ * This is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 3.0 of the
+ *  License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * You should have received a copy of the GNU General Public License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site:
- * http://www.fsf.org.
+ * You should have received a copy of the GNU General Public License along with this software;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+/*
+ * TODO
+ *
+ * @author <a href="mailto:">Allen Wei</a>
  */
 var Model = function() {
     function Model() {
@@ -32,28 +39,30 @@ var Model = function() {
         };
 
         self.removeUpdateListener = function(view) {
-			for (var index in _updateListeners) {
-				if (compare(_updateListeners[index],view)) {
-					_updateListeners.splice(index);
-				}
-			}
+          for (var index in _updateListeners) {
+            if (compare(_updateListeners[index],view)) {
+              _updateListeners.splice(index);
+            }
+          }
         };
 
-
-
         self.removeDeleteListener = function(view) {
-           	for (var index in _deleteListeners) {
-				if (compare(_deleteListeners[index],view)) {
-					_deleteListeners.splice(index);
-				}
-			}
+          for (var index in _deleteListeners) {
+            if (compare(_deleteListeners[index],view)) {
+              _deleteListeners.splice(index);
+            }
+          }
         };
 
         function compare(obj1, obj2) {
             if (obj1 === null || obj1 === undefined || obj2 === null || obj2 === undefined) {
-                if (obj1 === obj2) return true;
-            } else
-            if (obj1 == obj2 && obj1.constructor.toString() == obj2.constructor) return true;
+              if (obj1 === obj2)
+                return true;
+            }
+            else
+              if (obj1 == obj2 && obj1.constructor.toString() == obj2.constructor)
+                return true;
+
             return false;
         }
 
@@ -67,13 +76,13 @@ var Model = function() {
         };
 
         self.deleteModel = function() {
-            for (var index in _deleteListeners) {
-                if (_deleteListeners[index] != null && _deleteListeners[index].deleteView !== undefined) {
-                    _deleteListeners[index].deleteView(self);
-                }
+          for (var index in _deleteListeners) {
+            if (_deleteListeners[index] != null && _deleteListeners[index].deleteView !== undefined) {
+              _deleteListeners[index].deleteView(self);
             }
+          }
         };
-
     }
+
     return Model;
 } ();
