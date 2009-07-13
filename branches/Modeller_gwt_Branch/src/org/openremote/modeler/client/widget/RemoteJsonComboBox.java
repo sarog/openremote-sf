457677991ed7c5fe7562a57f22c67f7ec08d5e2c
allenwei <allenwei@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-07-13 05:23:35 +0000

    add some comments

--- branches/Modeller_gwt_Branch/src/org/openremote/modeler/client/widget/RemoteJsonComboBox.java
+++ branches/Modeller_gwt_Branch/src/org/openremote/modeler/client/widget/RemoteJsonComboBox.java
@@ -1,3 +1,23 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2009, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.openremote.modeler.client.widget;
 
 import org.openremote.modeler.client.utils.NestedJsonLoadResultReader;
@@ -16,11 +36,23 @@ import com.extjs.gxt.ui.client.store.StoreEvent;
 import com.extjs.gxt.ui.client.store.StoreListener;
 import com.extjs.gxt.ui.client.widget.form.ComboBox;
 
+/**
+ * The Class RemoteJsonComboBox.
+ */
 public class RemoteJsonComboBox<D extends ModelData> extends ComboBox<D> {
 
+   /** The remote json url. */
    private String remoteJsonURL = "";
+   
+   /** The model type. */
    private ModelType modelType = null;
 
+   /**
+    * Instantiates a new remote json combo box.
+    * 
+    * @param remoteJsonURL the remote json url
+    * @param modelType the model type
+    */
    public RemoteJsonComboBox(String remoteJsonURL, ModelType modelType) {
       super();
       this.remoteJsonURL = remoteJsonURL;
@@ -28,6 +60,11 @@ public class RemoteJsonComboBox<D extends ModelData> extends ComboBox<D> {
       setup();
    }
 
+   /**
+    * Reload list store with a different url.
+    * 
+    * @param url the url
+    */
    public void reloadListStoreWithUrl(String url) {
 
       final RemoteJsonComboBox<D> box = this;
@@ -48,6 +85,9 @@ public class RemoteJsonComboBox<D extends ModelData> extends ComboBox<D> {
       loader.load();
    }
 
+   /**
+    * Setup.
+    */
    private void setup() {
       ScriptTagProxy<ListLoadResult<D>> scriptTagProxy = new ScriptTagProxy<ListLoadResult<D>>(remoteJsonURL);
       NestedJsonLoadResultReader<ListLoadResult<D>> reader = new NestedJsonLoadResultReader<ListLoadResult<D>>(
@@ -68,6 +108,7 @@ public class RemoteJsonComboBox<D extends ModelData> extends ComboBox<D> {
       addListener(Events.BeforeQuery, new Listener<FieldEvent>() {
 
          public void handleEvent(FieldEvent be) {
+            // cancel default event, we will handler query request.
             be.setCancelled(true);  
             RemoteJsonComboBox<D> box = be.getComponent();
             System.out.println(box.getRawValue());
@@ -84,9 +125,13 @@ public class RemoteJsonComboBox<D extends ModelData> extends ComboBox<D> {
       });
    }
 
+   /* (non-Javadoc)
+    * @see com.extjs.gxt.ui.client.widget.form.ComboBox#onTriggerClick(com.extjs.gxt.ui.client.event.ComponentEvent)
+    */
    @Override
    protected void onTriggerClick(ComponentEvent ce) {
       final RemoteJsonComboBox<D> box = this;
+      // click the trigger of the combobox will show all the options. 
       box.setRawValue("");
       super.onTriggerClick(ce);
       
