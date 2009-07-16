11fa0408ef600b65c63887f599dfdd4aab24d354
allenwei <allenwei@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-07-16 06:06:10 +0000

    use local cache on tree render

--- branches/Modeller_gwt_Branch/src/org/openremote/modeler/client/widget/MacroPanel.java
+++ branches/Modeller_gwt_Branch/src/org/openremote/modeler/client/widget/MacroPanel.java
@@ -1,27 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2009, OpenRemote Inc.
+/*
+ * OpenRemote, the Home of the Digital Home. Copyright 2008-2009, OpenRemote Inc.
  * 
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
+ * See the contributors.txt file in the distribution for a full listing of individual contributors.
  * 
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3.0 of
- * the License, or (at your option) any later version.
+ * This is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of the License, or (at your option) any later version.
  * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * 
- * You should have received a copy of the GNU General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU General Public License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site:
+ * http://www.fsf.org.
  */
 package org.openremote.modeler.client.widget;
 
 import org.openremote.modeler.client.icon.Icons;
 import org.openremote.modeler.client.model.TreeDataModel;
+import org.openremote.modeler.client.rpc.AsyncSuccessCallback;
 import org.openremote.modeler.client.rpc.DeviceMacroService;
 import org.openremote.modeler.client.rpc.DeviceMacroServiceAsync;
 import org.openremote.modeler.domain.DeviceMacro;
@@ -34,13 +30,11 @@ import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.mvc.AppEvent;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.extjs.gxt.ui.client.widget.treepanel.TreePanel;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * The Class MacroPanel.
@@ -49,7 +43,7 @@ public class MacroPanel extends ContentPanel {
 
    /** The device macro service. */
    private DeviceMacroServiceAsync deviceMacroService = (DeviceMacroServiceAsync) GWT.create(DeviceMacroService.class);
-   
+
    /** The icons. */
    private Icons icons = GWT.create(Icons.class);
 
@@ -81,15 +75,12 @@ public class MacroPanel extends ContentPanel {
          @Override
          public void componentSelected(ButtonEvent ce) {
             final MacroWindow macroWindow = new MacroWindow();
-            
+
             macroWindow.addSubmitListener(new Listener<AppEvent>() {
 
                public void handleEvent(AppEvent be) {
                   DeviceMacro deviceMacro = be.getData();
-                  deviceMacroService.saveDeviceMacro(deviceMacro, new AsyncCallback<DeviceMacro>() {
-                     public void onFailure(Throwable e) {
-                        MessageBox.alert("Error", e.getLocalizedMessage(), null);
-                     }
+                  deviceMacroService.saveDeviceMacro(deviceMacro, new AsyncSuccessCallback<DeviceMacro>() {
 
                      public void onSuccess(DeviceMacro deviceMacro) {
                         if (macroTree != null) {
@@ -115,34 +106,28 @@ public class MacroPanel extends ContentPanel {
          @Override
          public void componentSelected(ButtonEvent ce) {
             if (macroTree.getSelectionModel().getSelectedItem() != null) {
-                  final TreeDataModel dataModel = macroTree.getSelectionModel().getSelectedItem();
-                  if (dataModel.getData() instanceof DeviceMacro) {
-                     final MacroWindow macroWindow = new MacroWindow((DeviceMacro) dataModel.getData());
-                     macroWindow.addSubmitListener(new Listener<AppEvent>() {
-                        public void handleEvent(AppEvent be) {
-                           DeviceMacro deviceMacro = be.getData();
-                           deviceMacroService.updateDeviceMacro(deviceMacro, new AsyncCallback<DeviceMacro>() {
+               final TreeDataModel dataModel = macroTree.getSelectionModel().getSelectedItem();
+               if (dataModel.getData() instanceof DeviceMacro) {
+                  final MacroWindow macroWindow = new MacroWindow((DeviceMacro) dataModel.getData());
+                  macroWindow.addSubmitListener(new Listener<AppEvent>() {
+                     public void handleEvent(AppEvent be) {
+                        DeviceMacro deviceMacro = be.getData();
+                        deviceMacroService.updateDeviceMacro(deviceMacro, new AsyncSuccessCallback<DeviceMacro>() {
+                           public void onSuccess(DeviceMacro result) {
+                              int index = macroTree.getStore().indexOf(dataModel);
+                              macroTree.getStore().remove(dataModel);
+                              TreeDataModel newModel = createModelWithDeviceMacro(result);
+                              macroTree.getStore().insert(newModel, index, true);
+                              macroTree.setExpanded(newModel, true);
+                              macroWindow.hide();
+                              macroTree.getSelectionModel().select(newModel, false);
+                           }
+                        });
+                     }
 
-                              public void onFailure(Throwable caught) {
-                                 MessageBox.alert("Error", caught.getLocalizedMessage(), null);
-                              }
+                  });
+                  macroWindow.show();
 
-                              public void onSuccess(DeviceMacro result) {
-                                 int index = macroTree.getStore().indexOf(dataModel);
-                                 macroTree.getStore().remove(dataModel);
-                                 TreeDataModel newModel = createModelWithDeviceMacro(result);
-                                 macroTree.getStore().insert(newModel, index, true);
-                                 macroTree.setExpanded(newModel, true);
-                                 macroWindow.hide();
-                                 macroTree.getSelectionModel().select(newModel, false);
-                              }
-                           });
-                        }
-                       
-                     });
-                     macroWindow.show();
-
-                     
                }
             }
 
@@ -160,19 +145,15 @@ public class MacroPanel extends ContentPanel {
             if (macroTree.getSelectionModel().getSelectedItems().size() > 0) {
                for (final TreeDataModel data : macroTree.getSelectionModel().getSelectedItems()) {
                   if (data.getData() instanceof DeviceMacro) {
-                        DeviceMacro deviceMacro = (DeviceMacro) data.getData();
-                        deviceMacroService.deleteDeviceMacro(deviceMacro.getOid(), new AsyncCallback<Void>() {
-                           public void onFailure(Throwable caught) {
-                              MessageBox.alert("Error", caught.getLocalizedMessage(), null);
-                           }
+                     DeviceMacro deviceMacro = (DeviceMacro) data.getData();
+                     deviceMacroService.deleteDeviceMacro(deviceMacro.getOid(), new AsyncSuccessCallback<Void>() {
 
-                           public void onSuccess(Void result) {
-                              macroTree.getStore().remove(data);
-                           }
+                        public void onSuccess(Void result) {
+                           macroTree.getStore().remove(data);
+                        }
 
-                        });
-                     }
-                  
+                     });
+                  }
 
                }
             }
@@ -185,11 +166,12 @@ public class MacroPanel extends ContentPanel {
 
       setTopComponent(macroToolBar);
    }
-   
+
    /**
     * Creates the model with device macro.
     * 
-    * @param deviceMacro the device macro
+    * @param deviceMacro
+    *           the device macro
     * 
     * @return the tree data model< device macro>
     */
@@ -223,12 +205,14 @@ public class MacroPanel extends ContentPanel {
 
    }
 
-   /* (non-Javadoc)
+   /*
+    * (non-Javadoc)
+    * 
     * @see com.extjs.gxt.ui.client.widget.ContentPanel#afterExpand()
     */
    @Override
    protected void afterExpand() {
-      //TODO This is not a good way to solve tree can't display at first. 
+      // TODO This is not a good way to solve tree can't display at first.
       macroTree.expandAll();
       macroTree.collapseAll();
       super.afterExpand();
