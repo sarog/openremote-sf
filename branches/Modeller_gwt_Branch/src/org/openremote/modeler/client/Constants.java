4ee9becd7d1b4193e05d9c48972182d703f57f9f
allenwei <allenwei@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-07-17 02:47:24 +0000

    extract beehive url to config.properies

--- branches/Modeller_gwt_Branch/src/org/openremote/modeler/client/Constants.java
+++ branches/Modeller_gwt_Branch/src/org/openremote/modeler/client/Constants.java
@@ -1,6 +1,5 @@
 package org.openremote.modeler.client;
 
 public class Constants {
-   public static final String BEEHIVE_REST_URL="http://openremote.finalist.hk/beehive/rest/lirc/";
    public static final String INFRARED_TYPE = "infrared";
 }
