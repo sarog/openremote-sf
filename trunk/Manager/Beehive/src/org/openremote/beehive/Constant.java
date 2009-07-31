0e31d44fc43cfcfb602c67739ff7829fb2ebf9ce
tomskywang <tomskywang@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-07-31 08:33:15 +0000

    updates the copyright to 2008-2009 and the license changes from GPL v3 to AGPL v3

--- trunk/Manager/Beehive/src/org/openremote/beehive/Constant.java
+++ trunk/Manager/Beehive/src/org/openremote/beehive/Constant.java
@@ -1,45 +1,54 @@
 /* OpenRemote, the Home of the Digital Home.
- * Copyright 2008, OpenRemote Inc.
- * 
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
- * 
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3.0 of
- * the License, or (at your option) any later version.
- * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.beehive;
 
+// TODO: Auto-generated Javadoc
 /**
+ * The Class Constant.
+ * 
  * @author Tomsky
- *
  */
 public final class Constant {
+   
+   /**
+    * Instantiates a new constant.
+    */
    private Constant(){
    }
    
-   /**
-    * The value for the progress file
-    */
+   /** The value for the progress file. */
    public static final String SYNC_PROGRESS_FILE = "syncProgress.txt";
+   
+   /** The Constant COMMIT_PROGRESS_FILE. */
    public static final String COMMIT_PROGRESS_FILE = "commitProgress.txt";
+   
+   /** The Constant LIRC_ROOT_URL. */
    public static final String LIRC_ROOT_URL = "http://lirc.sourceforge.net/remotes/";
    
-   /**
-    * The value for svn repo and workCopy root directory
-    */
+   /** The value for svn repo and workCopy root directory. */
    public static final String ROOT_PATH = "";
+   
+   /** The Constant WORK_COPY. */
    public static final String WORK_COPY = "workCopy";
+   
+   /** The Constant SYNC_HISTORY. */
    public static final String SYNC_HISTORY = "syncHistory";
    
    
--- trunk/Manager/Beehive/src/org/openremote/beehive/api/service/CodeService.java
+++ trunk/Manager/Beehive/src/org/openremote/beehive/api/service/CodeService.java
@@ -1,23 +1,22 @@
 /* OpenRemote, the Home of the Digital Home.
- * Copyright 2008, OpenRemote Inc.
- * 
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
- * 
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3.0 of
- * the License, or (at your option) any later version.
- * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.beehive.api.service;
 
 import java.util.List;
@@ -25,17 +24,17 @@ import java.util.List;
 import org.openremote.beehive.api.dto.CodeDTO;
 
 /**
- * Business service for <code>CodeDTO</code>
+ * Business service for <code>CodeDTO</code>.
  * 
  * @author allen.wei 2009-2-18
  */
 public interface CodeService {
 
    /**
-    * Finds <code>CodeDTOs</code> according to <code>RemoteSectionDTO</code> id
+    * Finds <code>CodeDTOs</code> according to <code>RemoteSectionDTO</code> id.
     * 
-    * @param remoteSectionId
-    *           RemoteSectionDTO id
+    * @param remoteSectionId RemoteSectionDTO id
+    * 
     * @return a list of CodeDTOs
     */
    List<CodeDTO> findByRemoteSectionId(long remoteSectionId);
