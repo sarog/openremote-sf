4ca7f83e65756df438ddfc8ca576d0c58cf3bb21
acoliver <acoliver@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-08-08 23:29:44 +0000

    pretty printing and some missing comments

--- trunk/Panels/Android/src/org/openremote/android/console/HTTPUtil.java
+++ trunk/Panels/Android/src/org/openremote/android/console/HTTPUtil.java
@@ -15,26 +15,28 @@ import org.apache.http.impl.client.DefaultHttpClient;
  */
 public class HTTPUtil {
 
-	public static final String CLICK = "click";
-	public static final String PRESS = "press";
-	public static final String RELEASE = "release"; 
-	
-	/**
-	 * Sends a button action of some kind
-	 * @param url 
-	 * @param id
-	 * @param command
-	 * @return http_status_code
-	 * @throws ClientProtocolException
-	 * @throws IOException
-	 */
-	public static int sendButton(String url, String id, String command) throws ClientProtocolException, IOException {		
-		String connectString = url+"/rest/button/"+id+"/"+command;
-		HttpClient client = new DefaultHttpClient();
-		HttpPost post = new HttpPost(connectString);
-		HttpResponse response = client.execute(post);
-		int code = response.getStatusLine().getStatusCode();
-		return code;
-	}
-	
+    public static final String CLICK = "click";
+    public static final String PRESS = "press";
+    public static final String RELEASE = "release";
+
+    /**
+     * Sends a button action of some kind
+     * 
+     * @param url
+     * @param id
+     * @param command
+     * @return http_status_code
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    public static int sendButton(String url, String id, String command)
+            throws ClientProtocolException, IOException {
+        String connectString = url + "/rest/button/" + id + "/" + command;
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(connectString);
+        HttpResponse response = client.execute(post);
+        int code = response.getStatusLine().getStatusCode();
+        return code;
+    }
+
 }
--- trunk/Panels/Android/src/org/openremote/android/xml/SimpleBinder.java
+++ trunk/Panels/Android/src/org/openremote/android/xml/SimpleBinder.java
@@ -44,141 +44,183 @@ import android.util.Log;
  *
  */
 public class SimpleBinder {
-	private String root;
-	@SuppressWarnings("unchecked")
-	private List<Class> classes;
-	private List<String> ignoreNodes;
-	
-	/**
-	 * Construct an instance of SimpleBinder, you can reuse these if you have some reason to.
-	 * @param root root node element name, we could check this, right now we set it and do nothing with it.
-	 * @param classes classes to bind, intended to be a collection of bind classes
-	 * @param ignoreNodes List of strings containing the name of each node we want to ignore for binding, instead we bind the ignore node's children
-	 */
-	@SuppressWarnings("unchecked")
-	public SimpleBinder(String root, List<Class> classes, List<String> ignoreNodes) {
-		this.root = root;
-		this.classes = classes;
-		this.ignoreNodes = ignoreNodes;
-	}
-	
-	/**
-	 * The action is here, we bind things and return them in a list, recursion is used for collections.  Various exceptions are thrown due to parser,
-	 * IO, or class reflection binding issues.  
-	 * @param stream containing the XML file we're binding
-	 * @return collection of classes we bound, instances of things in the classes list passed in to the constructor
-	 * @throws ParserConfigurationException
-	 * @throws SAXException
-	 * @throws IOException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
-	 * @throws IllegalArgumentException
-	 * @throws InvocationTargetException
-	 */
-	@SuppressWarnings("unchecked")
-	public List bindStuff(InputStream stream) throws ParserConfigurationException, SAXException, IOException, IllegalAccessException, InstantiationException, IllegalArgumentException, InvocationTargetException {
-		Log.d(this.toString(), "binding stuff with root "+root);
-		List objs = new ArrayList();
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		Document dom = builder.parse(stream);
+    private String root;
+    @SuppressWarnings("unchecked")
+    private List<Class> classes;
+    private List<String> ignoreNodes;
+
+    /**
+     * Construct an instance of SimpleBinder, you can reuse these if you have
+     * some reason to.
+     * 
+     * @param root
+     *            root node element name, we could check this, right now we set
+     *            it and do nothing with it.
+     * @param classes
+     *            classes to bind, intended to be a collection of bind classes
+     * @param ignoreNodes
+     *            List of strings containing the name of each node we want to
+     *            ignore for binding, instead we bind the ignore node's children
+     */
+    @SuppressWarnings("unchecked")
+    public SimpleBinder(String root, List<Class> classes,
+            List<String> ignoreNodes) {
+        this.root = root;
+        this.classes = classes;
+        this.ignoreNodes = ignoreNodes;
+    }
+
+    /**
+     * The action is here, we bind things and return them in a list, recursion
+     * is used for collections. Various exceptions are thrown due to parser, IO,
+     * or class reflection binding issues.
+     * 
+     * @param stream
+     *            containing the XML file we're binding
+     * @return collection of classes we bound, instances of things in the
+     *         classes list passed in to the constructor
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     * @throws IOException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    @SuppressWarnings("unchecked")
+    public List bindStuff(InputStream stream)
+            throws ParserConfigurationException, SAXException, IOException,
+            IllegalAccessException, InstantiationException,
+            IllegalArgumentException, InvocationTargetException {
+        Log.d(this.toString(), "binding stuff with root " + root);
+        List objs = new ArrayList();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document dom = builder.parse(stream);
         Element root = dom.getDocumentElement();
         for (Class clazz : classes) {
-        	
-        	String nodeName = (""+clazz.getSimpleName().charAt(0)).toLowerCase()+clazz.getSimpleName().substring(1);
-//			Log.d(this.toString(),"binding class "+clazz.getSimpleName()+" as "+nodeName);
-			NodeList nodeList = root.getElementsByTagName(nodeName);
-			
-			for (int i = 0 ; i < nodeList.getLength(); i++) {
-				Node node = nodeList.item(i);
-				Object o = bindClass(clazz,node);
-				objs.add(o);
-			}
-		}
-       
+
+            String nodeName = ("" + clazz.getSimpleName().charAt(0))
+                    .toLowerCase()
+                    + clazz.getSimpleName().substring(1);
+            // Log.d(this.toString(),"binding class "+clazz.getSimpleName()+" as "+nodeName);
+            NodeList nodeList = root.getElementsByTagName(nodeName);
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                Object o = bindClass(clazz, node);
+                objs.add(o);
+            }
+        }
+
         return objs;
-	}
+    }
 
-	/**
-	 * Bind classes from a parent node's children.  The tclass parameter is what we want to bind, the parent contains the nodes we want to bind.
-	 * All types not matching the class are ignored.  By match we mean the Class.getName() with an initial lower case.  
-	 * @param tclass 
-	 * @param parent 
-	 * @return List of bound objects
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
-	 * @throws InvocationTargetException
-	 */
-	@SuppressWarnings("unchecked")
-	private List bindClasses(Class tclass, Node parent) throws IllegalArgumentException, IllegalAccessException, InstantiationException, InvocationTargetException {
-		String name = tclass.getSimpleName();
-		name = (""+name.charAt(0)).toLowerCase()+name.substring(1);
-		//Log.d(this.toString(),"bindclasses "+tclass.getName()+" for parent "+parent.getNodeName()+ " for the node "+name);
-		List list = new ArrayList();
-		NodeList nodes = parent.getChildNodes();
-		//Log.d(this.toString(),"the parent in bindClasses had "+nodes.getLength()+" children");
-		for (int i = 0; i < nodes.getLength(); i++) {
-			Node node = nodes.item(i);
-			//Log.d(this.toString(), "the node had a name "+node.getNodeName()+" which match to "+name+" is "+name.equals(node.getNodeName()));
-			if(node.getNodeName().equals(name)) {
-				Object obj = bindClass(tclass, node);
-				list.add(obj);
-			} else if (ignoreNodes.contains(node.getNodeName())) { //if it is one we're supposed to ignore we go down a level in the tree
-				nodes = node.getChildNodes(); // we don't want this, we want its children
-				i = -1; //so it will be 0 after the iteration
-			}
-		}
-		return list;
-	}
+    /**
+     * Bind classes from a parent node's children. The tclass parameter is what
+     * we want to bind, the parent contains the nodes we want to bind. All types
+     * not matching the class are ignored. By match we mean the Class.getName()
+     * with an initial lower case.
+     * 
+     * @param tclass
+     * @param parent
+     * @return List of bound objects
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws InvocationTargetException
+     */
+    @SuppressWarnings("unchecked")
+    private List bindClasses(Class tclass, Node parent)
+            throws IllegalArgumentException, IllegalAccessException,
+            InstantiationException, InvocationTargetException {
+        String name = tclass.getSimpleName();
+        name = ("" + name.charAt(0)).toLowerCase() + name.substring(1);
+        // Log.d(this.toString(),"bindclasses "+tclass.getName()+" for parent "+parent.getNodeName()+
+        // " for the node "+name);
+        List list = new ArrayList();
+        NodeList nodes = parent.getChildNodes();
+        // Log.d(this.toString(),"the parent in bindClasses had "+nodes.getLength()+" children");
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            // Log.d(this.toString(),
+            // "the node had a name "+node.getNodeName()+" which match to "+name+" is "+name.equals(node.getNodeName()));
+            if (node.getNodeName().equals(name)) {
+                Object obj = bindClass(tclass, node);
+                list.add(obj);
+            } else if (ignoreNodes.contains(node.getNodeName())) { // if it is
+                                                                   // one we're
+                                                                   // supposed
+                                                                   // to ignore
+                                                                   // we go down
+                                                                   // a level in
+                                                                   // the tree
+                nodes = node.getChildNodes(); // we don't want this, we want its
+                                              // children
+                i = -1; // so it will be 0 after the iteration
+            }
+        }
+        return list;
+    }
 
-	/**
-	 * Bind an individual class from an individual node.  This also binds any children which have a corresponding collection setter by calling back
-	 * to bindClasses(..) 
-	 * @param clazz
-	 * @param node
-	 * @return Object which is actually an instance of clazz
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
-	 * @throws IllegalArgumentException
-	 * @throws InvocationTargetException
-	 */
-	@SuppressWarnings("unchecked")
-	private Object bindClass(Class clazz, Node node) throws IllegalAccessException, InstantiationException, IllegalArgumentException, InvocationTargetException {
-		//Log.d(this.toString(),"binding class "+clazz.getName()+":"+node.getNodeName());
-		Method[] methods = clazz.getMethods();
-		Object o = clazz.newInstance();
-		NamedNodeMap attrs = node.getAttributes();
-		//Log.d(this.toString(),"the "+node.getNodeName()+" node had "+node.getAttributes().getLength()+" attributes and the class "+clazz.getName()+" had "+methods.length+" methods");
-		for (Method method : methods) {
-			if (method.getName().startsWith("set") && !method.getName().endsWith("s")) {
-				String name = method.getName().substring(4);
-				name = (""+method.getName().charAt(3)).toLowerCase() + name;
-				//Log.d(this.toString(),"getting attribute "+name);
-				if(attrs.getNamedItem(name) != null) { // if there is a value to bind
-					String val = attrs.getNamedItem(name).getNodeValue();
-					Class parm = method.getParameterTypes()[0];
-					String type = parm.getName();
-					//Log.d(this.toString(), "method is of type: "+type);
-					if (type.equals(String.class.getName())) {
-						//Log.d(this.toString(), "binding attribute as String");
-						method.invoke(o, new Object[]{val});
-					} else if (type.equals(int.class.getName()) || type.equals(Integer.class.getName())) {
-						//Log.d(this.toString(), "binding attribute as Integer");
-						method.invoke(o, new Object[]{new Integer(val)});
-					}
-				}
-			} else if (method.getName().startsWith("set") && method.getName().endsWith("s")) {
-				//Log.d(this.toString(), "binding a collection for " + method.getName());
-				Type type = method.getGenericParameterTypes()[0];
-				if (type instanceof ParameterizedType) {
-					ParameterizedType ptype = (ParameterizedType) type;
-					Class tclass = (Class)(ptype.getActualTypeArguments()[0]);
-					List list = bindClasses(tclass,node);
-					method.invoke(o, new Object[]{list});
-				}
-			}
-		}
-		return o;
-	}
+    /**
+     * Bind an individual class from an individual node. This also binds any
+     * children which have a corresponding collection setter by calling back to
+     * bindClasses(..)
+     * 
+     * @param clazz
+     * @param node
+     * @return Object which is actually an instance of clazz
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    @SuppressWarnings("unchecked")
+    private Object bindClass(Class clazz, Node node)
+            throws IllegalAccessException, InstantiationException,
+            IllegalArgumentException, InvocationTargetException {
+        // Log.d(this.toString(),"binding class "+clazz.getName()+":"+node.getNodeName());
+        Method[] methods = clazz.getMethods();
+        Object o = clazz.newInstance();
+        NamedNodeMap attrs = node.getAttributes();
+        // Log.d(this.toString(),"the "+node.getNodeName()+" node had "+node.getAttributes().getLength()+" attributes and the class "+clazz.getName()+" had "+methods.length+" methods");
+        for (Method method : methods) {
+            if (method.getName().startsWith("set")
+                    && !method.getName().endsWith("s")) {
+                String name = method.getName().substring(4);
+                name = ("" + method.getName().charAt(3)).toLowerCase() + name;
+                // Log.d(this.toString(),"getting attribute "+name);
+                if (attrs.getNamedItem(name) != null) { // if there is a value
+                                                        // to bind
+                    String val = attrs.getNamedItem(name).getNodeValue();
+                    Class parm = method.getParameterTypes()[0];
+                    String type = parm.getName();
+                    // Log.d(this.toString(), "method is of type: "+type);
+                    if (type.equals(String.class.getName())) {
+                        // Log.d(this.toString(),
+                        // "binding attribute as String");
+                        method.invoke(o, new Object[] { val });
+                    } else if (type.equals(int.class.getName())
+                            || type.equals(Integer.class.getName())) {
+                        // Log.d(this.toString(),
+                        // "binding attribute as Integer");
+                        method.invoke(o, new Object[] { new Integer(val) });
+                    }
+                }
+            } else if (method.getName().startsWith("set")
+                    && method.getName().endsWith("s")) {
+                // Log.d(this.toString(), "binding a collection for " +
+                // method.getName());
+                Type type = method.getGenericParameterTypes()[0];
+                if (type instanceof ParameterizedType) {
+                    ParameterizedType ptype = (ParameterizedType) type;
+                    Class tclass = (Class) (ptype.getActualTypeArguments()[0]);
+                    List list = bindClasses(tclass, node);
+                    method.invoke(o, new Object[] { list });
+                }
+            }
+        }
+        return o;
+    }
 }
