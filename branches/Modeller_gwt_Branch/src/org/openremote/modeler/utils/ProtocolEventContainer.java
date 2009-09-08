d3b96b67fc02a235f648036a8c931c3835d9e43f
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-09-08 06:20:19 +0000

    add javadoc.

--- branches/Modeller_gwt_Branch/src/org/openremote/modeler/utils/ProtocolEventContainer.java
+++ branches/Modeller_gwt_Branch/src/org/openremote/modeler/utils/ProtocolEventContainer.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.modeler.utils;
 
 import java.util.ArrayList;
@@ -11,10 +30,50 @@ import org.openremote.modeler.client.model.UIButtonEvent;
 import org.openremote.modeler.protocol.ProtocolContainer;
 import org.openremote.modeler.service.ProtocolParser;
 
+/**
+ * This class is used to contain <b>UIButtonEvent</b> of  different kinds of protocol types'.<br />
+ * The <b>UIButtonEvent</b> don't mean the event of GWT or some UIEvent,<br />  
+ * it's just a <b>POJO</b> class that <b>a java class of xml element(eg:&lt;irEvent name="" command="" /&gt;)</b> of <b>'controller.xml'</b> file.<br />
+ * So, iterating the property <b>protocolEvents</b> can generate the all avaliable events xml segment of different kinds of protocol types'.<br /><br />
+ * The generated xml segments' structure of events is similar following:<br />
+ * &lt;events&gt;<br />
+    &lt;x10Events&gt;<br />
+      &lt;x10Event id="" address="" command="" /&gt;<br />
+    &lt;/x10Events&gt;<br />
+    &lt;knxEvents&gt;<br />
+      &lt;knxEvent id="" command="" groupAddress="" /&gt;<br />
+    &lt;/knxEvents&gt;<br />
+    &lt;httpEvents&gt;<br />
+      &lt;httpEvent id="" url="" /&gt;<br />
+    &lt;/httpEvents&gt;<br />
+    &lt;irEvents&gt;<br />
+    &lt;/irEvents&gt;<br />
+    &lt;tcpipEvents&gt;<br />
+      &lt;tcpipEvent id="" port="" command="" ipAddress="" /&gt;<br />
+    &lt;/tcpipEvents&gt;<br />
+    &lt;telnetEvents&gt;<br />
+      &lt;telnetEvent id="" port="" command="" ipAddress="" /&gt;<br />
+    &lt;/telnetEvents&gt;<br />
+    &lt;/events&gt;<br />
+    
+ * @author handy.wang
+ */
 public class ProtocolEventContainer {
    
+   /** 
+    * This Map instance is used to contain UIButtonEvents of  different kinds of protocol types.<br />
+    * The <b>String type parameter</b> means protocol type.<br />
+    * The <b>List<UIButtonEvent></b> means all UIButtonEvents
+    */
    private Map<String, List<UIButtonEvent>> protocolEvents = new HashMap<String, List<UIButtonEvent>>();
    
+   /**
+    * Constructor<br />
+    * Initialize the property <b>protocolEvents</b> Using the <b>ProtocolContainer</b> instance,<br /> 
+    * so generating the events xml segment is protocol independence,<br />
+    * that's means when extending a new protocol, the <b>protocolContainer</b> will find the new protocol,<br />
+    * and then iterating the property <b>protocolEvents</b> will also generate the event xml segment for the new protocol.
+    */
    public ProtocolEventContainer() {
       if (ProtocolContainer.getInstance().getProtocols().size() == 0) {
          ProtocolParser parser = new ProtocolParser();
@@ -26,15 +85,20 @@ public class ProtocolEventContainer {
       }
    }
    
+   /**
+    * Adds a UIButtonEvent to property <b>protocolEvents</b> depend on the uiButtonEvent protocolType.
+    * 
+    * @param uiButtonEvent
+    */
    public void addUIButtonEvent(UIButtonEvent uiButtonEvent) {
       Set<String> protocolTypes = protocolEvents.keySet();
       for (String protocolType : protocolTypes) {
-         if (protocolType.equals(uiButtonEvent.getType())) {
+         if (protocolType.equals(uiButtonEvent.getProtocolType())) {
             List<UIButtonEvent> uiButtonEvents = protocolEvents.get(protocolType);
             for (UIButtonEvent uiBtnEvent : uiButtonEvents) {
                Map<String, String> protocolAttrs = uiBtnEvent.getProtocolAttrs();
                Set<String> protocolAttrKeySet = protocolAttrs.keySet();
-               boolean flag = true; // find the same uiButtonEvent?
+               boolean flag = true; //whether find the same uiButtonEvent.
                for (String key : protocolAttrKeySet) {
                   if (!protocolAttrs.get(key).equals(uiButtonEvent.getProtocolAttrs().get(key))) {
                      flag = false;
@@ -51,10 +115,22 @@ public class ProtocolEventContainer {
       }
    }
    
+   /**
+    * Gets the protocol events.
+    * 
+    * @return the protocol events
+    */
    public Map<String, List<UIButtonEvent>> getProtocolEvents () {
       return protocolEvents;
    }
    
+   /**
+    * Gets the uI button events.
+    * 
+    * @param protocolType the protocol type
+    * 
+    * @return the uI button events
+    */
    public List<UIButtonEvent> getUIButtonEvents(String protocolType) {
       if (protocolEvents.containsKey(protocolType)) {
          return protocolEvents.get(protocolType);
@@ -63,6 +139,11 @@ public class ProtocolEventContainer {
       }
    }
    
+   /**
+    * Iterating the property <b>protocolEvents</b> Generate events xml segment of all different protocols.
+    * 
+    * @return the xml string
+    */
    public String generateUIButtonEventsXml () {
       StringBuffer uiButtonEventXml = new StringBuffer();
       Set<String> protocolTypes = protocolEvents.keySet();
@@ -85,6 +166,9 @@ public class ProtocolEventContainer {
       return uiButtonEventXml.toString();
    }
    
+   /**
+    * Clear.
+    */
    public void clear() {
       protocolEvents.clear();
    }
