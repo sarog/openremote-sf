d567416a5ca1ed06f8c068e80fbf3dfa84dfee7f
tomskywang <tomskywang@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-09-27 09:33:12 +0000

    change Configuration variables copyLircdconf and resourceUpload from String to boolean.

--- trunk/Controller/src/org/openremote/controller/Configuration.java
+++ trunk/Controller/src/org/openremote/controller/Configuration.java
@@ -1,22 +1,23 @@
 /* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2009, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright 2008, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.openremote.controller;
 
 /**
@@ -33,7 +34,7 @@ public class Configuration {
    private String lircdconfPath;
    
    /** Whether copy lircd.conf for user. */
-   private String copyLircdconf;
+   private boolean copyLircdconf;
    
    /** The webapp port. */
    private String webappPort;
@@ -48,7 +49,7 @@ public class Configuration {
    private String resourcePath;
    
    /** The resource upload switch. */
-   private String resourceUpload;
+   private boolean resourceUpload;
 
    /**
     * Gets the irsend path.
@@ -86,21 +87,11 @@ public class Configuration {
       this.lircdconfPath = lircdconfPath;
    }
 
-   /**
-    * Gets the copy lircdconf.
-    * 
-    * @return the copy lircdconf
-    */
-   public String getCopyLircdconf() {
+   public boolean isCopyLircdconf() {
       return copyLircdconf;
    }
 
-   /**
-    * Sets the copy lircdconf.
-    * 
-    * @param copyLircdconf the new copy lircdconf
-    */
-   public void setCopyLircdconf(String copyLircdconf) {
+   public void setCopyLircdconf(boolean copyLircdconf) {
       this.copyLircdconf = copyLircdconf;
    }
 
@@ -177,11 +168,11 @@ public class Configuration {
    }
 
    /**
-    * Gets the resource upload.
+    * Checks if is resource upload.
     * 
-    * @return the resource upload
+    * @return true, if is resource upload
     */
-   public String getResourceUpload() {
+   public boolean isResourceUpload() {
       return resourceUpload;
    }
 
@@ -190,8 +181,8 @@ public class Configuration {
     * 
     * @param resourceUpload the new resource upload
     */
-   public void setResourceUpload(String resourceUpload) {
+   public void setResourceUpload(boolean resourceUpload) {
       this.resourceUpload = resourceUpload;
    }
-   
+
 }
