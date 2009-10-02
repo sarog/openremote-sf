86c61525e23e5edb1e603aa00351c7d641f1154c
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-02 14:33:06 +0000

    apply patch ORCJAVA-22 to source code (missing binary lib dependencies)

--- branches/feature/X10Event-jvelociter/src/org/openremote/controller/Configuration.java
+++ branches/feature/X10Event-jvelociter/src/org/openremote/controller/Configuration.java
@@ -1,22 +1,21 @@
 /* OpenRemote, the Home of the Digital Home.
- * Copyright 2008, OpenRemote Inc.
- * 
+ * Copyright 2008-2009, OpenRemote Inc.
+ *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
- * 
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3.0 of
- * the License, or (at your option) any later version.
- * 
- * This software is distributed in the hope that it will be useful,
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.openremote.controller;
 
@@ -24,6 +23,8 @@ package org.openremote.controller;
  * The Class Configuration.
  * 
  * @author Dan 2009-6-1
+ * @author Jerome Velociter
+ * @author <a href="mailto:juha@openremote.org>Juha Lindfors</a>
  */
 public class Configuration {
    
@@ -51,6 +52,12 @@ public class Configuration {
    /** The resource upload switch. */
    private boolean resourceUpload;
 
+   /** The COM (Serial) port the ORC should use (for example, to send X10 events)*/
+   private String comPort;
+   
+   /** The transmitter to use for X10 */
+   private String x10transmitter;
+   
    /**
     * Gets the irsend path.
     * 
@@ -184,5 +191,30 @@ public class Configuration {
    public void setResourceUpload(boolean resourceUpload) {
       this.resourceUpload = resourceUpload;
    }
+
+
+  /**
+   * @return the COM (Serial) port
+   */
+  public String getComPort() {
+    return comPort;
+  }
+
+  /**
+   * Sets the COM (Serial) port
+   *
+   * @param comPort the value of the COM (Serial) port to set
+   */
+  public void setComPort(String comPort) {
+    this.comPort = comPort;
+  }
+
+  public String getX10transmitter() {
+    return x10transmitter;
+  }
+
+  public void setX10transmitter(String x10transmitter) {
+    this.x10transmitter = x10transmitter;
+  }
 
 }
