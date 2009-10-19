03e4f4a2ae2d154c87bb65991b66d2fd18334fb4
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-19 09:45:28 +0000

    move iPhone 1.0.0 console beta tags into their own subdir

---
+++ iPhone_Console/iPhone_Console_1_0_0_Beta2/Classes/ActivitiesController.h
@@ -0,0 +1,15 @@
+//
+//  ActivitiesController.h
+//  openremote
+//
+//  Created by finalist on 2/26/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivitiesController : UITableViewController {
+	NSArray *activities;
+}
+
+@end
