ffaa18c6e0ab7025d776d20db862ae2bd2903e55
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-21 02:49:07 +0000

    add javadoc.

--- branches/feature/Controller_serverpush/src/org/openremote/controller/rest/ControlStatusPollingRESTServlet.java
+++ branches/feature/Controller_serverpush/src/org/openremote/controller/rest/ControlStatusPollingRESTServlet.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.controller.rest;
 
 import java.io.IOException;
@@ -19,49 +38,41 @@ import org.openremote.controller.status_cache.PollingThread;
 
 /**
  * Status Polling RESTful servlet of control.
- * It will response corresponding result with the RESTful URL. 
+ * It's responsiable for response corresponding result with the RESTful url.
  * 
  * @author Handy.Wang 2009-10-19
  */
 @SuppressWarnings("serial")
 public class ControlStatusPollingRESTServlet extends HttpServlet {
 
-   /** 
-    * This service is responsible for listen statuses change 
-    * and return the changed statuses(xml-formatted). 
-    */
+   /** This service is responsible for observe statuses change and return the changed statuses(xml-formatted). */
    private ControlStatusPollingService controlStatusPollingService = (ControlStatusPollingService) SpringContext.getInstance().getBean("controlStatusPollingService");;
 
    /** The max length of time of current servlet response. */
    private static final int MAX_TIME_OUT_SECONDS = 50;
    
+   /** A second equals how much mili seconds. */
    private static final int MILLI_SECONDS_A_SECOND = 1000;
    
-   /** Separator of control ids in the RESTful URL. */
+   /** Separator of control ids in the RESTful url. */
    private static final String CONTROL_ID_SEPARATOR = ",";
 
-   /** 
-    * This value will be responsed when current servlet couldn't get 
-    * the changed statuses in the <b>MAX_TIME_OUT_SECONDS</b>  
-    */
+   /** This value will be responsed when current servlet couldn't get the changed statuses in the <b>MAX_TIME_OUT_SECONDS</b>. */
    private static final String CONNECTION_TIME_OUT_MSG = "TIME_OUT";
 
    /**
-    * @see HttpServlet#HttpServlet()
+    * The Constructor.
     */
    public ControlStatusPollingRESTServlet() {
       super();
    }
 
-   /**
-    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-    */
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       doPost(request, response);
    }
 
    /**
-    * It's responsible for polling the changed statuses or TIME_OUT if time out.
+    * It's responsible for polling the <b>changed statuses</b> or <b>TIME_OUT</b> if time out.
     */
    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       long startTime = System.currentTimeMillis();
