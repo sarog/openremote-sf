9e776edf293edf94bb00c89562354bab2cc1fd20
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-21 21:53:20 +0000

    add javadoc

--- trunk/Controller/src/org/openremote/controller/Configuration.java
+++ trunk/Controller/src/org/openremote/controller/Configuration.java
@@ -21,11 +21,17 @@
 package org.openremote.controller;
 
 /**
- * TODO
+ * This class provides the Java bindings from config.properties file found in
+ * <tt>WEB-INF/classes</tt> directory of the web archive.
  *
- * @author Dan 2009-6-1
- * @author Jerome Velociter
+ * The actual value injection is currently configured via Spring frameworks'
+ * <tt>applicationContext.xml</tt> configuration file also found in the web archive.
+ * Each additional configuration property must be added to the XML definition
+ * as well as a Java accessor method implementation in this class.
+ *
+ * @author Dan Cong
  * @author <a href="mailto:juha@openremote.org>Juha Lindfors</a>
+ * @author Jerome Velociter
  */
 public class Configuration {
 
@@ -62,91 +68,294 @@ public class Configuration {
   private String x10transmitter;
 
 
+  /**
+   * Returns a string containing an operating system specific filesystem path to
+   * a executable LIRC 'irsend' command.
+   *
+   * @see #setIrsendPath(String)
+   *
+   * @return operating system specific filesystem path
+   */
   public String getIrsendPath() {
     return irsendPath;
   }
 
+  /**
+   * An operating system specific filesystem path as a string to a LIRC 'irsend'
+   * executable command.
+   *
+   * @see #getIrsendPath()
+   *
+   * @param irsendPath operating system specific filesystem path
+   */
   public void setIrsendPath(String irsendPath) {
+
+    // TODO :
+    //  could attempt to convert the string to a valid URI to support
+    //  a system neutral path format ?
+    //
+    // TODO :
+    //  should be explicit about (how-to) the use of relative paths
+
     this.irsendPath = irsendPath;
   }
 
+  /**
+   * Returns a string containing an operating system specific filesystem path to
+   * LIRC daemon configuration file.
+   *
+   * @see #setLircdconfPath(String)
+   *
+   * @return operating system specific filesystem path
+   */
   public String getLircdconfPath() {
     return lircdconfPath;
   }
 
+  /**
+   * An operating system specific filesystem path as a string to a LIRC 'lircd.conf' file
+   * (containing all the infrared remote codes).
+   *
+   * @see #getLircdconfPath()
+   *
+   * @param lircdconfPath operating system specific path
+   */
   public void setLircdconfPath(String lircdconfPath) {
+
+    // TODO :
+    //  could attempt to convert the string to a valid URI to support
+    //  a system neutral path format ?
+    //
+    // TODO :
+    //  should be explicit about (how-to) the use of relative paths
+
     this.lircdconfPath = lircdconfPath;
   }
 
+  /**
+   * Indicates whether an existing 'lircd.conf' file is overriden on the system
+   * when a new controller configuration is deployed through the web interface.
+   *
+   * @see #setCopyLircdconf(boolean)
+   *
+   * @return true if copy-over on configuration deployment is enabled; false otherwise
+   */
   public boolean isCopyLircdconf() {
     return copyLircdconf;
   }
 
+  /**
+   * Determines whether a 'lircd.conf' file in a deployed controller configuration should
+   * be copied over an existing LIRC configuration file found in the path returned by
+   * {@link #getLircdconfPath()} method.  <p>
+   *
+   * <b>Important Note:</b> This is potentially disruptive to the user's system if an existing,
+   * unrelated LIRC daemon configuration was already in use.
+   *
+   * @see #isCopyLircdconf()
+   *
+   * @param copyLircdconf  If true, enables copy-over behavior. If false, requires manual
+   *                       infrared configuration in 'lircd.conf' file.
+   */
   public void setCopyLircdconf(boolean copyLircdconf) {
     this.copyLircdconf = copyLircdconf;
   }
 
+  /**
+   * Returns the port number used by controller discovery service.
+   *
+   * @see #setWebappPort(String)
+   *
+   * @return port number
+   */
   public String getWebappPort() {
+
+    // TODO : this should not be stored as a string
+
     return webappPort;
   }
 
+  /**
+   * The port number the controller returns on a panel discovery request. It should
+   * match the port number configured on the hosting web container or otherwise
+   * things just don't work right.  <p>
+   *
+   * TODO:
+   *   This is a duplicate configuration and should be consolidated with a proper service
+   *   container around the web container. It should only be necessary to define the
+   *   port number once.
+   *
+   * @see #getWebappPort()
+   *
+   * @param webappPort  the HTTP listening port of the web container (e.g. Tomcat 8080)
+   */
   public void setWebappPort(String webappPort) {
     this.webappPort = webappPort;
   }
 
+  /**
+   * Returns the configured controller discovery multicast address
+   *
+   * @see #setMulticastAddress(String)
+   *
+   * @return IP multicast address as a string
+   */
   public String getMulticastAddress() {
     return multicastAddress;
   }
 
+  /**
+   * Sets the multicast address used for controller discovery by panels.
+   *
+   * @see #getMulticastAddress()
+   *
+   * @param multicastAddress IP multicast address as a string
+   */
   public void setMulticastAddress(String multicastAddress) {
+
+    // TODO : actually validate a correctly configured IP address
+
     this.multicastAddress = multicastAddress;
   }
 
+  /**
+   * Returns the configured multicast port used for controller discovery.
+   *
+   * @return  multicast port
+   */
   public int getMulticastPort() {
     return multicastPort;
   }
 
+  /**
+   * Sets the multicast port used for controller discovery.
+   *
+   * @see #getMulticastPort()
+   *
+   * @param multicastPort port number
+   */
   public void setMulticastPort(int multicastPort) {
     this.multicastPort = multicastPort;
   }
 
+  /**
+   * Returns an operating system specific string containing a filesystem path
+   * to a directory with the resource files of a controller
+   *
+   * @see #setResourcePath(String)
+   *
+   * @return operating system specific filesystem path as a string
+   */
   public String getResourcePath() {
     return resourcePath;
   }
 
+  /**
+   * The resource path points to a directory where the controller looks for all
+   * configuration files, images and other resources for panels and controllers.
+   *
+   * @see #getResourcePath()
+   *
+   * @param resourcePath  system specific filesystem path as a string to a directory containing
+   *                      the resource files of a controller
+   */
   public void setResourcePath(String resourcePath) {
+
+    // TODO :
+    //  could attempt to convert the string to a valid URI to support
+    //  a system neutral path format ?
+    //
+    // TODO :
+    //  should be explicit about (how-to) the use of relative paths
+
     this.resourcePath = resourcePath;
   }
 
+  /**
+   * Indicates if controller mapping configuration deployment has been
+   * enabled on the web user interface.
+   *
+   * @see #setResourceUpload(boolean)
+   *
+   * @return true if controller configuration can be uploaded through
+   *         web interface; false otherwise
+   */
   public boolean isResourceUpload() {
     return resourceUpload;
   }
 
+  /**
+   * Enables the ability to upload controller event and command mappings
+   * directly from the web user interface.  <p>
+   *
+   * This is a convenience feature for home users for easy deployment of
+   * the controller configuration in cases where all users of the network
+   * are trusted, or the controller is still being configured and tested. <p>
+   *
+   * As a security consideration, when deploying the controller on a public
+   * network, this setting should always be disabled. <p>
+   *
+   * When disabled, configuration must be handled manually by copying all
+   * the controller configuration files manually to a directory pointed by
+   * {@link #setResourcePath} property. Access to this directory in the
+   * filesystem should be properly secured with access restrictions. <p>
+   *
+   * @param resourceUpload  true to enable controller mapping uploads via
+   *                        web interface; false otherwise
+   *
+   * @see #setResourcePath
+   * @see #isResourceUpload
+   */
   public void setResourceUpload(boolean resourceUpload) {
     this.resourceUpload = resourceUpload;
   }
 
 
   /**
-   * @return the COM (Serial) port
+   * Returns the name of the serial port configured for X10 serial PLM unit
+   *
+   * @see #setComPort(String)
+   *
+   * @return the name of the COM (serial) port or device
    */
   public String getComPort() {
     return comPort;
   }
 
   /**
-   * Sets the COM (Serial) port
+   * Serial port or device to use with X10 serial power-line modules
+   * (e.g. CM11A or CM17A "FireCracker" modules).
    *
-   * @param comPort the value of the COM (Serial) port to set
+   * Serial port configuration is specific to the operating system in use.
+   * On Linux '/dev/ttyS0' or similar device name should be used. For
+   * Microsoft Windows systems use port names such as COM1, COM2, etc.
+   *
+   * @see #getComPort()
+   *
+   * @param comPort the name of the COM (serial) port or device
    */
   public void setComPort(String comPort) {
     this.comPort = comPort;
   }
 
+  /**
+   * TODO
+   *
+   * @see #setX10transmitter(String)
+   *
+   * @return
+   */
   public String getX10transmitter() {
     return x10transmitter;
   }
 
+  /**
+   * TODO
+   *
+   * @see #getX10transmitter()
+   *
+   * @param x10transmitter
+   */
   public void setX10transmitter(String x10transmitter) {
     this.x10transmitter = x10transmitter;
   }
