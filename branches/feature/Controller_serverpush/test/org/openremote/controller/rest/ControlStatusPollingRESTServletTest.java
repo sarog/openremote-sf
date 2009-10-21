d40395dffc621e20f3054f8c3f352070e0783c8c
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-21 02:45:08 +0000

    add javadoc.

--- branches/feature/Controller_serverpush/test/org/openremote/controller/rest/ControlStatusPollingRESTServletTest.java
+++ branches/feature/Controller_serverpush/test/org/openremote/controller/rest/ControlStatusPollingRESTServletTest.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.controller.rest;
 
 import junit.framework.TestCase;
@@ -22,7 +41,7 @@ public class ControlStatusPollingRESTServletTest extends TestCase {
     * Test StatusPolling RESTful Service when the App server didn't startup.
     * So plean run this method in the situation of app server wasn't running.
     * 
-    * @throws Exception
+    * @throws Exception the exception
     */
    public void testDoPostWithAppServerNotStartup() throws Exception {
       WebConversation wc = new WebConversation();
@@ -35,9 +54,13 @@ public class ControlStatusPollingRESTServletTest extends TestCase {
     * 
     * Test StatusPolling RESTful Service when app server was running but the response will be time out.
     * 
-    * If you want 
+    * If you want simulate several panels making polling request, you can run this method more times.
     * 
-    * @throws Exception
+    * And also, if you want simulate: 
+    *     some polling requests will time out, some won't.
+    * You can run this method at the same time running the next test method(testDoPostWithoutTimeOut).
+    * 
+    * @throws Exception the exception
     */
    public void testDoPostWithTimeOut() throws Exception {
       WebConversation wc = new WebConversation();
@@ -51,7 +74,12 @@ public class ControlStatusPollingRESTServletTest extends TestCase {
     * 
     * Test StatusPolling RESTful Service when app server was running and the response will be getted by client.
     * 
-    * @throws Exception
+    * If you want simulate several panels making polling request, you can run this method more times.
+    * And also, if you want simulate: 
+    *     some polling requests will time out, some won't.
+    * You can run this method at the same time running the previous test method(testDoPostWithoutTimeOut).
+    * 
+    * @throws Exception the exception
     */
    public void testDoPostWithoutTimeOut() throws Exception {
       WebConversation wc = new WebConversation();
