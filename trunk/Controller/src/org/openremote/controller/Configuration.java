e89bb8cb9d0c92605b4f8912b318f6ad37694b51
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-21 10:47:36 +0000

    remove autogenerated javadoc using non-sensical or redundant text

--- trunk/Controller/src/org/openremote/controller/Configuration.java
+++ trunk/Controller/src/org/openremote/controller/Configuration.java
@@ -20,7 +20,7 @@
 package org.openremote.controller;
 
 /**
- * The Class Configuration.
+ * TODO
  * 
  * @author Dan 2009-6-1
  * @author Jerome Velociter
@@ -28,25 +28,19 @@ package org.openremote.controller;
  */
 public class Configuration {
    
-   /** The irsend path. */
    private String irsendPath;
    
-   /** The lircdconf path. */
    private String lircdconfPath;
    
    /** Whether copy lircd.conf for user. */
    private boolean copyLircdconf;
    
-   /** The webapp port. */
    private String webappPort;
    
-   /** The multicast address. */
    private String multicastAddress;
    
-   /** The multicast port. */
    private int multicastPort;
    
-   /** The resource path. */
    private String resourcePath;
    
    /** The resource upload switch. */
@@ -57,39 +51,21 @@ public class Configuration {
    
    /** The transmitter to use for X10 */
    private String x10transmitter;
-   
-   /**
-    * Gets the irsend path.
-    * 
-    * @return the irsend path
-    */
+
+
+
    public String getIrsendPath() {
       return irsendPath;
    }
 
-   /**
-    * Sets the irsend path.
-    * 
-    * @param irsendPath the new irsend path
-    */
    public void setIrsendPath(String irsendPath) {
       this.irsendPath = irsendPath;
    }
 
-   /**
-    * Gets the lircdconf path.
-    * 
-    * @return the lircdconf path
-    */
    public String getLircdconfPath() {
       return lircdconfPath;
    }
 
-   /**
-    * Sets the lircdconf path.
-    * 
-    * @param lircdconfPath the new lircdconf path
-    */
    public void setLircdconfPath(String lircdconfPath) {
       this.lircdconfPath = lircdconfPath;
    }
@@ -102,92 +78,42 @@ public class Configuration {
       this.copyLircdconf = copyLircdconf;
    }
 
-   /**
-    * Gets the webapp port.
-    * 
-    * @return the webapp port
-    */
    public String getWebappPort() {
       return webappPort;
    }
 
-   /**
-    * Sets the webapp port.
-    * 
-    * @param webappPort the new webapp port
-    */
    public void setWebappPort(String webappPort) {
       this.webappPort = webappPort;
    }
 
-   /**
-    * Gets the multicast address.
-    * 
-    * @return the multicast address
-    */
    public String getMulticastAddress() {
       return multicastAddress;
    }
 
-   /**
-    * Sets the multicast address.
-    * 
-    * @param multicastAddress the new multicast address
-    */
    public void setMulticastAddress(String multicastAddress) {
       this.multicastAddress = multicastAddress;
    }
 
-   /**
-    * Gets the multicast port.
-    * 
-    * @return the multicast port
-    */
    public int getMulticastPort() {
       return multicastPort;
    }
 
-   /**
-    * Sets the multicast port.
-    * 
-    * @param multicastPort the new multicast port
-    */
    public void setMulticastPort(int multicastPort) {
       this.multicastPort = multicastPort;
    }
 
-   /**
-    * Gets the resource path.
-    * 
-    * @return the resource path
-    */
    public String getResourcePath() {
       return resourcePath;
    }
 
-   /**
-    * Sets the resource path.
-    * 
-    * @param resourcePath the new resource path
-    */
    public void setResourcePath(String resourcePath) {
       this.resourcePath = resourcePath;
    }
 
-   /**
-    * Checks if is resource upload.
-    * 
-    * @return true, if is resource upload
-    */
    public boolean isResourceUpload() {
       return resourceUpload;
    }
 
-   /**
-    * Sets the resource upload.
-    * 
-    * @param resourceUpload the new resource upload
-    */
    public void setResourceUpload(boolean resourceUpload) {
       this.resourceUpload = resourceUpload;
    }
