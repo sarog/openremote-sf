7bcc858446aeedfd6b2f0c9744b7efb4ed39730b
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-27 11:42:31 +0000

    the current old version of RXTX is crashing the JVM on Debian Linux (Voyage) -- trying to upgrade to a newer version of RXTX for resolution but that requires porting X10 lib from javax.comm to gnu.io first

---
+++ branches/feature/GNU-IO-migration/src/javax/comm/ParallelPortEvent.java
@@ -0,0 +1,103 @@
+/*
+ * ParallelPortEvent.java
+ * Copyright (C) 2004 The Free Software Foundation
+ *
+ * This file is part of GNU CommAPI, a library.
+ *
+ * GNU CommAPI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNU CommAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package javax.comm;
+
+import java.util.EventObject;
+
+/**
+ * An event on a parallel port.
+ *
+ * @author <a href='mailto:dog@gnu.org'>Chris Burdess</a>
+ * @version 2.0.3
+ */
+public class ParallelPortEvent extends EventObject
+{
+
+  /**
+   * @deprecated Replaced by <code>getEventType</code> method.
+   * For compatibility only.
+   */
+  public int eventType;
+
+  /**
+   * An error occurred.
+   */
+  public static final int PAR_EV_ERROR = 1;
+
+  /**
+   * The output buffer is empty.
+   */
+  public static final int PAR_EV_BUFFER = 2;
+
+  int eventtype;
+  boolean oldvalue;
+  boolean newvalue;
+
+  /**
+   * Constructor.
+   * This should only be called by the port driver.
+   * @param srcport the source port
+   * @param eventtype the event type: PAR_EV_ERROR or PAR_EV_BUFFER
+   * @param oldvalue the old value
+   * @param newvalue the new value
+   */
+  public ParallelPortEvent(ParallelPort srcport, int eventtype,
+      boolean oldvalue, boolean newvalue)
+  {
+    super(srcport);
+    this.eventtype = eventtype;
+    this.oldvalue = oldvalue;
+    this.newvalue = newvalue;
+    // Compatibility
+    eventType = eventtype;
+  }
+
+  /**
+   * Returns the type of event: PAR_EV_ERROR or PAR_EV_BUFFER
+   */
+  public int getEventType()
+  {
+    return eventtype;
+  }
+
+  /**
+   * Returns the new value of the state change that caused this event.
+   */
+  public boolean getNewValue()
+  {
+    return newvalue;
+  }
+
+  /**
+   * Returns the old value of the state change that caused this event.
+   */
+  public boolean getOldValue()
+  {
+    return oldvalue;
+  }
+
+}
