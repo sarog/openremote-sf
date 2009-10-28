8adff1b08fd598eb49386a7ebd3893aeaf78c012
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-28 02:08:17 +0000

    Modify the property type of webappPort from String to int in the class Configuration.java.

--- branches/feature/Controller_serverpush/src/org/openremote/controller/Configuration.java
+++ branches/feature/Controller_serverpush/src/org/openremote/controller/Configuration.java
@@ -1,23 +1,22 @@
 /* OpenRemote, the Home of the Digital Home.
- * Copyright 2008, OpenRemote Inc.
- * 
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
- * 
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3.0 of
- * the License, or (at your option) any later version.
- * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.controller;
 
 /**
@@ -37,7 +36,7 @@ public class Configuration {
    private boolean copyLircdconf;
    
    /** The webapp port. */
-   private String webappPort;
+   private int webappPort;
    
    /** The multicast address. */
    private String multicastAddress;
@@ -87,10 +86,20 @@ public class Configuration {
       this.lircdconfPath = lircdconfPath;
    }
 
+   /**
+    * Checks if is copy lircdconf.
+    * 
+    * @return true, if is copy lircdconf
+    */
    public boolean isCopyLircdconf() {
       return copyLircdconf;
    }
 
+   /**
+    * Sets the copy lircdconf.
+    * 
+    * @param copyLircdconf the new copy lircdconf
+    */
    public void setCopyLircdconf(boolean copyLircdconf) {
       this.copyLircdconf = copyLircdconf;
    }
@@ -100,7 +109,7 @@ public class Configuration {
     * 
     * @return the webapp port
     */
-   public String getWebappPort() {
+   public int getWebappPort() {
       return webappPort;
    }
 
@@ -109,7 +118,7 @@ public class Configuration {
     * 
     * @param webappPort the new webapp port
     */
-   public void setWebappPort(String webappPort) {
+   public void setWebappPort(int webappPort) {
       this.webappPort = webappPort;
    }
 
