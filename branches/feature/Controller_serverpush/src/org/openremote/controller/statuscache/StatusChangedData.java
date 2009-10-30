c401db6c388dfa57866b0765b8069a8096eb3667
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-10-30 09:39:18 +0000

    Renamed package status_cache to statuscache.

--- branches/feature/Controller_serverpush/src/org/openremote/controller/statuscache/StatusChangedData.java
+++ branches/feature/Controller_serverpush/src/org/openremote/controller/statuscache/StatusChangedData.java
@@ -17,7 +17,7 @@
 * You should have received a copy of the GNU Affero General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-package org.openremote.controller.status_cache;
+package org.openremote.controller.statuscache;
 
 /**
  * This class mainly store the data which status changed.<br /><br />
