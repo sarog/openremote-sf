208c2a38295d3d20fd6634783647630acf172adb
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-11-01 14:50:11 +0000

    Merged from /trunk/Controller:r1142-1143 to Controller 1.0 beta branch -- doing partial commits to resolve upstream conflicts

--- branches/Controller_1_0_0_Betas/src/org/openremote/controller/Constants.java
+++ branches/Controller_1_0_0_Betas/src/org/openremote/controller/Constants.java
@@ -21,30 +21,28 @@
 package org.openremote.controller;
 
 /**
- *
+ * TODO
  * 
  * @author Dan 2009-6-1
  */
 public class Constants {
 
-   public final static String LIRCD_CONF = "lircd.conf";
-   
-   public final static String CONTROLLER_XSD_PATH = "/controller-1.0-M3.xsd";
-   
-   public final static String CONTROLLER_XML = "controller.xml";
-   
-   public final static String OPENREMOTE_WEBSITE= "http://www.openremote.org";
-   
-   public final static String OPENREMOTE_NAMESPACE= "or";
-   
-   public final static String SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
-   
-   public final static String XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
-   
-   public final static String SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
-   
-   public final static String TRUE = "TRUE";
-   
-   public final static String OK = "OK";
+  public final static String LIRCD_CONF = "lircd.conf";
+
+  public final static String CONTROLLER_XSD_PATH = "/controller-1.0-M3.xsd";
+
+  public final static String CONTROLLER_XML = "controller.xml";
+
+  public final static String OPENREMOTE_WEBSITE = "http://www.openremote.org";
+
+  public final static String OPENREMOTE_NAMESPACE = "or";
+
+  public final static String SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+
+  public final static String XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
+
+  public final static String SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
+
+  public final static String OK = "OK";
 
 }
