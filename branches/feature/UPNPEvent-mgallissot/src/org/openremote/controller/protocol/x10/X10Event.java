7fd077ec9540dc973b88428b4df351bb588d23d1
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-11-02 22:11:37 +0000

    intermediate commit of merge (to resolve some conflicts down stream) of /trunk/Controller to this branch (working up to Controller 1.0 FINAL)

--- branches/feature/UPNPEvent-mgallissot/src/org/openremote/controller/protocol/x10/X10Event.java
+++ branches/feature/UPNPEvent-mgallissot/src/org/openremote/controller/protocol/x10/X10Event.java
@@ -1,38 +1,52 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2009, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
+/* 
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2009, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 package org.openremote.controller.protocol.x10;
 
+import org.apache.log4j.Logger;
 import org.openremote.controller.event.Event;
 
 /**
  * The X10 Event.
  * 
  * @author Dan 2009-4-20
+ * @author Jerome Velociter
  */
 public class X10Event extends Event {
-   
+
    /** The address. */
    private String address;
-   
+
    /** The command. */
-   private String command;
+   private X10Command command;
+
+   /** Controller manager */
+   private X10ControllerManager controllerManager;
+
+   private final static Logger log = Logger.getLogger(X10EventBuilder.X10_LOG_CATEGORY);
+
+   public X10Event(X10ControllerManager manager, String address, X10Command command) {
+      this.controllerManager = manager;
+      this.address = address;
+      this.command = command;
+   }
 
    /**
     * Gets the address.
@@ -46,7 +60,8 @@ public class X10Event extends Event {
    /**
     * Sets the address.
     * 
-    * @param address the new address
+    * @param address
+    *           the new address
     */
    public void setAddress(String address) {
       this.address = address;
@@ -57,18 +72,37 @@ public class X10Event extends Event {
     * 
     * @return the command
     */
-   public String getCommand() {
+   public X10Command getCommand() {
       return command;
    }
 
    /**
     * Sets the command.
     * 
-    * @param command the new command
+    * @param command
+    *           the new command
     */
-   public void setCommand(String command) {
+   public void setCommand(X10Command command) {
       this.command = command;
    }
-   
-   
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public void exec() {
+
+      X10Controller device;
+
+      try {
+         device = this.controllerManager.getDevice();
+
+         device.send(this.getAddress(), command);
+
+      } catch (ConnectionException e) {
+         log.error(e);
+      }
+
+   }
+
 }
