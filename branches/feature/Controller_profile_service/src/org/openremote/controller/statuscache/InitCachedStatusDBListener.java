c975341cf9225f626e86197483b40dbbcb165972
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-11-03 03:09:12 +0000

    made a copy

---
+++ branches/feature/Controller_profile_service/src/org/openremote/controller/statuscache/InitCachedStatusDBListener.java
@@ -0,0 +1,173 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.controller.statuscache;
+
+import java.sql.Connection;
+import java.util.List;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.Logger;
+import org.openremote.controller.service.StatusCacheService;
+import org.openremote.controller.spring.SpringContext;
+import org.springframework.context.support.ApplicationObjectSupport;
+
+/**
+ * This class is mainly responsible for <b>Initializing</b> the cached statuses database where in the memory.
+ * 
+ * @author Handy.Wang 2009-10-21
+ */
+public class InitCachedStatusDBListener extends ApplicationObjectSupport implements ServletContextListener {
+
+   /** The logger. */
+   private Logger logger = Logger.getLogger(InitCachedStatusDBListener.class.getName());
+   
+   /** The connection. */
+   public static Connection connection;
+   
+   /**
+    * TIME_OUT table instance.
+    */
+   private SkippedStatusTable skippedStatusTable = (SkippedStatusTable) SpringContext.getInstance().getBean("skippedStatusTable");
+   private StatusCacheService statusCacheService = (StatusCacheService) SpringContext.getInstance().getBean("statusCacheService");
+
+   /* (non-Javadoc)
+    * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+    */
+   @Override
+   public void contextInitialized(ServletContextEvent event) {
+      try {
+         InitStatusCache();
+         simulateStatusCacheControlID1();
+         simulateStatusCacheControlID2();
+         simulateStatusCacheControlID3();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+   
+   /**
+    * Init 4 components initial status.
+    */
+   private void InitStatusCache() {
+      statusCacheService.saveOrUpdateStatus(1, "OFF");
+      statusCacheService.saveOrUpdateStatus(2, "OFF");
+      statusCacheService.saveOrUpdateStatus(3, "OFF");
+      statusCacheService.saveOrUpdateStatus(4, "OFF");
+   }
+   
+   /**
+    * Simulate Case1 of StatusCahe.<br />
+    * 
+    * The device of control id is 1 will switch ON/OFF every 10 seconds.
+    */
+   private void simulateStatusCacheControlID1() {
+      Thread thread = new Thread() {
+         @Override
+         public void run() {
+            int i = 0;
+            for(; ; i++) {
+               if (i % 2 == 0) {
+                  statusCacheService.saveOrUpdateStatus(1, "ON");
+               } else {
+                  statusCacheService.saveOrUpdateStatus(1, "OFF");
+               }
+               try {
+                  sleep(10000);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }
+      };
+      thread.start();
+   }
+   
+   /**
+    * Simulate Case 2 of StatusCache.<br />
+    * 
+    * DESC: First polling request was time out, second polling request can find previous time out record and <br />
+    *   get the changed status during two polling requests.<br /> 
+    *   So, the client will get timeout response in odd times and changed statuses in even times.
+    */
+   private void simulateStatusCacheControlID2() {
+      Thread simulateThread = new Thread() {
+         @Override
+         public void run() {
+            int i = 0;
+            for (;; i++) {
+               List<SkippedStatusRecord> skippedStatusRecord = skippedStatusTable.query(2);
+               if (skippedStatusRecord != null && skippedStatusRecord.size() != 0) {
+                  if (i % 2 == 0) {
+                     statusCacheService.saveOrUpdateStatus(2, "ON");
+                  } else {
+                     statusCacheService.saveOrUpdateStatus(2, "OFF");
+                  }
+                  try {
+                     sleep(10000);
+                  } catch (InterruptedException e) {
+                     e.printStackTrace();
+                  }
+               }
+            }
+         }
+      };
+      simulateThread.start();
+   }
+   
+   private void simulateStatusCacheControlID3() {
+      Thread simulateThread = new Thread() {
+         @Override
+         public void run() {
+            int i = 0;
+            for (;; i++) {
+               try {
+                  Thread.sleep(80000);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+               if (i % 2 == 0) {
+                  statusCacheService.saveOrUpdateStatus(3, "ON");
+               } else {
+                  statusCacheService.saveOrUpdateStatus(3, "OFF");
+               }
+               try {
+                  sleep(10000);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }
+      };
+      simulateThread.start();
+   }
+   
+   //
+
+   /* (non-Javadoc)
+    * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.ServletContextEvent)
+    */
+   @Override
+   public void contextDestroyed(ServletContextEvent event) {
+   }
+
+
+}
---
+++ branches/feature/Controller_profile_service/src/org/openremote/controller/statuscache/SkippedStatusRecord.java
@@ -0,0 +1,164 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.controller.statuscache;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * A skippedStatus  record.
+ * 
+ * @author Handy.Wang 2009-10-23
+ */
+public class SkippedStatusRecord {
+   
+   private String deviceID;
+
+   private List<Integer> pollingControlIDs;
+
+   private Set<Integer> statusChangedIDs;
+
+   public SkippedStatusRecord() {
+      super();
+      deviceID = "";
+      pollingControlIDs = new ArrayList<Integer>();
+      statusChangedIDs = new HashSet<Integer>();
+   }
+
+   public SkippedStatusRecord(String deviceID, List<Integer> pollingControlIDs) {
+      super();
+      this.deviceID = deviceID;
+      this.pollingControlIDs = pollingControlIDs;
+      this.statusChangedIDs = new HashSet<Integer>();
+   }
+   
+   public SkippedStatusRecord(String deviceID, Integer[] pollingControlIDs) {
+      super();
+      this.deviceID = deviceID;
+      this.pollingControlIDs = new ArrayList<Integer>();
+      this.setPollingControlIDs(pollingControlIDs);
+      this.statusChangedIDs = new HashSet<Integer>();
+   }
+
+   public SkippedStatusRecord(String deviceID, String[] pollingControlIDs) {
+      super();
+      this.pollingControlIDs = new ArrayList<Integer>();
+      for(String s:pollingControlIDs){
+         this.pollingControlIDs.add(Integer.parseInt(s));
+      }
+      this.deviceID = deviceID;
+      this.statusChangedIDs = new HashSet<Integer>();
+   }
+   
+   public SkippedStatusRecord(String deviceID, Collection<Integer> pollingControlIDs) {
+      super();
+      this.pollingControlIDs = new ArrayList<Integer>();
+      for(Integer i:pollingControlIDs){
+         this.pollingControlIDs.add(i);
+      }
+      this.deviceID = deviceID;
+      this.statusChangedIDs = new HashSet<Integer>();
+   }
+   
+   public String getDeviceID() {
+      return deviceID;
+   }
+
+   public void setDeviceID(String deviceID) {
+      this.deviceID = deviceID;
+   }
+
+   /**
+    * Add pollingControl id into polling control id list. 
+    */
+   public void addPollingControlID(Integer pollingControlID) {
+      this.pollingControlIDs.add(pollingControlID);
+   }
+   
+   /**
+    * Add statusChanged id into status changed id list. 
+    */
+   public void addStatusChangedID(Integer statusChangedID) {
+      this.statusChangedIDs.add(statusChangedID);
+   }
+
+   @Override
+   public boolean equals(Object obj) {
+      if (obj == null || !(obj instanceof SkippedStatusRecord)) {
+         return false;
+      }
+      SkippedStatusRecord timeoutRecord = (SkippedStatusRecord)obj;
+      if ("".equals(timeoutRecord.getDeviceID()) || !timeoutRecord.getDeviceID().equals(this.deviceID)) {
+         return false;
+      }
+      if (timeoutRecord.getPollingControlIDs().size() == 0 || timeoutRecord.getPollingControlIDs().size() != this.pollingControlIDs.size()) {
+         return false;
+      }
+      
+      Collections.sort(this.getPollingControlIDs(), new PollingControlIDListComparator());      
+      Collections.sort(timeoutRecord.getPollingControlIDs(), new PollingControlIDListComparator());
+      for (int i = 0; i < timeoutRecord.getPollingControlIDs().size(); i++) {
+         if (!this.getPollingControlIDs().get(i).equals(timeoutRecord.getPollingControlIDs().get(i))) {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   /**
+    * Overload the method setPollingControlIDs with the parameter type "String[]". 
+    */
+   public void setPollingControlIDs(Integer[] pollingControlIDs) {
+      for (Integer pollingControlID : pollingControlIDs) {
+         this.pollingControlIDs.add(pollingControlID);
+      }
+   }
+
+   public List<Integer> getPollingControlIDs() {
+      return pollingControlIDs;
+   }
+
+   public void setPollingControlIDs(List<Integer> pollingControlIDs) {
+      this.pollingControlIDs = pollingControlIDs;
+   }
+
+   public Set<Integer> getStatusChangedIDs() {
+      return statusChangedIDs;
+   }
+
+   public void setStatusChangedIDs(Set<Integer> statusChangedIDs) {
+      this.statusChangedIDs = statusChangedIDs;
+   }
+   
+   @Override
+   public String toString(){
+      StringBuffer sb = new StringBuffer();
+      
+      sb.append("DEVICEID:"+deviceID);
+      sb.append("\tcomponentID:"+this.pollingControlIDs.toString());
+      sb.append("statusChangedComponentID:"+this.statusChangedIDs);
+      return sb.toString();
+   }
+   
+}
