26f21411aaf862cf5fc96d5392ca3b20e77e88aa
javenzhang <javenzhang@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-11-23 09:14:41 +0000

    some error happened with my svn client. commit the missing package and java source.

---
+++ branches/Modeller_gwt_Branch/src/org/openremote/modeler/client/widget/component/ScreenComponent.java
@@ -0,0 +1,53 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.modeler.client.widget.component;
+
+import org.openremote.modeler.domain.component.UIButton;
+import org.openremote.modeler.domain.component.UIComponent;
+import org.openremote.modeler.domain.component.UISwitch;
+
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
+
+/**
+ * ScreenControl as the component's super class.
+ */
+public abstract class ScreenComponent extends LayoutContainer {
+   /**
+    * Sets the display name.
+    */
+   public abstract void setName(String name);
+   
+   public abstract String getName();
+   
+   /**
+    * Builds the ScreenControl according to uiControl type.
+    */
+   public static ScreenComponent build(UIComponent uiComponent) {
+      if (uiComponent instanceof UIButton) {
+         return new ScreenButton((UIButton)uiComponent);
+      } else if(uiComponent instanceof UISwitch) {
+         return new ScreenSwitch((UISwitch)uiComponent);
+      }
+      return null;
+   }
+   
+   public abstract FormPanel buildPropertiesForm();
+}
