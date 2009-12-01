226d5f7e3aae7dad8f20a09ab7b4cfd914af0ab4
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-12-01 08:15:30 +0000

    Accomplished issue iPhone-33(Image) and iPhone-34(Label).

---
+++ branches/feature/iPhone_profile_service/Classes/LabelView.m
@@ -0,0 +1,57 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2009, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+#import "LabelView.h"
+#import "Label.h"
+
+@interface LabelView(Private)
+-(void) initLabel;
+@end
+
+@implementation LabelView
+
+#pragma mark Overridden methods
+
+// This method is abstract method of direct superclass ControlView's.
+// So, this method must be overridden in subclass.
+- (void)setPollingStatus:(NSNotification *)notification {
+}
+
+// This method is abstract method of indirect superclass UIView's.
+- (void)layoutSubviews {
+	NSLog(@"layoutSubviews of LabelView.");
+	[self initLabel];
+}
+
+#pragma mark Private methods implementation
+
+-(void) initLabel {
+	UILabel *uiLabel = [[UILabel alloc] initWithFrame:[self bounds]];
+	//[uiLabel backgroundColor:[UIColor blackColor]];
+	[uiLabel setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0]];
+	[uiLabel setTextAlignment:UITextAlignmentCenter];
+	Label *labelModel = (Label *)control;
+	uiLabel.text = labelModel.value;
+	uiLabel.textColor = [UIColor whiteColor];
+	[self addSubview:uiLabel];
+}
+
+@end
