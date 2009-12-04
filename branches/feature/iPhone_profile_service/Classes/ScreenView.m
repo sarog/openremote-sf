9135a9c3f1e3ad51b36122451a3e1ef62e9a78be
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2009-12-04 03:54:29 +0000

    Developed issue iPhone-33, only "relative position of screen background image" implementation left.

--- branches/feature/iPhone_profile_service/Classes/ScreenView.m
+++ branches/feature/iPhone_profile_service/Classes/ScreenView.m
@@ -28,11 +28,13 @@
 #import "ServerDefinition.h"
 #import "ViewHelper.h"
 #import "DirectoryDefinition.h"
+#import "BackgroundRelativePositionConstant.h"
 
 
 @interface ScreenView (Private) 
 - (void)createLayout;
-
+-(UIImage *)clipUIImage:(UIImage *)uiImage dependingOnUIView:(UIView *)uiView;
+-(UIImage *)doClipWithUIImage:(UIImage *)uiImage withCGSize:(CGSize)cgSize;
 @end
 
 @implementation ScreenView
@@ -60,27 +62,93 @@
 - (void)createLayout {
 	
 	if ([[[screen background] backgroundImage] src] && [[NSFileManager defaultManager] fileExistsAtPath:[[DirectoryDefinition imageCacheFolder] stringByAppendingPathComponent:[[[screen background] backgroundImage] src]]]) {
-		UIImage *background = [[UIImage alloc] initWithContentsOfFile:[[DirectoryDefinition imageCacheFolder] stringByAppendingPathComponent:[[[screen background] backgroundImage] src]]];
-		UIColor *color = [[UIColor alloc] initWithPatternImage:background];
+		UIImage *backgroundImage = [[UIImage alloc] initWithContentsOfFile:[[DirectoryDefinition imageCacheFolder] stringByAppendingPathComponent:[[[screen background] backgroundImage] src]]];
+		
 		int screenBackgroundImageViewWidth = IPHONE_SCREEN_WIDTH;
 		int screenBackgroundImageViewHeight = IPHONE_SCREEN_HEIGHT - IPHONE_SCREEN_STATUS_BAR_HEIGHT - IPHONE_SCREEN_BOTTOM_PAGE_SWITCH_CONTROL_HEIGHT;
-		UIView *backgroundImageView = [[UIView alloc] init];
-		[backgroundImageView setContentMode:UIViewContentModeScaleAspectFill];
-		if ([[screen background] isBackgroundImageAbsolutePosition]) {
-			int x = [[screen background] backgroundImageAbsolutePositionLeft];
-			int y = [[screen background] backgroundImageAbsolutePositionTop];
-			NSLog(@"x is %d, y is %d", x, y);
-			[backgroundImageView setFrame:CGRectMake(x, y, screenBackgroundImageViewWidth, screenBackgroundImageViewHeight)];
-			[backgroundImageView setContentStretch:CGRectMake(0, 0, screenBackgroundImageViewWidth, screenBackgroundImageViewHeight)];
-			[backgroundImageView 
-			[backgroundImageView setBackgroundColor:color];
-		} else {
+		
+		// fullscreen is false
+		if (![[screen background] fullScreen]) {
+			UIImageView *backgroundImageView = [[UIImageView alloc] init];		
+			//[backgroundImageView setContentMode:UIViewContentModeScaleAspectFill];
+			NSLog(@"BackgroundImage's original width:%f, height:%f", backgroundImage.size.width, backgroundImage.size.height);
 			
-		}		
+			// ablosute position of screen background.
+			if ([[screen background] isBackgroundImageAbsolutePosition]) {
+				int left = [[screen background] backgroundImageAbsolutePositionLeft];
+				int top = [[screen background] backgroundImageAbsolutePositionTop];
+				[backgroundImageView setFrame:CGRectMake(left, top, screenBackgroundImageViewWidth-left, screenBackgroundImageViewHeight-top)];
+				if (backgroundImage && backgroundImageView) {
+					CGSize uiImageSize = backgroundImage.size;
+					CGSize uiViewSize = backgroundImageView.bounds.size;
+					CGSize clipCGSize;
+					if(uiImageSize.width > uiViewSize.width && uiImageSize.height > uiViewSize.height) {
+						clipCGSize = uiViewSize;
+					} else if (uiImageSize.width > uiViewSize.width && uiImageSize.height <= uiViewSize.height) {
+						clipCGSize = CGSizeMake(uiViewSize.width, uiImageSize.height);
+					} else if (uiImageSize.width <= uiViewSize.width && uiImageSize.height > uiViewSize.height) {
+						clipCGSize = CGSizeMake(uiImageSize.width, uiViewSize.height);
+					} else {
+						clipCGSize = uiImageSize;
+					}
+					UIGraphicsBeginImageContext(clipCGSize);
+					[backgroundImage drawInRect:CGRectMake(0, 0, clipCGSize.width, clipCGSize.height)];
+					backgroundImage = UIGraphicsGetImageFromCurrentImageContext();
+					[backgroundImage retain];
+					UIGraphicsEndImageContext();
+				}
+				[backgroundImageView setImage:backgroundImage];
+				[backgroundImageView sizeToFit];
+				//UIColor *color = [[UIColor alloc] initWithPatternImage:backgroundImage];				
+				//[backgroundImageView setBackgroundColor:color];
+				//[color release];
+				NSLog(@"Clipped BackgroundImage's width:%f, height:%f", backgroundImage.size.width, backgroundImage.size.height);
+				NSLog(@"BackgroundImageView's width:%f, height:%f", backgroundImageView.bounds.size.width, backgroundImageView.bounds.size.height);
+				NSLog(@"BackgroundImageView's left is %d, top is %d", left, top);
+			}
+			// relative position of screen background.
+			else {
+				// relative position is LEFT
+				NSString *backgroundImageRelativePosition = screen.background.backgroundImageRelativePosition;
+				//if ([backgroundImageRelativePosition isEqualToString:BG_IMAGE_RELATIVE_POSITION_LEFT]) {
+//					
+//				}
+			}
+			[self addSubview:backgroundImageView];
+		}
+		// fullscreen is true
+		else {
+			UIImageView *uiImageView = [[UIImageView alloc] init];
+			[uiImageView setContentMode:UIViewContentModeScaleAspectFill];
+			[uiImageView setFrame:CGRectMake(0, 0, screenBackgroundImageViewWidth, screenBackgroundImageViewHeight)];
+			// Begin clip image
+			if (backgroundImage && uiImageView) {
+				CGSize uiImageSize = backgroundImage.size;
+				CGSize uiImageViewSize = uiImageView.bounds.size;
+				CGSize clipCGSize;
+				if(uiImageSize.width > uiImageViewSize.width && uiImageSize.height > uiImageViewSize.height) {
+					clipCGSize = uiImageViewSize;
+				} else if (uiImageSize.width > uiImageViewSize.width && uiImageSize.height <= uiImageViewSize.height) {
+					clipCGSize = CGSizeMake(uiImageViewSize.width, uiImageSize.height);
+				} else if (uiImageSize.width <= uiImageViewSize.width && uiImageSize.height > uiImageViewSize.height) {
+					clipCGSize = CGSizeMake(uiImageSize.width, uiImageViewSize.height);
+				} else {
+					clipCGSize = uiImageSize;
+				}
+				UIGraphicsBeginImageContext(clipCGSize);
+				[backgroundImage drawInRect:CGRectMake(0, 0, clipCGSize.width, clipCGSize.height)];
+				backgroundImage = UIGraphicsGetImageFromCurrentImageContext();
+				[backgroundImage retain];
+				UIGraphicsEndImageContext();
+			}
+			// End clip image
+			[uiImageView setImage:backgroundImage];
+			[uiImageView sizeToFit];
+			//[uiImageView setContentMode:UIViewContentModeScaleAspectFill];
+			[self addSubview:uiImageView];
+		}
 		NSLog(@"Added width: %d, height: %d backgroundImageView", screenBackgroundImageViewWidth, screenBackgroundImageViewHeight);
-		[self addSubview:backgroundImageView];
-		[color release];
-		[background release];
+		[backgroundImage release];
 	}
 	
 	for (LayoutContainer *layout in screen.layouts) { 
@@ -94,15 +162,10 @@
 //override layoutSubviews method of UIView, In order to resize the ControlView when add subview
 // Only in this time we can know this view's size
 - (void)layoutSubviews {
-	
-	
-	
 }
-
 
 - (void)dealloc {
 	[screen release];
-
 	[super dealloc];
 }
 
