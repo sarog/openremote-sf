7a1c85c673c917a1311042ab2edef6ac33df5976
tomskywang <tomskywang@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-01-08 01:36:04 +0000

    removed unused image property(state) in panelXML.vm, removed setBorder from ResourceServiceImplTest.java.

--- branches/Modeller_gwt_Branch/test/org/openremote/modeler/service/ResourceServiceImplTest.java
+++ branches/Modeller_gwt_Branch/test/org/openremote/modeler/service/ResourceServiceImplTest.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.modeler.service;
 
 import static org.junit.Assert.fail;
@@ -280,7 +299,6 @@ public void testScreenHasGesture(){
       nav.setToLogical(ToLogicalType.back);
       
       ImageSource image = new ImageSource();
-      image.setBorder(14);
       image.setSrc("http://finalist.cn/logo.ico");
       
       UITabbarItem item = new UITabbarItem();
@@ -305,7 +323,6 @@ public void testScreenHasGesture(){
       nav.setToLogical(ToLogicalType.back);
       
       ImageSource image = new ImageSource();
-      image.setBorder(14);
       image.setSrc("http://finalist.cn/logo.ico");
       
       UITabbarItem item = new UITabbarItem();
