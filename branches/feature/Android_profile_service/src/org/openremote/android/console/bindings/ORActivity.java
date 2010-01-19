d159b5964d09aeb34e58d9c867e1f1ecd7b2ea92
tomskywang <tomskywang@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-01-19 07:59:40 +0000

    make a new android branch for develop android 2.0

---
+++ branches/feature/Android_profile_service/src/org/openremote/android/console/bindings/ORActivity.java
@@ -0,0 +1,47 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2009, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.android.console.bindings;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * This is a simple convenience interface so that you do not have to type the
+ * FQN of Activity each time due to name conflicts. SimpleBinder needs the real
+ * class.
+ * 
+ * @see org.openremote.android.console.bindings.Activity
+ * @author Andrew C. Oliver <acoliver osintegrators.com>
+ */
+public interface ORActivity extends Serializable {
+
+    public List<Screen> getScreens();
+
+    public void setScreens(List<Screen> screens);
+
+    public String getId();
+
+    public void setId(String id);
+
+    public void setName(String name);
+
+    public String getName();
+
+}
