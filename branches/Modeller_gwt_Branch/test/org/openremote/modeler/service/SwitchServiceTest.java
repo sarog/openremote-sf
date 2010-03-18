3d461be4e1d0faeb47cd48d2b2ac73abdeb580a7
javenzhang <javenzhang@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-03-18 09:42:18 +0000

    improve switchService and test case for switch service.

--- branches/Modeller_gwt_Branch/test/org/openremote/modeler/service/SwitchServiceTest.java
+++ branches/Modeller_gwt_Branch/test/org/openremote/modeler/service/SwitchServiceTest.java
@@ -6,9 +6,12 @@ import org.openremote.modeler.SpringTestContext;
 import org.openremote.modeler.client.Constants;
 import org.openremote.modeler.domain.DeviceCommand;
 import org.openremote.modeler.domain.Protocol;
+import org.openremote.modeler.domain.Sensor;
+import org.openremote.modeler.domain.SensorCommandRef;
 import org.openremote.modeler.domain.Switch;
 import org.openremote.modeler.domain.SwitchCommandOffRef;
 import org.openremote.modeler.domain.SwitchCommandOnRef;
+import org.openremote.modeler.domain.SwitchSensorRef;
 import org.springframework.security.Authentication;
 import org.springframework.security.context.SecurityContextHolder;
 import org.springframework.security.providers.UsernamePasswordAuthenticationToken;
@@ -19,16 +22,19 @@ import org.testng.annotations.Test;
 public class SwitchServiceTest {
    private SwitchService service = null;
    private DeviceCommandService deviceCommandService;
+   private SensorService sensorService = null;
    @BeforeClass
    public void setUp(){
       service = (SwitchService) SpringTestContext.getInstance().getBean("switchService");
       deviceCommandService = (DeviceCommandService) SpringTestContext.getInstance().getBean("deviceCommandService");
+      sensorService = (SensorService)SpringTestContext.getInstance().getBean("sensorService");
       Authentication authentication = new UsernamePasswordAuthenticationToken("javen","javen");
       SecurityContextHolder.getContext().setAuthentication(authentication);
    }
    
    @Test
    public void testSaveSwitch(){
+      
       Switch swh = new Switch();
       Protocol protocol = new Protocol();
       protocol.setType(Constants.INFRARED_TYPE);
@@ -37,6 +43,20 @@ public class SwitchServiceTest {
       cmd.setProtocol(protocol);
       cmd.setName("testLirc");
       deviceCommandService.save(cmd);
+      
+      Sensor sensor = new Sensor();
+      sensor.setName("Test");
+      SensorCommandRef sensorCmdRef = new SensorCommandRef();
+      sensorCmdRef.setDeviceCommand(cmd);
+      sensor.setSensorCommandRef(sensorCmdRef);
+      
+      sensorCmdRef.setSensor(sensor);
+      sensorService.saveSensor(sensor);
+      SwitchSensorRef sensorRef = new SwitchSensorRef();
+      sensorRef.setSensor(sensor);
+      sensorRef.setSwitchToggle(swh);
+      
+      swh.setSwitchSensorRef(sensorRef);
       
       swh.setName("testName");
       
@@ -55,7 +75,12 @@ public class SwitchServiceTest {
 //      swh.setOffDeviceCommandRef(cmdRef);
       
       Switch swh2 = new Switch();
-      swh.setName("testName2");
+      swh2.setSwitchSensorRef(sensorRef);
+      
+      swh2.setName("testName2");
+      
+      swh2.setSwitchCommandOnRef(swhOnCmdRef);
+      swh2.setSwitchCommandOffRef(swhOffCmdRef);
       
       service.save(swh);
       service.save(swh2);
@@ -63,14 +88,49 @@ public class SwitchServiceTest {
       Switch switchFromTable = service.loadAll().get(0);
       Assert.assertEquals(swh.getOid(),1);
       Assert.assertEquals(swh2.getOid(),2);
+      Assert.assertTrue(swh.getSwitchCommandOffRef().getOid()>=1);
+      Assert.assertTrue(swh.getSwitchCommandOnRef().getOid()>=1);
+      Assert.assertTrue(swh.getSwitchSensorRef().getOid()>=1);
       Assert.assertEquals(switchFromTable.getSwitchCommandOnRef().getDeviceCommand().getName(), "testLirc");
       Assert.assertEquals(switchFromTable.getSwitchCommandOffRef().getDeviceCommand().getName(), "testLirc");
    }
    @Test(dependsOnMethods="testSaveSwitch")
    public void testUpdate(){
       Switch swh = new Switch();
-      swh.setName("testUpdate");
+      Protocol protocol = new Protocol();
+      protocol.setType(Constants.INFRARED_TYPE);
+      
+      DeviceCommand cmd = new DeviceCommand();
+      cmd.setProtocol(protocol);
+      cmd.setName("testLirc2");
+      deviceCommandService.save(cmd);
+      
+      Sensor sensor = new Sensor();
+      sensor.setName("SensorAfterBeUpdated");
+      SensorCommandRef sensorCmdRef = new SensorCommandRef();
+      sensorCmdRef.setDeviceCommand(cmd);
+      sensor.setSensorCommandRef(sensorCmdRef);
+      
+      sensorCmdRef.setSensor(sensor);
+      sensorService.saveSensor(sensor);
+      SwitchSensorRef sensorRef = new SwitchSensorRef();
+      sensorRef.setSensor(sensor);
+      sensorRef.setSwitchToggle(swh);
+      
+      swh.setSwitchSensorRef(sensorRef);
+      
+      SwitchCommandOnRef swhOnCmdRef = new SwitchCommandOnRef();
+      swhOnCmdRef.setDeviceCommand(cmd);
+      swhOnCmdRef.setOnSwitch(swh);
+      
+      SwitchCommandOffRef swhOffCmdRef = new SwitchCommandOffRef();
+      swhOffCmdRef.setDeviceCommand(cmd);
+      swhOffCmdRef.setOffSwitch(swh);
+      
+      swh.setSwitchCommandOnRef(swhOnCmdRef);
+      swh.setSwitchCommandOffRef(swhOffCmdRef);
       swh.setOid(1);
+      swh.setName("testUpdate");
       service.update(swh);
       
       Collection<Switch> switchs = service.loadAll();
@@ -78,6 +138,9 @@ public class SwitchServiceTest {
       for(Switch s: switchs){
          if(s.getOid() == 1){
             Assert.assertEquals(s.getName(), "testUpdate");
+            Assert.assertTrue(s.getSwitchCommandOffRef().getDeviceCommand().getName().equals("testLirc2"));
+            Assert.assertTrue(s.getSwitchCommandOnRef().getDeviceCommand().getName().equals("testLirc2"));
+            Assert.assertTrue(s.getSwitchSensorRef().getSensor().getName().equals("SensorAfterBeUpdated"));
             break;
          }
       }
