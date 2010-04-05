aa951aca0292feeb3b8403c89525a458e00ebdb7
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-04-05 05:05:17 +0000

    tag Beehive 3.0.0 Alpha3

---
+++ Beehive/Beehive_3_0_0_Alpha3/src/org/openremote/beehive/domain/Account.java
@@ -0,0 +1,80 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.beehive.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+/**
+ * The Class Account.
+ * 
+ * @author Dan 2009-7-7
+ */
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "account")
+public class Account extends BusinessEntity {
+
+   private User user;
+   
+   private List<Template> templates;
+
+
+   public Account() {
+      templates = new ArrayList<Template>();
+   }
+
+
+   @OneToOne
+   public User getUser() {
+      return user;
+   }
+
+
+   public void setUser(User user) {
+      this.user = user;
+   }
+
+
+   @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
+   public List<Template> getTemplates() {
+      return templates;
+   }
+
+
+   public void setTemplates(List<Template> templates) {
+      this.templates = templates;
+   }
+   
+   public void addTemplate(Template t) {
+      if (templates != null) {
+         templates.add(t);
+      }
+   }
+
+   
+}
