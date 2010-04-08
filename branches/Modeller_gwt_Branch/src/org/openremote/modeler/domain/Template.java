56b7920714f7896ac4b5872aa173abadf896e1dc
javenzhang <javenzhang@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-04-08 23:06:52 +0000

    refactor TemplateController. Modified Template.java: added method equals.

--- branches/Modeller_gwt_Branch/src/org/openremote/modeler/domain/Template.java
+++ branches/Modeller_gwt_Branch/src/org/openremote/modeler/domain/Template.java
@@ -101,4 +101,25 @@ public class Template extends BusinessEntity {
    public String getDisplayName() {
       return name + "( " +keywords +" )";
    }
+
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj) return true;
+      if (obj == null) return false;
+      if (getClass() != obj.getClass()) return false;
+      Template other = (Template) obj;
+      if (content == null) {
+         if (other.content != null) return false;
+      } else if (!content.equals(other.content)) return false;
+      if (keywords == null) {
+         if (other.keywords != null) return false;
+      } else if (!keywords.equals(other.keywords)) return false;
+      if (name == null) {
+         if (other.name != null) return false;
+      } else if (!name.equals(other.name)) return false;
+      if (shared != other.shared) return false;
+      return true;
+   }
+   
+   
 }
