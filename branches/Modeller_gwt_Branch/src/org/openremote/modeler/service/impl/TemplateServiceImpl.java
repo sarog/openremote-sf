1ff03b6a131ed522ad9a3c01a9ede3d856392aee
javenzhang <javenzhang@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-04-15 08:36:49 +0000

    fixed bug: all the public templates will be set to private when it is edited.

--- branches/Modeller_gwt_Branch/src/org/openremote/modeler/service/impl/TemplateServiceImpl.java
+++ branches/Modeller_gwt_Branch/src/org/openremote/modeler/service/impl/TemplateServiceImpl.java
@@ -1060,6 +1060,7 @@ public class TemplateServiceImpl implements TemplateService {
       private String content;
       private String name;
       private String keywords;
+      private boolean shared = false;
 
       public int getId() {
          return id;
@@ -1093,6 +1094,14 @@ public class TemplateServiceImpl implements TemplateService {
       public void setKeywords(String keywords) {
          this.keywords = keywords;
       }
+      
+      public boolean isShared() {
+         return shared;
+      }
+
+      public void setShared(boolean shared) {
+         this.shared = shared;
+      }
 
       public Template toTemplate() {
          Template template = new Template();
@@ -1100,6 +1109,7 @@ public class TemplateServiceImpl implements TemplateService {
          template.setContent(content);
          template.setOid(id);
          template.setKeywords(keywords);
+         template.setShared(shared);
          return template;
       }
    }
