b0d4e3c836d93c99bdf8836af2ed5b2bc68150fd
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-05-14 06:12:10 +0000

    1)Fixed bug. DESC: If slider doesn't contain subelements: link, min, max, the logic of render sliderview got NumberFormatException. Solution: Judge the min and max, before set progress.
    2)Fixed the ORConnection for 401 loginview(UI thread), ButtonView sending repeated commands and SliderView sending command on release thumb.

--- branches/feature/Android_profile_service/src/org/openremote/android/console/net/ORConnection.java
+++ branches/feature/Android_profile_service/src/org/openremote/android/console/net/ORConnection.java
@@ -38,6 +38,8 @@ import org.openremote.android.console.exceptions.ORConnectionException;
 import org.openremote.android.console.util.SecurityUtil;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 
 /**
@@ -47,142 +49,152 @@ import android.util.Log;
  *
  */
 
-public class ORConnection {	
-	private HttpClient httpClient;
-	private HttpRequestBase httpRequest;
-	private HttpResponse httpResponse;
-	private ORConnectionDelegate delegate;
-	private Context context;
-	/** 
-	 * Establish the HttpBasicAuthentication httpconnection depend on param <b>isNeedHttpBasicAuth</b> with url for caller,<br />
-	 * and then the caller can deal with the httprequest result within ORConnectionDelegate instance.
-	 */
-	public ORConnection (Context context, ORHttpMethod httpMethod, boolean isNeedHttpBasicAuth, String url, ORConnectionDelegate delegateParam) {
-		delegate = delegateParam;
-		this.context = context;
-		HttpParams params = new BasicHttpParams();
+public class ORConnection {   
+   private HttpClient httpClient;
+   private HttpRequestBase httpRequest;
+   private HttpResponse httpResponse;
+   private ORConnectionDelegate delegate;
+   private Context context;
+   /** 
+    * Establish the HttpBasicAuthentication httpconnection depend on param <b>isNeedHttpBasicAuth</b> with url for caller,<br />
+    * and then the caller can deal with the httprequest result within ORConnectionDelegate instance.
+    */
+   public ORConnection (final Context context, ORHttpMethod httpMethod, boolean isNeedHttpBasicAuth, String url, ORConnectionDelegate delegateParam) {
+      handler = new Handler() {
+         @Override
+         public void handleMessage(Message msg) {
+            int statusCode = msg.what;
+            if (statusCode == 0) {
+               connectionDidFailWithException(context, new ORConnectionException("Httpclient execute httprequest fail."));
+            } else {
+               dealWithResponse();
+            }
+         }
+     };
+      
+      delegate = delegateParam;
+      this.context = context;
+      HttpParams params = new BasicHttpParams();
       HttpConnectionParams.setConnectionTimeout(params, 50 * 1000);
       HttpConnectionParams.setSoTimeout(params, 50 * 1000);
-		httpClient = new DefaultHttpClient(params);
-		if (ORHttpMethod.POST.equals(httpMethod)) {
-			httpRequest = new HttpPost(url);
-		} else if (ORHttpMethod.GET.equals(httpMethod)) {
-			httpRequest = new HttpGet(url);
-		}
-		
-		if (httpRequest == null) {
-			throw new ORConnectionException("Create HttpRequest fail.");
-		}
-		
-		if (isNeedHttpBasicAuth) {
-			SecurityUtil.addCredentialToHttpRequest(context, httpRequest);
-		}
-		execute();
-	}
+      httpClient = new DefaultHttpClient(params);
+      if (ORHttpMethod.POST.equals(httpMethod)) {
+         httpRequest = new HttpPost(url);
+      } else if (ORHttpMethod.GET.equals(httpMethod)) {
+         httpRequest = new HttpGet(url);
+      }
+      
+      if (httpRequest == null) {
+         throw new ORConnectionException("Create HttpRequest fail.");
+      }
+      
+      if (isNeedHttpBasicAuth) {
+         SecurityUtil.addCredentialToHttpRequest(context, httpRequest);
+      }
+      execute();
+   }
 
-	/** Execute the http request.*/
-	public void execute() {
-	   new Thread(new Runnable() {
+   /** Execute the http request.*/
+   public void execute() {
+      new Thread(new Runnable() {
          public void run() {
             try {
                httpResponse = httpClient.execute(httpRequest);
             } catch (SocketTimeoutException e) {
-               connectionDidFailWithException(context, new ORConnectionException("Httpclient execute httprequest fail."));
-               return;
+               handler.sendEmptyMessage(0);
             } catch (ClientProtocolException e) {
-               connectionDidFailWithException(context, new ORConnectionException("Httpclient execute httprequest fail."));
-               return;
+               handler.sendEmptyMessage(0);
             } catch (IOException e) {
-               connectionDidFailWithException(context, new ORConnectionException("Httpclient execute httprequest fail."));
-               return;
+               handler.sendEmptyMessage(0);
             }
-            dealWithResponse();
+            handler.sendEmptyMessage(200);
          }
       }).start(); 
-	}
-	
-	/** Deal with the response while httpconnection of android console to controller success. */
-	private void dealWithResponse() {
-		connectionDidReceiveResponse();
-		connectionDidReceiveData();
-	}
-	
-	/**  delegate methods of self */
-	
-	/** 
-	 * This method is invoked by self while the connection of android console to controller was failed,
-	 * and sends a notification to delegate with <b>urlConnectionDidFailWithException</b> method calling and
-	 * switching the connection of android console to a available controller server in groupmembers of self.
-	 */
-	private void connectionDidFailWithException(Context context, ORConnectionException e) {
-		delegate.urlConnectionDidFailWithException(e);
-		ORControllerServerSwitcher.doSwitch(context);
-	}
-	
-	/** 
-	 * This method is invoked by self while the connection of android console to controller received response,
-	 * and sends a notification to delegate with <b>urlConnectionDidReceiveResponse</b> method calling.
-	 */
-	private void connectionDidReceiveResponse() {
-		delegate.urlConnectionDidReceiveResponse(httpResponse);
-	}
-	
-	/**
-	 * This method is invoked by self while the connection of android console to controller received data,
-	 * and sends a notification to delegate with <b>urlConnectionDidReceiveData</b> method calling.
-	 */
-	private void connectionDidReceiveData() {
-		try {
-			if (httpResponse.getStatusLine().getStatusCode() == Constants.HTTP_SUCCESS) {
-				delegate.urlConnectionDidReceiveData(httpResponse.getEntity().getContent());
-			} else {
-				new ORConnectionException("Get the entity's content of httpresponse fail."); 
-			}
-		} catch (IllegalStateException e) {
-			throw new ORConnectionException("Get the entity's content of httpresponse fail.", e);
-		} catch (IOException e) {
-			throw new ORConnectionException("Get the entity's content of httpresponse fail.", e);
-		}
-	}
-	
-	/** 
-	 * Establish the httpconnection with url for caller<br />
-	 * and then the caller can deal with the httprequest result within ORConnectionDelegate instance.
-	 */
-	@SuppressWarnings("finally")
+   }
+   
+   /** Deal with the response while httpconnection of android console to controller success. */
+   private void dealWithResponse() {
+      connectionDidReceiveResponse();
+      connectionDidReceiveData();
+   }
+   
+   /**  delegate methods of self */
+   
+   /** 
+    * This method is invoked by self while the connection of android console to controller was failed,
+    * and sends a notification to delegate with <b>urlConnectionDidFailWithException</b> method calling and
+    * switching the connection of android console to a available controller server in groupmembers of self.
+    */
+   private void connectionDidFailWithException(Context context, ORConnectionException e) {
+      delegate.urlConnectionDidFailWithException(e);
+      ORControllerServerSwitcher.doSwitch(context);
+   }
+   
+   /** 
+    * This method is invoked by self while the connection of android console to controller received response,
+    * and sends a notification to delegate with <b>urlConnectionDidReceiveResponse</b> method calling.
+    */
+   private void connectionDidReceiveResponse() {
+      delegate.urlConnectionDidReceiveResponse(httpResponse);
+   }
+   
+   /**
+    * This method is invoked by self while the connection of android console to controller received data,
+    * and sends a notification to delegate with <b>urlConnectionDidReceiveData</b> method calling.
+    */
+   private void connectionDidReceiveData() {
+      try {
+         if (httpResponse.getStatusLine().getStatusCode() == Constants.HTTP_SUCCESS) {
+            delegate.urlConnectionDidReceiveData(httpResponse.getEntity().getContent());
+         } else {
+            new ORConnectionException("Get the entity's content of httpresponse fail."); 
+         }
+      } catch (IllegalStateException e) {
+         throw new ORConnectionException("Get the entity's content of httpresponse fail.", e);
+      } catch (IOException e) {
+         throw new ORConnectionException("Get the entity's content of httpresponse fail.", e);
+      }
+   }
+   
+   /** 
+    * Establish the httpconnection with url for caller<br />
+    * and then the caller can deal with the httprequest result within ORConnectionDelegate instance.
+    */
+   @SuppressWarnings("finally")
    public static HttpResponse checkURLWithHTTPProtocol (Context context, ORHttpMethod httpMethod, String url, boolean isNeedBasicAuth) {
-		HttpRequestBase request = null;
-		HttpResponse response = null;
-		
-		HttpParams params = new BasicHttpParams();
+      HttpRequestBase request = null;
+      HttpResponse response = null;
+      
+      HttpParams params = new BasicHttpParams();
       HttpConnectionParams.setConnectionTimeout(params, 5 * 1000);
       HttpConnectionParams.setSoTimeout(params, 5 * 1000);
-		HttpClient client = new DefaultHttpClient(params);
-		if (ORHttpMethod.POST.equals(httpMethod)) {
-			request = new HttpPost(url);
-		} else if (ORHttpMethod.GET.equals(httpMethod)) {
-			request = new HttpGet(url);
-		}
-		
-		if (request == null) {
-			throw new ORConnectionException("Create HttpRequest fail.");
-		}
-		if (isNeedBasicAuth) {
-			SecurityUtil.addCredentialToHttpRequest(context, request);
-		}
-		
+      HttpClient client = new DefaultHttpClient(params);
+      if (ORHttpMethod.POST.equals(httpMethod)) {
+         request = new HttpPost(url);
+      } else if (ORHttpMethod.GET.equals(httpMethod)) {
+         request = new HttpGet(url);
+      }
+      
+      if (request == null) {
+         throw new ORConnectionException("Create HttpRequest fail.");
+      }
+      if (isNeedBasicAuth) {
+         SecurityUtil.addCredentialToHttpRequest(context, request);
+      }
+      
         try {
-        	response = client.execute(request);
-        	return response;
-		} catch (ClientProtocolException e) {
-		   Log.e("ERROR", "ClientProtocolException while checking URLWithHTTPProtocol.");
-		} catch (SocketTimeoutException e) {
-		   Log.e("ERROR", "SocketTimeoutException while checking URLWithHTTPProtocol.");
-		} catch (IOException e) {
-		   Log.e("ERROR", "IOException while checking URLWithHTTPProtocol.");
-		} finally {
-		   return response;
-		}
-	}
-	
+         response = client.execute(request);
+         return response;
+      } catch (ClientProtocolException e) {
+         Log.e("ERROR", "ClientProtocolException while checking URLWithHTTPProtocol.");
+      } catch (SocketTimeoutException e) {
+         Log.e("ERROR", "SocketTimeoutException while checking URLWithHTTPProtocol.");
+      } catch (IOException e) {
+         Log.e("ERROR", "IOException while checking URLWithHTTPProtocol.");
+      } finally {
+         return response;
+      }
+   }
+   
+   private Handler handler;
 }
