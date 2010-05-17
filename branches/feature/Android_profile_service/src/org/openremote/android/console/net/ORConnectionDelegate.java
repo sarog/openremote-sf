a5f04dad9c39f4199d6653345802ae4fb78b22c1
dancong <dancong@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-05-17 08:45:37 +0000

    fixed bug : #ANDROID-54 Unable to add window when PanelSelectorActivity.urlConnectionDidFailWithException

--- branches/feature/Android_profile_service/src/org/openremote/android/console/net/ORConnectionDelegate.java
+++ branches/feature/Android_profile_service/src/org/openremote/android/console/net/ORConnectionDelegate.java
@@ -35,10 +35,10 @@ public interface ORConnectionDelegate {
 	/** This callback method is called in ORConnection fail condition. */
 	public void urlConnectionDidFailWithException(Exception e);
 
-	/** This callback method is involked while ORConnection getting httpresponse. */
+	/** This callback method is involked while ORConnection getting http response (not 200). */
 	public void urlConnectionDidReceiveResponse(HttpResponse httpResponse);
 
-	/** This callback method is called while ORConnection receiving data included in httpresponse. */
+	/** This callback method is called while ORConnection receiving data included in http response (200). */
 	public void urlConnectionDidReceiveData(InputStream data);
 
 }
