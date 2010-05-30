24fd88cda7837bb6c14f73c8d5f4af9175d1e385
dancong <dancong@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-05-30 16:19:14 +0000

    first import, added 2 launch images, app icon image, supported all 4 orientations for iPad according to iPad programming guide. fixed some UI layout issues.

---
+++ branches/feature/iPad_iPhone_universal_app_profile_service/Classes/ChoosePanelViewController.m
@@ -0,0 +1,253 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2009, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+#import "ChoosePanelViewController.h"
+#import "URLConnectionHelper.h"
+#import "ServerDefinition.h"
+#import "AppSettingsDefinition.h"
+#import "ControllerException.h"
+#import "CredentialUtil.h"
+#import "Definition.h"
+#import "DataBaseService.h"
+#import "NotificationConstant.h"
+
+@interface ChoosePanelViewController (Private)
+
+- (void)requestPanelList;
+
+@end
+
+
+@implementation ChoosePanelViewController
+
+
+- (id)init {
+	if (self = [super initWithStyle:UITableViewStyleGrouped]) {
+		[self setTitle:@"Panel List"];
+		panels = [[NSMutableArray alloc] init];
+		chosenPanel = [[AppSettingsDefinition getPanelIdentityDic] objectForKey:@"identity"];
+		[self requestPanelList];
+	}
+	return self;
+}
+
+- (void)requestPanelList {
+	[[NSNotificationCenter defaultCenter] postNotificationName:NotificationShowLoading object:nil];
+	NSString *location = [[NSString alloc] initWithFormat:[ServerDefinition panelsRESTUrl]];
+	NSURL *url = [[NSURL alloc]initWithString:location];
+	NSLog(@"panels:%@",location);
+	
+	//assemble put request 
+	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+	[request setURL:url];
+	[request setHTTPMethod:@"GET"];
+	[CredentialUtil addCredentialToNSMutableURLRequest:request];
+	
+	URLConnectionHelper *connection = [[URLConnectionHelper alloc]initWithRequest:request  delegate:self];
+	
+	[location release];
+	[url	 release];
+	[request release];
+	[connection autorelease];	
+}
+
+#pragma mark Table view methods
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+		return @"Choose Your Panel Identity:";
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
+	return @"UI differs according to different panel identity.";
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return 1;
+}
+
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return panels.count;
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	static NSString *panelCellIdentifier = @"panelCell";
+	UITableViewCell *panelCell = [tableView dequeueReusableCellWithIdentifier:panelCellIdentifier];
+	
+	if (panelCell == nil) {
+		panelCell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:panelCellIdentifier] autorelease];
+	}
+	
+	panelCell.textLabel.text = [panels objectAtIndex:indexPath.row];
+	panelCell.selectionStyle = UITableViewCellSelectionStyleNone;
+	
+	if ([panelCell.textLabel.text isEqualToString:chosenPanel]) {
+		panelCell.accessoryType = UITableViewCellAccessoryCheckmark;
+	}
+	return panelCell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	
+	UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+	if (currentSelectedPanelIndex) {
+		UITableViewCell *oldCell = [tableView cellForRowAtIndexPath:currentSelectedPanelIndex];
+		if (cell.accessoryType  == oldCell.accessoryType) {
+			return;
+		}
+		if (oldCell.accessoryType == UITableViewCellAccessoryCheckmark) {
+			oldCell.accessoryType = UITableViewCellAccessoryNone;
+		} 
+	} 
+	if (cell.accessoryType == UITableViewCellAccessoryNone) {
+		cell.accessoryType = UITableViewCellAccessoryCheckmark;
+		[[AppSettingsDefinition getPanelIdentityDic] setObject:cell.textLabel.text forKey:@"identity"];
+	} 
+	
+	currentSelectedPanelIndex = indexPath;
+	
+	[AppSettingsDefinition writeToFile];
+	[self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)showLoginAlert {
+	
+	UIAlertView *prompt = [[UIAlertView alloc] initWithTitle:@"Please Sign In" 
+																									 message:@"\n\n\n" // IMPORTANT
+																									delegate:self 
+																				 cancelButtonTitle:@"Cancel" 
+																				 otherButtonTitles:@"OK", nil];
+	
+	textField = [[UITextField alloc] initWithFrame:CGRectMake(22.0, 50.0, 240.0, 25.0)]; 
+	[textField setBackgroundColor:[UIColor whiteColor]];
+	if ([Definition sharedDefinition].username == nil) {
+		[textField setPlaceholder:@"username"];
+	} else {
+		[textField setText:[Definition sharedDefinition].username];
+	}
+	
+	[prompt addSubview:textField];
+	
+	textField2 = [[UITextField alloc] initWithFrame:CGRectMake(22.0, 85.0, 240.0, 25.0)]; 
+	[textField2 setBackgroundColor:[UIColor whiteColor]];
+	[textField2 setPlaceholder:@"password"];
+	[textField2 setSecureTextEntry:YES];
+	[prompt addSubview:textField2];
+	
+	// set place
+	[prompt setTransform:CGAffineTransformMakeTranslation(0.0, 110.0)];
+	[prompt show];
+	[prompt release];
+	
+	// set cursor and show keyboard
+	[textField becomeFirstResponder];
+	
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+	if (buttonIndex == 1) {
+		[Definition sharedDefinition].username = textField.text;
+		[Definition sharedDefinition].password = textField2.text;
+		[[DataBaseService sharedDataBaseService] saveCurrentUser];
+		[self requestPanelList];
+	} 
+}
+
+- (void)handleServerErrorWithStatusCode:(int) statusCode {
+	if (statusCode != 200) {
+		if (statusCode == 401) {
+			[self showLoginAlert];
+		} else {
+			[ViewHelper showAlertViewWithTitle:@"Panel List Error" Message:[ControllerException exceptionMessageOfCode:statusCode]];	
+		}
+	} 
+}
+
+
+- (void)updateTableView {
+	UITableView *tv = (UITableView *)self.view;
+	[tv beginUpdates];
+	
+	NSArray *newArray = nil;
+	newArray = panels;
+	
+	NSMutableArray *insertIndexPaths = [[NSMutableArray alloc] init];
+	for (int j = 0; j < newArray.count; j++){
+		[insertIndexPaths addObject:[NSIndexPath indexPathForRow:j inSection:0]];
+	}
+	[tv insertRowsAtIndexPaths:insertIndexPaths withRowAnimation:UITableViewRowAnimationBottom];
+	
+	[tv endUpdates];
+	
+	[insertIndexPaths release];
+}
+
+
+#pragma mark delegate method of NSURLConnection
+- (void) definitionURLConnectionDidFailWithError:(NSError *)error {
+	
+}
+
+
+- (void)definitionURLConnectionDidFinishLoading:(NSData *)data {
+	NSString *result = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+	
+	NSXMLParser *xmlParser = [[NSXMLParser alloc] initWithData:data];
+	[xmlParser setDelegate:self];
+	[xmlParser parse];
+	[[NSNotificationCenter defaultCenter] postNotificationName:NotificationHideLoading object:nil];
+	[self updateTableView];
+	[xmlParser release];
+	[result release];
+}
+
+- (void)definitionURLConnectionDidReceiveResponse:(NSURLResponse *)response {
+	NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)response;
+	[self handleServerErrorWithStatusCode:[httpResp statusCode]];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+	[[NSNotificationCenter defaultCenter] postNotificationName:NotificationHideLoading object:nil];
+}
+
+#pragma mark delegate method of NSXMLParser
+//when find a panel start we get its *name* attribute as logical identity
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName attributes:(NSDictionary *)attributeDict{
+	if ([elementName isEqualToString:@"panel"]) {
+		NSLog(@"panel logical id : %@",[attributeDict valueForKey:@"name"]);
+		[panels addObject:[attributeDict valueForKey:@"name"]]; 
+	}
+}
+
+
+- (void)dealloc {
+	[panels release];
+	[currentSelectedPanelIndex release];
+	[chosenPanel release];
+	
+	[super dealloc];
+}
+
+
+@end
+
---
+++ branches/feature/iPad_iPhone_universal_app_profile_service/Classes/GroupController.m
@@ -0,0 +1,309 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2009, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+#import "GroupController.h"
+#import "NotificationConstant.h"
+#import "PaginationController.h"
+#import "Screen.h"
+#import "ScreenViewController.h"
+
+
+@interface GroupController (Private)
+
+- (NSMutableArray *)initScreenViewControllers:(NSArray *)screens;
+- (void)showErrorView;
+- (void)detectDeviceOrientation;
+- (void)printOrientation:(UIInterfaceOrientation)toInterfaceOrientation;
+
+@end
+
+
+
+@implementation GroupController
+
+@synthesize group;
+
+- (id)initWithGroup:(Group *)newGroup {
+	if (self = [super init]) {
+		if (newGroup) {
+			group = [newGroup retain];// must retain newGroup here!!!
+		}
+		[self detectDeviceOrientation];
+	}
+	return self;
+}
+
+- (id)initWithGroup:(Group *)newGroup orientation:(UIInterfaceOrientation)thatOrientation {
+	if (self = [super init]) {
+		if (newGroup) {
+			group = [newGroup retain];// must retain newGroup here!!!
+		}
+		currentOrientation = thatOrientation;
+	}
+	return self;
+}
+
+- (BOOL)isNew {
+	return paginationController.selectedIndex == 0;
+}
+
+- (BOOL)switchToFirstScreen {
+	return [paginationController switchToFirstScreen];
+}
+
+- (void)detectDeviceOrientation {
+	[[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+	currentOrientation = [[UIDevice currentDevice] orientation];
+	
+	if (currentOrientation == UIDeviceOrientationUnknown) {
+		currentOrientation = UIInterfaceOrientationPortrait;
+		NSLog(@"it's using simulator, set portrait by default");
+	}
+	
+	if (currentOrientation == UIDeviceOrientationFaceUp || 
+			currentOrientation == UIDeviceOrientationFaceDown) {
+		currentOrientation = UIInterfaceOrientationPortrait;
+		NSLog(@"it's facing up/down, set portrait by default");
+	}
+}
+
+- (UIInterfaceOrientation)getCurrentOrientation {
+	return currentOrientation;
+}
+
+- (void)setNewOrientation:(UIInterfaceOrientation)newOrientation {
+	[self willRotateToInterfaceOrientation:newOrientation duration:0];
+}
+
+- (BOOL)isOrientationLandscape {
+	return UIInterfaceOrientationIsLandscape(currentOrientation);
+}
+
+- (CGRect)getFullFrame {
+	CGRect frame = self.view.frame;
+	CGSize size = [UIScreen mainScreen].bounds.size;
+	BOOL isLandscape = [self isOrientationLandscape];
+	frame.size.height = isLandscape ? size.width : size.height;
+	frame.size.width = isLandscape ? size.height : size.width;
+	return frame;
+}
+
+- (BOOL)hasNoViewInThatOrientation:(BOOL)isLandscape {
+	if (isLandscape) {
+		return [group getLandscapeScreens].count == 0;
+	} else {
+		return [group getPortraitScreens].count == 0;
+	}
+	return NO;
+}
+
+- (int)groupId {
+	return group.groupId;
+}
+
+- (NSMutableArray *)initScreenViewControllers:(NSArray *)screens {
+	NSMutableArray *viewControllers = [[NSMutableArray alloc] init];
+	
+	for (Screen *screen in screens) {
+		NSLog(@"init screen = %@", screen.name);
+		ScreenViewController *viewController = [[ScreenViewController alloc]init];
+		[viewController setScreen:screen];
+		[viewControllers addObject:viewController];
+		[viewController release];
+	}
+	return viewControllers;
+}
+
+- (PaginationController *)currentPaginationController {	
+	return paginationController;
+}
+
+- (void)showLandscapeOrientation:(BOOL)isLandscape {
+	NSArray *screens = isLandscape ? [group getLandscapeScreens] : [group getPortraitScreens];
+	if (screens.count > 0) {
+		[[paginationController currentScreenViewController] stopPolling];
+		if (lastPaginationController == nil) {
+			lastPaginationController = [[PaginationController alloc] init];
+			NSMutableArray *viewControllers = [self initScreenViewControllers:screens];
+			[lastPaginationController setViewControllers:viewControllers isLandscape:isLandscape];
+			[viewControllers release];
+		}
+		PaginationController *temp = lastPaginationController;
+		lastPaginationController = paginationController;
+		paginationController = temp;
+		[self setView:paginationController.view];
+		[[paginationController currentScreenViewController] startPolling];
+	} else {
+		[self showErrorView];
+	}
+
+}
+
+- (void)showPortrait {
+	NSLog(@"show portrait");
+	[self showLandscapeOrientation:NO];
+}
+
+- (void)showLandscape {
+	NSLog(@"show landscape");
+	[self showLandscapeOrientation:YES];
+}
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	[self.navigationController setNavigationBarHidden:YES];
+	if (UIInterfaceOrientationIsPortrait(currentOrientation)) {
+		NSLog(@"view did load show portrait");
+		[self showPortrait];
+	} else if (UIInterfaceOrientationIsLandscape(currentOrientation)) {
+		NSLog(@"view did load show landscape");
+		[self showLandscape];
+	} 
+}
+
+- (void)showErrorView {
+	errorViewController = [[ErrorViewController alloc] 
+															initWithErrorTitle:@"No Screen Found" 
+																				 message:@"Please associate screens with this group of this orientation."];
+	[self setView:errorViewController.view];	
+}
+
+- (ScreenViewController *)currentScreenViewController {
+	return [[self currentPaginationController] currentScreenViewController]; 
+}
+
+- (Screen *)currentScreen {
+	return [self currentScreenViewController].screen;
+}
+
+- (int)currentScreenId {
+	return [self currentScreen].screenId;
+}
+
+- (void)startPolling {
+	if ([self currentPaginationController].viewControllers.count > 0) {
+		[[self currentScreenViewController] startPolling];
+		NSLog(@"start polling screen_id = %d",[self currentScreenId]);
+	}
+}
+
+- (void)stopPolling {
+	for (ScreenViewController *svc in [self currentPaginationController].viewControllers) {
+		NSLog(@"stop polling screen_id = %d",svc.screen.screenId);
+		[svc stopPolling];
+	}
+}
+
+- (BOOL)switchToScreen:(int)screenId {
+	NSLog(@"switch to screen %d", screenId);
+	return [[self currentPaginationController] switchToScreen:screenId];
+}
+
+- (BOOL)previousScreen {
+	return [[self currentPaginationController] previousScreen];
+}
+
+- (BOOL)nextScreen {
+	return [[self currentPaginationController] nextScreen];
+}
+
+- (void)performGesture:(Gesture *)gesture {
+	return [[self currentScreenViewController] performGesture:gesture];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	if (UIInterfaceOrientationIsPortrait(currentOrientation) == UIInterfaceOrientationIsPortrait(interfaceOrientation)) {
+		return YES;
+	}
+	if ([self currentScreenId] > 0) {
+		return [[self currentScreen] inverseScreenId] > 0;
+	} else {
+		return ![self hasNoViewInThatOrientation:UIInterfaceOrientationIsLandscape(interfaceOrientation)];
+	}
+
+	return YES;
+}
+
+
+- (void)printOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+	switch (toInterfaceOrientation) {
+		case UIInterfaceOrientationPortrait:
+			NSLog(@"is portrait");
+			break;
+		case UIInterfaceOrientationLandscapeLeft:
+			NSLog(@"is landscape left");
+			break;
+		case UIInterfaceOrientationLandscapeRight:
+			NSLog(@"is landscape right");
+			break;
+		case UIInterfaceOrientationPortraitUpsideDown:
+			NSLog(@"is portrait upsidedown");
+			break;
+		default:
+			NSLog(@"is unknown orientation, it's using simulator");
+			break;
+	}
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+	[self printOrientation:currentOrientation];
+	[self printOrientation:toInterfaceOrientation];
+	if (UIInterfaceOrientationIsPortrait(toInterfaceOrientation) == UIInterfaceOrientationIsPortrait(currentOrientation)) {
+		NSLog(@"same orientation");
+		return;
+	} else {
+		NSLog(@"diff orientation");
+		
+		int inverseScreenId = -1;
+		if ([self currentScreenId] > 0) {
+			inverseScreenId = [self currentScreen].inverseScreenId;
+		}
+		NSLog(@"inverseScreenId=%d", inverseScreenId);
+		if (inverseScreenId == 0) {
+			return;
+		}
+		currentOrientation = toInterfaceOrientation;
+		
+		if (UIInterfaceOrientationIsPortrait(currentOrientation)) {
+			[self showPortrait];
+		} else {
+			[self showLandscape];
+		}
+		
+		if (inverseScreenId > 0) {
+			NSLog(@"switch screen from %d - > %d", [self currentScreenId], inverseScreenId);
+			[self switchToScreen:inverseScreenId];
+		} 
+
+	}
+}
+
+- (void)dealloc {
+	[paginationController release];
+	[lastPaginationController release];
+	[errorViewController release];
+	//[group release];
+	
+	[super dealloc];
+}
+
+
+@end
