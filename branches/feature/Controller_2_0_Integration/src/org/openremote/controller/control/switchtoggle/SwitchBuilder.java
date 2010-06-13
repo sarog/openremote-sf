0f9f43427c514d7b648bebb6c781f63e0fd2fbc7
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-06-13 21:12:53 +0000

    merge R1156 (oops) from /branches/feature/Controller_serverpush branch

--- branches/feature/Controller_2_0_Integration/src/org/openremote/controller/control/switchtoggle/SwitchBuilder.java
+++ branches/feature/Controller_2_0_Integration/src/org/openremote/controller/control/switchtoggle/SwitchBuilder.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2009, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.controller.control.switchtoggle;
 
 import java.util.List;
@@ -11,8 +30,16 @@ import org.openremote.controller.control.ControlBuilder;
 import org.openremote.controller.control.Status;
 import org.openremote.controller.exception.NoSuchCommandException;
 
+/**
+ * It is mainly responsible for build Switch control with control element and commandParam.
+ * 
+ * @author Handy.Wang 2009-10-23
+ */
 public class SwitchBuilder extends ControlBuilder {
 
+   /* (non-Javadoc)
+    * @see org.openremote.controller.control.ControlBuilder#build(org.jdom.Element, java.lang.String)
+    */
    @SuppressWarnings("unchecked")
    @Override
    public Control build(Element controlElement, String commandParam) {
@@ -48,6 +75,11 @@ public class SwitchBuilder extends ControlBuilder {
       return switchToggle;
    }
    
+   /**
+    * Checks if is contain action.
+    * 
+    * @return true, if is contain action
+    */
    private boolean isContainAction(String commandParam) {
       for (String action : Switch.AVAILABLE_ACTIONS) {
          if (action.equalsIgnoreCase(commandParam)) {
