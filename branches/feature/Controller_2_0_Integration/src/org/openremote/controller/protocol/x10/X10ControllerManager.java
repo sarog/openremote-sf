dca624bcc63bb87722068fe951eb0b1c98b87988
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-06-13 12:53:30 +0000

    attempt to resolve the three conflict in R1101 (/branches/feature/Controller_Event_Refactor branch) -- two files were renamed : X10Event to X10Command and X10EventBuilder to X10CommandBuilder; the X10Command had a parallel implementation added in 'Controller_Event_Refactor' branch which has been manually merged with previous X10Event.java impl and the 'obstructing' X10Command.java implementation has been renamed to X10CommandType.java

--- branches/feature/Controller_2_0_Integration/src/org/openremote/controller/protocol/x10/X10ControllerManager.java
+++ branches/feature/Controller_2_0_Integration/src/org/openremote/controller/protocol/x10/X10ControllerManager.java
@@ -54,7 +54,7 @@ public class X10ControllerManager {
    /**
     * X10 logger. Uses a common category for all KNX related logging.
     */
-   private final static Logger log = Logger.getLogger(X10EventBuilder.X10_LOG_CATEGORY);
+   private final static Logger log = Logger.getLogger(X10CommandBuilder.X10_LOG_CATEGORY);
 
    /**
     * The default serial port to use when none is indicated in the configuration file.
@@ -120,7 +120,7 @@ public class X10ControllerManager {
       }
 
       @Override
-      public void send(String address, X10Command command) {
+      public void send(String address, X10CommandType commandType) {
 
          final char houseCodeChar = address.charAt(0);
          final int deviceCodeInt = Integer.valueOf(address.substring(1));
@@ -130,7 +130,7 @@ public class X10ControllerManager {
 
          boolean needsAddressEvent = true;
 
-         switch (command) {
+         switch (commandType) {
          case SWITCH_ON:
             commandEvent = new OnEvent(this, houseCodeChar);
             break;
