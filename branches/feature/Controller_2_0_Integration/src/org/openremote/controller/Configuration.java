fc4e11ae67799a7043cc59770d48fd0c5bd0557c
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-06-15 13:13:03 +0000

    merge R1520 (roundrobin part 1) from /branches/feature/Controller_profile_service branch

--- branches/feature/Controller_2_0_Integration/src/org/openremote/controller/Configuration.java
+++ branches/feature/Controller_2_0_Integration/src/org/openremote/controller/Configuration.java
@@ -63,7 +63,22 @@ public class Configuration {
   private long macroIRExecutionDelay = 500;
 
   private String webappIp;
-  
+
+  /**  Discovery multicast address of round robin. */
+  private String roundRobinMulticastAddress;
+
+  /** Discovery multicast port of round robin. */
+  private int roundRobinMulticastPort;
+
+  /** Group name of Controller app. */
+  private String controllerGroupName;
+
+  /** ApplicationName of Controller. */
+  private String controllerApplicationName;
+
+  /** TCP server socket port of Round Robin */
+  public int roundRobinTCPServerSocketPort;
+
   /**
    * The COM (Serial) port the ORC should use (for example, to send X10 events)
    */
@@ -383,4 +398,45 @@ public class Configuration {
    public void setWebappIp(String webappIp) {
       this.webappIp = webappIp;
    }
+
+   public String getRoundRobinMulticastAddress() {
+      return roundRobinMulticastAddress;
+   }
+
+   public void setRoundRobinMulticastAddress(String roundRobinMulticastAddress) {
+      this.roundRobinMulticastAddress = roundRobinMulticastAddress;
+   }
+
+   public int getRoundRobinMulticastPort() {
+      return roundRobinMulticastPort;
+   }
+
+   public void setRoundRobinMulticastPort(int roundRobinMulticastPort) {
+      this.roundRobinMulticastPort = roundRobinMulticastPort;
+   }
+
+   public String getControllerGroupName() {
+      return controllerGroupName;
+   }
+
+   public void setControllerGroupName(String controllerGroupName) {
+      this.controllerGroupName = controllerGroupName;
+   }
+
+   public String getControllerApplicationName() {
+      return controllerApplicationName;
+   }
+
+   public void setControllerApplicationName(String controllerApplicationName) {
+      this.controllerApplicationName = controllerApplicationName;
+   }
+
+   public int getRoundRobinTCPServerSocketPort() {
+      return roundRobinTCPServerSocketPort;
+   }
+
+   public void setRoundRobinTCPServerSocketPort(int roundRobinTCPServerSocketPort) {
+      this.roundRobinTCPServerSocketPort = roundRobinTCPServerSocketPort;
+   }
+   
 }
