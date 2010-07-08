5d0385ae1949da5b614afb1cfd8779c7b551d227
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-07-08 11:56:46 +0000

    fix a broken unit test on KNX command parsing (using the wrong XML structure) - reduce mutator methods on KNXCommand class

--- branches/project/Controller_2_0_0_Alphas/test/org/openremote/controller/control/protocol/knx/KNXCommandBuilderTest.java
+++ branches/project/Controller_2_0_0_Alphas/test/org/openremote/controller/control/protocol/knx/KNXCommandBuilderTest.java
@@ -1,23 +1,24 @@
 /* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2010, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright 2008-2010, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.control.protocol.knx;
+
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
@@ -32,44 +33,68 @@ import org.openremote.controller.protocol.knx.KNXCommandBuilder;
 import org.openremote.controller.protocol.knx.KNXCommandType;
 
 /**
- * KNXCommandBuilder Test
- * 
+ * Basic unit tests for parsing XML elements in
+ * {@link org.openremote.controller.protocol.knx.KNXCommandBuilder}.
+ *
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  * @author Javen
  *
  */
-public class KNXCommandBuilderTest {
-   private KNXCommandBuilder builder = null;
+public class KNXCommandBuilderTest
+{
 
-   @Before
-   public void setUp() {
-      builder = new KNXCommandBuilder();
-   }
+  // Test Setup -----------------------------------------------------------------------------------
 
-   @Test
-   public void testKNXOn() {
-      Command cmd = getCommand("ON", "testGroupAddress");
-      assertTrue(cmd instanceof KNXCommand);
-      KNXCommand exeCmd = (KNXCommand) cmd;
-      assertEquals(exeCmd.getGroupAddress(), "testGroupAddress");
-      assertEquals(exeCmd.getKnxCommandType(), KNXCommandType.SWITCH_ON);
-   }
+  private KNXCommandBuilder builder = null;
 
-   @Test
-   public void testKNXOff() {
-      Command cmd = getCommand("OFF", "testGroupAddress");
-      assertTrue(cmd instanceof KNXCommand);
-      KNXCommand exeCmd = (KNXCommand) cmd;
-      assertEquals(exeCmd.getGroupAddress(), "testGroupAddress");
-      assertEquals(exeCmd.getKnxCommandType(), KNXCommandType.SWITCH_OFF);
-   }
+  @Before public void setUp() {
+    builder = new KNXCommandBuilder();
+  }
+
+
+  // Tests ----------------------------------------------------------------------------------------
+
+  /**
+   * Test KNX command parsing with "ON" as the command string and 1/1/1 as group address.
+   */
+  @Test public void testKNXOn() 
+  {
+    Command cmd = getCommand("ON", "1/1/1");
+
+    assertTrue(cmd instanceof KNXCommand);
+  }
+
+  /**
+   * Test KNX command parsing with "on", "On" and "oN" as the command string and 1/1/1 as
+   * group address.
+   */
+  @Test public void testKNXOnMixedCase()
+  {
+    Command cmd1 = getCommand("on", "1/1/1");
+
+    assertTrue(cmd1 instanceof KNXCommand);
+
+    Command cmd2 = getCommand("On", "1/1/1");
+
+    assertTrue(cmd2 instanceof KNXCommand);
+
+    Command cmd3 = getCommand("oN", "1/1/1");
+
+    assertTrue(cmd3 instanceof KNXCommand);
+  }
+
+
+
+  @Test public void testKNXOff()
+  {
+    Command cmd = getCommand("OFF", "testGroupAddress");
+    assertTrue(cmd instanceof KNXCommand);
+  }
 
    @Test
    public void testKNXStatus() {
       Command cmd = getCommand("STATUS", "testGroupAddress");
       assertTrue(cmd instanceof KNXCommand);
-      KNXCommand exeCmd = (KNXCommand) cmd;
-      assertEquals(exeCmd.getGroupAddress(), "testGroupAddress");
-      assertEquals(KNXCommandType.STATUS, exeCmd.getKnxCommandType());
    }
 
    @Test
@@ -81,17 +106,26 @@ public class KNXCommandBuilderTest {
       }
    }
 
+
+  // Helpers --------------------------------------------------------------------------------------
+  
    private Command getCommand(String cmd, String groupAddress) {
       Element ele = new Element("command");
       ele.setAttribute("id", "test");
       ele.setAttribute("protocol", "knx");
-      ele.setAttribute("value", cmd);
+      //ele.setAttribute("value", cmd);
 
       Element propAddr = new Element("property");
-      propAddr.setAttribute("name", KNXCommandBuilder.GROUP_ADDRESS_XML_ATTRIBUTE);
+      propAddr.setAttribute("name", KNXCommandBuilder.KNX_XMLPROPERTY_GROUPADDRESS);
       propAddr.setAttribute("value", groupAddress);
 
       ele.addContent(propAddr);
+
+      Element propAddr2 = new Element("property");
+      propAddr2.setAttribute("name", "command");
+      propAddr2.setAttribute("value", cmd);
+
+      ele.addContent(propAddr2);
 
       return builder.build(ele);
    }
