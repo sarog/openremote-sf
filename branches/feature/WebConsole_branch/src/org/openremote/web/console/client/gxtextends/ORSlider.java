f62835f63e58b82aa2fc35f98c374e34f7bca66a
tomskywang <tomskywang@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-07-12 09:40:34 +0000

    #WEBCONSOLE-19,Support slider(can not change images).

---
+++ branches/feature/WebConsole_branch/src/org/openremote/web/console/client/gxtextends/ORSlider.java
@@ -0,0 +1,68 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2010, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.web.console.client.gxtextends;
+
+import org.openremote.web.console.client.widget.ScreenSlider;
+
+import com.extjs.gxt.ui.client.event.ComponentEvent;
+import com.extjs.gxt.ui.client.event.DragEvent;
+import com.extjs.gxt.ui.client.widget.Slider;
+
+/**
+ * The Class ORSlider.
+ */
+public class ORSlider extends Slider {
+
+   private org.openremote.web.console.domain.Slider slider;
+   
+   public ORSlider(org.openremote.web.console.domain.Slider slider) {
+      super();
+      this.slider = slider;
+      setMinValue(slider.getMinValue());
+      setMaxValue(slider.getMaxValue());
+      setIncrement(1);
+      if (slider.isVertical()) {
+         setVertical(true);
+         setHeight(slider.getFrameHeight());
+      } else {
+         setWidth(slider.getFrameWidth());
+      }
+      if (slider.isPassive()) {
+         setDraggable(false);
+         setClickToChange(false);
+      }
+   }
+   
+   @Override
+   protected void onDragEnd(DragEvent de) {
+      super.onDragEnd(de);
+      if (!slider.isPassive()) {
+         ((ScreenSlider) getParent()).sendCommand("" + getValue());
+      }
+   }
+
+   @Override
+   protected void onClick(ComponentEvent ce) {
+      super.onClick(ce);
+      if (!slider.isPassive()) {
+         ((ScreenSlider) getParent()).sendCommand("" + getValue());
+      }
+   }
+}
