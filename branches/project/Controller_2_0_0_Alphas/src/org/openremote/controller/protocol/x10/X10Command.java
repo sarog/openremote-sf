c927b7124ec2a145d427b777edca8acf71c8ff85
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-07-12 19:32:48 +0000

    make X10Command immutable

--- branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/protocol/x10/X10Command.java
+++ branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/protocol/x10/X10Command.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2010, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2010, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.protocol.x10;
 
 import java.util.Map;
@@ -29,90 +30,62 @@ import org.openremote.controller.component.EnumSensorType;
 
 
 /**
- * The X10 Event.
+ * TODO
  * 
- * @author Dan 2009-4-20
  * @author Jerome Velociter
+ * @author <a href="mailto:juha@openremote.org>Juha Lindfors</a>
+ * @author Dan 2009-4-20
  */
-public class X10Command implements ExecutableCommand, StatusCommand {
+public class X10Command implements ExecutableCommand, StatusCommand
+{
+
+  // Private Instance Fields ----------------------------------------------------------------------
+
+  private String address;
    
-   /** The address. */
-   private String address;
-   
-  /** The command. */
   private X10CommandType commandType;
 
-  /** Controller manager */
   private X10ControllerManager controllerManager;
 
   private final static Logger log = Logger.getLogger(X10CommandBuilder.X10_LOG_CATEGORY);
 
-  public X10Command(X10ControllerManager manager, String address, X10CommandType commandType) {
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  public X10Command(X10ControllerManager manager, String address, X10CommandType commandType)
+  {
      this.controllerManager = manager;
      this.address = address;
      this.commandType = commandType;
   }
 
 
-   /**
-    * Gets the address.
-    * 
-    * @return the address
-    */
-   public String getAddress() {
-      return address;
-   }
+  // Implements ExecutableCommand -----------------------------------------------------------------
 
-   /**
-    * Sets the address.
-    * 
-    * @param address the new address
-    */
-   public void setAddress(String address) {
-      this.address = address;
-   }
+  public void send()
+  {
+    X10Controller device;
 
-  /**
-   * Gets the command.
-   *
-   * @return the command
-   */
-  public X10CommandType getCommand() {
-     return commandType;
-  }
+    try
+    {
+      device = this.controllerManager.getDevice();
 
-  /**
-   * Sets the command.
-   *
-   * @param command
-   *           the new command
-   */
-  public void setCommand(X10CommandType command) {
-     this.commandType = command;
-  }
+      device.send(address, commandType);
 
-
-   @Override
-   public void send()
-   {
-     X10Controller device;
-
-     try {
-        device = this.controllerManager.getDevice();
-
-        device.send(this.getAddress(), commandType);
-
-     } catch (ConnectionException e) {
+    }
+    catch (ConnectionException e)
+    {
         log.error(e);
-     }
+    }
+  }
 
-   }
+  // Implements StatusCommand ---------------------------------------------------------------------
 
-   @Override
-   public String read(EnumSensorType sensoryType, Map<String, String> statusMap) {
-      // TODO Auto-generated method stub
-      return null;
-   }
+  public String read(EnumSensorType sensoryType, Map<String, String> statusMap)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
    
    
 }
