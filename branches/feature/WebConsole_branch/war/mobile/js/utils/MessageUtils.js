441cc7b35cd1af56b828bddbb97d935c2e1bfdea
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-07-15 02:12:44 +0000

    Modified panel parsing framework.

--- branches/feature/WebConsole_branch/war/mobile/js/utils/MessageUtils.js
+++ branches/feature/WebConsole_branch/war/mobile/js/utils/MessageUtils.js
@@ -4,14 +4,6 @@
  * auther: handy.wang 2010-07-08
  */
 MessageUtils = (function(){
-  {$("#messageDialog").dialog({
-		modal: true,
-		buttons: {
-			Ok: function() {
-				$(this).dialog('close');
-			}
-		}
-	});}
   
   return {
     showLoading:function(message) {
@@ -19,20 +11,42 @@ MessageUtils = (function(){
       $("#process-loading").show();
       $("#loading-info").html(message);
     },
+    updateLoadingMessage:function(message) {
+      $("#loading-info").html(message);
+    },
     hideLoading:function() {
       $("#grayMask").hide();
       $("#process-loading").hide();
       $("#loading-info").html("");
     },
-    showMessageDialog:function(message) {
+    showMessageDialog:function(titleParam, messageParam) {
       $("#messageDialog").dialog("destroy");
-      $("#messageDialog").html(message);
+      $("#messageDialog").html(messageParam);
 		  $("#messageDialog").dialog({
   			modal: true,
-  			title: "Message",
+  			title: titleParam,
   			position: "top",
   			buttons: {
-  				Ok: function() {
+  			  OK: function() {
+  			    $(this).dialog("close");
+  				}
+  			}
+  		});
+  		$("#messageDialog").dialog("open");
+    },
+    showMessageDialogWithSettings:function(titleParam, messageParam) {
+      $("#messageDialog").dialog("destroy");
+      $("#messageDialog").html(messageParam);
+		  $("#messageDialog").dialog({
+  			modal: true,
+  			title: titleParam,
+  			position: "top",
+  			buttons: {
+  			  "Settings": function() {
+  			    AppSettings.getInstance().show();
+  			    $(this).dialog("close");
+  				},
+  				"Leave it": function() {
   					$(this).dialog('close');
   				}
   			}
