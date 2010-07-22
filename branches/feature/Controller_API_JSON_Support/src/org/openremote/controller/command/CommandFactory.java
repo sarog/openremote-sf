053662df2da1800498df2155e6ee12dc486eefa1
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-07-22 08:48:22 +0000

    sync this branch against /branches/project/Controller_2_0_0_Alphas up until R2640

--- branches/feature/Controller_API_JSON_Support/src/org/openremote/controller/command/CommandFactory.java
+++ branches/feature/Controller_API_JSON_Support/src/org/openremote/controller/command/CommandFactory.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2010, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/* 
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2010, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.command;
 
 import java.util.Properties;
@@ -32,7 +33,8 @@ import org.springframework.context.support.ApplicationObjectSupport;
  * 
  * @author Handy.Wang 2009-10-13
  */
-public class CommandFactory extends ApplicationObjectSupport{
+public class CommandFactory extends ApplicationObjectSupport
+{
    
    /** The command builders. */
    private Properties commandBuilders;
@@ -44,19 +46,29 @@ public class CommandFactory extends ApplicationObjectSupport{
     * 
     * @return the command
     */
-   public Command getCommand(Element element) {
-      if (element == null) {
+   public Command getCommand(Element element)
+   {
+      if (element == null)
+      {
          throw new CommandBuildException("Command DOM element is null.");
       }
-      String protocolType = element.getAttributeValue(Command.PROTOCOL_ATTRIBUTE_NAME);
-      if (protocolType == null || "".equals(protocolType)) {
+
+      String protocolType = element.getAttributeValue(CommandBuilder.PROTOCOL_ATTRIBUTE_NAME);
+
+      if (protocolType == null || "".equals(protocolType))
+      {
          throw new CommandBuildException("Protocol type is null.");
       }
+
       String builder = commandBuilders.getProperty(protocolType);
-      if (builder == null){
+
+      if (builder == null)
+      {
          throw new NoSuchCommandBuilderException("Cannot find '" + protocolType + "Builder' by '" + protocolType + "' protocol.");
       }
+
       CommandBuilder commandBuilder = (CommandBuilder) getApplicationContext().getBean(builder);
+
       return commandBuilder.build(element);
    }
 
@@ -65,7 +77,8 @@ public class CommandFactory extends ApplicationObjectSupport{
     * 
     * @param commandBuilders the new command builders
     */
-   public void setCommandBuilders(Properties commandBuilders) {
+   public void setCommandBuilders(Properties commandBuilders)
+   {
       this.commandBuilders = commandBuilders;
    }
 }
