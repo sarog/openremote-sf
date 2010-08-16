d467e4237bee12579ff9aa2ad33e1ded47b9996f
dancong <dancong@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-08-16 19:23:33 +0000

    merged from iPhone_profile_service branch:
    new feature: #IPHONE-69 Don't prevent navigation based on orientation. now navigation between different orientation will trigger rotation, user should turn his device to the new orientation.

--- branches/feature/iPad_iPhone_universal_app_profile_service/Classes/DefaultViewController.m
+++ branches/feature/iPad_iPhone_universal_app_profile_service/Classes/DefaultViewController.m
@@ -21,6 +21,8 @@
 
 #import "DefaultViewController.h"
 
+#define degreesToRadian(x) (M_PI * (x) / 180.0)
+
 @interface DefaultViewController (Private)
 
 
@@ -70,18 +72,69 @@
 	[self.view setBackgroundColor:[UIColor blackColor]];
 	
 
+	//Must add xib file view into window to detect current device orientation.
+	
+	//Init the error view with xib
 	errorViewController = [[ErrorViewController alloc] initWithErrorTitle:@"No Group Found" 
 																													 message:@"Please check your setting or define a group with screens first."];
 	[self.view addSubview:errorViewController.view];
 
 	
-	//Init the loading view
+	//Init the loading view with xib
 	initViewController = [[InitViewController alloc] init];
 	[self.view addSubview:initViewController.view];
 }
 
 - (void)refreshPolling {
 	[currentGroupController startPolling];
+}
+
+/**
+ * About recovering to last group and screen.
+ * I)Currently, there are two use cases which relate with recovery mechanism.
+ * 1) While setting.
+ *    DESC: User presses setting item in tabbar or in certain screen when user had switch
+ *    to certain screen of certain group. After Uesr done setting, the app must switch to 
+ *    the screen which before user pressing setting.
+ *    
+ * 2) While switching to groupmember controller.
+ *    DESC: If current controller down, app will switch to groupmember controller of crashed controller.
+ *    However, the process is tranparent. That means user won't feel controller-switch. So, the app must
+ *    keep the same screen before and after switching controller.
+ *
+ * II)Technically speaking, app will save the groupId and screenId when user switch to certain group and screen 
+ * or navigage to certain screen. The follows are in detail:
+ *    1)Navigate action: Append code in self method *navigateToWithHistory*
+ *    2)Scroll screen action: Apend code in method *setViewControllers* and *updateViewForCurrentPageAndBothSides*
+ *    of class PaginationController.
+ *    3)Finished the initGroups: Append code in tail of self method *initGroups*:[self saveLastGroupIdAndScreenId];
+ *
+ * III)The saved groupId and screenId will be used in following situation:
+ *    While app initializing groups(see method initGroups) in current classs, app gets groupId and screenId stored, and then switch
+ *    to the destination described by groupId and screenId.
+ */
+- (GroupController *)recoverLastOrCreateGroup {
+	NSArray *groups = [[Definition sharedDefinition] groups];
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	GroupController *gc = nil;
+	if ([userDefaults objectForKey:@"lastGroupId"]) {
+		int lastGroupId = [[userDefaults objectForKey:@"lastGroupId"] intValue];
+		Group *lastGroup = nil;
+		for (Group *tempGroup in groups) {
+			if (lastGroupId == tempGroup.groupId) {
+				lastGroup = tempGroup;
+				break;
+			}
+		}
+		if (lastGroup) {
+			gc = [[GroupController alloc] initWithGroup:lastGroup];
+		} else {
+			gc = [[GroupController alloc] initWithGroup:((Group *)[groups objectAtIndex:0])];
+		}
+	} else {
+		gc = [[GroupController alloc] initWithGroup:((Group *)[groups objectAtIndex:0])];
+	}
+	return gc;
 }
 
 - (void)initGroups {
@@ -94,51 +147,7 @@
 	
 	if (groups.count > 0) {
 		
-		/**
-		 * About recovering to last group and screen.
-		 * I)Currently, there are two use cases which relate with recovery mechanism.
-		 * 1) While setting.
-		 *    DESC: User presses setting item in tabbar or in certain screen when user had switch
-		 *    to certain screen of certain group. After Uesr done setting, the app must switch to 
-		 *    the screen which before user pressing setting.
-		 *    
-		 * 2) While switching to groupmember controller.
-		 *    DESC: If current controller down, app will switch to groupmember controller of crashed controller.
-		 *    However, the process is tranparent. That means user won't feel controller-switch. So, the app must
-		 *    keep the same screen before and after switching controller.
-		 *
-		 * II)Technically speaking, app will save the groupId and screenId when user switch to certain group and screen 
-		 * or navigage to certain screen. The follows are in detail:
-		 *    1)Navigate action: Append code in self method *navigateToWithHistory*
-		 *    2)Scroll screen action: Apend code in method *setViewControllers* and *updateViewForCurrentPageAndBothSides*
-		 *    of class PaginationController.
-		 *    3)Finished the initGroups: Append code in tail of self method *initGroups*:[self saveLastGroupIdAndScreenId];
-		 *
-		 * III)The saved groupId and screenId will be used in following situation:
-		 *    While app initializing groups(see method initGroups) in current classs, app gets groupId and screenId stored, and then switch
-		 *    to the destination described by groupId and screenId.
-		 */
-		//Begin: Recover the last group
-		NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-		GroupController *gc = nil;
-		if ([userDefaults objectForKey:@"lastGroupId"]) {
-			int lastGroupId = [[userDefaults objectForKey:@"lastGroupId"] intValue];
-			Group *lastGroup = nil;
-			for (Group *tempGroup in groups) {
-				if (lastGroupId == tempGroup.groupId) {
-					lastGroup = tempGroup;
-					break;
-				}
-			}
-			if (lastGroup) {
-				gc = [[GroupController alloc] initWithGroup:lastGroup];
-			} else {
-				gc = [[GroupController alloc] initWithGroup:((Group *)[groups objectAtIndex:0])];
-			}
-		} else {
-			gc = [[GroupController alloc] initWithGroup:((Group *)[groups objectAtIndex:0])];
-		}
-		//End: Recover the last group
+		GroupController *gc = [self recoverLastOrCreateGroup];
 		
 		[groupControllers addObject:gc];
 		[groupViewMap setObject:gc.view forKey:[NSString stringWithFormat:@"%d", gc.group.groupId]];	
@@ -150,6 +159,9 @@
 			localTabBarController = [[TabBarController alloc] initWithGroupController:currentGroupController tabBar:localTabBar];
 			[self.view addSubview:localTabBarController.view];
 			lastSubView = localTabBarController.view;
+			
+			[tabBarControllers addObject:localTabBarController];
+			[tabBarControllerViewMap setObject:localTabBarController.view forKey:[NSString stringWithFormat:@"%d", gc.group.groupId]];
 		}		
 		// global tabBar
 		else if ([[Definition sharedDefinition] tabBar]) {
@@ -191,9 +203,6 @@
 	}
 	
 	NSLog(@"navi history count = %d", navigationHistory.count);
-//	for (Navigate *n in navigationHistory) {
-//		NSLog(@"navi history from group %d screen %d", n.fromGroup, n.fromScreen);
-//	}
 }
 
 - (void)saveLastGroupIdAndScreenId {
@@ -204,7 +213,7 @@
 }
 
 // Returned BOOL value is whether to save history
-// if YES, save history
+// if YES, should save history
 // if NO, don't save history
 - (BOOL)navigateTo:(Navigate *)navi {
 	
@@ -220,7 +229,8 @@
 		return [self navigateToNextScreen];
 	}
 	
-	//the following is non-history
+	//the following should not generate history record
+	
 	else if (navi.isBack) {										//toBack
 		[self navigateBackwardInHistory:nil]; 
 		return NO;
@@ -244,21 +254,112 @@
 	return NO;
 }
 
-- (BOOL)navigateToGroup:(int)groupId toScreen:(int)screenId {
-	GroupController *targetGroupController = nil;
-	BOOL isOrientationLandscape = [currentGroupController isOrientationLandscape];
+//if the group view is not cached, tabbar can't show group view when rotation happens.
+//rerendering the tabbar will show group view.
+- (void)rerenderTabbarWithNewOrientation {
+	if (currentGroupController.group.tabBar) {
+		[localTabBarController updateTabItems];
+	} else if ([[Definition sharedDefinition] tabBar]) {
+		[globalTabBarController updateTabItems];
+	}
+}
+
+//transform view +/- 90 degrees
+- (void)transformToOppositeOrientation {
 	
-	BOOL notItSelf = groupId != [currentGroupController groupId];
+	[UIView beginAnimations:@"View transform" context:nil];
+	[UIView setAnimationDuration:0.5f];
+	[UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+	self.view.transform = CGAffineTransformIdentity;
+	CGFloat h = self.view.frame.size.height;
+	CGFloat w = self.view.frame.size.width;
+	if (h > w) {
+		NSLog(@"view did transform to landscape");
+		[currentGroupController transformToOrientation:UIInterfaceOrientationLandscapeRight];
+		
+		//move transform centre, make portrait view and landscape view have the same centre.
+		//by default portrait view and landscape view have the same origin.
+		self.view.frame = CGRectMake((w-h)/2.0, (h-w)/2.0, h, w);
+		
+		self.view.transform = CGAffineTransformMakeRotation(degreesToRadian(90));
+	} else {
+		NSLog(@"view did transform to portrait");
+		[currentGroupController transformToOrientation:UIInterfaceOrientationPortrait];
+		//by default it's portart, needn't to specify -90 degrees.
+		self.view.frame = CGRectMake(0, 0, h, w);
+	}
 	
-	//if screenId is specified, and is not current group, jump to that group
-	if (groupId > 0 && notItSelf) {
-		//find in cache first
-		for (GroupController *gc in groupControllers) {
-			if ([gc groupId] == groupId) {
-				targetGroupController = gc;
+	[UIView commitAnimations];
+	
+}
+
+- (void)updateGlobalOrLocalTabbarViewToGroupController:(GroupController *)targetGroupController withGroupId:(int)groupId {
+	
+	if ([targetGroupController getCurrentOrientation] != [currentGroupController getCurrentOrientation]) {
+		[targetGroupController setNewOrientation:[currentGroupController getCurrentOrientation]];
+	}
+	
+
+//	[lastSubView removeFromSuperview];
+	
+	for (UIView *vi in self.view.subviews) {
+		[vi retain];//cache it, or it will be release after removeFromSuperview.
+		[vi removeFromSuperview];
+	}
+	
+	UIView *v = [groupViewMap objectForKey:[NSString stringWithFormat:@"%d", groupId]];
+
+	//if local tabbar exists
+	if (targetGroupController.group.tabBar) {
+		BOOL findCachedLocalTabBarController = NO;
+		for (TabBarController *lTabBarController in tabBarControllers) {
+			if (lTabBarController.groupController.group.groupId == groupId) {
+				localTabBarController = lTabBarController;
+				findCachedLocalTabBarController = YES;
 				break;
 			}
 		}
+		if (!findCachedLocalTabBarController) {
+			localTabBarController = [[TabBarController alloc] initWithGroupController:targetGroupController tabBar:targetGroupController.group.tabBar];
+			[tabBarControllers addObject:localTabBarController];
+			[tabBarControllerViewMap setObject:localTabBarController.view forKey:[NSString stringWithFormat:@"%d", groupId]];
+		} else {
+			[localTabBarController updateGroupController:targetGroupController];
+		}
+		v = [tabBarControllerViewMap objectForKey:[NSString stringWithFormat:@"%d", groupId]];
+	}
+	//if global tabbar exists
+	else if ([[Definition sharedDefinition] tabBar]) {
+		if (globalTabBarController) {
+			[globalTabBarController updateGroupController:targetGroupController];
+		} else {
+			globalTabBarController = [[TabBarController alloc] initWithGroupController:targetGroupController tabBar:[[Definition sharedDefinition] tabBar]];
+		}
+		v = globalTabBarController.view;
+	}
+	//lastSubView = v;
+
+	[self.view addSubview:v];
+
+	currentGroupController = targetGroupController;
+}
+
+
+- (BOOL)navigateToGroup:(int)groupId toScreen:(int)screenId {
+	GroupController *targetGroupController = nil;
+	
+	BOOL isAnotherGroup = groupId != [currentGroupController groupId];
+	BOOL isLastOrientationLandscape = [currentGroupController isOrientationLandscape];
+	
+	//if screenId is specified, and is not in current group, jump to that group
+	if (groupId > 0 && isAnotherGroup) {
+		//should find in cache first, but this cached GroupController may cause view bug, so don't use cache for now.
+//		for (GroupController *gc in groupControllers) {
+//			if ([gc groupId] == groupId) {
+//				targetGroupController = gc;
+//				break;
+//			}
+//		}
 		//if not found in cache, create one
 		if (targetGroupController == nil) {
 			Group *group = [[Definition sharedDefinition] findGroupById:groupId];
@@ -270,66 +371,17 @@
 				return NO;
 			}
 		}
-		if ([targetGroupController hasNoViewInThatOrientation:[currentGroupController isOrientationLandscape]]) {
-			[ViewHelper showAlertViewWithTitle:@"" Message:@"No screen is in that group of this orientation"];
-			return NO;
-		} 
-		if (screenId > 0) {
-			if (![targetGroupController.group canFindScreenById:screenId inOrientation:isOrientationLandscape]) {
-				[ViewHelper showAlertViewWithTitle:@"" Message:@"That screen doesn't support this orientation"];
-				return NO;
-			}
-		}
-
-		[targetGroupController setNewOrientation:[currentGroupController getCurrentOrientation]];
-		[lastSubView removeFromSuperview];
 		
-		UIView *view = [groupViewMap objectForKey:[NSString stringWithFormat:@"%d", groupId]];
+		[self updateGlobalOrLocalTabbarViewToGroupController:targetGroupController withGroupId:groupId];
 		
-		// begin tabBar and view(local or global)
-		//if local tabbar exists
-		if (targetGroupController.group.tabBar) {
-			BOOL findCachedTargetTabBarController = NO;
-			for (TabBarController *tempTargetTabBarController in tabBarControllers) {
-				if (tempTargetTabBarController.groupController.group.groupId == targetGroupController.group.groupId) {
-					localTabBarController = tempTargetTabBarController;
-					findCachedTargetTabBarController = YES;
-					break;
-				}
-			}
-			if (!findCachedTargetTabBarController) {
-				localTabBarController = [[TabBarController alloc] initWithGroupController:targetGroupController tabBar:targetGroupController.group.tabBar];
-				[tabBarControllers addObject:localTabBarController];
-				[tabBarControllerViewMap setObject:localTabBarController.view forKey:[NSString stringWithFormat:@"%d", localTabBarController.groupController.group.groupId]];
-			} else {
-				[localTabBarController updateGroupController:targetGroupController];
-			}
-			view = [tabBarControllerViewMap objectForKey:[NSString stringWithFormat:@"%d", localTabBarController.groupController.group.groupId]];
-		} 
-		//if global tabbar exists
-		else if ([[Definition sharedDefinition] tabBar]) {
-			if (globalTabBarController) {
-				[globalTabBarController updateGroupController:targetGroupController];
-			} else {
-				globalTabBarController = [[TabBarController alloc] initWithGroupController:targetGroupController tabBar:[[Definition sharedDefinition] tabBar]];
-			}
-			view = globalTabBarController.view;
-		}
-		// end tabBar and view(local or global)
-		
-		[currentGroupController stopPolling];
-		[targetGroupController startPolling];
-		lastSubView = view;
-		[self.view addSubview:view];
-		
-		currentGroupController = targetGroupController;
 	}
 	
 	//if screenId is specified, jump to that screen
 	if (screenId > 0) {
-		if (![currentGroupController.group canFindScreenById:screenId inOrientation:isOrientationLandscape]) {
-			[ViewHelper showAlertViewWithTitle:@"" Message:@"That screen doesn't support this orientation"];
-			return NO;
+		//if navigate to opposite orientation, need to transform view +/- 90 degrees.
+		if (isLastOrientationLandscape != [currentGroupController isOrientationLandscapeWithScreenId:screenId]) {
+			[self transformToOppositeOrientation];
+			[self rerenderTabbarWithNewOrientation];
 		}
 		return [currentGroupController switchToScreen:screenId];
 	}
@@ -337,6 +389,7 @@
 	return YES;
 }
 
+//logout only when password is saved.
 - (void)logout {
 	if ([Definition sharedDefinition].password) {
 		LogoutHelper *logout = [[LogoutHelper alloc] init];
