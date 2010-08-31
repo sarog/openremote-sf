e40f38268b0e08b9335406079c336dced0b64a5b
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-08-31 11:55:53 +0000

    testing some delays

---
+++ branches/feature/Controller_2_0_StressTests_WinXP/test/org/openremote/controller/protocol/mockcam/MockCamFrameEvent.java
@@ -0,0 +1,73 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2010, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.mockcam;
+
+import java.util.Set;
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
+ */
+public class MockCamFrameEvent implements Runnable
+{
+
+  public void run()
+  {
+    int iterations = 100 /* 100 times per second */ * 50; /* for 50 seconds */
+    long[] measuredDelays = new long[iterations];
+
+    try
+    {
+      for (int i = 0; i < iterations; ++i)
+      {
+
+        long time = System.currentTimeMillis();
+
+        Thread.sleep(10);
+
+        long end = System.currentTimeMillis();
+
+        measuredDelays[i] = end - time;
+      }
+    }
+    catch (InterruptedException e)
+    {
+      // if we get interrupted, we'll just wind down to finish
+    }
+
+    // calculate the measured delay averages
+
+    long sum = 0;
+    int linenum = 0;
+
+    for (long value : measuredDelays)
+    {
+      sum += value;
+
+      System.out.println("" + linenum++ + " Value: " + value + "ms");
+
+    }
+
+    System.out.println("========== Recorded avg measured delay of " + (double)sum/iterations + "ms");
+  }
+}
+
