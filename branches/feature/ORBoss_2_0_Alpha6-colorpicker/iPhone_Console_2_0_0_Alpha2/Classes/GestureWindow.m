77ec63182c2f0dfc9fa915a2f3318a32e0c548db
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-08-31 10:10:35 +0000

    branch for marcus colorpicker work

---
+++ branches/feature/ORBoss_2_0_Alpha6-colorpicker/iPhone_Console_2_0_0_Alpha2/Classes/GestureWindow.m
@@ -0,0 +1,180 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2009, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+#import "GestureWindow.h"
+#import "Gesture.h"
+#import "NotificationConstant.h"
+
+@interface GestureWindow (Private)
+
+- (void)showLoading;
+- (void)hideLoading;
+
+@end
+
+
+
+/*
+ * UIWindow to intercept touch events as gesture, this doesn't break the event delivery.
+ *
+ * When the iPhone detects a touch it determines the first responder for that event 
+ * by recursively calling hitTest:withEvent: to descend down the tree of UIResponder 
+ * objects in the application’s window. 
+ * The first reponder is then sent the event and can either respond to it 
+ * or pass it up the responder chain from view to view controller to parent view.
+ * To detect gestures anywhere in the app so I chose to override sendEvent: in UIWindow. 
+ * This allowed me to intercept touch events before they were sent to the first responder.
+ * See also : http://developer.apple.com/iphone/library/documentation/iPhone/Conceptual/iPhoneOSProgrammingGuide/EventHandling/EventHandling.html#//apple_ref/doc/uid/TP40007072-CH9-SW9
+ */
+
+@implementation GestureWindow
+
+- (id)initWithDelegate:(id)delegate{
+	if (self = [super initWithFrame:[UIScreen mainScreen].bounds]) {
+		theDelegate = delegate;
+		
+		loading  = [[LoadingHUDView alloc] initWithTitle:@"Loading"];
+		loading.center = self.center;
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(showLoading) name:NotificationShowLoading object:nil];
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(hideLoading) name:NotificationHideLoading object:nil];
+		UIAccelerometer *accelerometer = [UIAccelerometer sharedAccelerometer];
+		accelerometer.delegate = self; 
+		accelerometer.updateInterval = 1.0f/60.0f; 
+		
+//		/*accelerometer simulator, see http://www.iphonearch.com/topic/6/iphone-accelerometer-simulator/*/
+//		zUIAccelerometer *am;
+//    am = [zUIAccelerometer alloc];
+//    [am setDelegate:self];
+//    [am startFakeAccelerometer];    
+//		/*accelerometer simulator*/
+		
+
+	}
+	
+	return self;
+}
+
+//To detect gestures, intercept touch events
+- (void)sendEvent:(UIEvent *)event {
+	NSSet *touches = [event allTouches];
+	
+	//single touch (single finger)
+	if ([touches count] == 1) {
+		UITouch *touch = [touches anyObject];
+		CGPoint location = [touch locationInView:self];
+		if (touch.phase == UITouchPhaseBegan) {
+			previousTouchLocation = [touch locationInView:self];
+		} else if (touch.phase == UITouchPhaseEnded) {
+			CGFloat deltaX = fabsf(location.x - previousTouchLocation.x);
+			CGFloat deltaY = fabsf(location.y - previousTouchLocation.y);
+			
+			//Horizontal
+			if (deltaX >= MINIMUM_GESTURE_LENGTH && deltaY <= MAXIMUM_VARIANCE) {
+				//evaluate gesture
+				if (previousTouchLocation.x < location.x) {
+					//left to right -->
+					Gesture *g = [[Gesture alloc] initWithGestureSwipeType:GestureSwipeTypeLeftToRight orientation:orientation];
+					[theDelegate performSelector:@selector(performGesture:) withObject:g];
+				} else if (previousTouchLocation.x > location.x) {
+					//right to left <--
+					Gesture *g = [[Gesture alloc] initWithGestureSwipeType:GestureSwipeTypeRightToLeft orientation:orientation];
+					[theDelegate performSelector:@selector(performGesture:) withObject:g];
+				}
+			} 
+			
+			//Vertical
+			else if (deltaY >= MINIMUM_GESTURE_LENGTH && deltaX <= MAXIMUM_VARIANCE) {
+				if (location.y > previousTouchLocation.y) {
+					//           |
+					//up to down V
+					Gesture *g = [[Gesture alloc] initWithGestureSwipeType:GestureSwipeTypeTopToBottom orientation:orientation];
+					[theDelegate performSelector:@selector(performGesture:) withObject:g];
+				} else if (previousTouchLocation.y > location.y) {
+					//donw to up ^
+					//           |
+					Gesture *g = [[Gesture alloc] initWithGestureSwipeType:GestureSwipeTypeBottomToTop orientation:orientation];
+					[theDelegate performSelector:@selector(performGesture:) withObject:g];
+				}
+			}
+		} else if (touch.phase == UITouchPhaseMoved) {
+			// nothing
+		} else if (touch.phase == UITouchPhaseCancelled) {
+			// nothing
+		}
+	}
+	
+	// multi touches (multi fingers)
+	else {
+		// nothing
+	}
+	[super sendEvent:event];
+}
+
+/*accelerometer simulator*/
+//- (void)accelerometer:(zUIAccelerometer *)accelerometer didAccelerate:(zUIAcceleration *)acceleration {
+//
+//	if (acceleration.y <= -0.5f) {
+//		orientation = UIInterfaceOrientationPortrait;
+//	} else if (acceleration.x >= 0.5f) {
+//		orientation = UIInterfaceOrientationLandscapeRight;
+//	} else if (acceleration.y >= 0.5f) {
+//		orientation = UIInterfaceOrientationPortraitUpsideDown;
+//	} else if (acceleration.x <= -0.5f) {
+//		orientation = UIInterfaceOrientationLandscapeLeft;
+//	} else {
+//		orientation = UIInterfaceOrientationPortrait;
+//	}
+//
+//
+//}
+/*accelerometer simulator*/
+
+
+- (void)accelerometer:(UIAccelerometer *)accelerometer didAccelerate:(UIAcceleration *)acceleration { 
+	if (acceleration.y <= -0.5f) {
+		orientation = UIInterfaceOrientationPortrait;
+	} else if (acceleration.x >= 0.5f) {
+		orientation = UIInterfaceOrientationLandscapeRight;
+	} else if (acceleration.y >= 0.5f) {
+		orientation = UIInterfaceOrientationPortraitUpsideDown;
+	} else if (acceleration.x <= -0.5f) {
+		orientation = UIInterfaceOrientationLandscapeLeft;
+	} else {
+		orientation = UIInterfaceOrientationPortrait;
+	}
+}
+
+
+- (void)showLoading {
+	if (loading.superview != self) {
+		[self addSubview:loading];
+	}
+	[loading startAnimating];
+}
+
+- (void)hideLoading {
+	[loading stopAnimating];
+	[loading removeFromSuperview];
+}
+
+@end
+
+
---
+++ branches/feature/ORBoss_2_0_Alpha6-colorpicker/iPhone_Console_2_0_0_Alpha2/Classes/URLConnectionHelper.m
@@ -0,0 +1,425 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2009, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+
+#import "URLConnectionHelper.h"
+#import "AppSettingsDefinition.h"
+#import "CheckNetwork.h"
+#import "CheckNetworkException.h"
+#import "DataBaseService.h"
+#import "UpdateController.h"
+#import "NotificationConstant.h"
+
+#pragma mark inner class NSURLConnectionDataCollector
+@interface _NSURLConnectionDataCollector : NSObject <NSURLProtocolClient>
+{
+  NSURLConnection	*_connection;	// Not retained
+  NSMutableData		*_data;
+  NSError		**_error;
+  NSURLResponse		**_response;
+  BOOL			_done;
+}
+
+- (id) initWithResponsePointer: (NSURLResponse **)response andErrorPointer: (NSError **)error;
+- (NSData*) _data;
+- (BOOL) _done;
+- (void) _setConnection: (NSURLConnection *)c;
+
+@end
+
+@implementation _NSURLConnectionDataCollector
+
+- (id) initWithResponsePointer: (NSURLResponse **)response andErrorPointer: (NSError **)error
+{
+  if ((self = [super init]) != nil)
+	{
+		_response = response;
+		_error = error;
+	}
+  return self;
+}
+
+- (void) dealloc
+{
+  [super dealloc];
+	[_data release];
+}
+
+- (BOOL) _done
+{
+  return _done;
+}
+
+- (NSData*) _data
+{
+  return _data;
+}
+
+- (void) _setConnection: (NSURLConnection*)c
+{
+  _connection = c;
+}
+
+// notification handler
+
+- (void) URLProtocol: (NSURLProtocol*)proto cachedResponseIsValid: (NSCachedURLResponse*)resp
+{
+  return;
+}
+
+- (void) URLProtocol: (NSURLProtocol*)proto didReceiveAuthenticationChallenge: (NSURLAuthenticationChallenge *)challenge
+{
+  return;
+}
+
+- (void) URLProtocol: (NSURLProtocol*)proto didCancelAuthenticationChallenge: (NSURLAuthenticationChallenge *)challenge
+{
+  return;
+}
+
+- (void) URLProtocol: (NSURLProtocol*)proto wasRedirectedToRequest: (NSURLRequest*)request
+		redirectResponse: (NSURLResponse*)redirectResponse
+{
+  return;
+}
+
+- (void) URLProtocol: (NSURLProtocol*)proto didFailWithError: (NSError*)error
+{
+  *_error = error;
+  _done = YES;
+}
+
+- (void) connection: (NSURLConnection *)connection didFailWithError: (NSError *)error
+{
+  *_error = [error retain];
+  _done = YES;
+}
+
+- (void) URLProtocol: (NSURLProtocol*)proto didReceiveResponse: (NSURLResponse*)response
+  cacheStoragePolicy: (NSURLCacheStoragePolicy)policy
+{
+  *_response = response;
+}
+
+- (void) URLProtocolDidFinishLoading: (NSURLProtocol*)proto
+{
+  _done = YES;
+}
+
+- (void) connectionDidFinishLoading: (NSURLConnection *)connection
+{
+  _done = YES;
+}
+
+
+- (void) URLProtocol: (NSURLProtocol*)proto didLoadData: (NSData*)data
+{
+  if (_data == nil)
+	{
+		_data = [data mutableCopy];
+	}
+  else
+	{
+		[_data appendData: data];
+	}
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	 *_response = [response retain];
+}
+
+- (void) connection: (NSURLConnection *)connection didReceiveData: (NSData *)data 
+{
+  if (_data == nil)
+	{
+		_data = [data mutableCopy];
+	}
+  else
+	{
+		[_data appendData: data];
+	}
+}
+
+//HTTPS self-certificate
+- (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
+	return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+	NSLog(@"[sync] use HTTPS self-certificate");
+	if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+		[challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
+	}
+}
+
+@end
+
+
+#pragma mark main class URLConnectionHelper
+//Helper for NSURLConnection async or sync request. use HTTPS self-certificate. 
+//switch to another server when connection fail.
+@interface URLConnectionHelper (Private)
+- (void) removeBadCurrentServerURL;
+- (void) swithToGroupMemberServer;
+- (NSString *) checkGroupMemberServers;
+- (void) updateControllerWith:(NSString *)groupMemberUrl;
+@end
+
+@implementation URLConnectionHelper 
+
+@synthesize delegate, connection, errorMsg, autoDiscoverController, getAutoServersTimer;
+
+
+- (id)initWithURL:(NSURL *)url delegate:(id <URLConnectionHelperDelegate>)d  {
+	if (self = [super init]) {
+		[self setDelegate:d];
+		
+		receivedData = [[NSMutableData alloc] init];
+		
+		NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:15];
+		
+		//the initWithRequest constractor will invoke the request
+		connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+		[request release];
+	}
+	return self;
+}
+
+- (id)initWithRequest:(NSURLRequest *)request delegate:(id <URLConnectionHelperDelegate>)d  {
+	if (self = [super init]) {
+		[self setDelegate:d];
+		
+		receivedData = [[NSMutableData alloc] init];
+		
+		connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+	}
+	return self;
+}
+
+- (void)cancelConnection {
+	if (connection) {
+		NSLog(@"cancel url connection");
+		[connection cancel];
+		if (connection) {
+			[connection release];
+			connection = nil;
+		}
+		
+	}
+}
+
+#pragma mark reimplement +[NSURLConnection sendSynchronousRequest:returningResponse:error:]
+//According to the documentation, +[NSURLConnection sendSynchronousRequest:returningResponse:error:] 
+//is built on top of the asynchronous loading code made available by NSURLConnection. It would not be 
+//difficult to reimplement this by spawning and blocking on an NSThread, running the request asynchronously 
+//in the background on a run loop and ending the thread once either connectionDidFinishLoading: or 
+//connection:didFailWithError: is received.
+- (NSData *)sendSynchronousRequest:(NSURLRequest *)request returningResponse:(NSURLResponse **)response error:(NSError **)error {
+	
+	NSData	*data = nil;
+	
+	_NSURLConnectionDataCollector	*collector;
+	NSURLConnection			*conn;
+	NSRunLoop				*loop;
+	
+	collector = [_NSURLConnectionDataCollector alloc];
+	collector = [collector initWithResponsePointer: response andErrorPointer: error];
+	conn = [NSURLConnection alloc];
+	conn = [conn initWithRequest: request delegate:[collector autorelease]];
+	[collector _setConnection: conn];
+	loop = [NSRunLoop currentRunLoop];
+	while ([collector _done] == NO)
+	{
+		NSDate	*limit;
+		
+		limit = [[NSDate alloc] initWithTimeIntervalSinceNow: 1.0];
+		[loop runMode: NSDefaultRunLoopMode beforeDate: limit];
+		[limit release];
+	}
+	data = [[collector _data] retain];
+  return [data autorelease];
+}
+
+
+#pragma mark Instance method
+
+// Swith to groupmember controller.
+- (void) swithToGroupMemberServer {
+	[self removeBadCurrentServerURL];
+	NSString *aAvailableGroupMemberUrl = [self checkGroupMemberServers];
+	
+	if (aAvailableGroupMemberUrl != nil && ![@"" isEqualToString:aAvailableGroupMemberUrl]) {
+		[self updateControllerWith:aAvailableGroupMemberUrl];
+	} else {
+		ViewHelper *viewHelper = [[ViewHelper alloc] init];
+		[viewHelper showAlertViewWithTitleAndSettingNavigation:@"Connection failed" Message:@"There's no server available. Leave this problem?"];
+	}
+}
+
+- (void) removeBadCurrentServerURL {
+	NSString *tempCurrentServerUrl = [AppSettingsDefinition getCurrentServerUrl];
+	if ([AppSettingsDefinition isAutoDiscoveryEnable]) {
+		NSMutableArray *autoServers = [AppSettingsDefinition getAutoServers];
+		for (int i=0; i < [autoServers count]; i++) {
+			[[autoServers objectAtIndex:i] setValue:[NSNumber numberWithBool:NO] forKey:@"choose"];
+			if ([tempCurrentServerUrl isEqualToString:[[autoServers objectAtIndex:i] objectForKey:@"url"]]) {
+				[[AppSettingsDefinition getAutoServers] removeObjectAtIndex:i];
+			}
+		}
+	} else {
+		NSMutableArray *customServers = [AppSettingsDefinition getCustomServers];
+		for (int i=0; i < [customServers count]; i++) {
+			[[customServers objectAtIndex:i] setValue:[NSNumber numberWithBool:NO] forKey:@"choose"];
+			if ([tempCurrentServerUrl isEqualToString:[[customServers objectAtIndex:i] objectForKey:@"url"]]) {
+				//[[AppSettingsDefinition getCustomServers] removeObjectAtIndex:i];
+			}
+		}
+	}
+}
+
+// Check whether the url of groupmember is available.
+- (NSString *) checkGroupMemberServers {
+	NSMutableArray *groupMembers = [[DataBaseService sharedDataBaseService] findAllGroupMembers];
+	for (GroupMember *gm in groupMembers) {
+		@try {
+			[AppSettingsDefinition setCurrentServerUrl:gm.url];
+			[CheckNetwork checkAll];
+			
+			NSMutableDictionary *groupMemberServer = [NSMutableDictionary dictionaryWithObject:gm.url forKey:@"url"];
+			[groupMemberServer setValue:[NSNumber numberWithBool:YES] forKey:@"choose"];
+			
+			BOOL hadSameUrlBefore = NO;
+			if ([AppSettingsDefinition isAutoDiscoveryEnable]) {
+				for (int i=0; i<[[AppSettingsDefinition getAutoServers] count]; i++) {
+					if ([gm.url isEqualToString:[[[AppSettingsDefinition getAutoServers] objectAtIndex:i] objectForKey:@"url"]]) {
+						[[[AppSettingsDefinition getAutoServers] objectAtIndex:i] setValue:[NSNumber numberWithBool:YES] forKey:@"choose"];
+						hadSameUrlBefore = YES;
+					}
+				}
+				if (!hadSameUrlBefore) {
+					[AppSettingsDefinition addAutoServer:groupMemberServer];
+				}
+			} else {
+				for (int i=0; i<[[AppSettingsDefinition getCustomServers] count]; i++) {
+					if ([gm.url isEqualToString:[[[AppSettingsDefinition getCustomServers] objectAtIndex:i] objectForKey:@"url"]]) {
+						[[[AppSettingsDefinition getCustomServers] objectAtIndex:i] setValue:[NSNumber numberWithBool:YES] forKey:@"choose"];
+						hadSameUrlBefore = YES;
+					}
+				}
+				if (!hadSameUrlBefore) {
+					[[AppSettingsDefinition getCustomServers] addObject:groupMemberServer];
+				}
+			}
+			[AppSettingsDefinition writeToFile];
+			return gm.url;
+		}
+		@catch (NSException * e) {
+			continue;
+		}
+	}
+	return nil;
+}
+
+- (void) updateControllerWith:(NSString *)groupMemberUrl {
+	NSLog(@"Switching to groupmember controller server %@, please wait...", groupMemberUrl);
+	UpdateController *updateController = [[UpdateController alloc] initWithDelegate:self];
+	[updateController checkConfigAndUpdate];
+}
+
+#pragma mark delegate method of NSURLConnection
+
+//Called we connection receive data
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+	// Append data
+	[receivedData appendData:data];
+}
+
+// When finished the connection invoke the deleget method
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+	// Send data to delegate
+	//[delegate performSelector:@selector(definitionURLConnectionDidFinishLoading:) withObject:receivedData afterDelay:5];
+	[delegate definitionURLConnectionDidFinishLoading:receivedData];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+	
+	if ([delegate respondsToSelector:@selector(definitionURLConnectionDidFailWithError:)]) {
+		[delegate definitionURLConnectionDidFailWithError:error];
+		self.errorMsg = error;
+		[self swithToGroupMemberServer];
+	} else {
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error Occured" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+		[alert show];
+		[alert release];
+	}
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	if (delegate && [delegate respondsToSelector:@selector(definitionURLConnectionDidReceiveResponse:)]) {
+		[delegate definitionURLConnectionDidReceiveResponse:response];
+	}
+}
+
+// HTTPS self-certificate
+- (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
+	return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+	NSLog(@"[async] use HTTPS self-certificate");
+	if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust]) {
+		[challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
+	}
+}
+
+
+#pragma mark Delegate method of ServerAutoDiscoveryController
+- (void)onFindServer:(NSString *)serverUrl {
+	//TODO: donothing
+}
+
+- (void)onFindServerFail:(NSString *)errorMessage {
+	NSLog(@"Find Server Error in class URLConnectionHelper. %@", errorMessage);
+	[ViewHelper showAlertViewWithTitle:@"Auto Discovery" Message:errorMessage];
+}
+
+#pragma mark Delegate method of UpdateController
+- (void)didUpadted {
+	[[NSNotificationCenter defaultCenter] postNotificationName:NotificationRefreshGroupsView object:nil];
+}
+
+- (void)didUseLocalCache:(NSString *)errorMessage {
+	if ([errorMessage isEqualToString:@"401"]) {
+		[[NSNotificationCenter defaultCenter] postNotificationName:NotificationPopulateCredentialView object:nil];
+	} else {
+		[ViewHelper showAlertViewWithTitle:@"Use Local Cache" Message:errorMessage];
+	}
+}
+
+- (void)dealloc {
+	[receivedData release];
+	[connection release];
+	[autoDiscoverController release];
+	[getAutoServersTimer release];
+	[super dealloc];
+}
+
+@end
