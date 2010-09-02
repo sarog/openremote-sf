67375e8e8c41929a3612c05a3f5ae4b8362c4fe6
handyw <handyw@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-09-02 10:17:57 +0000

    Appended support for picking color while moving in ColorPickerView.
    ColorPickerView sends command every specified intervals while finger moving in it.
    This specified interval is macro variable named MIN_VALID_MOVE_DISTANCE in file ColorPickerView.h and the default value is 2 pixels.
    
    NOTE:
    1) ColorPickerView won't send command at the end of moving action, but at end of touching action.
    2) Finger can move on ColorPickerView to send color command and Screens can be switched while sliding over ColorPickerView.

--- branches/feature/ORBoss_2_0_Alpha6-colorpicker/iPhone_Console_2_0_0_Alpha2/Classes/ColorPickerView.m
+++ branches/feature/ORBoss_2_0_Alpha6-colorpicker/iPhone_Console_2_0_0_Alpha2/Classes/ColorPickerView.m
@@ -22,6 +22,13 @@
 #import "ColorPickerView.h"
 #import "ColorPicker.h"
 #import "DirectoryDefinition.h"
+#import "AbsoluteLayoutContainerView.h"
+#import "GridCellView.h"
+
+@interface ColorPickerView (Private)
+- (void) enableScrollView:(BOOL) booleanValue;
+- (CGFloat)distanceBetweenTwoPoints:(CGPoint)fromPoint toPoint:(CGPoint)toPoint;
+@end
 
 @implementation ColorPickerView
 
@@ -44,13 +51,55 @@
 	[self sendCommandRequest:[NSString stringWithFormat:@"%02x%02x%02x", (int)(c[0]*255), (int)(c[1]*255), (int)(c[2]*255)]];
 }
 
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+	NSLog(@"ColorPikcerView began event......");
+	[self enableScrollView:NO];
+	
+	UITouch* touch = [touches anyObject];
+	touchBeginPoint = [touch previousLocationInView:self];
+}
+
+- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
 	NSLog(@"ColorPikcerView moving event......");
+	
+	movingTag = YES;
+	UITouch* touch = [touches anyObject];
+	CGPoint currentPoint = [touch locationInView:self];
+	CGFloat distance = [self distanceBetweenTwoPoints:touchBeginPoint toPoint:currentPoint];
+	if (distance > MIN_VALID_MOVE_DISTANCE) {
+		[imageView touchesMoved:touches withEvent:event];
+		touchBeginPoint = currentPoint;
+		NSLog(@"The distance %f of moving is greater than %d, so command will be sent.", distance, MIN_VALID_MOVE_DISTANCE);
+	} else {
+		NSLog(@"The distance %f of moving is less than %d, so command won't be sent.", distance, MIN_VALID_MOVE_DISTANCE);
+	}
 }
 
 - (void) touchesEnded:(NSSet*)touches withEvent:(UIEvent*)event {
 	NSLog(@"ColorPikcerView end event......");
-	[imageView touchesEnded:touches withEvent:event];
+	if (movingTag == NO) {
+		[imageView touchesEnded:touches withEvent:event];
+	}
+	movingTag = NO;
+	[self enableScrollView:YES];
+}
+
+- (CGFloat)distanceBetweenTwoPoints:(CGPoint)fromPoint toPoint:(CGPoint)toPoint {
+	float x = toPoint.x - fromPoint.x;
+	float y = toPoint.y - fromPoint.y;
+	return sqrt(x * x + y * y);
+}
+
+- (void) enableScrollView:(BOOL) booleanValue {
+	UIScrollView *scrollViewOfPaginationController = nil;
+	if ([self.superview isMemberOfClass:[AbsoluteLayoutContainerView class]]) {
+		scrollViewOfPaginationController = (UIScrollView *)self.superview.superview.superview;
+	} else if ([self.superview isMemberOfClass:[GridCellView class]]) {
+		scrollViewOfPaginationController = (UIScrollView *)self.superview.superview.superview.superview;
+	}
+	if (scrollViewOfPaginationController != nil) {
+		[scrollViewOfPaginationController setScrollEnabled:booleanValue];
+	}
 }
 
 - (void)dealloc {
