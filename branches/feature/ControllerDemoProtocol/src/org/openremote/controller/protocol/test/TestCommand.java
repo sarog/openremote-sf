b2bb33d8bf7129711ac9991c79c487f2ae68a141
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-09-30 19:59:51 +0000

    demo protocol for virtual rooms

---
+++ branches/feature/ControllerDemoProtocol/src/org/openremote/controller/protocol/test/TestCommand.java
@@ -0,0 +1,56 @@
+package org.openremote.controller.protocol.test;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.openremote.controller.command.ExecutableCommand;
+import org.openremote.controller.command.StatusCommand;
+import org.openremote.controller.component.EnumSensorType;
+
+
+public class TestCommand implements ExecutableCommand, StatusCommand {
+   
+   private TestCommandType command;
+   
+   private Logger logger = Logger.getLogger(this.getClass().getName());
+   
+   private String commandValue;
+
+  private static String state = "ON";
+   
+   public TestCommand() {
+      super();
+   }
+
+   public TestCommand(TestCommandType command) {
+      super();
+      this.command = command;
+   }
+
+   public TestCommandType getCommand() {
+      return command;
+   }
+
+   public void setCommand(TestCommandType command) {
+      this.command = command;
+   }
+
+   public String getCommandValue() {
+      return commandValue;
+   }
+
+   public void setCommandValue(String commandValue) {
+      this.commandValue = commandValue;
+   }
+   
+   @Override
+   public void send() {
+      state = commandValue;
+   }
+   
+   @Override
+   public String read(EnumSensorType sensorType, Map<String, String> statusMap) {
+      return state;
+   }
+   
+}
