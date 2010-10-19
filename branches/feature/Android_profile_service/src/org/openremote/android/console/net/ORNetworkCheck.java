0a4f67498fbb318310d264b697e0e43f3b25448f
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-10-19 11:44:24 +0000

    Merged R3044-3060 from /branches/project/Android_Console_2_0_0_Alphas/

--- branches/feature/Android_profile_service/src/org/openremote/android/console/net/ORNetworkCheck.java
+++ branches/feature/Android_profile_service/src/org/openremote/android/console/net/ORNetworkCheck.java
@@ -1,99 +1,202 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2010, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2010, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.android.console.net;
 
 import org.apache.http.HttpResponse;
 import org.openremote.android.console.Constants;
 import org.openremote.android.console.model.AppSettingsModel;
 import org.openremote.android.console.util.HTTPUtil;
-import org.openremote.android.console.util.IpUitl;
+import org.openremote.android.console.util.IpUtil;
 
 import android.content.Context;
+import android.util.Log;
+import android.net.wifi.WifiManager;
+import android.net.NetworkInfo;
+import android.net.ConnectivityManager;
+
 
 /**
- * This is responsible for network check.
+ * TODO: Checks access to OpenRemote controller.
  * 
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  * @author handy 2010-04-28
- *
  */
-public class ORNetworkCheck {
-	
-	/**
-	 * Check all related to the specified controller server url.
-	 */
-	public static HttpResponse checkAllWithControllerServerURL(Context context, String controllerServerURL) {
-		AppSettingsModel.setCurrentServer(context, controllerServerURL);
-		return checkPanelXMlOfCurrentPanelIdentity(context);
-	}
-	
-	/**
-	 * Check if the RESTful url {controllerServerURL}/rest/panel/{panel identity} is available.
-	 */
-	private static HttpResponse checkPanelXMlOfCurrentPanelIdentity(Context context) {
-	   HttpResponse response = checkControllerAvailable(context);
-		if(response !=null && response.getStatusLine().getStatusCode() == Constants.HTTP_SUCCESS) {
-			String currentControllerServerURL = AppSettingsModel.getSecuredServer(context);
-			if (currentControllerServerURL == null || "".equals(currentControllerServerURL)) {
-				return null;
-			}
-			String currentPanelIdentity = AppSettingsModel.getCurrentPanelIdentity(context);
-			if (currentPanelIdentity == null || "".equals(currentPanelIdentity)) {
-				return null;
-			}
-			String restfulPanelURL = currentControllerServerURL + "/rest/panel/" + HTTPUtil.encodePercentUri(currentPanelIdentity);
-			return ORConnection.checkURLWithHTTPProtocol(context, ORHttpMethod.GET, restfulPanelURL, true);
-		}
-		return response;
-	}
+public class ORNetworkCheck
+{
 
-	/**
-	 * Check if the ControllerServerURL is available.
-	 */
-	private static HttpResponse checkControllerAvailable(Context context) {
-		if (checkControllerIPAddress(context)) {
-			String currentControllerServerURL = AppSettingsModel.getSecuredServer(context);
-			if (currentControllerServerURL == null || "".equals(currentControllerServerURL)) {
-				return null;
-			}
-			return ORConnection.checkURLWithHTTPProtocol(context, ORHttpMethod.GET, currentControllerServerURL, false);
-		}
-		return null;
-	}
+  // Constants ------------------------------------------------------------------------------------
 
-	/**
-	 * Check if the IP of controller is reachable.
-	 */
-	private static boolean checkControllerIPAddress(Context context) {
-	   if (!IPAutoDiscoveryClient.isNetworkTypeWIFI) {
-	      return true;
-	   }
-		if (ORWifiReachability.getInstance(context).canReachWifiNetwork()) {
-			String currentControllerServerURL = AppSettingsModel.getCurrentServer(context);
-			if (currentControllerServerURL == null || "".equals(currentControllerServerURL)) {
-				return false;
+  /**
+   * Log category name used by this class.
+   */
+  private final static String LOG_CATEGORY = Constants.LOG_CATEGORY + "WiFi";
+
+
+
+  // Class Members --------------------------------------------------------------------------------
+  
+  /**
+   * Verifies the network access to the currently configured controller URL by checking if
+   * REST API {controllerServerURL}/rest/panel/{panel identity} is available.
+   *
+   * @param context               global Android application context
+   * @param url                   an URL to a controller instance
+   *
+   * @return TODO: returns null or HttpResponse
+   */
+  public static HttpResponse verifyControllerURL(Context context, String url)
+  {
+    // TODO : Use URL class instead of string in param
+
+
+    // TODO : modifying the settings probably doesn't belong here, as it is an undocumented side-effect
+    AppSettingsModel.setCurrentServer(context, url);
+
+
+    HttpResponse response = checkControllerAvailable(context);
+
+    Log.d(LOG_CATEGORY, "HTTP Response: " + response);
+
+    if(response != null && response.getStatusLine().getStatusCode() == Constants.HTTP_SUCCESS)
+    {
+      String controllerURL = AppSettingsModel.getSecuredServer(context);
+
+      if (controllerURL == null || "".equals(controllerURL))
+      {
+        return null;
 			}
-			String currentControllerServerIp = IpUitl.splitIpFromURL(currentControllerServerURL);
-			return ORWifiReachability.getInstance(context).checkIpString(currentControllerServerIp);
-		}
-		return false;
-	}
+
+      String currentPanelIdentity = AppSettingsModel.getCurrentPanelIdentity(context);
+
+      if (currentPanelIdentity == null || "".equals(currentPanelIdentity))
+      {
+        return null;
+      }
+
+      String restfulPanelURL = controllerURL + "/rest/panel/" + HTTPUtil.encodePercentUri(currentPanelIdentity);
+
+      Log.i(LOG_CATEGORY, "Getting panel URL " + restfulPanelURL);
+
+      return ORConnection.checkURLWithHTTPProtocol(context, ORHttpMethod.GET, restfulPanelURL, true);
+    }
+
+    return response;
+  }
+
+  /**
+   * Check if the Controller URL is available.
+   *
+   * @param context   a global Android application context
+   *
+   * @return TODO
+   */
+  private static HttpResponse checkControllerAvailable(Context context)
+  {
+    if (checkControllerIPAddress(context))
+    {
+      String controllerURL = AppSettingsModel.getSecuredServer(context);
+
+      Log.i(LOG_CATEGORY, "controllerURL: " + controllerURL);
+
+      if (controllerURL == null || "".equals(controllerURL))
+      {
+        return null;
+      }
+
+      return ORConnection.checkURLWithHTTPProtocol(context, ORHttpMethod.GET, controllerURL, false);
+    }
+
+    return null;
+  }
+
+  /**
+   * Check if the IP of controller is reachable.
+   *
+   * @param context   global Android application context
+   *
+   * @return TODO
+   */
+  private static boolean checkControllerIPAddress(Context context)
+  {
+
+    if (!IPAutoDiscoveryClient.isNetworkTypeWIFI)     // TODO : questionable use of global static field
+    {
+        return true;
+    }
+
+    if (!canReachWifiNetwork(context))
+    {
+      return false;
+    }
+
+    String controllerURL = AppSettingsModel.getCurrentServer(context);
+
+    Log.i(LOG_CATEGORY, "controllerURL: " + controllerURL);
+
+    if (controllerURL == null || "".equals(controllerURL))
+    {
+      return false;
+    }
+
+    String controllerIPAddress = IpUtil.splitIpFromURL(controllerURL);  // TODO : class name has a typo
+
+    Log.d(LOG_CATEGORY, "ControllerIPAddress: " + controllerIPAddress);
+
+    return true;
+  }
+
+  /**
+   * Detects the current WiFi status.
+   *
+   * @param ctx     global Android application context
+   *
+   * @return TODO
+   */
+  private static boolean canReachWifiNetwork(Context ctx)
+  {
+
+    WifiManager wifiManager = (WifiManager)ctx.getSystemService(Context.WIFI_SERVICE);
+    ConnectivityManager connectivityManager = (ConnectivityManager)ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+    if (!wifiManager.isWifiEnabled() || wifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLED)
+    {
+      Log.i(LOG_CATEGORY, "WiFi not enabled or WiFi network not detected.");
+
+      return false;
+    }
+
+    NetworkInfo wifiNetworkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+    if (!wifiNetworkInfo.isAvailable())
+    {
+      Log.i(LOG_CATEGORY, "Wifi network detected but wasn't available.");
+
+      return false;
+    }
+
+    else
+    {
+      return true;
+    }
+
+  }
 
 }
---
+++ branches/feature/Android_profile_service/tests/src/org/openremote/android/test/console/net/ORWifiReachabilityTest.java
@@ -0,0 +1,124 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2010, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.android.test.console.net;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.test.AndroidTestCase;
+import org.openremote.android.console.net.ORWifiReachability;
+
+/**
+ * Unit tests for {@link org.openremote.android.console.net.ORWifiReachability} class.
+ *
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
+ */
+public class ORWifiReachabilityTest extends AndroidTestCase
+{
+
+//
+//  /**
+//   * Basic test to ensure the singleton is created correctly.
+//   */
+//  public void testSingletonCreation()
+//  {
+//    ORWifiReachability reachability1 = ORWifiReachability.getInstance(getContext());
+//    ORWifiReachability reachability2 = ORWifiReachability.getInstance(getContext());
+//
+//    assertNotNull(reachability1);
+//    assertNotNull(reachability2);
+//
+//    assertEquals(reachability1, reachability2);
+//    assertEquals(reachability2, reachability1);
+//  }
+//
+//  /**
+//   * Basic test of {@link org.openremote.android.console.net.ORWifiReachability#canReachWifiNetwork()}
+//   *
+//   * NOTE: assumes WiFi is available, generally needs to be run on device, emulator tends not
+//   *       to have access to localhost Wifi
+//   */
+//  public void testCanReachWifiNetwork()
+//  {
+//    Context ctx = getContext();
+//    WifiManager wifi = (WifiManager)ctx.getSystemService(Context.WIFI_SERVICE);
+//
+//    if(!wifi.isWifiEnabled())
+//      fail(wifiRequired());
+//
+//    ORWifiReachability reachability = ORWifiReachability.getInstance(ctx);
+//
+//    assertTrue(reachability.canReachWifiNetwork());
+//  }
+//
+//
+//
+//  public void testCanReachWifiNetworkNotAvailable()
+//  {
+//    Context ctx = getContext();
+//
+//    WifiManager wifi = (WifiManager)ctx.getSystemService(Context.WIFI_SERVICE);
+//
+//    try
+//    {
+//      wifi.setWifiEnabled(false);
+//
+//      ORWifiReachability reachability = ORWifiReachability.getInstance(ctx);
+//
+//      assertFalse(reachability.canReachWifiNetwork());
+//    }
+//    finally
+//    {
+//      wifi.setWifiEnabled(true);
+//    }
+//  }
+//
+//  // somewhat poor substitute for the above (but always works on emulator) will do for now
+//  public void testCanReachWifiNetworkNotAvailable()
+//  {
+//    Context ctx = getContext();
+//    WifiManager wifi = (WifiManager)ctx.getSystemService(Context.WIFI_SERVICE);
+//
+//    if (!wifi.isWifiEnabled())
+//    {
+//      ORWifiReachability reachability = ORWifiReachability.getInstance(ctx);
+//
+//      assertTrue(!reachability.canReachWifiNetwork());
+//    }
+//  }
+//
+//
+//
+//
+//  // Helper methods for tests ---------------------------------------------------------------------
+//
+//  private String wifiRequired()
+//  {
+//    return
+//        "\n\n******************************\n\n" +
+//        " This test assumes availability of WiFi network.\n" +
+//        " If you're running in the emulator, WiFi may not be available.\n" +
+//        " Run the full test suite on an Android device to include this test.\n\n" +
+//        "******************************\n";
+//  }
+
+}
+
+
