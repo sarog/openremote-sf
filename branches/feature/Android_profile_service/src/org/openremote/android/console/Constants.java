2482fcedd5907e67201dee2c986206ea3318411f
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-10-19 11:27:13 +0000

    merged revisions 3034-3039 from /branches/project/Android_Console_2_0_0_Alphas/

--- branches/feature/Android_profile_service/src/org/openremote/android/console/Constants.java
+++ branches/feature/Android_profile_service/src/org/openremote/android/console/Constants.java
@@ -1,4 +1,5 @@
-/* OpenRemote, the Home of the Digital Home.
+/*
+ * OpenRemote, the Home of the Digital Home.
  * Copyright 2008-2010, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
@@ -25,36 +26,39 @@ import android.view.Menu;
  * constants used throughout the application in more than one place
  * 
  * @author Andrew C. Oliver <acoliver osintegrators.com>
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  * 
  */
-public class Constants {
-   
-   private Constants() {
-   }
-   public static final long BINDING_VERSION = 1L;
-   public static final long DEFAULT_FONT_SIZE = 16;
-   public static final long MODEL_VERSION = 2L;
-   public static final long CACHE_VERSION = 3L;
-   public static final int MENU_ITEM_SETTING = Menu.FIRST + 1;
-   public static final int MENU_ITEM_LOGOUT = Menu.FIRST + 2;
-   public static final int MENU_ITEM_QUIT = Menu.FIRST + 3;
-   public static final String ERROR = "error";
-   public static final int DIALOG_ERROR_ID = 0;
-   public static final int HTTP_SUCCESS = 200;
-   public static final String ACTIVITY = "activity";
-   public static final String ELEMENT_OPENREMOTE = "openremote";
-   public static final String ELEMENT_BUTTONS = "buttons";
-   public static final String URL = "url";
-   public static final String LOADER = "loader";
-   public static final int REQUEST_CODE = 1;
-   public static final int RESULT_CONTROLLER_URL = 2;
-   public static final int RESULT_PANEL_SELECTED = 3;
-   public static final String MULTICAST_ADDRESS = "224.0.1.100";
-   public static final String NON_WIFI_MULTICAST_ADDRESS = "10.0.2.2";
-   public static final int MULTICAST_PORT = 3333;
-   public static final int LOCAL_SERVER_PORT = 2346;
-   public static final String PANEL_XML = "panel.xml";
-   public static final String FILE_FOLDER_PATH = "/data/data/org.openremote.android.console/files/";
-   public static final int SECURED_HTTP_PORT = 8443;
-   
+public class Constants
+{
+
+  private Constants() {}
+
+
+  public static final long BINDING_VERSION = 1L;
+  public static final long DEFAULT_FONT_SIZE = 16;
+  public static final long MODEL_VERSION = 2L;
+  public static final long CACHE_VERSION = 3L;
+  public static final int MENU_ITEM_SETTING = Menu.FIRST + 1;
+  public static final int MENU_ITEM_LOGOUT = Menu.FIRST + 2;
+  public static final int MENU_ITEM_QUIT = Menu.FIRST + 3;
+  public static final String ERROR = "error";
+  public static final int DIALOG_ERROR_ID = 0;
+  public static final int HTTP_SUCCESS = 200;
+  public static final String ACTIVITY = "activity";
+  public static final String ELEMENT_OPENREMOTE = "openremote";
+  public static final String ELEMENT_BUTTONS = "buttons";
+  public static final String URL = "url";
+  public static final String LOADER = "loader";
+  public static final int REQUEST_CODE = 1;
+  public static final int RESULT_CONTROLLER_URL = 2;
+  public static final int RESULT_PANEL_SELECTED = 3;
+  public static final String MULTICAST_ADDRESS = "224.0.1.100";
+  public static final String NON_WIFI_MULTICAST_ADDRESS = "10.0.2.2";
+  public static final int MULTICAST_PORT = 3333;
+  public static final int LOCAL_SERVER_PORT = 2346;
+  public static final String PANEL_XML = "panel.xml";
+  public static final String FILE_FOLDER_PATH = "/data/data/org.openremote.android.console/files/";
+  public static final int SECURED_HTTP_PORT = 8443;
+
 }
--- branches/feature/Android_profile_service/src/org/openremote/android/console/net/ORWifiReachability.java
+++ branches/feature/Android_profile_service/src/org/openremote/android/console/net/ORWifiReachability.java
@@ -1,96 +1,168 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2010, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2010, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.openremote.android.console.net;
 
-import org.openremote.android.console.util.IpUitl;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
+import org.openremote.android.console.Constants;
 
 /**
- * It's responsible for checking the wifi reachability.
- * 
+ * Utilities for WiFi accessibility.
+ *
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  * @author handy 2010-04-28
  *
  */
-public class ORWifiReachability {
+public class ORWifiReachability
+{
 
-	private static ORWifiReachability reachability;
-	private Context androidAppContext;
-	
-	private WifiManager wifiManager;
-	private ConnectivityManager connectivityManager;
-	
-	private ORWifiConnectionStatus wifiConnectionStatus;
-	
-	private ORWifiReachability(Context androidAppContextParam) {
-		super();
-		wifiConnectionStatus = ORWifiConnectionStatus.UNREACHABLE;
-		androidAppContext = androidAppContextParam;
-		wifiManager = (WifiManager) androidAppContext.getSystemService(Context.WIFI_SERVICE);
-		connectivityManager = (ConnectivityManager) androidAppContext.getSystemService(Context.CONNECTIVITY_SERVICE); 
-	}
-	
-	public static ORWifiReachability getInstance(Context androidAppContext) {
-		if (reachability == null) {
-			reachability = new ORWifiReachability(androidAppContext);
-		}
-		return reachability;
-	}
-	
-	/** Check whether network connectivity is possible. */
-	public boolean canReachWifiNetwork() {
-		localWifiConnectionStatus();
-		return !ORWifiConnectionStatus.UNREACHABLE.equals(this.wifiConnectionStatus);
-	}
-	
-	/** 
-	 * Ensure that a network route exists to deliver traffic to the specified host via the specified network interface.
-	 * 
-	 * @param ip The format must follow "xxx.xxx.xxx.xxx", eg: 192.168.1.11  
-	 */
-	public boolean checkIpString(String ip) {
-	   if (ip != null) {
-	      return connectivityManager.requestRouteToHost(ConnectivityManager.TYPE_WIFI, (int) IpUitl.ipStringToLong(ip));
-	   }
-	   return false;
-	}
-	
-	private void localWifiConnectionStatus() {
-		if (!wifiManager.isWifiEnabled() || wifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLED) {
-			this.wifiConnectionStatus = ORWifiConnectionStatus.UNREACHABLE;
-			Log.i("OpenRemote-INFO", "Wifi in handset wasn't enabled or wifi network didn't detect.");
-			return;
-		}
-		
-		NetworkInfo wifiNetworkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-		if (!wifiNetworkInfo.isAvailable()) {// Indicates whether network connectivity is possible.
-			Log.i("OpenRemote-INFO", "Wifi network detected but wasn't available.");
-			this.wifiConnectionStatus = ORWifiConnectionStatus.UNREACHABLE;
-		} else {
-			this.wifiConnectionStatus = ORWifiConnectionStatus.REACHABLE_VIA_WIFINETWORK;
-		}
-	}	
-	
+  // Constants ------------------------------------------------------------------------------------
+
+  /**
+   * Log category name used by this class.
+   */
+  private final static String LOG_CATEGORY = Constants.LOG_CATEGORY + "WiFi";
+
+
+  // Class Members --------------------------------------------------------------------------------
+
+  /**
+   * Thread lock to guarantee only one singleton is ever created.
+   */
+  private final static Boolean LOCK = true;
+
+  /**
+   * Singleton instance of this class.
+   */
+  private static ORWifiReachability reachability;
+
+
+  /**
+   * Returns a singleton instance of this class.
+   *
+   * @param androidAppContext   a global android application context for accessing services
+   *
+   * @return a single, shared instance of this class
+   */
+  public static ORWifiReachability getInstance(Context androidAppContext)
+  {
+    synchronized (LOCK)
+    {
+      if (reachability == null)
+      {
+        reachability = new ORWifiReachability(androidAppContext);
+      }
+    }
+
+    return reachability;
+  }
+
+
+  // Private Instance Fields ----------------------------------------------------------------------
+
+  private WifiManager wifiManager;
+
+  private ConnectivityManager connectivityManager;
+
+  private ORWifiConnectionStatus wifiConnectionStatus;
+
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  /**
+   * Private singleton constructor.
+   *
+   * @param androidAppContext   a global android application context for accessing services
+   */
+  private ORWifiReachability(Context androidAppContext)
+  {
+    wifiConnectionStatus = ORWifiConnectionStatus.UNREACHABLE;
+    wifiManager = (WifiManager)androidAppContext.getSystemService(Context.WIFI_SERVICE);
+    connectivityManager = (ConnectivityManager)androidAppContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+  }
+
+  /**
+   * Check whether network connectivity is possible.
+   */
+  public boolean canReachWifiNetwork()
+  {
+    localWifiConnectionStatus();
+
+    return !ORWifiConnectionStatus.UNREACHABLE.equals(this.wifiConnectionStatus);
+  }
+
+
+// Commented out the following checkIPString method -- while it worked fine on
+// some devices (e.g. Google G1 with Android 1.5) it failed on others (e.g.
+// Samsung Galaxy on Android 2.1-update1) even though the configured IP address
+// can be routed (despite requestRouteToHost returning false). So relaxing the
+// requirements and blindly trusting that we can route the packets.
+//                                                                    - [JPL]
+//
+//
+//  /**
+//   * Ensure that a network route exists to deliver traffic to the specified host via the specified
+//   * network interface.
+//   *
+//   * @param ip The format must follow "xxx.xxx.xxx.xxx", eg: 192.168.1.11
+//   */
+//  public boolean checkIpString(String ip)
+//  {
+//     if (ip != null)
+//     {
+//        return connectivityManager.requestRouteToHost(ConnectivityManager.TYPE_WIFI, (int) IpUitl.ipStringToLong(ip));
+//     }
+//     return false;
+//    return true;
+//  }
+
+  private void localWifiConnectionStatus()
+  {
+    if (!wifiManager.isWifiEnabled() || wifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLED)
+    {
+      this.wifiConnectionStatus = ORWifiConnectionStatus.UNREACHABLE;
+
+      Log.i(LOG_CATEGORY, "Wifi in handset wasn't enabled or wifi network didn't detect.");
+
+      return;
+    }
+
+    NetworkInfo wifiNetworkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+    if (!wifiNetworkInfo.isAvailable()) // Indicates whether network connectivity is possible.
+    {
+      Log.i(LOG_CATEGORY, "Wifi network detected but wasn't available.");
+
+      this.wifiConnectionStatus = ORWifiConnectionStatus.UNREACHABLE;
+    }
+
+    else
+    {
+      this.wifiConnectionStatus = ORWifiConnectionStatus.REACHABLE_VIA_WIFINETWORK;
+    }
+  }
+
 }
