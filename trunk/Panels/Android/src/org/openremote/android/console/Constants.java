be99926b941053bc46d42cb9aa7fe1bce7cf4cae
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-11-17 19:52:02 +0000

    sync trunk with /branches/project/Android_2_0_0_Betas branch

--- trunk/Panels/Android/src/org/openremote/android/console/Constants.java
+++ trunk/Panels/Android/src/org/openremote/android/console/Constants.java
@@ -64,7 +64,6 @@ public class Constants
   public static final String PANEL_XML = "panel.xml";
   public static final String FILE_FOLDER_PATH = "/data/data/org.openremote.android.console/files/";
   public static final int SECURED_HTTP_PORT = 8443;
-
   /**
    * Prefix for logging so OpenRemote related entries can be easily filtered.
    */
