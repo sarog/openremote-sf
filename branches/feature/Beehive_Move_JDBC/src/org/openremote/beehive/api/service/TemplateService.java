977fb074ecb3b4ffd39a78e72bef43ea780c6fc5
tomskywang <tomskywang@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-11-30 10:04:16 +0000

    merged from Beehive_crowd_integration. make the beehive support crowd API.

--- branches/feature/Beehive_Move_JDBC/src/org/openremote/beehive/api/service/TemplateService.java
+++ branches/feature/Beehive_Move_JDBC/src/org/openremote/beehive/api/service/TemplateService.java
@@ -55,4 +55,5 @@ public interface TemplateService {
    List<TemplateDTO> loadPublicTemplatesByKeywordsAndPage(String keywords,int page);
    
    TemplateDTO updateTemplate(Template t);
+   List<TemplateDTO> loadPrivateTemplatesByKeywordsAndPage(long accountOid, String keywords,int page);
 }
