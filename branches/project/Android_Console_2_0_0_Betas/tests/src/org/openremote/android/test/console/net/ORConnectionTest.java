14732840351065701a74a654c9efdbcf8d142cd9
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2010-12-04 09:08:11 +0000

    some connectivity tests

--- branches/project/Android_Console_2_0_0_Betas/tests/src/org/openremote/android/test/console/net/ORConnectionTest.java
+++ branches/project/Android_Console_2_0_0_Betas/tests/src/org/openremote/android/test/console/net/ORConnectionTest.java
@@ -20,6 +20,8 @@
  */
 package org.openremote.android.test.console.net;
 
+import java.net.HttpURLConnection;
+
 import org.openremote.android.console.net.ORConnection;
 import org.openremote.android.console.net.ORHttpMethod;
 import org.openremote.android.console.AppSettingsActivity;
@@ -28,7 +30,7 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.app.Activity;
 
 /**
- * TODO
+ * Tests for {@link ORConnection} class.
  *
  * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  */
@@ -43,12 +45,16 @@ public class ORConnectionTest extends ActivityInstrumentationTestCase2<AppSettin
   private Activity activity;
 
 
+
+
   // Constructors ---------------------------------------------------------------------------------
 
   public ORConnectionTest()
   {
     super("org.openremote.android.console", AppSettingsActivity.class);
   }
+
+
 
 
   // Test Setup -----------------------------------------------------------------------------------
@@ -67,18 +73,103 @@ public class ORConnectionTest extends ActivityInstrumentationTestCase2<AppSettin
     this.activity = getActivity();
   }
 
+
+
   // Tests ----------------------------------------------------------------------------------------
 
-  public void testURLConnectionBasic()
+
+
+  /**
+   * Tests a basic GET through {@link ORConnection#checkURLWithHTTPProtocol} method.
+   */
+  public void testURLConnectionBasicGET()
   {
-    final String URL = "http://contoller.openremote.org/test/controller";
+    final String URL = "http://controller.openremote.org/test/controller";
+    final String HTTP_CONTENT_TYPE_HEADER = "content-type";
+    final String HTTP_MIME_CONTENT_TYPE = "text/html";
+
     final boolean NO_HTTP_AUTH = false;
+
 
     HttpResponse response = ORConnection.checkURLWithHTTPProtocol(
         activity, ORHttpMethod.GET, URL, NO_HTTP_AUTH
     );
 
-    
+    assertTrue("Expected response to " + URL + ", got <null>", response != null);
+
+    int  httpResponseCode = response.getStatusLine().getStatusCode();
+    long httpResponseContentLen = response.getEntity().getContentLength();
+    String httpContentTypeHeader = response.getEntity().getContentType().getName();
+    String httpMimeContentType = response.getEntity().getContentType().getValue();
+
+    assertTrue(
+        "Expected HTTP Response '" + HttpURLConnection.HTTP_OK + "', got '" + httpResponseCode + "'.",
+        httpResponseCode == HttpURLConnection.HTTP_OK
+    );
+
+    assertTrue(
+        "Expected content length > 0, got " + httpResponseContentLen,
+        response.getEntity().getContentLength() > 0
+    );
+
+    assertTrue(
+        "Expected HTTP Header '" + HTTP_CONTENT_TYPE_HEADER + "', got '" + httpContentTypeHeader + "'.",
+        httpContentTypeHeader.equalsIgnoreCase(HTTP_CONTENT_TYPE_HEADER)
+    );
+
+    assertTrue(
+        "Expected HTTP Mime type '" + HTTP_MIME_CONTENT_TYPE + "', got '" + httpMimeContentType + "'.",
+        httpMimeContentType.startsWith("text/html")
+    );
   }
+
+
+  /**
+   * Tests a GET behavior on an unknown (no DNS resolution) host.
+   */
+  public void testURLConnectionUnknownHost()
+  {
+    final String URL = "http://controller.openremotetest.org/test/controller";
+
+    final boolean NO_HTTP_AUTH = false;
+
+
+    HttpResponse response = ORConnection.checkURLWithHTTPProtocol(
+        activity, ORHttpMethod.GET, URL, NO_HTTP_AUTH
+    );
+
+
+    // This is currently expected but very bad design -- incorrectly configured URL errors
+    // should be propagated to user
+
+    assertTrue(response == null);
+  }
+
+  /**
+   * Tests a GET behavior on an unknown (no DNS resolution) host.
+   */
+  public void testURLConnectionUnknownHost_BAD_API_DESIGN()
+  {
+    final String URL = "http://controller.openremotetest.org/test/controller";
+
+    final boolean NO_HTTP_AUTH = false;
+
+
+    try
+    {
+      HttpResponse response = ORConnection.checkURLWithHTTPProtocol(
+          activity, ORHttpMethod.GET, URL, NO_HTTP_AUTH
+      );
+
+      fail (
+          "should create an exception with error details rather than non-informative null return value"
+      );
+    }
+    catch (Throwable t)
+    {
+      // *ANY* exception would be better than returning null...
+    }
+  }
+
 }
 
