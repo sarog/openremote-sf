029bd076e734683af69aad9009f74f2d8bc81204
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-01-03 14:58:00 +0000

    formatting

--- branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/rest/support/json/JSONTranslator.java
+++ branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/rest/support/json/JSONTranslator.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.rest.support.json;
 
 import java.net.HttpURLConnection;
@@ -29,58 +30,74 @@ import net.sf.json.xml.XMLSerializer;
 import org.openremote.controller.Constants;
 
 /**
- * 
- * @author handy 2010-06-28
- * 
- * This is responsible for translating xml data to json data.
+ * TODO : This is responsible for translating xml data to json data.
  *
+ * @author handy 2010-06-28
  */
-public class JSONTranslator {
+public class JSONTranslator
+{
 
-   // translate xml data to json object with HTTPServletRequest.
-   public static String toDesiredData(HttpServletRequest request, HttpServletResponse response, String xml) {
-      String acceptTypeInHeader = request.getHeader(Constants.HTTP_ACCEPT_HEADER);
-      return doTransalteXMLToJSONString(acceptTypeInHeader, response, xml);
-   }
-   
-   public static String doTransalteXMLToJSONString(String acceptTypeInHeader, HttpServletResponse response, String xml) {
-      if (Constants.HTTP_HEADER_ACCEPT_JSON_TYPE.equalsIgnoreCase(acceptTypeInHeader)) {
-         response.setContentType(Constants.HTTP_HEADER_ACCEPT_JSON_TYPE);
-         return translate(response, xml);
-      } else {
-         response.setContentType(Constants.MIME_APPLICATION_XML);
-         return xml;
-      }
-   }
-   
-   public static String toDesiredData(HttpServletRequest request, HttpServletResponse response, int errorCode, String xml) {
-      String acceptTypeInHeader = request.getHeader(Constants.HTTP_ACCEPT_HEADER);
-      return doTransalteXMLToJSONString(acceptTypeInHeader, response, errorCode, xml);
-   }
-   
-   private static String doTransalteXMLToJSONString(String acceptTypeInHeader, HttpServletResponse response, int errorCode, String xml) {
-      if (Constants.HTTP_HEADER_ACCEPT_JSON_TYPE.equalsIgnoreCase(acceptTypeInHeader)) {
-         response.setContentType(Constants.HTTP_HEADER_ACCEPT_JSON_TYPE);
-         return translate(response, xml);
-      } else {
-         response.setContentType(Constants.MIME_APPLICATION_XML);
-         response.setStatus(errorCode);
-         return xml;
-      }
-   }
-   
+  // translate xml data to json object with HTTPServletRequest.
+  public static String toDesiredData(HttpServletRequest request, HttpServletResponse response, String xml)
+  {
+    String acceptTypeInHeader = request.getHeader(Constants.HTTP_ACCEPT_HEADER);
+    return doTransalteXMLToJSONString(acceptTypeInHeader, response, xml);
+  }
 
-   private static String translate(HttpServletResponse response, String xml) {
-      if (response != null) {
-         response.setStatus(HttpURLConnection.HTTP_OK);
-      }
-      xml = xml.replaceAll("xsi:schemaLocation=\".*\"", " ");
-      XMLSerializer xmlSerializer = new XMLSerializer(); 
-      xmlSerializer.setTypeHintsEnabled(false);
-      xmlSerializer.setTypeHintsCompatibility(false);
-      xmlSerializer.setSkipNamespaces(true);
-      JSON json = xmlSerializer.read(xml);
-      return json.toString(3);
-   }
-   
+  public static String doTransalteXMLToJSONString(String acceptTypeInHeader, HttpServletResponse response, String xml)
+  {
+    if (Constants.HTTP_HEADER_ACCEPT_JSON_TYPE.equalsIgnoreCase(acceptTypeInHeader))
+    {
+      response.setContentType(Constants.HTTP_HEADER_ACCEPT_JSON_TYPE);
+      return translate(response, xml);
+    }
+
+    else
+    {
+      response.setContentType(Constants.MIME_APPLICATION_XML);
+      return xml;
+    }
+  }
+
+  public static String toDesiredData(HttpServletRequest request, HttpServletResponse response, int errorCode, String xml)
+  {
+    String acceptTypeInHeader = request.getHeader(Constants.HTTP_ACCEPT_HEADER);
+    return doTransalteXMLToJSONString(acceptTypeInHeader, response, errorCode, xml);
+  }
+
+  private static String doTransalteXMLToJSONString(String acceptTypeInHeader, HttpServletResponse response, int errorCode, String xml)
+  {
+    if (Constants.HTTP_HEADER_ACCEPT_JSON_TYPE.equalsIgnoreCase(acceptTypeInHeader))
+    {
+      response.setContentType(Constants.HTTP_HEADER_ACCEPT_JSON_TYPE);
+      return translate(response, xml);
+    }
+
+    else
+    {
+      response.setContentType(Constants.MIME_APPLICATION_XML);
+      response.setStatus(errorCode);
+      return xml;
+    }
+  }
+
+
+  private static String translate(HttpServletResponse response, String xml)
+  {
+    if (response != null)
+    {
+      response.setStatus(HttpURLConnection.HTTP_OK);
+    }
+
+    xml = xml.replaceAll("xsi:schemaLocation=\".*\"", " ");
+    XMLSerializer xmlSerializer = new XMLSerializer();
+    xmlSerializer.setTypeHintsEnabled(false);
+    xmlSerializer.setTypeHintsCompatibility(false);
+    xmlSerializer.setSkipNamespaces(true);
+
+    JSON json = xmlSerializer.read(xml);
+
+    return json.toString(3);
+  }
+
 }
