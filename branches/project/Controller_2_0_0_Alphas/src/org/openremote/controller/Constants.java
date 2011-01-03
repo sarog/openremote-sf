b73817eb9fc26b6115d126859d735cb82e550049
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-01-03 12:27:30 +0000

    format

--- branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/Constants.java
+++ branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/Constants.java
@@ -28,75 +28,75 @@ package org.openremote.controller;
 public class Constants
 {
 
-   public final static String LIRCD_CONF = "lircd.conf";
-   
-   public final static String CONTROLLER_XSD_PATH = "/controller-2.0-M7.xsd";
-   
-   public final static String PANEL_XSD_PATH = "/panel-2.0-M7.xsd";
-   
-   public final static String CONTROLLER_XML = "controller.xml";
-   
-   public final static String OPENREMOTE_WEBSITE= "http://www.openremote.org";
-   
-   public final static String OPENREMOTE_NAMESPACE= "or";
-   
-   public final static String SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
-   
-   public final static String XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
-   
-   public final static String SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
-   
-   public final static String TRUE = "TRUE";
-   
-   public final static String OK = "OK";
-   
-   public static final String SERVER_RESPONSE_TIME_OUT = "TIMEOUT";
-   
-   public static final String PANEL_XML = "panel.xml";
-   
-   /** #The following constants are about status and polling action# */
-   /** The Constant SENSOR_ID_SEPARATOR. */
-   public static final String STATUS_POLLING_SENSOR_IDS_SEPARATOR = ",";
-   
-   /** The Constant xmlHeader of composed xml-formatted status results. */
-   public static final String STATUS_XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<openremote xmlns=\"http://www.openremote.org\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.openremote.org http://www.openremote.org/schemas/controller.xsd\">\n";
-   
-   /** The Constant XML_STATUS_RESULT_ELEMENT_NAME composed xml-formatted status results. */
-   public static final String STATUS_XML_STATUS_RESULT_ELEMENT_NAME = "status";
-   
-   public static final String SENSORS_ELEMENT_NAME = "sensors";
-   
-   public static final String INCLUDE_ELEMENT_NAME = "include";
-   
-   public static final String SENSOR_TYPE_ATTRIBUTE_NAME = "type";
-   
-   public static final String ID_ATTRIBUTE_NAME = "id";
-   
-   public static final String REF_ATTRIBUTE_NAME = "ref";
-   
-   /** The Constant XML_STATUS_RESULT_ELEMENT_SENSOR_IDENTITY composed xml-formatted status results. */
-   public static final String STATUS_XML_STATUS_RESULT_ELEMENT_SENSOR_IDENTITY = "id";
-   
-   /** The Constant XML_TAIL of composed xml-formatted status results. */
-   public static final String STATUS_XML_TAIL = "</openremote>";
+  public final static String LIRCD_CONF = "lircd.conf";
 
-   public static final String HTTP_BASIC_AUTH_HEADER_NAME = "Authorization";
+  public final static String CONTROLLER_XSD_PATH = "/controller-2.0-M7.xsd";
 
-   public static final String HTTP_BASIC_AUTH_HEADER_VALUE_PREFIX = "Basic ";
-   
-   public static final String CALLBACK_PARAM_NAME = "callback";
+  public final static String PANEL_XSD_PATH = "/panel-2.0-M7.xsd";
 
-   public static final String CHARACTER_ENCODING_UTF8 = "utf-8";
+  public final static String CONTROLLER_XML = "controller.xml";
 
-   public static final String HTTP_ACCEPT_HEADER_NAME = "accept";
+  public final static String OPENREMOTE_WEBSITE= "http://www.openremote.org";
+
+  public final static String OPENREMOTE_NAMESPACE= "or";
+
+  public final static String SCHEMA_LANGUAGE = "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+
+  public final static String XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
+
+  public final static String SCHEMA_SOURCE = "http://java.sun.com/xml/jaxp/properties/schemaSource";
+
+  public final static String TRUE = "TRUE";
+
+  public final static String OK = "OK";
+
+  public static final String SERVER_RESPONSE_TIME_OUT = "TIMEOUT";
+
+  public static final String PANEL_XML = "panel.xml";
+
+  /** #The following constants are about status and polling action# */
+  /** The Constant SENSOR_ID_SEPARATOR. */
+  public static final String STATUS_POLLING_SENSOR_IDS_SEPARATOR = ",";
+
+  /** The Constant xmlHeader of composed xml-formatted status results. */
+  public static final String STATUS_XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<openremote xmlns=\"http://www.openremote.org\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.openremote.org http://www.openremote.org/schemas/controller.xsd\">\n";
+
+  /** The Constant XML_STATUS_RESULT_ELEMENT_NAME composed xml-formatted status results. */
+  public static final String STATUS_XML_STATUS_RESULT_ELEMENT_NAME = "status";
+
+  public static final String SENSORS_ELEMENT_NAME = "sensors";
+
+  public static final String INCLUDE_ELEMENT_NAME = "include";
+
+  public static final String SENSOR_TYPE_ATTRIBUTE_NAME = "type";
+
+  public static final String ID_ATTRIBUTE_NAME = "id";
+
+  public static final String REF_ATTRIBUTE_NAME = "ref";
+
+  /** The Constant XML_STATUS_RESULT_ELEMENT_SENSOR_IDENTITY composed xml-formatted status results. */
+  public static final String STATUS_XML_STATUS_RESULT_ELEMENT_SENSOR_IDENTITY = "id";
+
+  /** The Constant XML_TAIL of composed xml-formatted status results. */
+  public static final String STATUS_XML_TAIL = "</openremote>";
+
+  public static final String HTTP_BASIC_AUTH_HEADER_NAME = "Authorization";
+
+  public static final String HTTP_BASIC_AUTH_HEADER_VALUE_PREFIX = "Basic ";
+
+  public static final String CALLBACK_PARAM_NAME = "callback";
+
+  public static final String CHARACTER_ENCODING_UTF8 = "utf-8";
+
+  public static final String HTTP_ACCEPT_HEADER_NAME = "accept";
 
   /**
    * MIME type string for 'application/xml' content.
    */
-   public static final String MIME_APPLICATION_XML = "application/xml";
-   
-   public static final String HTTP_HEADER_ACCEPT_JSON_TYPE = "application/json";
+  public static final String MIME_APPLICATION_XML = "application/xml";
 
-   public static final int RESPONSE_SUCCESS = 200;
+  public static final String HTTP_HEADER_ACCEPT_JSON_TYPE = "application/json";
+
+  public static final int RESPONSE_SUCCESS = 200;
 
 }
