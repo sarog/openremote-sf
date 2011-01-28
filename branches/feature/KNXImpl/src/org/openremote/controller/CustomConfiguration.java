636918cffa3a6e6863d6b6bd5b56c29c680dedde
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-01-28 11:50:34 +0000

    formatting

--- branches/feature/KNXImpl/src/org/openremote/controller/CustomConfiguration.java
+++ branches/feature/KNXImpl/src/org/openremote/controller/CustomConfiguration.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/* 
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller;
 
 import java.util.HashMap;
@@ -32,12 +33,13 @@ import org.openremote.controller.command.RemoteActionXMLParser;
  * 
  * @author Dan Cong
  */
-public class CustomConfiguration {
+public class CustomConfiguration
+{
 
 
    
-   /** custom attributes map, will use these values first, other than default values from config.properties */
-   private Map<String, String> customAttrMap = new HashMap<String, String>();
+  /** custom attributes map, will use these values first, other than default values from config.properties */
+  private Map<String, String> customAttrMap = new HashMap<String, String>();
 
 
 
@@ -61,36 +63,45 @@ public class CustomConfiguration {
 
 
 
-   public Map<String, String> getCustomAttrMap() {
-      return customAttrMap;
-   }
+  public Map<String, String> getCustomAttrMap()
+  {
+    return customAttrMap;
+  }
 
-   public void setCustomAttrMap(Map<String, String> customAttrMap) {
-      if (customAttrMap == null) {
-         return;
-      }
-      this.customAttrMap = customAttrMap;
-   }
+  public void setCustomAttrMap(Map<String, String> customAttrMap)
+  {
+    if (customAttrMap == null)
+    {
+       return;
+    }
 
-   protected String preferAttrCustomValue(String attrName, String defaultValue) {
-      return customAttrMap.containsKey(attrName) ? customAttrMap.get(attrName) : defaultValue;
-   }
+    this.customAttrMap = customAttrMap;
+  }
 
-   protected boolean preferAttrCustomValue(String attrName, boolean defaultValue) {
-      return customAttrMap.containsKey(attrName) ? Boolean.valueOf(customAttrMap.get(attrName)) : defaultValue;
-   }
+  protected String preferAttrCustomValue(String attrName, String defaultValue)
+  {
+    return customAttrMap.containsKey(attrName) ? customAttrMap.get(attrName) : defaultValue;
+  }
 
-   protected int preferAttrCustomValue(String attrName, int defaultValue) {
-      return customAttrMap.containsKey(attrName) ? Integer.valueOf(customAttrMap.get(attrName)) : defaultValue;
-   }
+  protected boolean preferAttrCustomValue(String attrName, boolean defaultValue)
+  {
+    return customAttrMap.containsKey(attrName) ? Boolean.valueOf(customAttrMap.get(attrName)) : defaultValue;
+  }
 
-   protected long preferAttrCustomValue(String attrName, long defaultValue) {
-      return customAttrMap.containsKey(attrName) ? Long.valueOf(customAttrMap.get(attrName)) : defaultValue;
-   }
-   
-   protected String[] preferAttrCustomValue(String attrName, String[] defaultValue) {
-      return customAttrMap.containsKey(attrName) ? customAttrMap.get(attrName).split(",") : defaultValue;
-   }
+  protected int preferAttrCustomValue(String attrName, int defaultValue)
+  {
+    return customAttrMap.containsKey(attrName) ? Integer.valueOf(customAttrMap.get(attrName)) : defaultValue;
+  }
+
+  protected long preferAttrCustomValue(String attrName, long defaultValue)
+  {
+    return customAttrMap.containsKey(attrName) ? Long.valueOf(customAttrMap.get(attrName)) : defaultValue;
+  }
+
+  protected String[] preferAttrCustomValue(String attrName, String[] defaultValue)
+  {
+    return customAttrMap.containsKey(attrName) ? customAttrMap.get(attrName).split(",") : defaultValue;
+  }
 
 
 
