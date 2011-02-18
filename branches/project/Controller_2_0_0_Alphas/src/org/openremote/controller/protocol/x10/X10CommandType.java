69ea9b07d9d35d4c218de9d7334de09501bf8d72
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-02-18 18:52:35 +0000

    add patch to X10 for DIM/BRIGHT and ALL LIGHTS ON commands from Fekete Kamosh

--- branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/protocol/x10/X10CommandType.java
+++ branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/protocol/x10/X10CommandType.java
@@ -23,10 +23,11 @@ package org.openremote.controller.protocol.x10;
 /**
  * Enumeration of supported X10 commands that can be sent over the power line.
  * 
- * Right now supports ON, OFF and ALL UNITS OFF.
+ * Right now supports ON, OFF and ALL UNITS OFF, ALL LIGHTS ON, BRIGHT, DIM.
  *
  * @author Jerome Velociter
  * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
+ * @author Fekete Kamosh
  */
 public enum X10CommandType
 {
@@ -34,32 +35,60 @@ public enum X10CommandType
   /**
    * Switches on the target unit
    */
-  SWITCH_ON(new String[] { "ON" }),
+  SWITCH_ON(new String[] { "ON" }, true),
 
   /**
    * Switches off the target unit
    */
-  SWITCH_OFF(new String[] { "OFF" }),
+  SWITCH_OFF(new String[] { "OFF" }, true),
 
   /**
    * Switches off all units within the house code
    */
   ALL_UNITS_OFF(new String[] { "ALL_OFF", "ALL_UNITS_OFF",
-                               "ALL OFF", "ALL UNITS OFF" });
+                               "ALL OFF", "ALL UNITS OFF" },
+                               false),
+  
+  /**
+   * Switches on all light units within the house code
+   */
+  ALL_LIGHTS_ON(new String[] { "ALL_LIGHTS_ON", "ALL LIGHTS ON",
+                               "LIGHTS ON"},
+                               false),
+  
+  /**
+   * Dims the light unit  
+   */
+  DIM(new String[] {"DIM"}, true),
+  
+  /**
+   * Brights the light unit 
+   */
+  BRIGHT(new String[] {"BRIGHT"}, true);
 
 
   // Enum Fields ----------------------------------------------------------------------------------
 
   private String[] commandTranslations = null;
+  
+  private boolean requiresDeviceCode = true;
 
 
   // Enum Constructors ----------------------------------------------------------------------------
 
-  private X10CommandType(String[] commandTranslations)
+  private X10CommandType(String[] commandTranslations, boolean requiresDeviceCode)
   {
     this.commandTranslations = commandTranslations;
+    this.requiresDeviceCode = requiresDeviceCode;
   }
 
+
+  /**
+   * Does this command type require also device code along with house code? 
+   */  
+  public boolean requiresDeviceCode() {
+	  return requiresDeviceCode;
+  }
 
   // Enum Methods ---------------------------------------------------------------------------------
 
