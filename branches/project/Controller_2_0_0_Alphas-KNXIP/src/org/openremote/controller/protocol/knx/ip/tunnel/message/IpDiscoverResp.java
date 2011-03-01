c120bdae0df8b8c2cc57ebbe95ef473d96f25756
ogandit <ogandit@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-03-01 22:12:15 +0000

    KNX-IP layer enhancements

--- branches/project/Controller_2_0_0_Alphas-KNXIP/src/org/openremote/controller/protocol/knx/ip/tunnel/message/IpDiscoverResp.java
+++ branches/project/Controller_2_0_0_Alphas-KNXIP/src/org/openremote/controller/protocol/knx/ip/tunnel/message/IpDiscoverResp.java
@@ -3,11 +3,11 @@ package org.openremote.controller.protocol.knx.ip.tunnel.message;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class IpDiscoverAck extends IpMessage {
+public class IpDiscoverResp extends IpMessage {
   public static final int STI = 0x202;
   private Hpai            controlEndpoint;
 
-  public IpDiscoverAck(InputStream is, int vl) throws IOException {
+  public IpDiscoverResp(InputStream is, int vl) throws IOException {
     super(STI, vl);
     this.controlEndpoint = new Hpai(is);
 
@@ -22,7 +22,7 @@ public class IpDiscoverAck extends IpMessage {
 
   @Override
   public Primitive getPrimitive() {
-    return Primitive.ACK;
+    return Primitive.RESP;
   }
 
   public Hpai getControlEndpoint() {
