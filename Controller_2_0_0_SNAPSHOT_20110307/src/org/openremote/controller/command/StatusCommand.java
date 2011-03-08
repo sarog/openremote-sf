8a1e0c6e58bd317aa54d47d3553535149c9b2c2d
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-03-08 20:44:28 +0000

    tag the controller snapshot with 1-wire support

---
+++ Controller_2_0_0_SNAPSHOT_20110307/src/org/openremote/controller/command/StatusCommand.java
@@ -0,0 +1,50 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2011, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.controller.command;
+
+import java.util.Map;
+
+import org.openremote.controller.component.EnumSensorType;
+
+
+/**
+ * Interface StatusCommand.
+ * 
+ * @author Handy.Wang 2009-10-15
+ */
+public interface StatusCommand extends Command {
+   
+   public final static String UNKNOWN_STATUS = "N/A";
+
+   /**
+    * Read raw status from device and return parsed status.<br />
+    * And you also can translate the raw status to readable string according to sensoryType and state map.
+    * 
+    * @param sensorType
+    *           sensor type
+    * @param stateMap
+    *           state map, key is state name, value is the returned raw string related to the state. 
+    *           NOTE: if sensor type is RANGE, this map only contains min/max states.
+    * @return parsed status string
+    */
+   public String read(EnumSensorType sensorType, Map<String, String> stateMap);
+   
+    
+}
