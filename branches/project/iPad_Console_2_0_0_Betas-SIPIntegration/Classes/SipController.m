46ce18f0aba7f9f853b9b209a0b455a628400db7
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-03-09 13:37:59 +0000

    Added pjsip / Siphon code and integration into OR code

---
+++ branches/project/iPad_Console_2_0_0_Betas-SIPIntegration/Classes/SipController.m
@@ -0,0 +1,164 @@
+//
+//  SipController.m
+//  openremote
+//
+//  Created by Eric Bariaux on 03/03/11.
+//  Copyright 2011 OpenRemote, Inc. All rights reserved.
+//
+
+#import "SipController.h"
+#import "AppDelegate.h"
+
+@implementation SipController
+
+- (id)init {
+	if (self = [super init]) {
+		[[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:
+																 @"siphon", @"username",
+																 @"siphon", @"authname",
+																 @"192.168.11.104", @"server",
+																 [NSNumber numberWithInt:30], @"regTimeout",
+																 [NSNumber numberWithBool:TRUE], @"enableGSM",
+																 [NSNumber numberWithBool:TRUE], @"enableG711a",
+																 [NSNumber numberWithBool:TRUE], @"enableG722",
+																 nil]];
+		// Start as not connected
+		_sip_acc_id = PJSUA_INVALID_ID;
+
+	}
+	return self;
+}
+
+- (void)processCallState:(NSNotification *)notification {
+	NSLog(@"processCallState: %@", notification);
+	
+	int state = [[[ notification userInfo ] objectForKey: @"State"] intValue];
+	
+	switch(state)
+	{
+		
+		case PJSIP_INV_STATE_INCOMING:
+			[((AppDelegate *)[[UIApplication sharedApplication] delegate]).localContext setObject:@"ring" forKey:@"RING"];
+			[((AppDelegate *)[[UIApplication sharedApplication] delegate]).localContext setObject:[[notification userInfo] objectForKey:@"CallID"] forKey:@"CallID"];
+			break;
+		case PJSIP_INV_STATE_DISCONNECTED:
+			[((AppDelegate *)[[UIApplication sharedApplication] delegate]).localContext setObject:@"" forKey:@"RING"];
+			break;
+	}
+}
+
+- (void)processRegState:(NSNotification *)notification {
+	NSLog(@"processRegState: %@", notification);
+}
+
+/* */
+- (BOOL)sipStartup
+{
+	if (_app_config.pool)
+		return YES;
+	
+//	self.networkActivityIndicatorVisible = YES;
+	
+	if (sip_startup(&_app_config) != PJ_SUCCESS)
+	{
+//		self.networkActivityIndicatorVisible = NO;
+		return NO;
+	}
+//	self.networkActivityIndicatorVisible = NO;
+	
+	/** Call management **/
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(processCallState:)
+												 name: kSIPCallState object:nil];
+	
+	/** Registration management */
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(processRegState:)
+												 name: kSIPRegState object:nil];
+	
+	return YES;
+}
+
+/* */
+- (BOOL)sipDisconnect
+{
+	if ((_sip_acc_id != PJSUA_INVALID_ID) &&
+		(sip_disconnect(&_sip_acc_id) != PJ_SUCCESS))
+	{
+		return FALSE;
+	}
+	
+	_sip_acc_id = PJSUA_INVALID_ID;
+	
+	//	isConnected = FALSE;
+	
+	return TRUE;
+}
+
+
+/* */
+- (void)sipCleanup
+{
+	//[[NSNotificationCenter defaultCenter] removeObserver:self];
+	[[NSNotificationCenter defaultCenter] removeObserver:self
+													name: kSIPRegState
+												  object:nil];
+	[[NSNotificationCenter defaultCenter] removeObserver:self 
+													name:kSIPCallState 
+												  object:nil];
+	[self sipDisconnect];
+	
+	if (_app_config.pool != NULL)
+	{
+		sip_cleanup(&_app_config);
+	}
+}
+
+/* */
+- (BOOL)sipConnect
+{
+	pj_status_t status;
+	
+	if (![self sipStartup])
+		return FALSE;
+	
+//	if ([self wakeUpNetwork] == NO)
+//		return NO;
+	
+	if (_sip_acc_id == PJSUA_INVALID_ID)
+	{
+//		self.networkActivityIndicatorVisible = YES;
+		if ((status = sip_connect(_app_config.pool, &_sip_acc_id)) != PJ_SUCCESS)
+		{
+//			self.networkActivityIndicatorVisible = NO;
+			return FALSE;
+		}
+	}
+	
+	return TRUE;
+}
+
+- (app_config_t *)pjsipConfig
+{
+	return &_app_config;
+}
+
+
++ (NSString *)getRingSensorValue:(NSMutableDictionary *)context {
+	NSString *retValue = [context valueForKey:@"RING"];
+	return (retValue)?retValue:@"";
+}
+
++ (void)answerCall:(NSMutableDictionary *)context {
+	NSLog(@"Answer call");
+	pjsua_call_id call_id = [[context objectForKey:@"CallID"] intValue];
+	sip_answer(&call_id);
+}
+
++ (void)hangupCall:(NSMutableDictionary *)context {
+	pjsua_call_id call_id = [[context objectForKey:@"CallID"] intValue];
+	sip_hangup(&call_id);
+	[context removeObjectForKey:@"CallID"];
+}
+
+@end
