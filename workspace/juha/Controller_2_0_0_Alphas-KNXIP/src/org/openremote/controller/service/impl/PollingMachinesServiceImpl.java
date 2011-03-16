5febed021114c5329fc28ae822147dbf5804768c
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-03-16 21:17:38 +0000

    fix an issue where an incorrect sensor configuration in controller.xml could prevent the entire controller from starting... also handle thread interruptions correctly

--- workspace/juha/Controller_2_0_0_Alphas-KNXIP/src/org/openremote/controller/service/impl/PollingMachinesServiceImpl.java
+++ workspace/juha/Controller_2_0_0_Alphas-KNXIP/src/org/openremote/controller/service/impl/PollingMachinesServiceImpl.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.service.impl;
 
 import java.io.File;
@@ -24,85 +25,149 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.log4j.Logger;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.openremote.controller.Constants;
 import org.openremote.controller.ControllerConfiguration;
 import org.openremote.controller.command.RemoteActionXMLParser;
+import org.openremote.controller.command.StatusCommand;
 import org.openremote.controller.component.Sensor;
 import org.openremote.controller.component.SensorBuilder;
 import org.openremote.controller.config.ControllerXMLListenSharingData;
 import org.openremote.controller.exception.ControllerXMLNotFoundException;
+import org.openremote.controller.exception.NoSuchCommandException;
 import org.openremote.controller.service.PollingMachinesService;
 import org.openremote.controller.service.StatusCacheService;
 import org.openremote.controller.statuscache.PollingMachineThread;
 import org.openremote.controller.utils.PathUtil;
+import org.openremote.controller.utils.Logger;
 
 /**
- * Polling status from devices by sensor status command. 
+ * TODO : Polling status from devices by sensor status command.
  * 
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  * @author Handy.Wang 2010-03-17
  *
  */
 public class PollingMachinesServiceImpl implements PollingMachinesService {
-   
-   private static Logger log = Logger.getLogger(PollingMachinesServiceImpl.class);
-   private StatusCacheService statusCacheService;
-   private RemoteActionXMLParser remoteActionXMLParser;
-   private ControllerXMLListenSharingData controllerXMLListenSharingData;
 
-   private Logger logger = Logger.getLogger("INIT");
-   private SensorBuilder sensorBuilder;
-   
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public void initStatusCacheWithControllerXML(Document document) {
-      List<Element> sensorElements = null;
-      try {
-         if (document == null) {
-            sensorElements = remoteActionXMLParser.queryElementsFromXMLByName("sensor");
-         } else {
-            sensorElements = remoteActionXMLParser.queryElementsFromXMLByName(document, "sensor");
-         }
-      } catch (ControllerXMLNotFoundException e) {
-         log.warn("No sensor to init, controller.xml not found.");
-         return;
-      }
-      
-      if (sensorElements != null)
+
+  // Class Members --------------------------------------------------------------------------------
+
+  /**
+   * Common log category for startup related events.
+   */
+  private final static Logger log = Logger.getLogger(Constants.INIT_LOG_CATEGORY);
+
+
+  // Private Instance Fields ----------------------------------------------------------------------
+
+  private StatusCacheService statusCacheService;
+  private RemoteActionXMLParser remoteActionXMLParser;
+  private ControllerXMLListenSharingData controllerXMLListenSharingData;
+  private SensorBuilder sensorBuilder;
+
+
+
+  // Implements PollingMachinesService ------------------------------------------------------------
+
+  @Override public void initStatusCacheWithControllerXML(Document document)
+  {
+    List<Element> sensorElements = null;
+
+    try
+    {
+      if (document == null)
       {
-         for (Element sensorElement : sensorElements)
-         {
-            Sensor sensor = sensorBuilder.build(document, sensorElement);
-
-            controllerXMLListenSharingData.addSensor(sensor);
-
-            statusCacheService.saveOrUpdateStatus(sensor.getSensorID(), "N/A");
-         }
+        sensorElements = remoteActionXMLParser.queryElementsFromXMLByName("sensor");
       }
-      
-   }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public void startPollingMachineMultiThread() {
-      for (Sensor sensor : controllerXMLListenSharingData.getSensors()) {
-         PollingMachineThread pollingMachineThread = new PollingMachineThread(sensor, statusCacheService);
-         pollingMachineThread.start();
-         nap(3);
-         controllerXMLListenSharingData.addPollingMachineThread(pollingMachineThread);
+      else
+      {
+        sensorElements = remoteActionXMLParser.queryElementsFromXMLByName(document, "sensor");
       }
-      
-      storeXMLContent(Constants.CONTROLLER_XML);
-      storeXMLContent(Constants.PANEL_XML);
-   }
-   
-   private void storeXMLContent(String xmlFileName) {
+    }
+    catch (ControllerXMLNotFoundException e)
+    {
+      log.info(
+         "\n\n" +
+         "********************************************************************************\n" +
+         "\n" +
+         " File controller.xml was not found in this OpenRemote Controller instance.      \n" +
+         "\n" +
+         " If you are starting the controller for the first time, please use your web     \n" +
+         " browser to connect to the controller home page and synchronize it with your    \n" +
+         " online account. \n" +
+         "\n" +
+         "********************************************************************************\n\n"
+      );
+
+      log.debug(e.getMessage(), e);
+
+      // No controller.xml meeans no sensors to init, so just return back...
+
+      return;
+    }
+
+    if (sensorElements != null)
+    {
+      for (Element sensorElement : sensorElements)
+      {
+        try
+        {
+          Sensor sensor = sensorBuilder.build(document, sensorElement);
+
+          controllerXMLListenSharingData.addSensor(sensor);
+
+          statusCacheService.saveOrUpdateStatus(sensor.getSensorID(), StatusCommand.UNKNOWN_STATUS);
+        }
+        catch (NoSuchCommandException e)
+        {
+          // TODO : should be a checked exception so this doesn't accidentally propagate higher
+
+          log.warn(
+              "Unable to initialize sensor at startup due to configuration error: " +
+              e.getMessage(), e
+          );
+        }
+        catch (Throwable t)
+        {
+          // Catch all -- keep trying subsequent sensors...
+
+          log.warn("Failed to initialize a sensor: " + t.getMessage(), t);
+        }
+      }
+    }
+  }
+
+
+  @Override public void startPollingMachineMultiThread() throws InterruptedException
+  {
+    for (Sensor sensor : controllerXMLListenSharingData.getSensors())
+    {
+      if (sensor.isPolling())
+      {
+        PollingMachineThread pollingMachineThread = new PollingMachineThread(sensor, statusCacheService);
+        pollingMachineThread.start();
+
+        Thread.sleep(3);    // TODO : the nap makes no sense -- is it because thread synchronization has not been implemented ? [JPL]
+
+        controllerXMLListenSharingData.addPollingMachineThread(pollingMachineThread);
+      }
+
+      if (sensor.isEventListener())
+      {
+        sensor.initListener();
+      }
+    }
+
+    storeXMLContent(Constants.CONTROLLER_XML);
+    storeXMLContent(Constants.PANEL_XML);
+  }
+
+
+   private void storeXMLContent(String xmlFileName)
+   {
       String xmlFilePath = PathUtil.addSlashSuffix(ControllerConfiguration.readXML().getResourcePath()) + xmlFileName;
       File xmlFile = new File(xmlFilePath);
       try {
@@ -113,18 +178,18 @@ public class PollingMachinesServiceImpl implements PollingMachinesService {
             controllerXMLListenSharingData.setPanelXMLFileContent(fileContent);
          }
       } catch (IOException ioe) {
-         logger.warn("Skipped " + xmlFileName + " change check, Failed to read " + xmlFile.getAbsolutePath());
+         log.info("Skipped " + xmlFileName + " change check, failed to read " + xmlFile.getAbsolutePath());
       }
    }
    
-   /** Sleep for several seconds */
-   private void nap(long sec) {
-      try {
-         Thread.sleep(sec);
-      } catch (InterruptedException e) {
-         logger.error("Failed to sleep");
-      }
-   }
+//   /** Sleep for several seconds */
+//   private void nap(long sec) {
+//      try {
+//         Thread.sleep(sec);
+//      } catch (InterruptedException e) {
+//         Thread.currentThread().interrupt();
+//      }
+//   }
 
    public void setStatusCacheService(StatusCacheService statusCacheService) {
       this.statusCacheService = statusCacheService;
