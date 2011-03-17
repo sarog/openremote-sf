7dd01971c21112c7da93327cb3a84e7d8e3c0962
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-03-17 19:01:24 +0000

    formatting

--- workspace/juha/Controller_2_0_0_Alphas-KNXIP/src/org/openremote/controller/service/impl/ControlCommandServiceImpl.java
+++ workspace/juha/Controller_2_0_0_Alphas-KNXIP/src/org/openremote/controller/service/impl/ControlCommandServiceImpl.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.service.impl;
 
 import java.util.List;
@@ -33,48 +34,61 @@ import org.openremote.controller.utils.MacrosIrDelayUtil;
 
 
 /**
- * The implementation for ControlCommandService class.
+ * TODO
  * 
  * @author Handy.Wang
  */
-public class ControlCommandServiceImpl implements ControlCommandService {
+public class ControlCommandServiceImpl implements ControlCommandService
+{
 
-   private RemoteActionXMLParser remoteActionXMLParser;
-   
-   private ComponentFactory componentFactory;
-   
-   /**
-    * {@inheritDoc}
-    */
-   public void trigger(String controlID, String commandParam) {
-      
-      Control control = getControl(controlID, commandParam);
-      List<ExecutableCommand> executableCommands = control.getExecutableCommands();
-      MacrosIrDelayUtil.ensureDelayForIrCommand(executableCommands);
-      for (ExecutableCommand executableCommand : executableCommands) {
-         if (executableCommand != null) {
-            executableCommand.send();
-         } else {
-            throw new NoSuchCommandException("ExecutableCommand is null");
-         }
-      }
-      
-   }
-   
-   private Control getControl(String controlID, String commandParam) {
-      Element controlElement = remoteActionXMLParser.queryElementFromXMLById(controlID);
-      if (controlElement == null) {
-         throw new NoSuchComponentException("No such component id :" + controlID);
-      }
-      return (Control) componentFactory.getComponent(controlElement, commandParam);
-   }
-   
-   public void setRemoteActionXMLParser(RemoteActionXMLParser remoteActionXMLParser) {
-      this.remoteActionXMLParser = remoteActionXMLParser;
-   }
+  private RemoteActionXMLParser remoteActionXMLParser;
+  private ComponentFactory componentFactory;
 
-   public void setComponentFactory(ComponentFactory componentFactory) {
-      this.componentFactory = componentFactory;
-   }
-   
+  
+  public void trigger(String controlID, String commandParam)
+  {
+    Control control = getControl(controlID, commandParam);
+    List<ExecutableCommand> executableCommands = control.getExecutableCommands();
+    MacrosIrDelayUtil.ensureDelayForIrCommand(executableCommands);
+
+    for (ExecutableCommand executableCommand : executableCommands)
+    {
+      if (executableCommand != null)
+      {
+        executableCommand.send();
+      }
+
+      else
+      {
+        throw new NoSuchCommandException("ExecutableCommand is null");
+      }
+    }
+  }
+
+
+
+  public void setRemoteActionXMLParser(RemoteActionXMLParser remoteActionXMLParser)
+  {
+    this.remoteActionXMLParser = remoteActionXMLParser;
+  }
+
+  public void setComponentFactory(ComponentFactory componentFactory)
+  {
+    this.componentFactory = componentFactory;
+  }
+
+
+
+  private Control getControl(String controlID, String commandParam)
+  {
+    Element controlElement = remoteActionXMLParser.queryElementFromXMLById(controlID);
+
+    if (controlElement == null)
+    {
+       throw new NoSuchComponentException("No such component id :" + controlID);
+    }
+
+    return (Control) componentFactory.getComponent(controlElement, commandParam);
+  }
+
 }
