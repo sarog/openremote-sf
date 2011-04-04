461019cd44a18bae2962c175573061d43a6b3ffb
kurrazyman <kurrazyman@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-04-04 17:20:17 +0000

    Move protocol related code into gateway protocol package

--- branches/feature/Controller_2_0_0_Alpha11_Manager/src/org/openremote/controller/gateway/Protocol.java
+++ branches/feature/Controller_2_0_0_Alpha11_Manager/src/org/openremote/controller/gateway/Protocol.java
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.openremote.controller.gateway;
+package org.openremote.controller.gateway.protocol;
 
 import java.io.InputStream;
 import java.io.OutputStream;
--- branches/feature/Controller_2_0_0_Alpha11_Manager/src/org/openremote/controller/gateway/ProtocolFactory.java
+++ branches/feature/Controller_2_0_0_Alpha11_Manager/src/org/openremote/controller/gateway/ProtocolFactory.java
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.openremote.controller.gateway;
+package org.openremote.controller.gateway.protocol;
 
 import java.util.Properties;
 import org.openremote.controller.gateway.exception.GatewayException;
