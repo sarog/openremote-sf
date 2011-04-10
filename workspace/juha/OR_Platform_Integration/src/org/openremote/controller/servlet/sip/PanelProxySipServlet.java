8ad9f6fa3cd2e65dd7a7455d88f3fe17c10a7139
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-04-10 19:54:57 +0000

    merge /workspace/ebariaux/Controller_2_0_0_Alphas_SIPIntegration

---
+++ workspace/juha/OR_Platform_Integration/src/org/openremote/controller/servlet/sip/PanelProxySipServlet.java
@@ -0,0 +1,169 @@
+package org.openremote.controller.servlet.sip;
+
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2011, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.sip.Address;
+import javax.servlet.sip.Proxy;
+import javax.servlet.sip.SipFactory;
+import javax.servlet.sip.SipServlet;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipServletResponse;
+import javax.servlet.sip.SipURI;
+import javax.servlet.sip.URI;
+
+import org.apache.log4j.Logger;
+
+@javax.servlet.sip.annotation.SipServlet (applicationName="org.openremote.controller.servlet.sip.PanelProxyServiceApplication")
+public class PanelProxySipServlet extends SipServlet {
+    private static final long serialVersionUID = 1L;
+
+    private static Logger logger = Logger.getLogger(PanelProxySipServlet.class);
+    private static final String CONTACT_HEADER = "Contact";
+    Map<String, List<URI>> registeredUsers = null;
+    
+    public PanelProxySipServlet() {}
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+      if(logger.isInfoEnabled()) {
+        logger.info("the panel proxy sip servlet has been started");
+      }
+      super.init(servletConfig);
+    }
+
+    @Override
+    protected void doInvite(SipServletRequest request) throws ServletException,
+        IOException {
+      if(logger.isInfoEnabled()) {
+        logger.info("Got request:\n" + request.toString());   
+      }
+      SipFactory sipFactory = (SipFactory)getServletContext().getAttribute(SIP_FACTORY);
+      List<URI> contactAddresses  = new ArrayList<URI>();
+      HashMap<String, String> users = (HashMap<String, String>) getServletContext().getAttribute("panelUsersMap");
+          logger.info("Registered panel users : " + users);
+      for (String user : users.values()) {
+        contactAddresses.add(sipFactory.createURI(user));
+      }
+          
+      if(contactAddresses != null && contactAddresses.size() > 0) {     
+        Proxy proxy = request.getProxy();
+        proxy.setRecordRoute(false); // was true
+        proxy.setParallel(true);
+        proxy.setSupervised(false); // was false
+        proxy.proxyTo(contactAddresses);    
+      } else {
+        if(logger.isInfoEnabled()) {
+          logger.info(request.getRequestURI().toString() + " is not currently registered");
+        }
+        SipServletResponse sipServletResponse = 
+          request.createResponse(SipServletResponse.SC_MOVED_PERMANENTLY, "Moved Permanently");
+        sipServletResponse.send();
+      }
+    }
+    
+    @Override
+    protected void doRegister(SipServletRequest req) throws ServletException,
+        IOException {
+      if(logger.isInfoEnabled()) {
+        logger.info("Received register request: " + req.getTo());
+      }
+      
+      boolean isPanelUser = false;
+      if (req.getFrom().getURI().isSipURI()) {
+        logger.info("User " + ((SipURI)req.getFrom().getURI()).getUser());
+        if ("siphon".equals(((SipURI)req.getFrom().getURI()).getUser())) {
+          isPanelUser = true;
+        }
+      }
+      
+      int response = SipServletResponse.SC_OK;
+      SipServletResponse resp = req.createResponse(response);
+      HashMap<String, String> users;
+      if (isPanelUser) {
+        users= (HashMap<String, String>) getServletContext().getAttribute("panelUsersMap");
+        if (users == null) {
+          users = new HashMap<String, String>();
+          getServletContext().setAttribute("panelUsersMap", users);
+        }
+      } else {
+        users= (HashMap<String, String>) getServletContext().getAttribute("registeredUsersMap");
+        if (users == null) {
+          users = new HashMap<String, String>();
+          getServletContext().setAttribute("registeredUsersMap", users);
+        }
+      }
+      
+      Address address = req.getAddressHeader(CONTACT_HEADER);
+      String fromURI = req.getFrom().getURI().toString();
+      
+      int expires = address.getExpires();
+      if(expires < 0) {
+        expires = req.getExpires();
+      }
+      if(expires == 0) {
+        users.remove(fromURI);
+        if(logger.isInfoEnabled()) {
+          logger.info("User " + fromURI + " unregistered");
+        }
+      } else {
+        resp.setAddressHeader(CONTACT_HEADER, address);
+        users.put(fromURI, address.getURI().toString());
+        if(logger.isInfoEnabled()) {
+          logger.info("User " + fromURI + 
+            " registered with an Expire time of " + expires);
+        }
+      }
+      resp.send();
+    }
+
+    @Override
+    protected void doSubscribe(SipServletRequest req) throws ServletException, IOException {
+      SipServletResponse resp = req.createResponse(SipServletResponse.SC_NOT_IMPLEMENTED);
+      resp.send();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected void doResponse(SipServletResponse response)
+        throws ServletException, IOException {
+      if(logger.isInfoEnabled()) {
+        logger.info("SimpleProxyServlet: Got response:\n" + response);
+      }
+      // session should not be invalidated so fast
+//      if(SipServletResponse.SC_OK == response.getStatus() && "BYE".equalsIgnoreCase(response.getMethod())) {
+//        SipSession sipSession = response.getSession(false);
+//        if(sipSession != null) {
+//          SipApplicationSession sipApplicationSession = sipSession.getApplicationSession();
+//          sipSession.invalidate();
+//          sipApplicationSession.invalidate();
+//        }     
+//      }
+    }
+    
+  }
