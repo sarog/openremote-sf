5bd16155982dd4bf4caf6265e2bfe12852c49f70
cortextual <cortextual@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-04-13 21:04:23 +0000

    merge changeset 3890 from workspace/andrewball/Android_Console_2_0_0_Betas

--- branches/feature/Android_Update_Image/src/org/openremote/android/console/net/IPAutoDiscoveryServer.java
+++ branches/feature/Android_Update_Image/src/org/openremote/android/console/net/IPAutoDiscoveryServer.java
@@ -36,61 +36,61 @@ import android.util.Log;
 
 /**
  * Controller IP auto discovery server, this is a TCP server receiving IP from Controllers.
- * 
+ *
  * @author Tomsky Wang
  *
  */
 public class IPAutoDiscoveryServer extends AsyncTask<Void, Void, List<String>> {
 
-   /** Interrupted the current discovery. */
-   public static boolean isInterrupted;
-   
-   @Override
-   protected List<String> doInBackground(Void... params) {
-      publishProgress((Void)null);
-      ArrayList<String> autoServers = new ArrayList<String>();
-      boolean moreQuotes = true;
-      ServerSocket srvr = null;
-      try {
-         srvr = new ServerSocket(Constants.LOCAL_SERVER_PORT);
-         new IPAutoDiscoveryClient().run();
-         autoServers.clear();
-         srvr.setSoTimeout(1000);
-      } catch (BindException e) {
-         Log.e("OpenRemote-AUTO DISCOVER", "auto discovery server setup failed, the address is already in use");
-         autoServers.clear();
-         return autoServers;
-      } catch (IOException e) {
-         Log.e("OpenRemote-AUTO DISCOVER", "auto discovery server setup failed", e);
-         autoServers.clear();
-         return autoServers;
-      }
-      while (moreQuotes && !isInterrupted) {
-         try {
-            Socket connectionSocket = srvr.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            String line = inFromClient.readLine();
-            if (line != null && !"".equals(line)) {
-               autoServers.add(line);
-            }
-            connectionSocket.close();
-            Log.i("OpenRemote-AUTO DISCOVER", "auto discovery result: " + autoServers);
-            Thread.sleep(3);
-         } catch (SocketTimeoutException e) {
-            moreQuotes = false;
-         } catch (InterruptedException e) {
-            moreQuotes = false;
-         } catch (IOException e) {
-            moreQuotes = false;
-         }
-      }
-      try {
-         srvr.close();
-      } catch (IOException e) {
-         Log.e("OpenRemote-AUTO DISCOVER", "auto discovery ServerSocket close failed " , e);
-         return autoServers;
-      }
+  /** Interrupted the current discovery. */
+  public static boolean isInterrupted;
+
+  @Override
+  protected List<String> doInBackground(Void... params) {
+    publishProgress((Void) null);
+    ArrayList<String> autoServers = new ArrayList<String>();
+    boolean moreQuotes = true;
+    ServerSocket srvr = null;
+    try {
+      srvr = new ServerSocket(Constants.LOCAL_SERVER_PORT);
+      new IPAutoDiscoveryClient().run();
+      autoServers.clear();
+      srvr.setSoTimeout(1000);
+    } catch (BindException e) {
+      Log.e("OpenRemote-AUTO DISCOVER", "auto discovery server setup failed, the address is already in use");
+      autoServers.clear();
       return autoServers;
-   }
+    } catch (IOException e) {
+      Log.e("OpenRemote-AUTO DISCOVER", "auto discovery server setup failed", e);
+      autoServers.clear();
+      return autoServers;
+    }
+    while (moreQuotes && !isInterrupted) {
+      try {
+        Socket connectionSocket = srvr.accept();
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        String line = inFromClient.readLine();
+        if (line != null && !"".equals(line)) {
+          autoServers.add(line);
+        }
+        connectionSocket.close();
+        Log.i("OpenRemote-AUTO DISCOVER", "auto discovery result: " + autoServers);
+        Thread.sleep(3);
+      } catch (SocketTimeoutException e) {
+        moreQuotes = false;
+      } catch (InterruptedException e) {
+        moreQuotes = false;
+      } catch (IOException e) {
+        moreQuotes = false;
+      }
+    }
+    try {
+      srvr.close();
+    } catch (IOException e) {
+      Log.e("OpenRemote-AUTO DISCOVER", "auto discovery ServerSocket close failed " , e);
+      return autoServers;
+    }
+    return autoServers;
+  }
 
 }
