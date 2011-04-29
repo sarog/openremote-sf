a256e17c218d8610707f755185204e57ad62e23f
cortextual <cortextual@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-04-29 23:47:57 +0000

    Add class to represent "web" panel.xml tag, along with unit tests.

---
+++ workspace/andrewball/Android_Console_2_0_0_Betas_web_element/src/org/openremote/android/console/bindings/Web.java
@@ -0,0 +1,83 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.android.console.bindings;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+/**
+ * Represents a web element from a panel definition, which represents an area of the screen where a
+ * web page will be rendered.
+ */
+@SuppressWarnings("serial")
+public class Web extends Component
+{
+  /** the URL to view */
+  private URL src;
+  /** optional username to user for HTTP basic authentication */
+  private String username;
+  /** optional password to use for HTTP basic authentication */
+  private String password;
+
+  /**
+   * Constructs a new Web object from an XML Node
+   *
+   * @param node node representing the "web" element
+   */
+  public Web(Node node) throws MalformedURLException
+  {
+    NamedNodeMap attributes = node.getAttributes();
+    setComponentId(Integer.valueOf(attributes.getNamedItem(ID).getNodeValue()));
+    this.src = new URL(attributes.getNamedItem(SRC).getNodeValue());
+
+    Node usernameNode = attributes.getNamedItem(USERNAME);
+    if (usernameNode != null)
+    {
+      username = usernameNode.getNodeValue();
+    }
+
+    Node passwordNode = attributes.getNamedItem(PASSWORD);
+    if (passwordNode != null)
+    {
+      password = passwordNode.getNodeValue();
+    }
+  }
+
+  /** Returns the source URL */
+  public URL getSrc()
+  {
+    return src;
+  }
+
+  /** Returns the username specified for HTTP basic authentication (or null if none set) */
+  public String getUsername()
+  {
+    return username;
+  }
+
+  /** Returns the password specified for HTTP basic authentication (or null if none set) */
+  public String getPassword()
+  {
+    return password;
+  }
+}
