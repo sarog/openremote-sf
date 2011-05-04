16b9da2ed15e85b91f4eec87ace345fd2ff2213a
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-05-04 22:05:01 +0000

    Synchronizing this branch with release branch /branches/project/Controller_2_0_0_Alphas in preparation of foreign merge to external repository.
    Merge R4102: Separating formatting from code changes in R3696 of /branches/project/Controller_2_0_0_Alphas-KNXIP
    Merge R4103: Actual code changes of R3696 of /branches/project/Controller_2_0_0_Alphas-KNXIP
    Merge R4104: (record-only) of earlier merged R3697 of /branches/project/Controller_2_0_0_Alphas-KNXIP
    Merge R4105: (record-only) Separating formatting from code changes in R3698 of /branches/project/Controller_2_0_0_Alphas-KNXIP
    Merge R4106: actual code changes of R3698 of /branches/project/Controller_2_0_0_Alphas-KNXIP
    Merge R4107: Separating formatting from code changes in R3699 of /branches/project/Controller_2_0_0_Alphas-KNXIP
    Merge R4108: actual code changes of R3699 of /branches/project/Controller_2_0_0_Alphas-KNXIP
    Merge R4109: (record-only) or earlier merged R3909 of /branches/project/Controller_2_0_0_Alphas-KNXIP
    Merge R4110: Lutron improvements

--- workspace/juha/OR_Platform_Integration/src/org/openremote/controller/protocol/lutron/GrafikEyeCommand.java
+++ workspace/juha/OR_Platform_Integration/src/org/openremote/controller/protocol/lutron/GrafikEyeCommand.java
@@ -35,86 +35,96 @@ import org.openremote.controller.exception.NoSuchCommandException;
  */
 public class GrafikEyeCommand extends LutronHomeWorksCommand implements ExecutableCommand, StatusCommand {
 
-	// Class Members --------------------------------------------------------------------------------
+  // Class Members --------------------------------------------------------------------------------
 
-	/**
-	 * Lutron logger. Uses a common category for all Lutron related logging.
-	 */
-	private final static Logger log = Logger.getLogger(LutronHomeWorksCommandBuilder.LUTRON_LOG_CATEGORY);
+  /**
+   * Lutron logger. Uses a common category for all Lutron related logging.
+   */
+  private final static Logger log = Logger.getLogger(LutronHomeWorksCommandBuilder.LUTRON_LOG_CATEGORY);
 
-	public static LutronHomeWorksCommand createCommand(String name, LutronHomeWorksGateway gateway, LutronHomeWorksAddress address, Integer scene, Integer key, Integer level) {
-		
-		log.info("createCommand ("+ name + "," + gateway + "," + address + "," + scene + "," +key + "," + level + ")");
+  public static LutronHomeWorksCommand createCommand(String name, LutronHomeWorksGateway gateway, LutronHomeWorksAddress address, Integer scene, Integer key, Integer level) {
 
-		// Check for mandatory attributes
-		if (address == null) {
-		    throw new NoSuchCommandException("Address is required for any GRAFIK Eye command");
-		}
-		
-		if (!"STATUS_SCENE".equalsIgnoreCase(name) && scene == null) {
-		    throw new NoSuchCommandException("Scene is required for 'write' GRAFIK Eye command");
-		}
-		
+    log.info("createCommand (" + name + "," + gateway + "," + address + "," + scene + "," + key + "," + level + ")");
+
+    // Check for mandatory attributes
+    if (address == null) {
+      throw new NoSuchCommandException("Address is required for any GRAFIK Eye command");
+    }
+
+    if (!"STATUS_SCENE".equalsIgnoreCase(name) && scene == null) {
+      throw new NoSuchCommandException("Scene is required for 'write' GRAFIK Eye command");
+    }
+
     if (!address.isValidGrafikEyeAddress()) {
       throw new NoSuchCommandException("Address must be one of a GRAFIK Eye");
     }
 
-		return new GrafikEyeCommand(name, gateway, address, scene);
-	}
+    return new GrafikEyeCommand(name, gateway, address, scene);
+  }
 
-	// Private Instance Fields
-	// ----------------------------------------------------------------------
+  // Private Instance Fields
+  // ----------------------------------------------------------------------
 
-	/**
-	 * Destination address for this command.
-	 */
-	private LutronHomeWorksAddress address;
-	
-	/**
-	 * Number of the scene this command must select.
-	 */
-	private Integer scene;
+  /**
+   * Destination address for this command.
+   */
+  private LutronHomeWorksAddress address;
 
-	// Constructors
-	// ---------------------------------------------------------------------------------
+  /**
+   * Number of the scene this command must select.
+   */
+  private Integer scene;
 
-	public GrafikEyeCommand(String name, LutronHomeWorksGateway gateway, LutronHomeWorksAddress address, Integer scene) {
-		super(name, gateway);
-		this.address = address;
-		this.scene = scene;
-	}
+  // Constructors
+  // ---------------------------------------------------------------------------------
 
-	  // Implements ExecutableCommand -----------------------------------------------------------------
+  public GrafikEyeCommand(String name, LutronHomeWorksGateway gateway, LutronHomeWorksAddress address, Integer scene) {
+    super(name, gateway);
+    this.address = address;
+    this.scene = scene;
+  }
 
-	  /**
-	   * {@inheritDoc}
-	   */
-	  public void send()
-	  {
-		  GrafikEye grafikEye = (GrafikEye) gateway.getHomeWorksDevice(address, GrafikEye.class);
-		  grafikEye.selectScene(scene);
-	  }
+  // Implements ExecutableCommand -----------------------------------------------------------------
 
-	@Override
-	public String read(EnumSensorType sensorType, Map<String, String> stateMap) {
-		GrafikEye grafikEye = (GrafikEye) gateway.getHomeWorksDevice(address, GrafikEye.class);
-		if (grafikEye == null) {
-		  // This should never happen as above command is supposed to create device
-		  log.warn("Gateway could not create a GRAFIK Eye we're receiving feedback for (" + address + ")");
-			return "";
-		}
-		if (grafikEye.getSelectedScene() == null) {
-		  // We don't have any information about the state yet, ask Lutron processor about it
-		  grafikEye.queryScene();
-			return "";
-		}
-		if (sensorType == EnumSensorType.SWITCH) {
-			return (grafikEye.getSelectedScene() == scene)?"on":"off";
-		} else if (sensorType == EnumSensorType.RANGE) {
-			return Integer.toString(grafikEye.getSelectedScene());
-		} else {
-      log.warn("Query GRAFIK Eye status for incompatible sensor type " + sensorType);
-			return "";
-		}
-	}
+  /**
+   * {@inheritDoc}
+   */
+  public void send() {
+    try {
+      GrafikEye grafikEye = (GrafikEye) gateway.getHomeWorksDevice(address, GrafikEye.class);
+      grafikEye.selectScene(scene);
+    } catch (LutronHomeWorksDeviceException e) {
+      log.error("Impossible to get device", e);
+    }
+  }
+
+  // Implements StatusCommand -------------------------------------------------------------------
+
+  @Override
+  public String read(EnumSensorType sensorType, Map<String, String> stateMap) {
+    try {
+      GrafikEye grafikEye = (GrafikEye) gateway.getHomeWorksDevice(address, GrafikEye.class);
+      if (grafikEye == null) {
+        // This should never happen as above command is supposed to create device
+        log.warn("Gateway could not create a GRAFIK Eye we're receiving feedback for (" + address + ")");
+        return "";
+      }
+      if (grafikEye.getSelectedScene() == null) {
+        // We don't have any information about the state yet, ask Lutron processor about it
+        grafikEye.queryScene();
+        return "";
+      }
+      if (sensorType == EnumSensorType.SWITCH) {
+        return (grafikEye.getSelectedScene() == scene) ? "on" : "off";
+      } else if (sensorType == EnumSensorType.RANGE) {
+        return Integer.toString(grafikEye.getSelectedScene());
+      } else {
+        log.warn("Query GRAFIK Eye status for incompatible sensor type " + sensorType);
+        return "";
+      }
+    } catch (LutronHomeWorksDeviceException e) {
+      log.error("Impossible to get device", e);
+    }
+    return "";
+  }
 }
