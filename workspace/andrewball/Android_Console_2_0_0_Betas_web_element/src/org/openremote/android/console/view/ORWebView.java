74d564aa54dd692c25f78d2acba476af2a6e3252
cortextual <cortextual@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-05-05 22:23:54 +0000

    Web element: add support for HTTP authentication
    * Add a handler for HTTP authentication requests that attempts
      to use the username and password attributes from the web
      element in panel.xml, if specified.  Set the content on
      the WebView widget to an appropriate message if either
      the authentication failed or either a username or
      a password was not specified.

--- workspace/andrewball/Android_Console_2_0_0_Betas_web_element/src/org/openremote/android/console/view/ORWebView.java
+++ workspace/andrewball/Android_Console_2_0_0_Betas_web_element/src/org/openremote/android/console/view/ORWebView.java
@@ -21,10 +21,14 @@ package org.openremote.android.console.view;
 
 import java.net.URL;
 
+import org.openremote.android.console.Constants;
+import org.openremote.android.console.R;
 import org.openremote.android.console.bindings.Web;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.ViewGroup;
+import android.webkit.HttpAuthHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -35,21 +39,93 @@ import android.webkit.WebViewClient;
  */
 public class ORWebView extends ComponentView
 {
+  public static final String LOG_CATEGORY = Constants.LOG_CATEGORY + "ORWebView";
+
   private android.webkit.WebView webView;
 
   /**
    * This is used to prevent clicking on links in the WebView from launching the system
-   * browser.
-   *
-   * Adapted from http://developer.android.com/resources/tutorials/views/hello-webview.html
+   * browser as well as to respond to HTTP authentication requests (if a username
+   * and password was specified in the Web binding object)
    */
   private class ORWebViewClient extends WebViewClient
   {
+    public static final String LOG_CATEGORY = Constants.LOG_CATEGORY + "ORWebView.ORWebViewClient";
+
+    /**
+     * Make sure that links are handled by the associated WebView, rather than
+     * the system browser.
+     *
+     * Adapted from http://developer.android.com/resources/tutorials/views/hello-webview.html
+     */
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url)
     {
       view.loadUrl(url);
       return true;
+    }
+
+    /**
+     * Deal with HTTP authentication requests, using the username and password
+     * specified in panel.xml if present.
+     *
+     * A failed authentication will result in an error log message and a suitable
+     * HTML error message page's being written to the WebView.
+     *
+     * If either a username or a password is not specified in panel.xml for this
+     * web element, an error log message will be written and a suitable HTML
+     * error page will be written to the WebView.
+     *
+     * Adapted from
+     *     http://stackoverflow.com/questions/2585055/using-webview-sethttpauthusernamepassword
+     *
+     * If authentication has failed for the given HttpAuthHandler, this will not try to
+     * authenticate again.  Otherwise, this method would be called over and over
+     * again in the case of incorrect credentials.
+     *
+     * This allows us to authenticate without knowing the realm ahead of time.
+     **/
+    @Override
+    public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler,
+        String host, String realm)
+    {
+      if (!handler.useHttpAuthUsernamePassword())
+      {
+        Log.e(LOG_CATEGORY, "onReceivedHttpAuthRequest(): HTTP authentication failed, " +
+            "not trying again.");
+        String errorMessage = context.getString(R.string.http_auth_failed);
+        webView.loadData(composeHtmlErrorPage(errorMessage), "text/html", "utf-8");
+        return;
+      }
+
+      Web web = (Web) getComponent();
+      String username = web.getUsername();
+      String password = web.getPassword();
+      if (username != null && password != null)
+      {
+        handler.proceed(username, password);
+      }
+      else
+      {
+        Log.e(LOG_CATEGORY, "received HTTP authentication request but did not have both username" +
+            " and password from web element with id " + web.getComponentId());
+        String errorMessage =
+            context.getString(R.string.web_element_missing_username_or_password) + " " +
+            web.getComponentId();
+        webView.loadData(composeHtmlErrorPage(errorMessage), "text/html", "utf-8");
+      }
+    }
+
+    private String composeHtmlErrorPage(String errorMessage)
+    {
+      return String.format("<html>" +
+          "\n<head>" +
+          "\n<title>%s</title>" +
+          "\n</head>" +
+          "\n\n<body>" +
+          "\n    <p><b>%s</b></p>" +
+          "\n</body>" +
+          "\n</html>", errorMessage);
     }
   }
 
