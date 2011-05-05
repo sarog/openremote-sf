f90610ff7e4a874bf309a1186c18b981dfac0977
unfromage <unfromage@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-05-05 09:43:38 +0000

    New workspace for stef based on juha's integration branch

---
+++ workspace/fromage/Controller_2_0_0_Alphas_backend-commands2/src/org/openremote/controller/spring/SpringContext.java
@@ -0,0 +1,127 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.spring;
+
+import java.util.Properties;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.interceptor.TransactionProxyFactoryBean;
+import org.openremote.controller.service.ServiceContext;
+import org.openremote.controller.command.CommandFactory;
+
+/**
+ * TODO : ApplicationContext for Spring container
+ * 
+ * @author Dan 2009-2-16
+ */
+public class SpringContext extends ServiceContext
+{
+
+
+  // Class Members --------------------------------------------------------------------------------
+
+  private static String[] contextFiles = new String[] { "spring-context.xml" };
+
+
+  /**
+   * Gets a instance of <code>SpringContext</code>
+   *
+   * @return the instance of <code>SpringContext</code>
+   */
+  public synchronized static SpringContext getInstance()
+  {
+    return (SpringContext)ServiceContext.getInstance(); // TODO : should remove this method altogether
+  }
+
+
+
+  // Instance Fields ------------------------------------------------------------------------------
+
+  private ApplicationContext ctx;
+
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  public SpringContext() throws InstantiationException
+  {
+    this(contextFiles);
+  }
+
+  public SpringContext(String[] setting) throws InstantiationException
+  {
+    ctx = new ClassPathXmlApplicationContext(setting);
+
+    ServiceContext.registerServiceContext(this);
+  }
+
+
+  @Override public Object getService(ServiceName name, Object... params)
+  {
+    if (name == ServiceName.PROTOCOL && params != null)
+    {
+      CommandFactory cf = (CommandFactory)getBean(name.getSpringBeanName());
+      Map<String, String> protocols = cf.getProtocols();
+
+      String paramValue = (String)params[0];
+      String protocolImplName = protocols.get(paramValue);
+
+      return getBean(protocolImplName);
+    }
+
+    else if (name == ServiceName.XML_BINDING)
+    {
+      if (params == null || params[0] == null)
+        throw new IllegalArgumentException("XML Binding service requires a component name parameter");
+
+      String suffix = (String)params[0];
+
+      return getBean(suffix + name.getSpringBeanName());
+    }
+    else
+    {
+      return getBean(name.getSpringBeanName());
+    }
+  }
+
+  
+  /**
+   * Gets a bean instance with the given bean identifier
+   *
+   * @param beanId
+   *           the given bean identifier
+   * @return a bean instance
+   */
+  public Object getBean(String beanId)
+  {
+    Object o = ctx.getBean(beanId);
+
+    if (o instanceof TransactionProxyFactoryBean)
+    {
+       TransactionProxyFactoryBean factoryBean = (TransactionProxyFactoryBean) o;
+       o = factoryBean.getObject();
+    }
+
+    return o;
+  }
+
+}
