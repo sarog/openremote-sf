01a3305b4eec2dd5c84212bbfecdfe421cceba51
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-05-20 16:56:12 +0000

    add some very basic tests for Level event -- ORCJAVA-86

--- workspace/juha/Controller_EP_SNAP_20110520/test/org/openremote/controller/model/event/LevelTest.java
+++ workspace/juha/Controller_EP_SNAP_20110520/test/org/openremote/controller/model/event/LevelTest.java
@@ -24,19 +24,74 @@ import org.junit.Test;
 import org.junit.Assert;
 
 /**
- * TODO -- See ORCJAVA-86 http://jira.openremote.org/browse/ORCJAVA-86
+ * Basic tests for {@link org.openremote.controller.model.event.Level} class.  <p>
+ *
+ * (ORCJAVA-86 -- http://jira.openremote.org/browse/ORCJAVA-86)
  *
  * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  */
 public class LevelTest
 {
 
-  @Test public void noTestsImplemented()
+  /**
+   * Basic constructor and validation.
+   */
+  @Test public void basicConstruction()
   {
-    Assert.fail(
-        "Not Yet Implemented (See ORCJAVA-86 - http://jira.openremote.org/browse/ORCJAVA-86)"
-    );
+    Level l = new Level(0, "name", 100);
+
+    Assert.assertTrue(l.getValue() == 100);
+    Assert.assertTrue(l.getSource().equals("name"));
+    Assert.assertTrue(l.getSourceID() == 0);
+
+    Assert.assertTrue(l.serialize().equals("100"));
+
+    Assert.assertTrue(l.toString() != null);
+    Assert.assertFalse(l.toString().equals(""));
   }
+
+
+  /**
+   * Basic constructor and validation with value parameter above max limit of 100.
+   */
+  @Test public void basicConstructionMaxLimit()
+  {
+    Level l = new Level(4, "xXx", 101);
+
+    Assert.assertTrue(l.getValue() == 100);
+    Assert.assertTrue(l.getSource().equals("xXx"));
+    Assert.assertTrue(l.getSourceID() == 4);
+
+    Assert.assertTrue(l.serialize().equals("100"));
+
+    Assert.assertTrue(l.toString() != null);
+    Assert.assertFalse(l.toString().equals(""));
+  }
+
+
+  /**
+   * Basic constructor and validation with value parameter below min limit of 0.
+   */
+  @Test public void basicConstructionMinLimit()
+  {
+    Level l = new Level(444, "aaa", -1);
+
+    Assert.assertTrue(l.getValue() == 0);
+    Assert.assertTrue(l.getSource().equals("aaa"));
+    Assert.assertTrue(l.getSourceID() == 444);
+
+    Assert.assertTrue(l.serialize().equals("0"));
+
+    Assert.assertTrue(l.toString() != null);
+    Assert.assertFalse(l.toString().equals(""));
+  }
+
+
+  @Test public void testEquals()
+  {
+    Assert.fail("Not Yet Implemented -- See ORCJAVA-95 http://jira.openremote.org/browse/ORCJAVA-95");
+  }
+
 
 }
 
