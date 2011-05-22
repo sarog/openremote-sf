f26219885a40b765310c0ce6f6587d8c90eba943
tosterdahl <tosterdahl@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-05-22 15:36:48 +0000

    sync this branch with /brances/project/Controller_2_0_0_Alphas release branch

--- workspace/toesterdahl/Controller_2_0_0_Alpha11_DenonAVR/test/org/openremote/controller/protocol/knx/KNXIpConnectionManagerTest.java
+++ workspace/toesterdahl/Controller_2_0_0_Alpha11_DenonAVR/test/org/openremote/controller/protocol/knx/KNXIpConnectionManagerTest.java
@@ -1,3 +1,23 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.protocol.knx;
 
 import java.io.IOException;
@@ -21,7 +41,7 @@ public class KNXIpConnectionManagerTest {
          this.server.wait();
       }
    }
-   
+
    @After
    public void tearDown() throws InterruptedException {
       this.server.interrupt();
@@ -42,23 +62,24 @@ public class KNXIpConnectionManagerTest {
             (byte) 0x00), new CommandParameter("0")));
       c.send(GroupValueWrite.createCommand("off", DataPointType.BINARY_VALUE, mgr, new GroupAddress((byte) 0x80,
             (byte) 0x00), new CommandParameter("0")));
+      System.out.println("error = " + this.server.getError());
       Assert.assertNull(this.server.getError());
       discoverServer.interrupt();
       discoverServer.join();
    }
 
-//   @Test
-//   public void testConnect() throws ConnectionException, ConversionException, KnxIpException, IOException,
-//         InterruptedException {
-//      KNXIpConnectionManager mgr = new KNXIpConnectionManager(InetAddress.getByName("127.0.0.1"), new InetSocketAddress(
-//            "127.0.0.1", DummyServer.PORT));
-//      mgr.start();
-//      KNXConnection c = mgr.getConnection();
-//      c.send(GroupValueWrite.createCommand("on", DataPointType.BINARY_VALUE, mgr, new GroupAddress((byte) 0x80,
-//            (byte) 0x00), new CommandParameter("0")));
-//      c.send(GroupValueWrite.createCommand("off", DataPointType.BINARY_VALUE, mgr, new GroupAddress((byte) 0x80,
-//            (byte) 0x00), new CommandParameter("0")));
-//      mgr.stop();
-//      Assert.assertNull(this.server.getError());
-//   }
+   // @Test
+   // public void testConnect() throws ConnectionException, ConversionException, KnxIpException, IOException,
+   // InterruptedException {
+   // KNXIpConnectionManager mgr = new KNXIpConnectionManager(InetAddress.getByName("127.0.0.1"), new InetSocketAddress(
+   // "127.0.0.1", DummyServer.PORT));
+   // mgr.start();
+   // KNXConnection c = mgr.getConnection();
+   // c.send(GroupValueWrite.createCommand("on", DataPointType.BINARY_VALUE, mgr, new GroupAddress((byte) 0x80,
+   // (byte) 0x00), new CommandParameter("0")));
+   // c.send(GroupValueWrite.createCommand("off", DataPointType.BINARY_VALUE, mgr, new GroupAddress((byte) 0x80,
+   // (byte) 0x00), new CommandParameter("0")));
+   // mgr.stop();
+   // Assert.assertNull(this.server.getError());
+   // }
 }
