bfa244d8e55ba5583445286f524158bad07c8c94
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-06-01 11:00:38 +0000

    As before changes required for button enhancements

---
+++ tags/snapshot/iPad_Console_2_0_0_Betas_BeforeButtonEnhancements/Classes/InitViewController.h
@@ -0,0 +1,40 @@
+/* OpenRemote, the Home of the Digital Home.
+ *  * Copyright 2008-2011, OpenRemote Inc-2009, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+
+#import <UIKit/UIKit.h>
+
+/**
+ * This is Splash view of current client and show version of current client 
+ * and process progress of updating from controller server while client starting up.
+ */
+@interface InitViewController : UIViewController {
+	
+	// For render process progress of updating from controller server while client starting up.
+	IBOutlet UILabel *label;
+	
+	// For render version of current client, It's read from Info.plist .
+	IBOutlet UILabel *version;
+
+}
+
+
+@end
---
+++ tags/snapshot/iPad_Console_2_0_0_Betas_BeforeButtonEnhancements/Classes/ViewHelper.m
@@ -0,0 +1,57 @@
+/* OpenRemote, the Home of the Digital Home.
+ *  * Copyright 2008-2011, OpenRemote Inc-2009, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+
+#import "ViewHelper.h"
+#import "NotificationConstant.h"
+
+@implementation ViewHelper
+
++ (void)showAlertViewWithTitle:(NSString *)title Message:(NSString *)message  {
+	[[NSNotificationCenter defaultCenter] postNotificationName:NotificationHideLoading object:nil];
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+	[alert show];
+	[alert release];
+}
+
+- (void)showAlertViewWithTitleAndSettingNavigation:(NSString *)title Message:(NSString *)message  {
+	[[NSNotificationCenter defaultCenter] postNotificationName:NotificationHideLoading object:nil];
+    [self retain]; // We want to stick around as we set ourself as delegate
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+	[alert addButtonWithTitle:@"Settings"];
+	[alert show];
+	[alert release];
+}
+
+// Delegate method of UIAlertViewDelegate.
+// Called when a button is clicked. The view will be automatically dismissed after this call returns
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+	if (buttonIndex == 1) {//setting button
+		[[NSNotificationCenter defaultCenter] postNotificationName:NotificationPopulateSettingsView object:nil];
+	}
+}
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    alertView.delegate = nil; // Make sure we won't receive any further message after we release ourself
+    [self release];
+}
+
+@end
