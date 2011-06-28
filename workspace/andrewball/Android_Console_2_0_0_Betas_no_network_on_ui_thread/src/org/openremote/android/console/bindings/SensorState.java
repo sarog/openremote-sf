a8bc0ef534d1a305b652af7e7780929bada76728
cortextual <cortextual@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-06-28 17:10:05 +0000

    re ANDROID-90 network on UI thread exception
    * making a branch to fix this, as it has now been reproduced

---
+++ workspace/andrewball/Android_Console_2_0_0_Betas_no_network_on_ui_thread/src/org/openremote/android/console/bindings/SensorState.java
@@ -0,0 +1,46 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2011, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.android.console.bindings;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+/**
+ * Represents Sensor State in sensor.
+ * It use key-value structure, the name as key.
+ */
+@SuppressWarnings("serial")
+public class SensorState extends BusinessEntity {
+
+   private String name;
+   private String value;
+   
+   public SensorState(Node node) {
+      NamedNodeMap nodeMap = node.getAttributes();
+      this.name = nodeMap.getNamedItem("name").getNodeValue();
+      this.value = nodeMap.getNamedItem("value").getNodeValue();
+   }
+   public String getName() {
+      return name;
+   }
+   public String getValue() {
+      return value;
+   }
+}
