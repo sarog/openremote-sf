8709c78eb732e4015e8a078e8079584735600c31
kurrazyman <kurrazyman@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-06-30 20:55:58 +0000

    Mobile UI: Method 1 with scroll event detection

--- workspace/richturner/WebConsole_2_0_0_template/src/org/openremote/web/console/types/ResizableUnit.java
+++ workspace/richturner/WebConsole_2_0_0_template/src/org/openremote/web/console/types/ResizableUnit.java
@@ -3,10 +3,8 @@ package org.openremote.web.console.types;
 import org.openremote.web.console.client.ConsoleUnit;
 import org.openremote.web.console.components.ConsoleDisplay;
 
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Label;
 
 /**
@@ -17,25 +15,41 @@ import com.google.gwt.user.client.ui.Label;
  */
 public class ResizableUnit extends ConsoleUnit {
 	public static final int FRAME_WIDTH_TOP = 20;
-	public static final int FRAME_WIDTH_BOTTOM = 60;
+	public static final int FRAME_WIDTH_BOTTOM = 50;
 	public static final int FRAME_WIDTH_LEFT = 20;
 	public static final int FRAME_WIDTH_RIGHT = 20;
 	
-	public ResizableUnit(int displayWidth, int displayHeight) {
+	public ResizableUnit(int displayWidth, int displayHeight, ConsoleDisplay consoleDisplay) {
 		// Create basic Console Unit
-		super(displayWidth, displayHeight);
+		super(displayWidth, displayHeight, consoleDisplay);
 		
+		// Create console frame
+		createFrame();
+		
+		// Update Console dimensions
+		consoleWidth = consoleWidth + FRAME_WIDTH_LEFT + FRAME_WIDTH_RIGHT;
+		consoleHeight = consoleHeight + FRAME_WIDTH_TOP + FRAME_WIDTH_BOTTOM;		
+	}
+	
+	public static int requiredConsoleWidth(int requiredDisplayWidth) {
+		return requiredDisplayWidth + FRAME_WIDTH_LEFT + FRAME_WIDTH_RIGHT;
+	}
+	
+	public static int requiredConsoleHeight(int requiredDisplayHeight) {
+		return requiredDisplayHeight + FRAME_WIDTH_TOP + FRAME_WIDTH_BOTTOM;
+	}
+	
+	public void createFrame() {
 		// Add the frame for this re-sizable console unit by setting style attributes
-		DOM.setElementAttribute(this.getElement(), "style", "padding: " + FRAME_WIDTH_TOP + "px " + FRAME_WIDTH_RIGHT + "px 0px " + FRAME_WIDTH_LEFT + "px;");
-		this.addStyleName("consoleFrame");
-		ConsoleDisplay consoleDisplay = super.getConsoleDisplay();
-		consoleDisplay.addStyleName("consoleDisplayBoss");
+		this.getElement().setAttribute("style", "padding: " + FRAME_WIDTH_TOP + "px " + FRAME_WIDTH_RIGHT + "px 0px " + FRAME_WIDTH_LEFT + "px;");
+		addStyleName("consoleFrame");
+		getConsoleDisplay().addStyleName("consoleDisplayBoss");
 		
 		// Add the logo along the bottom of the frame
 		HorizontalPanel logoPanel = new HorizontalPanel();
 		logoPanel.setStylePrimaryName("consoleFrameLogo");
 		logoPanel.setHeight(FRAME_WIDTH_BOTTOM + "px");
-		DOM.setElementAttribute(logoPanel.getElement(), "style", "line-height: " + FRAME_WIDTH_BOTTOM + "px;");		
+		logoPanel.getElement().setAttribute("style", "line-height: " + FRAME_WIDTH_BOTTOM + "px;");		
 		Label logoLeft = new Label();
 		logoLeft.setText(LOGO_TEXT_LEFT);
 		DOM.setElementAttribute(logoLeft.getElement(), "id", "consoleFrameLogoLeft");
@@ -45,13 +59,5 @@ public class ResizableUnit extends ConsoleUnit {
 		DOM.setElementAttribute(logoRight.getElement(), "id", "consoleFrameLogoRight");
 		logoPanel.add(logoRight);
 		add(logoPanel);
-	}
-	
-	public static int requiredConsoleWidth(int requiredDisplayWidth) {
-		return requiredDisplayWidth + FRAME_WIDTH_LEFT + FRAME_WIDTH_RIGHT;
-	}
-	
-	public static int requiredConsoleHeight(int requiredDisplayHeight) {
-		return requiredDisplayHeight + FRAME_WIDTH_TOP + FRAME_WIDTH_BOTTOM;
 	}
 }
