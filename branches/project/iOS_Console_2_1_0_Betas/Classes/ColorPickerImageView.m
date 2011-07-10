a26b46601b7f59136dd98f3fc339c4e6f46b9d3a
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-07-10 09:40:16 +0000

    add clarifying statements from original author on class use

--- branches/project/iOS_Console_2_1_0_Betas/Classes/ColorPickerImageView.m
+++ branches/project/iOS_Console_2_1_0_Betas/Classes/ColorPickerImageView.m
@@ -2,6 +2,9 @@
 //  ColorPickerImageView.m
 //  ColorPicker
 //
+//  ColorPicker is free and can be used in anything without restriction. 
+//  Feel free to copy, redistribute, sell as you see fit in source code or compiled form.
+//
 //  Created by markj on 3/6/09.
 //  Copyright 2009 Mark Johnson. All rights reserved.
 //
