e7e8557c8602ad43736d5e77b0d5771629aa42f7
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-07-19 09:31:42 +0000

    Create a copy in Marcus workspace

---
+++ workspace/mredeker/Controller_2_0_0_Alphas/src/org/openremote/controller/exception/InvalidElementException.java
@@ -0,0 +1,51 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2011, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.openremote.controller.exception;
+
+/**
+ * Throws this exception when the element isn't the element of controller reserve.
+ * 
+ * @author Handy.Wang 2009-11-10
+ */
+@SuppressWarnings("serial")
+public class InvalidElementException extends ControlCommandException {
+
+   public InvalidElementException(String message, Throwable cause) {
+      super(message, cause);
+      setErrorCode(ControlCommandException.INVALID_ELEMENT);
+   }
+
+   public InvalidElementException(String message) {
+      super(message);
+      setErrorCode(ControlCommandException.INVALID_ELEMENT);
+   }
+
+   public InvalidElementException() {
+      super();
+      setErrorCode(ControlCommandException.INVALID_ELEMENT);
+   }
+
+   public InvalidElementException(Throwable cause) {
+      super(cause);
+      setErrorCode(ControlCommandException.INVALID_ELEMENT);
+   }
+
+}
