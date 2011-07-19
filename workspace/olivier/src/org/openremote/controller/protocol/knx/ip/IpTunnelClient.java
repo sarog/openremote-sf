80cad3d4f0376692b7e32bfdfa7a3f242a887c73
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-07-19 13:47:30 +0000

    merged a tiny cleanup patch from /workspace/juha/Controller_EP_Integration (R4602) -- handle shutdown hook exceptions gracefully which lets the rest of the connection termination logic complete normally.

--- workspace/olivier/src/org/openremote/controller/protocol/knx/ip/IpTunnelClient.java
+++ workspace/olivier/src/org/openremote/controller/protocol/knx/ip/IpTunnelClient.java
@@ -1,18 +1,22 @@
 /*
- * OpenRemote, the Home of the Digital Home. Copyright 2008-2011, OpenRemote Inc.
- * 
- * See the contributors.txt file in the distribution for a full listing of individual contributors.
- * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
- * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Affero General Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.openremote.controller.protocol.knx.ip;
 
@@ -168,8 +172,22 @@ public class IpTunnelClient implements IpProcessorListener {
    }
    
    public void terminateConnection() throws InterruptedException {
-      // Unregister shutdown hook
-      Runtime.getRuntime().removeShutdownHook(IpTunnelClient.this.shutdownHook);
+
+      try
+      {
+        // Unregister shutdown hook  -- TODO: should be executed in a privileged code block
+        Runtime.getRuntime().removeShutdownHook(IpTunnelClient.this.shutdownHook);
+      }
+      catch (IllegalStateException e)
+      {
+        // This may occur if the JVM has already initiated the shutdown sequence,
+        // writing a debug statement but that's all.
+
+        log.debug(
+            "Was unable to remove shutdown hook. This may be due to a JVM shutdown" +
+            "sequence already started: {0}", e, e.getMessage()
+        );
+      }
 
       // Stop heartbeat
       this.heartBeat.cancel();
