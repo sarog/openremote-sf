6e91c1f07824b5b8633bdcc05512d777651f8039
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-08-04 14:18:55 +0000

    migrate to deployer API

--- workspace/juha/Controller_EP_Integration/src/org/openremote/controller/component/control/button/ButtonBuilder.java
+++ workspace/juha/Controller_EP_Integration/src/org/openremote/controller/component/control/button/ButtonBuilder.java
@@ -28,39 +28,75 @@ import org.openremote.controller.command.ExecutableCommand;
 import org.openremote.controller.component.Component;
 import org.openremote.controller.component.ComponentBuilder;
 import org.openremote.controller.component.control.Control;
-import org.openremote.controller.exception.ConfigurationException;
+import org.openremote.controller.exception.InitializationException;
+import org.openremote.controller.service.Deployer;
 
 /**
  * TODO
- * 
- * @author Handy.Wang 2009-10-15
+ *
+ *   - see related tasks
+ *     ORCJAVA-155  (http://jira.openremote.org/browse/ORCJAVA-155)
+ *     ORCJAVA-156  (http://jira.openremote.org/browse/ORCJAVA-156)
+ *     ORCJAVA-157  (http://jira.openremote.org/browse/ORCJAVA-157)
+ *
  */
-public class ButtonBuilder extends ComponentBuilder {
-   
-    /**
-     * Build Button with button xml element.
-     * 
-     * Button instance has been initialized with property status = Status(new NoStatusCommand()). 
-     * So, if commandParam is non-executable command(e.g: status),
-     * the Button instance will get default status with read method in the StatusCommand. 
-     */
-    @SuppressWarnings("unchecked")
-   @Override
-    public Component build(Element componentElement, String commandParam) throws ConfigurationException {
-       Button button = new Button();
-       if (button.isValidActionWith(commandParam)) {
-          List<Element> commandRefElements = componentElement.getChildren();
-          for (Element commandRefElement : commandRefElements) {
-              if (Control.DELAY_ELEMENT_NAME.equalsIgnoreCase(commandRefElement.getName())) {
-                  button.addExecutableCommand(new DelayCommand(commandRefElement.getTextTrim()));
-                  continue;
-              }
-              String commandID = commandRefElement.getAttributeValue(Control.REF_ATTRIBUTE_NAME);
-              Element commandElement = remoteActionXMLParser.queryElementFromXMLById(componentElement.getDocument(),commandID);
-              ExecutableCommand command = (ExecutableCommand) commandFactory.getCommand(commandElement);
-              button.addExecutableCommand(command);
-          }
-       }
-       return button;
+public class ButtonBuilder extends ComponentBuilder
+{
+
+
+  // Instance Fields ------------------------------------------------------------------------------
+  
+  private Deployer deployer;
+
+
+
+  // Implements ComponentBuilder ------------------------------------------------------------------
+
+
+  /**
+   * Build Button with button xml element.
+   *
+   * Button instance has been initialized with property status = Status(new NoStatusCommand()).
+   * So, if commandParam is non-executable command(e.g: status),
+   * the Button instance will get default status with read method in the StatusCommand.
+   */
+  @Override public Component build(Element componentElement, String commandParam)
+      throws InitializationException
+  {
+    Button button = new Button();
+
+    if (button.isValidActionWith(commandParam))
+    {
+      List<Element> commandRefElements = componentElement.getChildren();
+
+      for (Element commandRefElement : commandRefElements)
+      {
+        if (Control.DELAY_ELEMENT_NAME.equalsIgnoreCase(commandRefElement.getName()))
+        {
+          button.addExecutableCommand(new DelayCommand(commandRefElement.getTextTrim()));
+          continue;
+        }
+
+        String commandID = commandRefElement.getAttributeValue(Control.REF_ATTRIBUTE_NAME);
+        Element commandElement = deployer.queryElementById(Integer.parseInt(commandID));
+        ExecutableCommand command = (ExecutableCommand) commandFactory.getCommand(commandElement);
+        button.addExecutableCommand(command);
+      }
     }
+    return button;
+  }
+
+
+  // Service Dependencies -------------------------------------------------------------------------
+
+  /**
+   * TODO : this dependency can/will be satisfied by ObjectBuilder implementation (see ORCJAVA-155)
+   *
+   * @param deployer
+   */
+  public void setDeployer(Deployer deployer)
+  {
+    this.deployer = deployer;
+  }
+
 }
