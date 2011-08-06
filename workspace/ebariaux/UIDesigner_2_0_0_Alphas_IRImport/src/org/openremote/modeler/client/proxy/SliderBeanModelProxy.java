d343288db41191ff9efab7aac53b2b7b122f9c67
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-08-06 17:09:02 +0000

    For import of IR files  and generation of GC/IRTrans commands

---
+++ workspace/ebariaux/UIDesigner_2_0_0_Alphas_IRImport/src/org/openremote/modeler/client/proxy/SliderBeanModelProxy.java
@@ -0,0 +1,108 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2010, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.modeler.client.proxy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openremote.modeler.client.model.TreeFolderBean;
+import org.openremote.modeler.client.rpc.AsyncServiceFactory;
+import org.openremote.modeler.client.rpc.AsyncSuccessCallback;
+import org.openremote.modeler.domain.Sensor;
+import org.openremote.modeler.domain.Slider;
+
+import com.extjs.gxt.ui.client.data.BeanModel;
+
+/**
+ * The proxy is for managing slider.
+ */
+public class SliderBeanModelProxy {
+   private SliderBeanModelProxy() {
+   }
+   public static void loadAll(BeanModel sliderBean, final AsyncSuccessCallback<List<BeanModel>> callback) {
+      if (sliderBean == null || sliderBean.getBean() instanceof TreeFolderBean) {
+         AsyncServiceFactory.getSliderRPCServiceAsync().loadAll(new AsyncSuccessCallback<List<Slider>>() {
+            @Override
+            public void onSuccess(List<Slider> result) {
+               List<BeanModel> sliderModels = Slider.createModels(result);
+               BeanModelDataBase.sliderTable.insertAll(sliderModels);
+               callback.onSuccess(sliderModels);
+            }
+         });
+      } else {
+         Slider slider = sliderBean.getBean();
+         List<BeanModel> sliderBeanModels = new ArrayList<BeanModel>();
+         sliderBeanModels.add(slider.getSetValueCmd().getBeanModel());
+         callback.onSuccess(sliderBeanModels);
+      }
+   }
+   
+   public static void delete(final BeanModel beanModel, final AsyncSuccessCallback<Void> callback) {
+      if (beanModel != null && beanModel.getBean() instanceof Slider) {
+         AsyncServiceFactory.getSliderRPCServiceAsync().delete(((Slider) (beanModel.getBean())).getOid(),
+               new AsyncSuccessCallback<Void>() {
+                  @Override
+                  public void onSuccess(Void result) {
+                     BeanModelDataBase.sliderTable.delete(beanModel);
+                     callback.onSuccess(result);
+                  }
+               });
+      }
+   }
+   
+   public static void save(final BeanModel beanModel,final AsyncSuccessCallback<Slider>callback){
+      if (beanModel != null && beanModel.getBean() instanceof Slider) {
+         AsyncServiceFactory.getSliderRPCServiceAsync().save((Slider) (beanModel.getBean()),
+               new AsyncSuccessCallback<Slider>() {
+
+                  @Override
+                  public void onSuccess(Slider result) {
+                     BeanModelDataBase.sliderTable.insert(result.getBeanModel());
+                     callback.onSuccess(result);
+                  }
+
+               });
+      }
+   }
+   
+   public static void update(final BeanModel beanModel,final AsyncSuccessCallback<Slider>callback) {
+      if (beanModel != null && beanModel.getBean() instanceof Slider) {
+         AsyncServiceFactory.getSliderRPCServiceAsync().update((Slider) (beanModel.getBean()),
+               new AsyncSuccessCallback<Slider>() {
+                  @Override
+                  public void onSuccess(Slider result) {
+                     BeanModelDataBase.sliderTable.update(result.getBeanModel());
+                     callback.onSuccess(result);
+                  }
+
+               });
+      }
+   }
+   
+   public static void saveSliderList(List<Slider> sliderList, final AsyncSuccessCallback<List<BeanModel>> asyncSuccessCallback) {
+     AsyncServiceFactory.getSliderRPCServiceAsync().saveAll(sliderList, new AsyncSuccessCallback<List<Slider>>() {
+         public void onSuccess(List<Slider> sliderList) {
+            List<BeanModel> sliderModels = Slider.createModels(sliderList);
+            BeanModelDataBase.sliderTable.insertAll(sliderModels);
+            asyncSuccessCallback.onSuccess(sliderModels);
+         }
+      });
+ }
+}
---
+++ workspace/ebariaux/UIDesigner_2_0_0_Alphas_IRImport/src/org/openremote/modeler/client/rpc/DeviceCommandRPCServiceAsync.java
@@ -0,0 +1,84 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2011, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.modeler.client.rpc;
+
+import java.util.List;
+
+import org.openremote.modeler.domain.DeviceCommand;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>DeviceCommandRPCService</code>.
+ */
+public interface DeviceCommandRPCServiceAsync {
+   
+   /**
+    * Save all.
+    * 
+    * @param deviceCommands the device commands
+    * @param callback the callback
+    */
+   void saveAll(List<DeviceCommand> deviceCommands, AsyncCallback<List<DeviceCommand>> callback);
+   
+   /**
+    * Save.
+    * 
+    * @param deviceCommand the device command
+    * @param callback the callback
+    */
+   void save(DeviceCommand deviceCommand, AsyncCallback<DeviceCommand> callback);
+   
+   /**
+    * Update.
+    * 
+    * @param deviceCommand the device command
+    * @param callback the callback
+    */
+   void update(DeviceCommand deviceCommand, AsyncCallback<DeviceCommand> callback);
+   
+   /**
+    * Load by id.
+    * 
+    * @param id the id
+    * @param callback the callback
+    */
+   void loadById(long id, AsyncCallback<DeviceCommand> callback);
+
+   /**
+    * Delete command.
+    * 
+    * @param id
+    *           the id
+    * @param callback
+    *           the callback
+    */
+   void deleteCommand(long id, AsyncCallback<Boolean> callback);
+
+   /**
+    * Load by device.
+    * 
+    * @param id the id
+    * @param asyncCallback the async callback
+    */
+   void loadByDevice(long id, AsyncCallback<List<DeviceCommand>> asyncCallback);
+   
+   
+}
