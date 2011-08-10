d248bbebb30ba5b119346483c4309b7b4e109ee6
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-08-10 11:22:35 +0000

    Use appropriate error handlers for HTTP error codes -- get rid of 'magic' return values and use HTTP return codes to indicate ok/error and simply print the response document content as message (if any)

--- workspace/juha/Controller_EP_Integration/web/js/index.js
+++ workspace/juha/Controller_EP_Integration/web/js/index.js
@@ -1,116 +1,174 @@
 /*
- * OpenRemote, the Home of the Digital Home. Copyright 2008, OpenRemote Inc.
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
  *
- * See the contributors.txt file in the distribution for a full listing of individual contributors.
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3.0 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site:
- * http://www.fsf.org.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 $(document).ready(function() {
+
+    // Form View -- Online Sync or Manual Upload ------------------------------
+
 	var checkedMode = $("input[name='mode']:checked").val();
+
 	if (checkedMode == 'offline') {
 		showOffline();
-	} else {
+	}
+
+    else {
 		showOnline();
 	}
 	
-	$('#online').click(function(){
+	$('#online').click(function() {
 		showOnline();
 	});
-	$('#offline').click( function() {
+
+	$('#offline').click(function() {
 		showOffline();
 	});
-    $('#uploadForm').ajaxForm(function(result) {
-    	if (result == 'OK') {
-			message("Upload complete!");
-		} else if (result == 'disabled') {
-			error("Upload is disabled");
-		} else {
-			error("Upload failed! " + result);
-		}
-    }); 
-    $('#syncForm').ajaxForm(function(result) {
-    	if (result == 'OK') {
-			message("Sync complete!");
-    	} else if (result == 'forbidden') {
-			error("The username or password you entered is incorrect.");
-    	} else if (result == 'n/a') {
-    		error("Can't connect to Beehive.");
-    	} else if (result == 'missing') {
-    		error("openremote.zip not found in account, please edit UI and save.");
-		} else {
-			error("Sync failed! " + result);
-		}
-    }); 
-    $('#syncSubmit').click(function(){
+
+
+    // Manual Upload ----------------------------------------------------------
+
+    var uploadFormOptions = {
+        
+        error: function(response) {
+            error(response.responseText);
+        },
+
+        success: function() {
+            message("Upload Complete.");
+        }
+    };
+
+    $('#uploadForm').ajaxForm(uploadFormOptions);
+
+    $('#uploadSubmit').click(function() {
+
+        clearMessage();
+        showUpdateIndicator();
+
+        var zipPath = $('#zip').val();
+
+        if (zipPath == '') {
+            error("Please select a zip first");
+            return false;
+        }
+
+        else if(!/.+?\.zip/.test(zipPath)) {
+            error("Only zip is allowed");
+            return false;
+        }
+    });
+
+
+
+    // Online Sync ------------------------------------------------------------
+
+    var syncFormOptions = {
+
+        error: function(response) {
+            error(response.responseText);
+        },
+
+        success: function() {
+            message("Sync Complete.");
+        }
+    };
+
+    $('#syncForm').ajaxForm(syncFormOptions);
+
+
+    $('#syncSubmit').click(function() {
     	clearMessage();
     	showUpdateIndicator();
     });
-	$('#uploadSubmit').click(function(){
-		clearMessage();
-		showUpdateIndicator();
-		var zipPath = $('#zip').val();
-		if(zipPath == ''){
-			error("Please select a zip first");
-			return false;
-		}else if(!/.+?\.zip/.test(zipPath)){
-			error("Only zip is allowed");
-			return false;
-		}
-	});
+
+
+
+    // Refresh Button ---------------------------------------------------------
+    
 	$('#refresh').click(function() {
+
 		clearMessage();
 		showRefreshIndicator();
-		$.get("config.htm?method=refreshController",
-			function(msg){
+
+        $.get("config.htm?method=refreshController",
+			function(msg) {
+
 				if (msg == 'OK') {
 					message("Finished reloading configuration.");
-				} else if (msg == 'latest') {
-					message("The cache is already up to date.");
-				} else {
+				}
+
+                else {
 					error("Failed to reload configuration and clear cache! " + msg);
 				}
 			}
 		 );
 	});
+
 	$("#version").append(getVersionLabel());
 });
+
+
+
+// Functions ------------------------------------------------------------------
+
 function showOnline() {
 	$('#online-cont').show();
 	$('#offline-cont').hide();
 	clearMessage();
 }
+
 function showOffline() {
 	$('#offline-cont').show();
 	$('#online-cont').hide();
 	clearMessage();
 }
-function message(msg){
+
+
+function message(msg) {
+
 	hideUpdateIndicator();
 	hideRefreshIndicator();
+
 	$('#errMsg').text("").hide();
+
 	if (msg == '') {
 		$('#msg').hide().text(msg);
-	} else {
+	}
+
+    else {
 		$('#msg').hide().show().text(msg);
 	}
 }
 
-function error(msg){
-	hideUpdateIndicator();
+function error(msg) {
+
+    hideUpdateIndicator();
 	hideRefreshIndicator();
+
 	$('#msg').text("").hide();
+
 	if (msg == '') {
 		$('#errMsg').hide().text(msg);
-	} else {
+	}
+
+    else {
 		$('#errMsg').hide().show().text(msg);
 	}
 }
@@ -123,6 +181,7 @@ function clearMessage() {
 function showUpdateIndicator() {
 	$('#update_indicator').show();
 }
+
 function hideUpdateIndicator() {
 	$('#update_indicator').hide();
 }
@@ -135,32 +194,48 @@ function hideRefreshIndicator() {
 	$('#refresh_indicator').hide();
 }
 
-function getVersionLabel(){
-	var headUrl = "$HeadURL$";
-	var revision = "$Revision$";
-	var result = "Untagged";
+function getVersionLabel() {
+
+    var headUrl = "$HeadURL$";
+    var revision = "$Revision$";
+    var result = "Untagged";
     var verStr = "";
     var tagStart = -1;
+
     if ((tagStart = headUrl.indexOf("tags")) >= 0) {
-       var tagsEnd = headUrl.indexOf("/", tagStart + 5);
-       if (tagsEnd >= 0) {
+
+        var tagsEnd = headUrl.indexOf("/", tagStart + 5);
+
+        if (tagsEnd >= 0) {
           verStr = headUrl.substring(tagStart + 5, tagsEnd);
-       }
-    } else if ((tagStart = headUrl.indexOf("branches")) >= 0) {
-       var tagsEnd = headUrl.indexOf("/", tagStart + 9);
-       if (tagsEnd >= 0) {
+        }
+    }
+
+    else if ((tagStart = headUrl.indexOf("branches")) >= 0) {
+
+        var tagsEnd = headUrl.indexOf("/", tagStart + 9);
+
+        if (tagsEnd >= 0) {
           verStr = headUrl.substring(tagStart + 9, tagsEnd);
           verStr = "Branch: " + verStr;
-       }
-    } else if (revision != null) {
-       tagStart = revision.indexOf("$Revision:");
-       var tagsEnd = revision.indexOf("$", tagStart + 10);
-       if (tagsEnd >= 0) {
-          verStr = " r" + $.trim(revision.substring(tagStart + 10, tagsEnd));
-       }
+        }
     }
+
+    else if (revision != null) {
+
+        tagStart = revision.indexOf("$Revision:");
+
+        var tagsEnd = revision.indexOf("$", tagStart + 10);
+
+        if (tagsEnd >= 0) {
+          verStr = " r" + $.trim(revision.substring(tagStart + 10, tagsEnd));
+        }
+    }
+
     if (verStr.length != 0) {
        result = verStr.replace('_', '.');
     }
+
     return result;
 }
+
