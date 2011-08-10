4d74a36436485448d78349c8675c7f3c02cea3fb
cortextual <cortextual@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-08-10 03:00:45 +0000

    re ANDROID-92 abstract controller interface -- inject ControllerService into ControlView

--- workspace/andrewball/Android_Console_2_0_0_Betas_ANDROID_85_failover/src/org/openremote/android/console/view/ControlView.java
+++ workspace/andrewball/Android_Console_2_0_0_Betas_ANDROID_85_failover/src/org/openremote/android/console/view/ControlView.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.util.Timer;
 
 import org.apache.http.HttpResponse;
+import org.openremote.android.console.Constants;
 import org.openremote.android.console.LoginDialog;
 import org.openremote.android.console.bindings.ColorPicker;
 import org.openremote.android.console.bindings.Component;
@@ -32,22 +33,42 @@ import org.openremote.android.console.bindings.Switch;
 import org.openremote.android.console.model.AppSettingsModel;
 import org.openremote.android.console.model.ControllerException;
 import org.openremote.android.console.model.ViewHelper;
+import org.openremote.android.console.net.ControllerService;
 import org.openremote.android.console.net.ORConnectionDelegate;
 import org.openremote.android.console.net.ORHttpMethod;
 import org.openremote.android.console.net.ORUnBlockConnection;
 
+import roboguice.inject.InjectorProvider;
+
 import android.content.Context;
+import android.util.Log;
 
 /**
  * The super class of all control view, include ButtonView, SwitchView and SliderView.
+ *
+ * TODO Remove reference to ORConnectionDelegate, using the controllerService and a RoboAsyncTask instead.
  */
 public class ControlView extends ComponentView implements ORConnectionDelegate {
+   public static final String LOG_CATEGORY = Constants.LOG_CATEGORY + "ControlView";
 
    /** The repeat send command timer. */
    private Timer timer;
 
+   private ControllerService controllerService;
+
+   public ControllerService getControllerService()
+   {
+      return controllerService;
+   }
+
+   public void setControllerservice(ControllerService controllerService)
+   {
+      this.controllerService = controllerService;
+   }
+
    protected ControlView(Context context) {
       super(context);
+      injectCollaboratorsFromContext(context);
    }
 
    public static ControlView buildWithControl(Context context, Component control) {
@@ -61,7 +82,36 @@ public class ControlView extends ComponentView implements ORConnectionDelegate {
       } else if (control instanceof ColorPicker) {
          controlView = new ColorPickerView(context, (ColorPicker)control);
       }
+
+      if (controlView != null) {
+         controlView.injectCollaboratorsFromContext(context);
+      }
+
       return controlView;
+   }
+
+   /**
+    * If a context that implements InjectorProvider is provided, inject
+    * instances of collaborators (currently just ControllerService).
+    *
+    * This is the only way that I know of to inject collaborators into a view
+    * with RoboGuice. (aball)
+    *
+    * RoboActivity implements InjectorProvider and would be the most likely
+    * Context used for instantiating a ControlView at runtime.
+    *
+    * If context does not implement InjectorProvider, an error message
+    * will be logged.
+    *
+    * @param context Android Context (useful only if implements InjectorProvider)
+    */
+   protected void injectCollaboratorsFromContext(Context context) {
+      try {
+         InjectorProvider ip = (InjectorProvider) context;
+         controllerService = ip.getInjector().getInstance(ControllerService.class);
+      } catch (ClassCastException e) {
+         Log.e(LOG_CATEGORY, "context is not a Guice InjectorProvider.  Cannot inject collaborators.");
+      }
    }
 
    /**
@@ -72,6 +122,7 @@ public class ControlView extends ComponentView implements ORConnectionDelegate {
     * @return true, if successful
     */
    public boolean sendCommandRequest(String commandType) {
+      // TODO Use controllerService here.
       new ORUnBlockConnection(this.context, ORHttpMethod.POST, true, AppSettingsModel.getSecuredServer(getContext())
             + "/rest/control/" + getComponent().getComponentId() + "/" + commandType, this);
       return true;
@@ -95,6 +146,8 @@ public class ControlView extends ComponentView implements ORConnectionDelegate {
     * Handle server error with status code.
     * If status code not equals 200, cancel the timer, and display alert with error message.
     * 
+    * TODO Get rid of this.  Use a RoboAsyncTask instead.
+    *
     * @param statusCode the status code
     */
    public void handleServerErrorWithStatusCode(int statusCode) {
@@ -105,16 +158,19 @@ public class ControlView extends ComponentView implements ORConnectionDelegate {
       }
    }
 
+   // TODO get rid of this (use a RoboAsyncTask instead)
    @Override
    public void urlConnectionDidFailWithException(Exception e) {
       cancelTimer();
    }
 
+   // TODO get rid of this (use a RoboAsyncTask instead)
    @Override
    public void urlConnectionDidReceiveData(InputStream data) {
       // do nothing.
    }
 
+   // TODO get rid of this (use a RoboAsyncTask instead)
    @Override
    public void urlConnectionDidReceiveResponse(HttpResponse httpResponse) {
       int responseCode = httpResponse.getStatusLine().getStatusCode();
