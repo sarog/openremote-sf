655d9fe1ebbab2d43e33946eafb35e5d35c89326
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-08-14 15:33:04 +0000

    commented out a bunch of code that didn't look like it was being used -- related to ORCJAVA-165

--- workspace/juha/Controller_EP_Integration/src/org/openremote/controller/statuscache/ChangedStatusRecord.java
+++ workspace/juha/Controller_EP_Integration/src/org/openremote/controller/statuscache/ChangedStatusRecord.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.statuscache;
 
 import java.util.ArrayList;
@@ -27,6 +28,13 @@ import java.util.List;
 import java.util.Set;
 
 /**
+ * TODO:
+ *
+ *  - See relevant task ORCJAVA-165 (http://jira.openremote.org/browse/ORCJAVA-165)
+ *
+ *
+ *
+ * 
  * A changed status record.<br />
  * This Record is used to record the skipped changed statuses and waited changed statuses .<br />
  * 
@@ -34,140 +42,191 @@ import java.util.Set;
  */
 public class ChangedStatusRecord {
    
-   /** A logical identity of panel */
-   private String deviceID;
+  /** A logical identity of panel */
+  private String deviceID;
 
-   /** The ids a polling request contains */
-   private List<Integer> pollingSensorIDs;
+  /** The ids a polling request contains */
+  private List<Integer> pollingSensorIDs = new ArrayList<Integer>(1);
 
-   /** The ids whose status had changed in the statusChangedSensorIDs */
-   private Set<Integer> statusChangedSensorIDs;
+  /** The ids whose status had changed in the statusChangedSensorIDs */
+  private Set<Integer> statusChangedSensorIDs = new HashSet<Integer>(3);
 
-   /** Default constructor */
-   public ChangedStatusRecord() {
-      super();
-      deviceID = "";
-      pollingSensorIDs = new ArrayList<Integer>();
-      statusChangedSensorIDs = new HashSet<Integer>();
-   }
 
-   public ChangedStatusRecord(String deviceID, List<Integer> pollingSensorIDs) {
-      super();
-      this.deviceID = deviceID;
-      this.pollingSensorIDs = pollingSensorIDs;
-      this.statusChangedSensorIDs = new HashSet<Integer>();
-   }
 
-   public ChangedStatusRecord(String deviceID, Integer[] pollingSensorIDs) {
-      super();
-      this.deviceID = deviceID;
-      this.pollingSensorIDs = new ArrayList<Integer>();
-      this.setPollingSensorIDs(pollingSensorIDs);
-      this.statusChangedSensorIDs = new HashSet<Integer>();
-   }
+  // Constructors ---------------------------------------------------------------------------------
 
-   public ChangedStatusRecord(String deviceID, String[] pollingSensorIDs) {
-      super();
-      this.pollingSensorIDs = new ArrayList<Integer>();
-      for (String s : pollingSensorIDs) {
-         this.pollingSensorIDs.add(Integer.parseInt(s));
-      }
-      this.deviceID = deviceID;
-      this.statusChangedSensorIDs = new HashSet<Integer>();
-   }
 
-   public ChangedStatusRecord(String deviceID, Collection<Integer> pollingSensorIDs) {
-      super();
-      this.pollingSensorIDs = new ArrayList<Integer>();
-      for (Integer i : pollingSensorIDs) {
-         this.pollingSensorIDs.add(i);
-      }
-      this.deviceID = deviceID;
-      this.statusChangedSensorIDs = new HashSet<Integer>();
-   }
+//   Looks like dead code [JPL]
+//
+//  public ChangedStatusRecord()
+//  {
+//    super();
+//    deviceID = "";
+//    pollingSensorIDs = new ArrayList<Integer>();
+//  //      statusChangedSensorIDs = new HashSet<Integer>();
+//  }
 
-   public String getDeviceID() {
-      return deviceID;
-   }
+  public ChangedStatusRecord(String deviceID, List<Integer> pollingSensorIDs)
+  {
+    this.deviceID = deviceID;
+    this.pollingSensorIDs = pollingSensorIDs;
+    //this.statusChangedSensorIDs = new HashSet<Integer>();
+  }
 
-   public void setDeviceID(String deviceID) {
-      this.deviceID = deviceID;
-   }
+//   Looks like dead code [JPL]
+//
+//   public ChangedStatusRecord(String deviceID, Integer[] pollingSensorIDs) {
+//      super();
+//      this.deviceID = deviceID;
+//      this.pollingSensorIDs = new ArrayList<Integer>();
+//      this.setPollingSensorIDs(pollingSensorIDs);
+//      this.statusChangedSensorIDs = new HashSet<Integer>();
+//   }
+//
+//
+//   Looks like dead code [JPL]
+//
+//  public ChangedStatusRecord(String deviceID, String[] pollingSensorIDs)
+//  {
+//    this.pollingSensorIDs = new ArrayList<Integer>();
+//
+//    for (String s : pollingSensorIDs) {
+//       this.pollingSensorIDs.add(Integer.parseInt(s));
+//    }
+//
+//    this.deviceID = deviceID;
+////      this.statusChangedSensorIDs = new HashSet<Integer>();
+//  }
+//
+//
+//   Looks like dead code [JPL]
+//
+//  public ChangedStatusRecord(String deviceID, Collection<Integer> pollingSensorIDs)
+//  {
+//    this.pollingSensorIDs = new ArrayList<Integer>();
+//    for (Integer i : pollingSensorIDs) {
+//       this.pollingSensorIDs.add(i);
+//    }
+//    this.deviceID = deviceID;
+////      this.statusChangedSensorIDs = new HashSet<Integer>();
+//  }
 
-   /**
-    * Add pollingSensor id into polling sensor id list. 
-    */
-   public void addPollingSensorID(Integer pollingSensorID) {
-      this.pollingSensorIDs.add(pollingSensorID);
-   }
-   
-   /**
-    * Add statusChanged id into status changed id list. 
-    */
-   public void addStatusChangedSensorID(Integer statusChangedSensorID) {
-      this.statusChangedSensorIDs.add(statusChangedSensorID);
-   }
 
-   @Override
-   public boolean equals(Object obj) {
-      if (obj == null || !(obj instanceof ChangedStatusRecord)) {
-         return false;
-      }
-      ChangedStatusRecord timeoutRecord = (ChangedStatusRecord) obj;
-      if ("".equals(timeoutRecord.getDeviceID()) || !timeoutRecord.getDeviceID().equals(this.deviceID)) {
-         return false;
-      }
-      if (timeoutRecord.getPollingSensorIDs().size() == 0
-            || timeoutRecord.getPollingSensorIDs().size() != this.pollingSensorIDs.size()) {
-         return false;
-      }
+//
+//   Looks like dead code [JPL]
+//
+//  public void setDeviceID(String deviceID)
+//  {
+//    this.deviceID = deviceID;
+//  }
 
-      Collections.sort(this.getPollingSensorIDs(), new PollingSensorIDListComparator());
-      Collections.sort(timeoutRecord.getPollingSensorIDs(), new PollingSensorIDListComparator());
-      for (int i = 0; i < timeoutRecord.getPollingSensorIDs().size(); i++) {
-         if (!this.getPollingSensorIDs().get(i).equals(timeoutRecord.getPollingSensorIDs().get(i))) {
-            return false;
-         }
-      }
-      return true;
-   }
-   
-   public List<Integer> getPollingSensorIDs() {
-      return pollingSensorIDs;
-   }
 
-   /**
-    * Overload the method setPollingSensorIDs with the parameter type "List". 
-    */
-   public void setPollingSensorIDs(Integer[] pollingSensorIDs) {
-      for (Integer pollingSensorID : pollingSensorIDs) {
-         this.pollingSensorIDs.add(pollingSensorID);
-      }
-   }
+  public String getDeviceID()
+  {
+    return deviceID;
+  }
 
-   /**
-    * Overload the method setPollingSensorIDs with the parameter type "Integer[]". 
-    */
-   public void setPollingSensorIDs(List<Integer> pollingSensorIDs) {
-      this.pollingSensorIDs = pollingSensorIDs;
-   }
+  public List<Integer> getPollingSensorIDs()
+  {
+   return pollingSensorIDs;
+  }
 
-   public Set<Integer> getStatusChangedSensorIDs() {
-      return statusChangedSensorIDs;
-   }
 
-   public void setStatusChangedSensorIDs(Set<Integer> statusChangedSensorIDs) {
-      this.statusChangedSensorIDs = statusChangedSensorIDs;
-   }
+  public Set<Integer> getStatusChangedSensorIDs()
+  {
+     return statusChangedSensorIDs;
+  }
 
-   @Override
-   public String toString() {
-      StringBuffer sb = new StringBuffer();
+  public void setStatusChangedSensorIDs(Set<Integer> statusChangedSensorIDs)
+  {
+     this.statusChangedSensorIDs = statusChangedSensorIDs;
+  }
 
-      sb.append("DEVICEID:" + deviceID);
-      sb.append("\tsensorID:" + this.pollingSensorIDs.toString());
-      sb.append("statusChangedSensorID:" + this.statusChangedSensorIDs);
-      return sb.toString();
-   }
+
+
+//   Looks like dead code [JPL]
+//
+//   /**
+//    * Add pollingSensor id into polling sensor id list.
+//    */
+//   public void addPollingSensorID(Integer pollingSensorID) {
+//      this.pollingSensorIDs.add(pollingSensorID);
+//   }
+//
+//
+//   Looks like dead code [JPL]
+//
+//   /**
+//    * Add statusChanged id into status changed id list.
+//    */
+//   public void addStatusChangedSensorID(Integer statusChangedSensorID) {
+//      this.statusChangedSensorIDs.add(statusChangedSensorID);
+//   }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (obj == null || !(obj instanceof ChangedStatusRecord))
+    {
+       return false;
+    }
+
+    ChangedStatusRecord timeoutRecord = (ChangedStatusRecord) obj;
+
+    if ("".equals(timeoutRecord.getDeviceID()) || !timeoutRecord.getDeviceID().equals(this.deviceID))
+    {
+       return false;
+    }
+
+    if (timeoutRecord.getPollingSensorIDs().isEmpty()
+          || timeoutRecord.getPollingSensorIDs().size() != this.pollingSensorIDs.size())
+    {
+       return false;
+    }
+
+    Collections.sort(this.getPollingSensorIDs(), new PollingSensorIDListComparator());
+    Collections.sort(timeoutRecord.getPollingSensorIDs(), new PollingSensorIDListComparator());
+
+    for (int i = 0; i < timeoutRecord.getPollingSensorIDs().size(); i++)
+    {
+       if (!this.getPollingSensorIDs().get(i).equals(timeoutRecord.getPollingSensorIDs().get(i)))
+       {
+          return false;
+       }
+    }
+
+    return true;
+  }
+
+
+  @Override
+  public String toString()
+  {
+     StringBuffer sb = new StringBuffer();
+
+     sb.append("DEVICEID:" + deviceID);
+     sb.append("\tsensorID:" + this.pollingSensorIDs.toString());
+     sb.append("statusChangedSensorID:" + this.statusChangedSensorIDs);
+     return sb.toString();
+  }
+
+
+//  Looks like dead code  [JPL]
+//
+//   private void setPollingSensorIDs(Integer[] pollingSensorIDs) {
+//      for (Integer pollingSensorID : pollingSensorIDs) {
+//         this.pollingSensorIDs.add(pollingSensorID);
+//      }
+//   }
+//
+//  Looks like dead code  [JPL]
+//
+//   /**
+//    * Overload the method setPollingSensorIDs with the parameter type "Integer[]".
+//    */
+//   public void setPollingSensorIDs(List<Integer> pollingSensorIDs) {
+//      this.pollingSensorIDs = pollingSensorIDs;
+//   }
+
 
 }
