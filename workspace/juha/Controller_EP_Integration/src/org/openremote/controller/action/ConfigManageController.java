798a9424a1c83d70f6710d97cf99dba98da1ad91
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-08-14 11:13:25 +0000

    rewrite -- isolated compile-time dependencies to controller runtime to single ServiceContext.getDeployer() call which should go away once a proper REST admin interface is in place (see ORCJAVA-173). Sanitized the HTTP implementation to return properly HTTP OK or HTTP error status code depending on controller responses. Deployment related file handling and unzipping made part of Deployer API (see ORCJAVA-171)

--- workspace/juha/Controller_EP_Integration/src/org/openremote/controller/action/ConfigManageController.java
+++ workspace/juha/Controller_EP_Integration/src/org/openremote/controller/action/ConfigManageController.java
@@ -1,134 +1,199 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.action;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.openremote.controller.ControllerConfiguration;
 import org.openremote.controller.Constants;
-import org.openremote.controller.exception.BeehiveNotAvailableException;
-import org.openremote.controller.exception.ControlCommandException;
-import org.openremote.controller.exception.ForbiddenException;
-import org.openremote.controller.exception.ResourceNotFoundException;
-import org.openremote.controller.service.FileService;
+import org.openremote.controller.utils.Logger;
 import org.openremote.controller.service.ServiceContext;
-import org.openremote.controller.service.Deployer;
-import org.openremote.controller.spring.SpringContext;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 /**
- * The controller for Configuration management.
+ * TODO
  * 
- * @author Dan 2009-5-14
+ *   See relevant tasks in:
+ *
+ *    - ORCJAVA-173 (http://jira.openremote.org/browse/ORCJAVA-173)
+ *    - ORCJAVA-171 (http://jira.openremote.org/browse/ORCJAVA-171)
+ * 
  */
-public class ConfigManageController extends MultiActionController {
-   
-   /** The file service. */
-   private FileService fileService;
-   
-   private ControllerConfiguration configuration;
-   
+public class ConfigManageController extends MultiActionController
+{
 
-   /**
-    * Upload zip.
-    * 
-    * @param request the request
-    * @param response the response
-    * 
-    * @return the model and view
-    * 
-    * @throws IOException Signals that an I/O exception has occurred.
-    * @throws ServletRequestBindingException the servlet request binding exception
-    */
-   public ModelAndView uploadZip(HttpServletRequest request, HttpServletResponse response) throws IOException,
-         ServletRequestBindingException {
-      try {
-         if (configuration.isResourceUpload()) {
-            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-            boolean success = fileService.uploadConfigZip(multipartRequest.getFile("zip_file").getInputStream());
-            response.getWriter().print(success ? Constants.OK : null);
-         } else {
-            response.getWriter().print("disabled");
-         }
-      } catch (Exception e) {
-         response.getWriter().print(e.getMessage());
-      }
-      return null;
-   }
-   
-   public ModelAndView syncOnline(HttpServletRequest request, HttpServletResponse response) throws IOException,
-         ServletRequestBindingException {
-      String username = request.getParameter("username");
-      String password = request.getParameter("password");
-      boolean success = false;
-      try {
-         success = ServiceContext.getFileResourceService().syncConfigurationWithModeler(username, password);
-         response.getWriter().print(success ? Constants.OK : null);
-      } catch (ForbiddenException e) {
-         response.getWriter().print("forbidden");
-      } catch (BeehiveNotAvailableException e) {
-         response.getWriter().print("n/a");
-      } catch (ResourceNotFoundException e) {
-         response.getWriter().print("missing");
-      } catch (ControlCommandException e) {
-         response.getWriter().print(e.getMessage());
-      } catch (Throwable t) {
-         t.printStackTrace();
-      }
-      return null;
-   }
-   
-   public ModelAndView refreshController(HttpServletRequest request, HttpServletResponse response) throws IOException,
-         ServletRequestBindingException {
-      try {
-         Deployer deployer = ServiceContext.getDeployer();
-         deployer.softRestart();
-         response.getWriter().print(Constants.OK);
-      } catch (Exception e) {
-         response.getWriter().print("failed :" + e.getMessage());
-      }
-      return null;
-   }
 
-   /**
-    * Sets the file service.
-    * 
-    * @param fileService the new file service
-    */
-   public void setFileService(FileService fileService) {
-      this.fileService = fileService;
-   }
+  // Constants ------------------------------------------------------------------------------------
 
-   /**
-    * Sets the configuration.
-    * 
-    * @param configuration the new configuration
-    */
-   public void setConfiguration(ControllerConfiguration configuration) {
-      this.configuration = configuration;
+  private final static String ZIP_FILE_INPUT_PARAMETER_NAME = "zip_file";
+
+  private final static String MIME_TEXT_PLAIN = "text/plain";
+
+
+
+  // Class Members --------------------------------------------------------------------------------
+
+
+  private final static Logger log = Logger.getLogger(Constants.DEPLOYER_LOG_CATEGORY);
+
+
+
+  // Public Instance Methods ----------------------------------------------------------------------
+
+  public ModelAndView uploadZip(HttpServletRequest request, HttpServletResponse response) throws
+      IOException, ServletRequestBindingException
+  {
+    response.setContentType(MIME_TEXT_PLAIN);
+    PrintWriter writer = response.getWriter();
+
+    InputStream zipInput = null;
+
+    try
+    {
+      MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+      zipInput = new BufferedInputStream(
+          multipartRequest.getFile(ZIP_FILE_INPUT_PARAMETER_NAME).getInputStream());
+
+      // TODO :
+      //  -- decouple from direct Java reference (and therefore from controller runtime),
+      //     and go over HTTP/REST instead. See ORCJAVA-173
+
+      ServiceContext.getDeployer().deployFromZip(zipInput);
+
+      response.setStatus(HttpURLConnection.HTTP_OK);
+      writer.println("Upload Complete.");
+    }
+
+    catch (Throwable t)
+    {
+      log.error("Manual configuration upload failed : {0}", t, t.getMessage());
+
+      response.setStatus(HttpURLConnection.HTTP_INTERNAL_ERROR);
+      writer.print(t.getMessage());
+    }
+
+    finally
+    {
+      if (zipInput != null)
+      {
+        try
+        {
+          zipInput.close();
+        }
+
+        catch (IOException e)
+        {
+          log.debug("Could not close the incoming zip upload stream : {0}", e, e.getMessage());
+        }
+      }
+
+      writer.close();
+      response.flushBuffer();
+    }
+    
+    return null;
+  }
+
+
+  public ModelAndView syncOnline(HttpServletRequest request, HttpServletResponse response)
+     throws IOException, ServletRequestBindingException
+  {
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+
+    response.setContentType(MIME_TEXT_PLAIN);
+    PrintWriter writer = response.getWriter();
+
+    try
+    {
+      // TODO :
+      //  -- decouple from direct Java reference (and therefore from controller runtime),
+      //     and go over HTTP/REST instead. See ORCJAVA-173
+
+      ServiceContext.getDeployer().deployFromOnline(username, password);
+
+      response.setStatus(HttpURLConnection.HTTP_OK);
+    }
+
+    catch (Throwable t)
+    {
+      log.error("Synchronizing controller with online account failed : {0}", t, t.getMessage());
+
+      response.setStatus(HttpURLConnection.HTTP_INTERNAL_ERROR);
+      writer.print(t.getMessage());
+    }
+
+    finally
+    {
+      writer.close();
+      response.flushBuffer();
+    }
+
+    return null;
+  }
+
+
+
+  public ModelAndView refreshController(HttpServletRequest request, HttpServletResponse response)
+     throws IOException, ServletRequestBindingException
+  {
+    response.setContentType(MIME_TEXT_PLAIN);
+    PrintWriter writer = response.getWriter();
+
+    try
+    {
+      // TODO :
+      //  -- decouple from direct Java reference (and therefore from controller runtime),
+      //     and go over HTTP/REST instead. See ORCJAVA-173
+
+      ServiceContext.getDeployer().softRestart();
+
+      response.setStatus(HttpURLConnection.HTTP_OK);
+    }
+
+    catch (Throwable t)
+    {
+      log.error("Redeploy failed : {0}", t, t.getMessage());
+
+      response.setStatus(HttpURLConnection.HTTP_INTERNAL_ERROR);
+      writer.print(t.getMessage());
+    }
+
+    finally
+    {
+      writer.close();
+      response.flushBuffer();
+    }
+
+    return null;
+
    }
 
 }
