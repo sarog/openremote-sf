066d9ecec291606f115873699c1b7528bd3efb44
wilbal <wilbal@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-10-06 15:59:49 +0000

    MODELER-257 : - xcfParser data handling

---
+++ workspace/ebariaux/UIDesigner_2_0_0_Alphas_IRImport/src/org/openremote/modeler/server/IRFileParserController.java
@@ -0,0 +1,18 @@
+package org.openremote.modeler.server;
+
+import java.util.List;
+
+import org.openremote.modeler.client.rpc.IRFileParserRPCService;
+import org.openremote.modeler.domain.Slider;
+import org.openremote.modeler.service.IRFileParserService;
+
+import com.tinsys.ir.database.Brand;
+import com.tinsys.ir.database.CodeSet;
+import com.tinsys.ir.database.Device;
+
+public class IRFileParserController implements IRFileParserRPCService {
+
+	
+
+
+}
---
+++ workspace/ebariaux/UIDesigner_2_0_0_Alphas_IRImport/src/org/openremote/modeler/service/impl/IRFileParserServiceImpl.java
@@ -0,0 +1,35 @@
+package org.openremote.modeler.service.impl;
+
+import java.util.List;
+
+import org.openremote.modeler.service.IRFileParserService;
+
+import com.tinsys.ir.database.Brand;
+import com.tinsys.ir.database.CodeSet;
+import com.tinsys.ir.database.Device;
+import com.tinsys.ir.database.IRCommand;
+import com.tinsys.pronto.irfiles.XCFFileParser;
+
+public class IRFileParserServiceImpl implements IRFileParserService {
+
+	private XCFFileParser xcfFileParser;
+	 
+	public void setXcfFileParser(XCFFileParser xcfFileParser) {
+		this.xcfFileParser = xcfFileParser;
+	}
+
+	@Override
+	public List<Device> getDevices(Brand brand) {
+
+		return xcfFileParser.getDevices(brand);
+	}
+
+	@Override
+	public List<CodeSet> getCodeSets(Device device) {
+		return xcfFileParser.getCodeSets(device);
+	}
+
+
+
+
+}
