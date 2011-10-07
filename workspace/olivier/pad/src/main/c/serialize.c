a36232614327503cfc0377e45c3b1e697c964f00
ogandit <ogandit@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-10-07 21:10:59 +0000

    started deserializing requests

--- workspace/olivier/pad/src/main/c/serialize.c
+++ workspace/olivier/pad/src/main/c/serialize.c
@@ -1,24 +1,52 @@
+#include <stdio.h>
+
+#include "apr_pools.h"
 #include "serialize.h"
 
-int readRequest(apr_socket_t *sock) {
-	char buf[2];
-	apr_size_t len = sizeof(buf);
-
-	apr_status_t rv = apr_socket_recv(sock, buf, &len);
-	if (rv == APR_EOF || len == 0) {
-		return -1;
-	}
-
-	printf("reading [%s]\n", (char *) buf);
-	return 0;
+void printMessage(message_t *message) {
+	printf("message code %c\n", message->code);
 }
 
-int writeResponse(apr_socket_t *sock) {
+int setCode(message_t *message, char code) {
+	if(strchr("PSGA", code) == NULL)
+		return R_INVALID_CODE;
+	message->code = (code_t) code;
+	return R_SUCCESS;
+}
+
+int readRequest(apr_socket_t *sock, message_t **message, apr_pool_t *pool) {
+	// Allocate message
+	*message = apr_palloc(pool, sizeof(message_t));
+
+	// Read version
+	char car;
+	int len = 1;
+	apr_status_t rv = apr_socket_recv(sock, &car, &len);
+	if (rv == APR_EOF || len == 0) {
+		return R_INVALID_MESSAGE;
+	}
+	if (car != 'a')
+		return R_INVALID_VERSION;
+
+	// Read message code
+	rv = apr_socket_recv(sock, &car, &len);
+	if (rv == APR_EOF || len == 0) {
+		return R_INVALID_MESSAGE;
+	}
+	rv = setCode(*message, car);
+	if(rv != R_SUCCESS) return rv;
+
+	printMessage(*message);
+
+	return R_SUCCESS;
+}
+
+int writeResponse(apr_socket_t *sock, message_t *message) {
 	const char *buf = "OK";
 	printf("writing [%s]\n", buf);
 
-  int len = strlen(buf);
-  apr_socket_send(sock, buf, &len);
+	int len = strlen(buf);
+	apr_socket_send(sock, buf, &len);
 
 	return 0;
 }
