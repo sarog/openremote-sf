157083e6e9298ad979c6c34e843ed541eb8ceea1
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-10-10 13:54:50 +0000

    Support import of ETS3 group address CSV file

--- workspace/mredeker/UIDesigner_2_0_0_Alphas_ETSImport/src/org/openremote/modeler/utils/KnxImporter.java
+++ workspace/mredeker/UIDesigner_2_0_0_Alphas_ETSImport/src/org/openremote/modeler/utils/KnxImporter.java
@@ -42,107 +42,149 @@ import org.jdom.input.SAXBuilder;
 import org.jdom.xpath.XPath;
 import org.openremote.modeler.domain.KnxGroupAddress;
 
-
 /**
- * The class takes a ets4 export file as inputstream and creates a list of KnxGroupAddress objects
+ * The class takes a ets4 export file as inputstream and creates a list of
+ * KnxGroupAddress objects
  * 
  * @author marcus@openremote.org
  */
-public class KnxImporter {
+public class KnxImporter
+{
 
-    private static final Logger LOGGER = Logger.getLogger(KnxImporter.class);
-    
-    @SuppressWarnings("unchecked")
-    public List<KnxGroupAddress> importETSConfiguration(InputStream inputStream) {
+  private static final Logger LOGGER = Logger.getLogger(KnxImporter.class);
 
-        List<KnxGroupAddress> result = new ArrayList<KnxGroupAddress>();
-        String xmlData = null;
-        DecimalFormat df = new DecimalFormat("000");
-        SAXBuilder builder = new SAXBuilder();
-        Document document = null ;
-        
-        try {
-            ZipInputStream zin = new ZipInputStream(inputStream);
-            ZipEntry zipEntry = zin.getNextEntry();
-            while (zipEntry != null) {
-               if (zipEntry.getName().endsWith("/0.xml")) {
-                  xmlData = convertStreamToString(zin);
-                  break;
-               }
-               zipEntry = zin.getNextEntry();
-            }
-            
-            if (xmlData != null) {
-               // parse the XML as a W3C Document
-               StringReader in = new StringReader(xmlData);
-               document = builder.build(in);
+  @SuppressWarnings("unchecked")
+  public List<KnxGroupAddress> importETS4Configuration(InputStream inputStream)
+  {
 
-               // Query all GroupAddress elements
-               XPath xpath = XPath.newInstance("//knx:GroupAddress");
-               xpath.addNamespace("knx", "http://knx.org/xml/project/10");
-               List<Element> xresult = xpath.selectNodes(document);
-               for (Element element : xresult) {
+    List<KnxGroupAddress> result = new ArrayList<KnxGroupAddress>();
+    String xmlData = null;
+    DecimalFormat df = new DecimalFormat("000");
+    SAXBuilder builder = new SAXBuilder();
+    Document document = null;
 
-                  String id = element.getAttributeValue("Id");
-                  String name = element.getAttributeValue("Name");
-                  String address = element.getAttributeValue("Address");
-                  String dpt = null; 
-                  // Query referenced ComObjectInstanceRef element which holds DPT
-                  xpath = XPath.newInstance("//knx:Send[@GroupAddressRefId='"+ id + "']/../..");
-                  xpath.addNamespace("knx", "http://knx.org/xml/project/10");
-                  List<Element> result2 = xpath.selectNodes(document);
-                  if (result2.size() > 0) {
-                     dpt = result2.get(0).getAttributeValue("DatapointType");
-                     if (dpt != null && StringUtils.isNotEmpty(dpt)) {
-                        StringTokenizer st = new StringTokenizer(dpt, "-");
-                        st.nextElement();
-                        dpt = st.nextToken() + "." + df.format(Integer.parseInt(st.nextToken()));
-                     } else {
-                        dpt = null;
-                     }
-                  }
-                  String levelAddress = getAddressFromInt(Integer.parseInt(address));
-                  result.add(new KnxGroupAddress(dpt, levelAddress, name));
-                  LOGGER.debug("Created GroupAddress: " + levelAddress + " - " + name + " - " + dpt);
-               }
-            }
-        } catch (Exception e) {
-            LOGGER.error("Could not parse ETS4 export file.", e);
+    try
+    {
+      ZipInputStream zin = new ZipInputStream(inputStream);
+      ZipEntry zipEntry = zin.getNextEntry();
+      while (zipEntry != null)
+      {
+        if (zipEntry.getName().endsWith("/0.xml"))
+        {
+          xmlData = convertStreamToString(zin);
+          break;
         }
-        
-        return result;
-    }
+        zipEntry = zin.getNextEntry();
+      }
 
-    
-    
+      if (xmlData != null)
+      {
+        // parse the XML as a W3C Document
+        StringReader in = new StringReader(xmlData);
+        document = builder.build(in);
 
-    public static String convertStreamToString(InputStream is) throws IOException {
-       if (is != null) {
-          Writer writer = new StringWriter();
+        // Query all GroupAddress elements
+        XPath xpath = XPath.newInstance("//knx:GroupAddress");
+        xpath.addNamespace("knx", "http://knx.org/xml/project/10");
+        List<Element> xresult = xpath.selectNodes(document);
+        for (Element element : xresult)
+        {
 
-          char[] buffer = new char[1024];
-          try {
-             Reader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"));
-             int n;
-             while ((n = reader.read(buffer)) != -1) {
-                writer.write(buffer, 0, n);
-             }
-          } finally {
-             is.close();
+          String id = element.getAttributeValue("Id");
+          String name = element.getAttributeValue("Name");
+          String address = element.getAttributeValue("Address");
+          String dpt = null;
+          // Query referenced ComObjectInstanceRef element which holds DPT
+          xpath = XPath.newInstance("//knx:Send[@GroupAddressRefId='" + id + "']/../..");
+          xpath.addNamespace("knx", "http://knx.org/xml/project/10");
+          List<Element> result2 = xpath.selectNodes(document);
+          if (result2.size() > 0)
+          {
+            dpt = result2.get(0).getAttributeValue("DatapointType");
+            if (dpt != null && StringUtils.isNotEmpty(dpt))
+            {
+              StringTokenizer st = new StringTokenizer(dpt, "-");
+              st.nextElement();
+              dpt = st.nextToken() + "." + df.format(Integer.parseInt(st.nextToken()));
+            } else
+            {
+              dpt = null;
+            }
           }
-          return writer.toString();
-       } else {
-          return "";
-       }
+          String levelAddress = getAddressFromInt(Integer.parseInt(address));
+          result.add(new KnxGroupAddress(dpt, levelAddress, name));
+          LOGGER.debug("Created GroupAddress: " + levelAddress + " - " + name + " - " + dpt);
+        }
+      }
+    } catch (Exception e)
+    {
+      LOGGER.error("Could not parse ETS4 export file.", e);
     }
 
-    public static String getAddressFromInt(int knxaddress) {
-       int maingroup, subgroup, group;
-       // extract values
-       maingroup = (knxaddress >> 11) & 0x0f;
-       subgroup = (knxaddress >> 8) & 0x07;
-       group = knxaddress & 0xff;
-       String erg = "" + maingroup + "/" + subgroup + "/" + group;
-       return erg;
+    return result;
+  }
+
+  public List<KnxGroupAddress> importETS3GroupAddressCsvExport(InputStream inputStream)
+  {
+    List<KnxGroupAddress> result = new ArrayList<KnxGroupAddress>();
+    try
+    {
+      String str;
+      BufferedReader in = new BufferedReader(new InputStreamReader(inputStream, "ISO-8859-1"));
+      while ((str = in.readLine()) != null)
+      {
+        StringTokenizer st = new StringTokenizer(str, ";");
+        String name = st.nextToken();
+        String ga = st.nextToken();
+        if (ga.indexOf("-") == -1)
+        {
+          result.add(new KnxGroupAddress(null, ga, name));
+        }
+      }
+
+    } catch (IOException e)
+    {
+      LOGGER.error("Could not parse ETS3 group address csv export file.", e);
     }
+
+    return result;
+  }
+
+  public static String convertStreamToString(InputStream is) throws IOException
+  {
+    if (is != null)
+    {
+      Writer writer = new StringWriter();
+
+      char[] buffer = new char[1024];
+      try
+      {
+        Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        int n;
+        while ((n = reader.read(buffer)) != -1)
+        {
+          writer.write(buffer, 0, n);
+        }
+      } finally
+      {
+        is.close();
+      }
+      return writer.toString();
+    } else
+    {
+      return "";
+    }
+  }
+
+  public static String getAddressFromInt(int knxaddress)
+  {
+    int maingroup, subgroup, group;
+    // extract values
+    maingroup = (knxaddress >> 11) & 0x0f;
+    subgroup = (knxaddress >> 8) & 0x07;
+    group = knxaddress & 0xff;
+    String erg = "" + maingroup + "/" + subgroup + "/" + group;
+    return erg;
+  }
+
 }
