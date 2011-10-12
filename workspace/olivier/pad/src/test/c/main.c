6370c346ee45fbd370a405fc3966aa1d5576eb84
ogandit <ogandit@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-10-12 20:40:25 +0000

    added simple unit test framework

---
+++ workspace/olivier/pad/src/test/c/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "apr_general.h"
+#include "test.h"
+#include "serialize.h"
+
+void testBuf2Int32() {
+  apr_int32_t v;
+  int r = buf2Int32("00000001", &v);
+  ASSERT(r, R_SUCCESS);
+  ASSERT(v, 1);
+  r = buf2Int32("FFFFFFFF", &v);
+  ASSERT(r, R_SUCCESS);
+  ASSERT(v, -1);
+  r = buf2Int32("GFFFFFFF", &v);
+  ASSERT(r, R_INVALID_MESSAGE);
+}
+
+void main(int argc, const char *argv[]) {
+  testBuf2Int32();
+  printf("\n");
+}
