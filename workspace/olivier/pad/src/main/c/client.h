657443671b98069d0d6739007c65fde21232825b
ogandit <ogandit@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-10-28 12:38:19 +0000

    preparing client

--- workspace/olivier/pad/src/main/c/client.h
+++ workspace/olivier/pad/src/main/c/client.h
@@ -1,6 +1,9 @@
 #ifndef _CLIENT_H
 #define _CLIENT_H
 
-int clientReceive(char *portId, char *buf, int len);
+#include "apr_network_io.h"
+#include "transaction.h"
+
+int operatePortData(apr_socket_t *sock, transaction_t *transaction, apr_pool_t *pool, char *portId, char *buf, int len);
 
 #endif
--- workspace/olivier/pad/src/main/c/server.h
+++ workspace/olivier/pad/src/main/c/server.h
@@ -2,17 +2,7 @@
 #define _SERVER_H
 
 #include "apr_network_io.h"
-#include "serialize.h"
-#include "osPort.h"  // TODO keep?
-
-typedef struct _transaction_t {
-	enum {
-		WAITING_FOR_REQUEST, WAITING_FOR_RESPONSE
-	} status;
-	message_t *request;
-	message_t *response;
-	portReceive_t portReceiveCb;
-} transaction_t;
+#include "transaction.h"
 
 int checkInputMessage(apr_socket_t *sock, char *code, messageTxType_t *type);
 int operateRequest(apr_socket_t *sock, transaction_t *transaction, apr_pool_t *pool, char code);
