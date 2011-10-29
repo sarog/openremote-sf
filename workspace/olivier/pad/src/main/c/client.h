5d8182de794c1d8fbdf3e71635f456bc153fe091
ogandit <ogandit@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-10-29 09:59:19 +0000

    ready to test data exchanges on serial port on linux

--- workspace/olivier/pad/src/main/c/client.h
+++ workspace/olivier/pad/src/main/c/client.h
@@ -1,9 +1,19 @@
 #ifndef _CLIENT_H
 #define _CLIENT_H
 
-#include "apr_network_io.h"
-#include "transaction.h"
+#include "apr_thread_cond.h"
+#include "serialize.h"
 
-int operatePortData(apr_socket_t *sock, transaction_t *transaction, apr_pool_t *pool, char *portId, char *buf, int len);
+typedef struct _clientTransaction_t {
+	message_t *request;
+	message_t *response;
+	apr_thread_cond_t *cond;
+	apr_thread_mutex_t *mutex;
+} clientTransaction_t;
+
+int createClientTransaction(apr_pool_t *pool, clientTransaction_t **tx);
+void clearClientTransaction(apr_pool_t *pool, clientTransaction_t **tx);
+int operatePortData(clientTransaction_t *clientTx, apr_pool_t *pool, char *portId, char *buf, int len);
+int operateResponse(apr_socket_t *sock, clientTransaction_t *clientTx, apr_pool_t *pool, char code);
 
 #endif
