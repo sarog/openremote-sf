00be346377f4e34ae9bfeb745b93cdb33a58ca46
kurrazyman <kurrazyman@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-11-14 20:42:07 +0000

    UDP Read Functionality added

--- branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/protocol/socket/UDPSocketCommand.java
+++ branches/project/Controller_2_0_0_Alphas/src/org/openremote/controller/protocol/socket/UDPSocketCommand.java
@@ -26,6 +26,10 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 import org.apache.log4j.Logger;
 import org.openremote.controller.command.ExecutableCommand;
 import org.openremote.controller.command.StatusCommand;
@@ -41,6 +45,9 @@ public class UDPSocketCommand implements ExecutableCommand, StatusCommand {
    /** The logger. */
    private static Logger logger = Logger.getLogger(UDPSocketCommand.class.getName());
 
+   /** The default timeout used to wait for a result */
+   public static final int DEFAULT_TIMEOUT = 1;
+   
    /** A name to identify event in controller.xml. */
    private String name;
 
@@ -52,8 +59,22 @@ public class UDPSocketCommand implements ExecutableCommand, StatusCommand {
 
    /** The port that is opened */
    private String port;
+   
+   /** The response from read command */
+   private String response;
 
-
+   /** The response filter */
+   private String responseFilter;
+   
+   /** The response fliter group */
+   private Integer responseFilterGroup = 0;
+   
+   /** The status default response */
+   private String statusDefault;
+   
+   /** The wait timeout period in seconds */
+   private Integer timeOut = DEFAULT_TIMEOUT;   
+   
    /**
     * Gets the command.
     *
@@ -122,32 +143,210 @@ public class UDPSocketCommand implements ExecutableCommand, StatusCommand {
    public void setPort(String port) {
       this.port = port;
    }
+   
+   /**
+    * Sets the response string 
+    * @param response the new response
+    */
+   public void setResponse(String response) {
+      this.response = response;
+   }
+
+   /**
+    * Gets the response 
+    * @return the response
+    */
+   public String getResponse() {
+      return response;
+   }
+   
+   /**
+    * Sets the timeOut
+    * @param timeOut the new timeOut
+    */   
+   public void setTimeOut(String timeOut) {
+      try {
+         Integer tempValue = Integer.parseInt(timeOut.trim());
+         if (tempValue > 0) {
+            this.timeOut = tempValue;
+         }
+      }
+      catch (NumberFormatException e) {
+         logger.error("time out property in controller.xml is not an integer", e);
+      }
+   }
+   
+   /**
+    * Gets the timeOut
+    * @return the timeOut
+    */
+   public Integer getTimeOut() {
+      return timeOut;  
+   }
+   
+   /**
+    * Sets the response filter
+    * @param responseFilter the new responseFilter
+    */   
+   public void setResponseFilter(String responseFilter) {
+      this.responseFilter = responseFilter;
+   }
+   
+   /**
+    * Gets the responseFilter
+    * @return the responseFilter
+    */
+   public String getResponseFilter() {
+      return responseFilter;  
+   }
+   
+   /**
+    * Sets the response filter group
+    * @param responseFilterGroup the new responseFilterGroup
+    */
+   public void setResponseFilterGroup(String responseFilterGroup) {
+      try {
+         this.responseFilterGroup = Integer.parseInt(responseFilterGroup.trim());
+      }
+      catch (NumberFormatException e) {
+         logger.error("filter group property in controller.xml is not an integer", e);
+      }
+   }
+
+   /**
+    * Gets the response filter group
+    * @return the response
+    */
+   public Integer getResponseFilterGroup() {
+      return responseFilterGroup;   
+   }
+
+   /**
+    * Sets the status default
+    * @param statusDefault the new statusDefault
+    */   
+   public void setStatusDefault(String statusDefault) {
+      this.statusDefault = statusDefault;  
+   }
+   
+   /**
+    * Gets the statusDefault
+    * @return the statusDefault
+    */
+   public String getStatusDefault() {
+      return statusDefault;
+   }
 
    /**
     * {@inheritDoc}
     */
    @Override
    public void send() {
+      send(false);
+   }
+
+   public void send(boolean readResponse) {
+      DatagramSocket socket = null;
+      if (readResponse) {
+         setResponse("");
+      }
       try {
-      	// Create socket no need to specify port for sending
-      	DatagramSocket socket = new DatagramSocket();
+      	// Create socket no need to specify port; port info is in the packet
+      	socket = new DatagramSocket();
       	
       	// Create packet
       	InetAddress address = InetAddress.getByName(getIp());
-      	int port = Integer.parseInt(getPort());
+         int port = Integer.parseInt(getPort());
       	byte[] bytes = getCommand().getBytes();
       	DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, port);
       	
       	// Send the packet
       	socket.send(packet);
+      	
+      	// Read the response if necessary
+         if (readResponse) {
+            byte[] buf = new byte[512];
+            packet = new DatagramPacket(buf, buf.length);
+            socket.setSoTimeout(getTimeOut());
+            socket.receive(packet);
+            response = new String(packet.getData(), 0, packet.getLength());
+         }
       } catch (Exception e) {
-      
+         logger.error("could not perform UDP Event", e);
+      } finally {
+         if (socket != null) {
+            socket.disconnect();            
+         }
       }
    }
 
    @Override
-   public String read(EnumSensorType sensoryType, Map<String, String> stateMap) {
-      return "";
+   public String read(EnumSensorType sensorType, Map<String, String> stateMap) {
+      String readResponse = statusDefault;
+      String filteredResponse = "";
+      send(true);
+      
+      try {
+         if ("".equals(getResponseFilter()) || getResponseFilter() == null) {
+            filteredResponse = getResponse();
+         } else {
+            Pattern p = Pattern.compile(getResponseFilter(), Pattern.CANON_EQ | Pattern.UNICODE_CASE);
+            Matcher m = p.matcher(getResponse());
+            boolean b = m.matches();
+            if (b) {
+               String matchedGroup = m.group(getResponseFilterGroup());
+               if (matchedGroup != null) {
+                  filteredResponse = matchedGroup;  
+               }
+            } else {
+               logger.error("UDP Read Status: No Match using Regex: '" + getResponseFilter() + "' on response from command '" + getCommand() + "'");
+            } 
+         }            
+      }
+      catch (PatternSyntaxException e) {
+         System.out.println("Telnet Read Status: REGEX ERROR");
+         logger.error("UDP Read Status: Invalid filter expression", e);
+      }
+      
+      if (!"".equals(filteredResponse)) {
+         switch (sensorType) {
+            // Switch: on or off response needed
+            case SWITCH:
+               filteredResponse.replaceAll("1|on", "true");
+               Boolean bool = Boolean.parseBoolean(filteredResponse);
+               if (bool) {
+                  readResponse = "on";
+               } else {
+                  readResponse = "off";  
+               }
+               break;
+            case LEVEL:
+            case RANGE:
+               try {
+                  Integer intVal = Integer.parseInt(filteredResponse);
+                  readResponse = filteredResponse;
+               }
+               catch (PatternSyntaxException e) {
+                  logger.info("Can't convert filteredResponse to type Integer: " + e);
+               }
+               break;
+            case CUSTOM:
+               readResponse = filteredResponse;
+               for (String stateKey : stateMap.keySet())
+               {
+                 String state = stateMap.get(stateKey);
+                 if (filteredResponse.equals(state)) {
+                    readResponse = stateKey;
+                    break;
+                 }
+               }
+               break;
+            default:
+               readResponse = filteredResponse;  
+         }
+      }
+
+      return readResponse;
    }
 
 }
