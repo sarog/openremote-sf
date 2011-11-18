de770fdee0157218e61acbca299a8a4e11a56b80
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-11-18 15:40:14 +0000

    First go at Domintell protocol support. DBIR working, must clean code a bit.

---
+++ workspace/ebariaux/Controller_EP_SNAP_20111102_Domintell/src/org/openremote/controller/protocol/domintell/DomintellCommand.java
@@ -0,0 +1,179 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */package org.openremote.controller.protocol.domintell;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.openremote.controller.command.Command;
+import org.openremote.controller.exception.NoSuchCommandException;
+import org.openremote.controller.model.sensor.Sensor;
+import org.openremote.controller.protocol.domintell.model.DomintellModule;
+
+/**
+ * 
+ * @author <a href="mailto:eric@openremote.org">Eric Bariaux</a>
+ *
+ */
+public abstract class DomintellCommand implements Command {
+
+   // Class Members --------------------------------------------------------------------------------
+
+   /**
+    * Domintell logger. Uses a common category for all Domintell related logging.
+    */
+   private final static Logger log = Logger.getLogger(DomintellCommandBuilder.DOMINTELL_LOG_CATEGORY);
+
+   // Keep a list of all the command strings we receive from OR Console and the associated command class to handle that command
+   private static HashMap<String, Class<? extends DomintellCommand>> commandClasses = new HashMap<String, Class<? extends DomintellCommand>>();
+
+   static {
+      commandClasses.put("ON", RelayCommand.class);
+      commandClasses.put("OFF", RelayCommand.class);
+      commandClasses.put("TOGGLE", RelayCommand.class);
+      commandClasses.put("STATUS", RelayCommand.class);
+   }
+
+   /**
+    * Factory method for creating Domintell command instances based on a
+    * human-readable configuration strings.
+    * 
+    * @return new Domintell command instance
+    */
+   static DomintellCommand createCommand(String name, DomintellGateway gateway, String moduleType, DomintellAddress address, Integer output) {
+    log.debug("Received request to build command with name " + name);
+    
+      name = name.trim().toUpperCase();
+      Class<? extends DomintellCommand> commandClass = commandClasses.get(name);
+
+      log.debug("This command maps to the command class " + commandClass);
+
+      if (commandClass == null) {
+         throw new NoSuchCommandException("Unknown command '" + name + "'.");
+      }
+      DomintellCommand cmd = null;
+      try {
+         Method method = commandClass.getMethod("createCommand", String.class, DomintellGateway.class, String.class, DomintellAddress.class, Integer.class);
+         log.debug("Got the creation method " + method + ", will call it");
+         
+         cmd = (DomintellCommand) method.invoke(null, name, gateway, moduleType, address, output);
+         log.debug("Creation successfull, got command " + cmd);
+      } catch (SecurityException e) {
+         // TODO: should this be logged, check other source code
+         throw new NoSuchCommandException("Impossible to create command '" + name + "'.");
+      } catch (NoSuchMethodException e) {
+         // TODO: should this be logged, check other source code
+         throw new NoSuchCommandException("Impossible to create command '" + name + "'.");
+      } catch (IllegalArgumentException e) {
+         // TODO: should this be logged, check other source code
+         throw new NoSuchCommandException("Impossible to create command '" + name + "'.");
+      } catch (IllegalAccessException e) {
+         // TODO: should this be logged, check other source code
+         throw new NoSuchCommandException("Impossible to create command '" + name + "'.");
+      } catch (InvocationTargetException e) {
+        if (e.getCause() instanceof NoSuchCommandException) {       
+        // This means method threw an exception, re-throw it as is
+        throw (NoSuchCommandException)e.getCause();
+        } else {
+          throw new NoSuchCommandException("Impossible to create command '" + name + "'.");
+        }
+      // TODO: should this be logged, check other source code
+      }
+      return cmd;
+   }
+
+   // Instance Fields ------------------------------------------------------------------------------
+
+   /**
+    * Gateway to be used to transmit this command.
+    */
+   protected DomintellGateway gateway;
+
+   /**
+    * Name of the command
+    */
+   protected String name;
+   
+   protected String moduleType;
+   
+   protected DomintellAddress address;
+   
+   /**
+    * Sensors we are a command of
+    */
+   protected List<Sensor> sensors = new ArrayList<Sensor>();
+
+   // Constructors ---------------------------------------------------------------------------------
+
+   /**
+    * Constructs a Domintell command with a given gateway.
+    * 
+    * @param gateway
+    *            Domintell gateway instance used for transmitting this command
+    */
+   public DomintellCommand(String name, DomintellGateway gateway, String moduleType, DomintellAddress address) {
+      this.name = name;
+      this.gateway = gateway;
+      this.moduleType = moduleType;
+      this.address = address;
+   }
+   
+   /**
+    * Add a sensor to update on value change.
+    * 
+    * @param sensor Sensor to add
+    */
+   public void addSensor(Sensor sensor) {
+      sensors.add(sensor);
+   }
+   
+   /**
+    * Remove a sensor to update on value change.
+    * 
+    * @param sensor Sensor to remove
+    */
+   public void removeSensor(Sensor sensor) {
+      sensors.remove(sensor);
+   }
+   
+   /**
+    * Update all registered sensor (because of value change on given device).
+    * 
+    * @param device DomintellModule that triggers the update
+    */
+   public void updateSensors(DomintellModule module) {
+      for (Sensor sensor : sensors) {
+         updateSensor(module, sensor);
+      }     
+   }
+
+   /**
+    * Update the specified sensor based on value of given device.
+    * 
+    * @param device DomintellModule that triggers the update
+    * @param sensor Sensor to update
+    */
+   abstract protected  void updateSensor(DomintellModule module, Sensor sensor);
+
+}
---
+++ workspace/ebariaux/Controller_EP_SNAP_20111102_Domintell/src/org/openremote/controller/protocol/domintell/model/RelayModule.java
@@ -0,0 +1,67 @@
+package org.openremote.controller.protocol.domintell.model;
+
+import org.apache.log4j.Logger;
+import org.openremote.controller.protocol.domintell.DomintellAddress;
+import org.openremote.controller.protocol.domintell.DomintellCommandBuilder;
+import org.openremote.controller.protocol.domintell.DomintellGateway;
+
+public class RelayModule extends DomintellModule implements Relay {
+
+   /**
+    * Domintell logger. Uses a common category for all Domintell related logging.
+    */
+   private final static Logger log = Logger.getLogger(DomintellCommandBuilder.DOMINTELL_LOG_CATEGORY);
+
+   private boolean[] state = new boolean[8];
+   
+   public RelayModule(DomintellGateway gateway, String moduleType, DomintellAddress address) {
+      super(gateway, moduleType, address);
+   }
+   
+   @Override
+   public void on(Integer output) {
+      // moduleType is supposed to be BIR
+      // address should be in hex, formatted on 6 characters
+      
+      gateway.sendCommand(moduleType + address + "-" + Integer.toString(output) + "%I");
+   }
+
+   @Override
+   public void off(Integer output) {
+      gateway.sendCommand(moduleType + address + "-" + Integer.toString(output) + "%O");
+   }
+
+   @Override
+   public void toggle(Integer output) {
+      gateway.sendCommand(moduleType + address + "-" + Integer.toString(output));
+   }
+
+   @Override
+   public void queryState(Integer output) {
+      gateway.sendCommand(moduleType + address + "-" + Integer.toString(output) + "%S");
+   }
+   
+   // Feedback method from HomeWorksDevice ---------------------------------------------------------
+
+   @Override
+   public void processUpdate(String info) {
+     try {
+        // O00
+        int value = Integer.parseInt(info.substring(1), 16);
+        int bitmask = 1;
+        for (int i = 0; i < 8; i++) {
+           state[i] = (value & bitmask) == bitmask;
+           bitmask = bitmask<<1;
+        }
+     } catch (NumberFormatException e) {
+       // Not understood as a scene, do not update ourself
+       log.warn("Invalid feedback received " + info, e);
+     }
+       
+     super.processUpdate(info);
+   }
+
+   public boolean getState(int output) {
+      return state[output - 1];
+   }
+}
