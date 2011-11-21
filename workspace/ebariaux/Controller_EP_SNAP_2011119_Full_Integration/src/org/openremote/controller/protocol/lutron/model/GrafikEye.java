40031101f6a100d2aaa112d477f49d7eeeeeca47
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-11-21 14:52:55 +0000

    Merged Lutron improvements (workspace/ebariaux/Controller_EP_SNAP_2011119_2_0_Integration -r5280:5282)

--- workspace/ebariaux/Controller_EP_SNAP_2011119_Full_Integration/src/org/openremote/controller/protocol/lutron/model/GrafikEye.java
+++ workspace/ebariaux/Controller_EP_SNAP_2011119_Full_Integration/src/org/openremote/controller/protocol/lutron/model/GrafikEye.java
@@ -18,9 +18,12 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.openremote.controller.protocol.lutron;
+package org.openremote.controller.protocol.lutron.model;
 
 import org.apache.log4j.Logger;
+import org.openremote.controller.protocol.lutron.LutronHomeWorksAddress;
+import org.openremote.controller.protocol.lutron.LutronHomeWorksCommandBuilder;
+import org.openremote.controller.protocol.lutron.LutronHomeWorksGateway;
 
 /**
  * Represents a GRAFIK Eye component on the Lutron bus.
@@ -78,6 +81,8 @@ public class GrafikEye extends HomeWorksDevice {
 	    // Not understood as a scene, do not update ourself
 	    log.warn("Invalid feedback received " + info, e);
 	  }
+	    
+     super.processUpdate(info);
 	}
 
 	// Getters/Setters ------------------------------------------------------------------------------
