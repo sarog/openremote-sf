be97fc7e886c75b6892be5b7f99cecb2fe3a05c1
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-12-01 19:10:46 +0000

    Merge R4596 from /workspace/olivier: generic bus abstraction for physical transport connections -- Added an abstraction of physical buses to allow mocking KNX physical network. This should be extensible to other buses.

--- workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/IpDiscoverer.java
+++ workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/IpDiscoverer.java
@@ -1,26 +1,23 @@
 /*
- * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
- *
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * OpenRemote, the Home of the Digital Home. Copyright 2008-2011, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a full listing of individual contributors.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package org.openremote.controller.protocol.knx.ip;
 
 import java.io.IOException;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
@@ -34,33 +31,26 @@ import org.openremote.controller.protocol.knx.ip.message.IpMessage;
 
 public class IpDiscoverer implements IpProcessorListener {
    private IpProcessor processor;
-   private MulticastSocket multicastSocket;
    private DiscoveryListener discoveryListener;
+   private InetAddress srcAddr;
 
    public IpDiscoverer(InetAddress srcAddr, DiscoveryListener discoveryListener) {
       this.discoveryListener = discoveryListener;
-      this.processor = new IpProcessor(srcAddr, this);
+      this.srcAddr = srcAddr;
+      this.processor = new IpProcessor(this);
    }
 
    public void start() throws KnxIpException, IOException, InterruptedException {
-      this.multicastSocket = new MulticastSocket();
-      try {
-         InetSocketAddress groupAddr = new InetSocketAddress(InetAddress.getByName("224.0.23.12"), 3671);
-         this.multicastSocket.joinGroup(groupAddr.getAddress());
+      InetAddress multicastAddr = InetAddress.getByName("224.0.23.12");
+      this.processor.start("discovery", this.srcAddr, IpDiscoverer.getOutSocket(srcAddr, multicastAddr));
 
-         this.processor.start("discovery");
-
-         // Start discovery process
-         // TODO send regularly requests until a response is received
-         this.processor.send(new IpDiscoverReq(new Hpai(this.processor.getSrcAddr())), groupAddr, this.multicastSocket);
-      } catch (UnknownHostException e) {
-         throw new KnxIpException(Code.unknownHost, e.getMessage());
-      }
+      // Start discovery process
+      // TODO send regularly requests until a response is received
+      this.processor.send(new IpDiscoverReq(new Hpai((InetSocketAddress) this.processor.getSrcSocketAddr())),
+            new InetSocketAddress(multicastAddr, 3671));
    }
 
    public void stop() throws InterruptedException {
-      this.multicastSocket.close();
-
       this.processor.stop();
    }
 
@@ -73,7 +63,20 @@ public class IpDiscoverer implements IpProcessorListener {
    }
 
    public InetAddress getSrcAddr() {
-      return this.processor.getSrcAddr().getAddress();
+      return this.processor.getSrcSocketAddr().getAddress();
    }
 
+   private static DatagramSocket getOutSocket(InetAddress srcAddr, InetAddress multicastAddr) throws IOException,
+         KnxIpException {
+      DatagramSocket s = null;
+      if (multicastAddr != null) {
+         s = new MulticastSocket(new InetSocketAddress(srcAddr, 0));
+         try {
+            ((MulticastSocket) s).joinGroup(multicastAddr);
+         } catch (UnknownHostException e) {
+            throw new KnxIpException(Code.unknownHost, e.getMessage());
+         }
+      }
+      return s;
+   }
 }
