1d9335fba64c32329d482440a19570cf31404ce3
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-12-03 09:47:46 +0000

    Merge R4860 from /workspace/olivier : added KNX confirmations for KNX runtime requests in the physical bus mock implementation

--- workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/PhysicalBusMock.java
+++ workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/PhysicalBusMock.java
@@ -1,8 +1,12 @@
 package org.openremote.controller.protocol.knx.ip;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.openremote.controller.protocol.bus.DatagramSocketMessage;
 import org.openremote.controller.protocol.bus.Message;
@@ -11,6 +15,7 @@ import org.openremote.controller.protocol.knx.ip.message.IpConnectReq;
 import org.openremote.controller.protocol.knx.ip.message.IpConnectionStateReq;
 import org.openremote.controller.protocol.knx.ip.message.IpDisconnectReq;
 import org.openremote.controller.protocol.knx.ip.message.IpDiscoverReq;
+import org.openremote.controller.protocol.knx.ip.message.IpTunnelingAck;
 import org.openremote.controller.protocol.knx.ip.message.IpTunnelingReq;
 
 // TODO add a KNX response send mechanism
@@ -22,12 +27,18 @@ public class PhysicalBusMock implements PhysicalBus {
    private static final byte[] DISCONNECT_RESP = new byte[] { 0x06, 0x10, 0x02, 0x0A, 0x00, 0x08, 0x15, 0x00 };
    private static final byte[] CONNECTIONSTATE_RESP = new byte[] { 0x06, 0x10, 0x02, 0x08, 0x00, 0x08, 0x15, 0x00 };
    private static final byte[] TUNNELING_ACK = new byte[] { 0x06, 0x10, 0x04, 0x21, 0x00, 0x0A, 0x04, 0x15, 0x00, 0x00 };
-   private byte[] resp;
    private InetSocketAddress srcAddr;
+   private byte seq;
+   private Queue<byte[]> notifications;
+
+   public PhysicalBusMock() {
+      this.notifications = new ConcurrentLinkedQueue<byte[]>();
+   }
 
    @Override
    public void start(Object inChannel, Object outChannel) {
       this.srcAddr = (InetSocketAddress) ((DatagramSocket) inChannel).getLocalSocketAddress();
+      this.seq = 0;
    }
 
    @Override
@@ -40,38 +51,56 @@ public class PhysicalBusMock implements PhysicalBus {
       int sti = (req[2] << 8) + req[3];
       switch (sti) {
       case IpDiscoverReq.STI:
-         this.sendResponse(DISCOVER_RESP);
+         this.notifyTelegram(DISCOVER_RESP);
          break;
       case IpConnectReq.STI:
-         this.sendResponse(CONNECT_RESP);
+         this.notifyTelegram(CONNECT_RESP);
          break;
       case IpDisconnectReq.STI:
-         this.sendResponse(DISCONNECT_RESP);
+         this.notifyTelegram(DISCONNECT_RESP);
          break;
       case IpConnectionStateReq.STI:
-         this.sendResponse(CONNECTIONSTATE_RESP);
+         this.notifyTelegram(CONNECTIONSTATE_RESP);
+         break;
+      case IpTunnelingAck.STI:
+         this.seq++;
          break;
       case IpTunnelingReq.STI:
          byte[] resp = TUNNELING_ACK;
          resp[8] = req[8];
-         this.sendResponse(TUNNELING_ACK);
+         this.notifyTelegram(TUNNELING_ACK);
+
+         // Notify a con telegram for GroupValue_Write.req and GroupValue_Read.req
+         if ((((req[19] & 0x3) == 0) && ((req[20] & 0xC0) == 0x80))
+               || (((req[19] & 0x3) == 0) && ((req[20] & 0xC0) == 0x00))) {
+            
+            // Prepare confirmation telegram
+            byte[] con = Arrays.copyOfRange(req, 10, req.length);
+            con[0] = 0x2E;
+            IpTunnelingReq r = new IpTunnelingReq(req[7], this.seq, con);
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            r.write(os);
+            this.notifyTelegram(os.toByteArray());
+         }
          break;
       }
    }
 
-   private void sendResponse(byte[] message) {
-      synchronized (this) {
-         this.resp = message;
-         this.notify();
+   private void notifyTelegram(byte[] message) {
+      synchronized (this.notifications) {
+         this.notifications.add(message);
+         this.notifications.notify();
       }
    }
 
    @Override
    public Message receive() throws IOException {
-      synchronized (this) {
+      synchronized (this.notifications) {
+         byte[] m = this.notifications.poll();
+         if (m != null) return new DatagramSocketMessage(this.srcAddr, m);
          try {
-            this.wait();
-            return new DatagramSocketMessage(this.srcAddr, this.resp);
+            this.notifications.wait();
+            return new DatagramSocketMessage(this.srcAddr, this.notifications.poll());
          } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
          }
