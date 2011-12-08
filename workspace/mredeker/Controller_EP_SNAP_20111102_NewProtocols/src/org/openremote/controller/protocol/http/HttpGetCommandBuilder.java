537f33b9cd0bc64476755e0c6392aee4ec147df1
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-12-08 23:46:08 +0000

    Update HttpProtocol to EventListner, support XPath value extraction and pollingInterval

--- workspace/mredeker/Controller_EP_SNAP_20111102_NewProtocols/src/org/openremote/controller/protocol/http/HttpGetCommandBuilder.java
+++ workspace/mredeker/Controller_EP_SNAP_20111102_NewProtocols/src/org/openremote/controller/protocol/http/HttpGetCommandBuilder.java
@@ -1,142 +1,166 @@
 /*
- * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
- *
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * OpenRemote, the Home of the Digital Home. Copyright 2008-2011, OpenRemote Inc.
+ * 
+ * See the contributors.txt file in the distribution for a full listing of individual contributors.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package org.openremote.controller.protocol.http;
 
-import java.util.List;
-import java.util.logging.Logger;
-import java.net.URL;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 import org.jdom.Element;
+import org.openremote.controller.Constants;
 import org.openremote.controller.command.Command;
 import org.openremote.controller.command.CommandBuilder;
-import org.openremote.controller.utils.CommandUtil;
-import org.openremote.controller.Constants;
 import org.openremote.controller.exception.NoSuchCommandException;
-
+import org.openremote.controller.utils.Logger;
+import org.openremote.controller.utils.Strings;
 
 /**
- * Builds HTTP GET command from XML element.
- * example:
+ * Builds HTTP GET command from XML element. example:
+ * 
  * <pre>
  * {@code 
- * <command id="xxx" protocol="httpGet">
- *      <property name="url" value="http://127.0.0.1:8080/xxx/light1_on" />
- * </command>
+ * &lt;command id=&quot;xxx&quot; protocol=&quot;httpGet&quot;&gt;
+ *      &lt;property name=&quot;url&quot; value=&quot;http://127.0.0.1:8080/xxx/light1_on&quot; /&gt;
+ * &lt;/command&gt;
  * }
  * </pre>
- *
+ * 
  * @author Marcus 2009-4-26
  * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  */
 public class HttpGetCommandBuilder implements CommandBuilder
 {
 
-
-  // Constants ------------------------------------------------------------------------------------
+  // Constants
+  // ------------------------------------------------------------------
+  // ------------------
 
   /**
    * Common log category name for all HTTP protocol related logging.
    */
-  public final static String HTTP_PROTOCOL_LOG_CATEGORY =
-      Constants.CONTROLLER_PROTOCOL_LOG_CATEGORY + "http";
+  public final static String HTTP_PROTOCOL_LOG_CATEGORY = Constants.CONTROLLER_PROTOCOL_LOG_CATEGORY + "http";
 
-
-  // Class Members --------------------------------------------------------------------------------
+  private final static String STR_ATTRIBUTE_NAME_URL = "url";
+  private final static String STR_ATTRIBUTE_NAME_USERNAME = "username";
+  private final static String STR_ATTRIBUTE_NAME_PASSWORD = "password";
+  private final static String STR_ATTRIBUTE_NAME_XPATH = "xpath";
+  private final static String STR_ATTRIBUTE_NAME_REGEX = "regex";
+  private final static String STR_ATTRIBUTE_NAME_INTERVAL = "interval";
+  // Class Members
+  // --------------------------------------------------------------
+  // ------------------
 
   /**
    * Logger for this HTTP protocol implementation.
    */
-  private final static Logger log = Logger.getLogger(HTTP_PROTOCOL_LOG_CATEGORY);
+  private final static Logger logger = Logger.getLogger(HTTP_PROTOCOL_LOG_CATEGORY);
 
+  // Implements CommandBuilder
+  // --------------------------------------------------------------------
 
-  // Implements CommandBuilder --------------------------------------------------------------------
-  
-   @SuppressWarnings("unchecked")
-   public Command build(Element element)
-   {
-      List<Element> propertyEles = element.getChildren("property", element.getNamespace());
+  @SuppressWarnings("unchecked")
+  public Command build(Element element)
+  {
+    logger.debug("Building HttGetCommand");
+    List<Element> propertyEles = element.getChildren("property", element.getNamespace());
 
-     String urlAsString = null;
-     String username = null;
-     String password = null;
+    String urlAsString = null;
+    String username = null;
+    String password = null;
+    String xpath = null;
+    String regex = null;
+    String interval = null;
+    Integer intervalInMillis = null;
 
-      for(Element ele : propertyEles)
+    // read values from config xml
+
+    for (Element ele : propertyEles)
+    {
+      String elementName = ele.getAttributeValue(CommandBuilder.XML_ATTRIBUTENAME_NAME);
+      String elementValue = ele.getAttributeValue(CommandBuilder.XML_ATTRIBUTENAME_VALUE);
+
+      if (STR_ATTRIBUTE_NAME_URL.equals(elementName))
       {
-         if("url".equals(ele.getAttributeValue("name")))
-         {
-           urlAsString = CommandUtil.parseStringWithParam(element, ele.getAttributeValue("value"));
-         }
-
-         else if("name".equals(ele.getAttributeValue("name")))
-         {
-           // getCmd.setName(ele.getAttributeValue("value"));
-         }
-
-         else if ("username".equals(ele.getAttributeValue("name")))
-         {
-           username = ele.getAttributeValue("value");
-         }
-
-         /**************************************************************************************
-          *
-          * TODO:
-          *
-          * THE USE OF PASSWORDS IN CONFIGURATION IS INHERENTLY UNSAFE AND SHOULD BE AVOIDED
-          *
-          * We need a better mechanism to handle sensitive configuration data.
-          *
-          **************************************************************************************/
-         else if ("password".equals(ele.getAttributeValue("name")))
-         {
-           password = ele.getAttributeValue("value");
-         }
+        urlAsString = elementValue;
+        logger.debug("HttpGetCommand: url = " + urlAsString);
+      } else if (STR_ATTRIBUTE_NAME_USERNAME.equals(elementName))
+      {
+        username = elementValue;
+        logger.debug("HttpGetCommand: username = " + username);
+      } else if (STR_ATTRIBUTE_NAME_PASSWORD.equals(elementName))
+      {
+        /**************************************************************************************
+         * TODO:
+         * 
+         * THE USE OF PASSWORDS IN CONFIGURATION IS INHERENTLY UNSAFE AND SHOULD
+         * BE AVOIDED
+         * 
+         * We need a better mechanism to handle sensitive configuration data.
+         * 
+         **************************************************************************************/
+        password = elementValue;
+        logger.debug("HttpGetCommand: password = " + password);
+      } else if (STR_ATTRIBUTE_NAME_INTERVAL.equals(elementName))
+      {
+        interval = elementValue;
+        logger.debug("HttpGetCommand: interval = " + interval);
+      } else if (STR_ATTRIBUTE_NAME_REGEX.equals(elementName))
+      {
+        regex = elementValue;
+        logger.debug("HttpGetCommand: regex = " + regex);
+      } else if (STR_ATTRIBUTE_NAME_XPATH.equals(elementName))
+      {
+        xpath = elementValue;
+        logger.debug("HttpGetCommand: xpath = " + xpath);
       }
+    }
 
-     URL url;
+    if (null != xpath && null != regex)
+    {
+      throw new NoSuchCommandException("Unable to create HttpGet command, only one of either Regex or Xpath allowed!");
+    }
+    
+    try
+    {
+      if (null != interval) {
+        intervalInMillis = Integer.valueOf(Strings.convertPollingIntervalString(interval));
+      }
+    } catch (Exception e1)
+    {
+      throw new NoSuchCommandException("Unable to create HttpGet command, interval could not be converted into milliseconds");
+    }
 
-     try
-     {
-       url = new URL(urlAsString);
+    URL url;
 
-       if (username == null || password == null)
-       {
-         return new HttpGetCommand(url);
-       }
+    try
+    {
+      url = new URL(urlAsString);
+    } catch (MalformedURLException e)
+    {
+      throw new NoSuchCommandException("Invalid URL: " + e.getMessage(), e);
+    }
 
-       else
-       {
-         return new HttpGetCommand(url, username, password.getBytes());
-       }
-     }
-     catch (MalformedURLException e)
-     {
-       // TODO:
-       //
-       //  bind the JUL logging to runtime log implementation...
-
-       //log.warn("Configuration error in HTTP protocol URL: " + e.getMessage(), e);
-
-       throw new NoSuchCommandException("Invalid URL: " + e.getMessage(), e);
-     }
-   }
+    if (null != username && null != password)
+    {
+      return new HttpGetCommand(url, username, password.getBytes(), xpath, regex, intervalInMillis);
+    } else
+    {
+      return new HttpGetCommand(url, xpath, regex, intervalInMillis);
+    }
+  }
 
 }
