17f2e7952879a6d4a8f2d4ec50c4a7c7b1288d37
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-12-20 15:11:05 +0000

    add license headers and author notices

--- workspace/juha/UIDesigner_2_0_0_Diogenes_KNXImport/src/org/openremote/modeler/client/utils/AutoCommitCheckColumnConfig.java
+++ workspace/juha/UIDesigner_2_0_0_Diogenes_KNXImport/src/org/openremote/modeler/client/utils/AutoCommitCheckColumnConfig.java
@@ -1,9 +1,32 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.modeler.client.utils;
 
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.event.GridEvent;
 import com.extjs.gxt.ui.client.widget.grid.CheckColumnConfig;
 
+/**
+ * @author marcus@openremote.org
+ */
 public class AutoCommitCheckColumnConfig extends CheckColumnConfig {
 
 
