92d92924696ea054ac7acc904fba066553ab9a2d
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2011-12-22 14:59:53 +0000

    Using Enum types in client also

--- workspace/ebariaux/UIDesigner_2_0_0_Alphas_HomeWorksImport/src/org/openremote/modeler/server/lutron/importmodel/Output.java
+++ workspace/ebariaux/UIDesigner_2_0_0_Alphas_HomeWorksImport/src/org/openremote/modeler/server/lutron/importmodel/Output.java
@@ -1,43 +1,8 @@
 package org.openremote.modeler.server.lutron.importmodel;
 
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
+import org.openremote.modeler.shared.lutron.OutputType;
 
 public class Output {
-
-  public enum OutputType {
-    Dimmer("DIMMER"),
-    Switch("SWITCH"),
-    Motor("MOTOR"),
-    GrafikEyeMainUnit("GRX MAIN UNIT"),
-    Fan("FAN"),
-    MaintainedOutput("MAINTAINED OUTPUT"),
-    PulsedOutput("PULSED OUTPUT"),
-    QEDShade("QED SHADE"),
-    SivoiaShadeKeypad("SIVOIA SHADE KEYPAD");
-    
-    private static final Map<String, OutputType> lookup = new HashMap<String, OutputType>();
-
-    static {
-      for(OutputType t : EnumSet.allOf(OutputType.class))
-         lookup.put(t.getXmlValue(), t);
-    }
-
-    private String xmlValue;
-    
-    public String getXmlValue() {
-      return xmlValue;
-    }
-
-    private OutputType(String xmlValue) {
-      this.xmlValue = xmlValue;
-    }
-    
-    public static OutputType getFromXmlValue(String xmlValue) {
-      return lookup.get(xmlValue);
-    }
-  };
   
   private String name;
   private OutputType type;
