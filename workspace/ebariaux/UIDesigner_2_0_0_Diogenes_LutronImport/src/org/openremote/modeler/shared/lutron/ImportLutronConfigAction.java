992aefef5364490efd66df45e53362e7c79a5ef4
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-01-10 10:24:25 +0000

    Set-up for using command pattern for server call of Lutron import logic, using GWT-dispatcher, GIN and Guice

---
+++ workspace/ebariaux/UIDesigner_2_0_0_Diogenes_LutronImport/src/org/openremote/modeler/shared/lutron/ImportLutronConfigAction.java
@@ -0,0 +1,10 @@
+package org.openremote.modeler.shared.lutron;
+
+import net.customware.gwt.dispatch.shared.Action;
+
+public class ImportLutronConfigAction implements Action<ImportLutronConfigResult> {
+
+  public ImportLutronConfigAction() {
+  }
+  
+}
---
+++ workspace/ebariaux/UIDesigner_2_0_0_Diogenes_LutronImport/src/org/openremote/modeler/shared/lutron/ImportLutronConfigResult.java
@@ -0,0 +1,10 @@
+package org.openremote.modeler.shared.lutron;
+
+import net.customware.gwt.dispatch.shared.Result;
+
+public class ImportLutronConfigResult implements Result {
+
+  public ImportLutronConfigResult() {
+  }
+  
+}
