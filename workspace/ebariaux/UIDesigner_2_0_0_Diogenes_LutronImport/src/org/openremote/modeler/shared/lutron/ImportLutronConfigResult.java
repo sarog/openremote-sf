14ec785aec8f6515139e37e3b439c130f4104868
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-01-11 11:56:28 +0000

    Did put in place some code to return created objects back to client but aborted due to issues with serialization of Hibernate beans back to client (must integrate Gilead with GWT-dispatch or move to DTO)

--- workspace/ebariaux/UIDesigner_2_0_0_Diogenes_LutronImport/src/org/openremote/modeler/shared/lutron/ImportLutronConfigResult.java
+++ workspace/ebariaux/UIDesigner_2_0_0_Diogenes_LutronImport/src/org/openremote/modeler/shared/lutron/ImportLutronConfigResult.java
@@ -1,10 +1,15 @@
 package org.openremote.modeler.shared.lutron;
 
+import java.util.ArrayList;
+
 import net.customware.gwt.dispatch.shared.Result;
+
+import org.openremote.modeler.domain.DeviceCommand;
 
 public class ImportLutronConfigResult implements Result {
 
   private String errorMessage;
+  private ArrayList<DeviceCommand> deviceCommands;
   
   public ImportLutronConfigResult() {
   }
@@ -16,5 +21,13 @@ public class ImportLutronConfigResult implements Result {
   public void setErrorMessage(String errorMessage) {
     this.errorMessage = errorMessage;
   }
-  
+
+  public ArrayList<DeviceCommand> getDeviceCommands() {
+    return deviceCommands;
+  }
+
+  public void setDeviceCommands(ArrayList<DeviceCommand> deviceCommands) {
+    this.deviceCommands = deviceCommands;
+  }
+
 }
