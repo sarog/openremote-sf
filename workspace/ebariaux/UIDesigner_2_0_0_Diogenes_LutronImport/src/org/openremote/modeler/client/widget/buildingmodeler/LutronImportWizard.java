280c57c6fe776a3586bed57c1435f656d58b3985
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-01-18 15:21:36 +0000

    Added standard OpenRemote header comment w/ license for newly added files

--- workspace/ebariaux/UIDesigner_2_0_0_Diogenes_LutronImport/src/org/openremote/modeler/client/widget/buildingmodeler/LutronImportWizard.java
+++ workspace/ebariaux/UIDesigner_2_0_0_Diogenes_LutronImport/src/org/openremote/modeler/client/widget/buildingmodeler/LutronImportWizard.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.modeler.client.widget.buildingmodeler;
 
 import java.util.ArrayList;
@@ -23,7 +42,6 @@ import org.openremote.modeler.shared.lutron.ImportLutronConfigResult;
 import org.openremote.modeler.shared.lutron.OutputImportConfig;
 import org.openremote.modeler.shared.lutron.OutputType;
 
-import com.extjs.gxt.ui.client.widget.Info;
 import com.google.gwt.cell.client.CheckboxCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
