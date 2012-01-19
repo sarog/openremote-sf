de14c8724998193a76105a66c9e5450c2817e8e2
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-01-19 11:29:08 +0000

    New version which supports regex and EventListener

--- workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/http/HttpGetCommand.java
+++ workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/http/HttpGetCommand.java
@@ -20,9 +20,17 @@
  */
 package org.openremote.controller.protocol.http;
 
-import java.util.Map;
+import java.io.ByteArrayInputStream;
 import java.net.URL;
-import java.math.BigInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -34,35 +42,27 @@ import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
 import org.openremote.controller.command.ExecutableCommand;
-import org.openremote.controller.command.StatusCommand;
-import org.openremote.controller.component.EnumSensorType;
 import org.openremote.controller.model.sensor.Sensor;
-import org.openremote.controller.exception.ConversionException;
+import org.openremote.controller.protocol.EventListener;
+import org.w3c.dom.Document;
 
 /**
  * TODO
- *
+ * 
  * @author Marcus 2009-4-26
  * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
- * @author Dan Cong
  */
-public class HttpGetCommand implements ExecutableCommand, StatusCommand
+public class HttpGetCommand implements ExecutableCommand, EventListener, Runnable
 {
 
-  // Class Members --------------------------------------------------------------------------------
-
+  // Class Members --------------------------------------------------------------
   /**
    * Common logging category.
    */
   private static Logger logger = Logger.getLogger(HttpGetCommandBuilder.HTTP_PROTOCOL_LOG_CATEGORY);
 
-
   
-  // Instance Fields ------------------------------------------------------------------------------
-
-  /** A name to identify command in controller.xml. */
-  private String name;
-
+  // Instance Fields ------------------------------------------------------------
   /** The url to perform the http get request on */
   private URL url;
 
@@ -72,308 +72,157 @@ public class HttpGetCommand implements ExecutableCommand, StatusCommand
   /** The password which is used for basic authentication */
   private byte[] password;
 
+  /** The xpath which is used to extract sensor data from received result */
+  private String xpathExpression;
 
+  /** The regex which is used to extract sensor data from received result */
+  private String regex;
 
-  // Constructors ---------------------------------------------------------------------------------
+  /** The polling interval which is used for the sensor update thread */
+  private Integer pollingInterval;
 
-  public HttpGetCommand(URL url)
+  /** The thread that is used to peridically update the sensor */
+  private Thread pollingThread;
+  
+  /** The sensor which is updated */
+  private Sensor sensor;
+  
+  /** Boolean to indicate if polling thread should run */
+  boolean doPoll = false;
+  
+  // Constructors  ----------------------------------------------------------------
+  public HttpGetCommand(URL url, String xpathExpression, String regex, Integer pollingInterval)
   {
     this.url = url;
+    this.xpathExpression = xpathExpression;
+    this.regex = regex;
+    this.pollingInterval = pollingInterval;
   }
 
-  public HttpGetCommand(URL url, String username, byte[] pwd)
+  public HttpGetCommand(URL url, String username, byte[] pwd, String xpath, String regex, Integer pollingInterval)
   {
-    this(url);
-
+    this(url, xpath, regex, pollingInterval);
     this.username = username;
     this.password = pwd;
   }
 
+  // Public Instance Methods
+  // ----------------------------------------------------------------------
 
-  // Public Instance Methods ----------------------------------------------------------------------
-  
   public URL getUrl()
   {
     return url;
   }
-
 
   public String getUsername()
   {
     return username;
   }
 
+  // Implements ExecutableCommand
+  // -----------------------------------------------------------------
 
-
-  // Implements ExecutableCommand -----------------------------------------------------------------
-
-  @Override public void send()
+  @Override
+  public void send()
   {
     requestURL();
   }
 
-
-  // Implements StatusCommand ---------------------------------------------------------------------
-
-  @Override public String read(EnumSensorType sensorType, Map<String, String> stateMap)
+  @Override
+  public void setSensor(Sensor sensor)
   {
-    String rawResult = requestURL();
-
-    if (sensorType == null)
-    {
-       return rawResult;
+    logger.debug("*** setSensor called as part of EventListener init *** sensor is: " + sensor);
+    if (pollingInterval == null) {
+      throw new RuntimeException("Could not set sensor because no polling interval was given");
     }
-
-    if ("".equals(rawResult))
-    {
-       return UNKNOWN_STATUS;
-    }
-
-    switch (sensorType)
-    {
-      case SWITCH:
-
-        rawResult = rawResult.trim();
-
-        if (rawResult.equalsIgnoreCase("on"))
-          return "on";
-
-        else if (rawResult.equalsIgnoreCase("off"))
-          return "off";
-
-        else
-          return UNKNOWN_STATUS;
-
-        
-      case RANGE:
-
-        try
-        {
-          int rangeMin = Integer.MIN_VALUE;
-          int rangeMax = Integer.MAX_VALUE;
-
-          int result = resolveResultAsInteger(rawResult);
-
-
-          if (stateMap != null)
-          {
-            rangeMin = resolveRangeMinimum(stateMap.get(Sensor.RANGE_MIN_STATE));
-            rangeMax = resolveRangeMaximum(stateMap.get(Sensor.RANGE_MAX_STATE));
-          }
-
-          return resolveToRangeSensorValue(result, rangeMin, rangeMax);
-        }
-
-        catch (ConversionException e)
-        {
-          return UNKNOWN_STATUS;
-        }
-
-
-      case LEVEL:
-
-        try
-        {
-          return resolveToLevelSensorValue(resolveResultAsInteger(rawResult));
-        }
-
-        catch (ConversionException e)
-        {
-          return UNKNOWN_STATUS;
-        }
-
-        
-      default://NOTE: if sensor type is RANGE, this map only contains min/max states.
-
-        for (String state : stateMap.keySet())
-        {
-          if (rawResult.equals(stateMap.get(state)))
-          {
-             return state;
-          }
-        }
-
-      break;
-    }
-
-    return rawResult;
+    this.sensor = sensor;
+    this.doPoll = true;
+    pollingThread = new Thread(this);
+    pollingThread.setName("Polling thread for sensor: " + sensor.getName());
+    pollingThread.start();
   }
 
-
-
+  @Override
+  public void stop(Sensor sensor)
+  {
+    this.doPoll = false;
+  }
+  
+  
   // Private Instance Methods ---------------------------------------------------------------------
-
   private String requestURL()
   {
     DefaultHttpClient client = new DefaultHttpClient();
-
     if (getUsername() != null)
     {
-       CredentialsProvider cred = new BasicCredentialsProvider();
-
-       cred.setCredentials(
-           new AuthScope(AuthScope.ANY),
-           new UsernamePasswordCredentials(getUsername(), new String(password))
-       );
-
-       client.setCredentialsProvider(cred);
+      CredentialsProvider cred = new BasicCredentialsProvider();
+      cred.setCredentials(new AuthScope(AuthScope.ANY), new UsernamePasswordCredentials(getUsername(), new String(password)));
+      client.setCredentialsProvider(cred);
     }
 
     HttpGet httpget = new HttpGet(url.toExternalForm());
-
     String resp = "";
-
     try
     {
       ResponseHandler<String> responseHandler = new BasicResponseHandler();
       resp = client.execute(httpget, responseHandler);
       logger.info("received message: " + resp);
     }
-
     catch (Exception e)
     {
       logger.error("HttpGetCommand could not execute", e);
     }
-
     return resp;
   }
+  
+  @Override
+  public void run() {
+     logger.debug("Sensor thread started for sensor: " + sensor);
+     while (doPoll) {
+        String readValue = this.requestURL();
+        if (regex != null) {
+          Pattern regexPattern = Pattern.compile(regex);
+          Matcher matcher = regexPattern.matcher(readValue);
+          if (matcher.find()) {
+            String result = matcher.group();
+            logger.info("result of regex evaluation: " + result);
+            sensor.update(result);
+          } else {
+            logger.info("regex evaluation did not find a match");
+            sensor.update("N/A");
+          }
+        } else if (xpathExpression != null) {
+          DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+          factory.setNamespaceAware(true); // never forget this!
+          String result;
+          try
+          {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            ByteArrayInputStream bin = new ByteArrayInputStream(readValue.getBytes());
+            Document doc = builder.parse(bin);
 
-
-  private int resolveRangeMinimum(String min)
-  {
-    if (min == null || min.equals(""))
-    {
-      return Integer.MIN_VALUE;
-    }
-
-    try
-    {
-      BigInteger minimum = new BigInteger(min);
-      BigInteger integerMin = new BigInteger(Integer.toString(Integer.MIN_VALUE));
-
-      if (minimum.compareTo(integerMin) < 0)
-      {
-        return Integer.MIN_VALUE;
-      }
-
-      else
-      {
-        return minimum.intValue();
-      }
-    }
-
-    catch (NumberFormatException e)
-    {
-      // TODO : log
-
-      return Integer.MIN_VALUE;
-    }
+            XPathFactory xfac = XPathFactory.newInstance();
+            XPath xpath = xfac.newXPath();
+            XPathExpression expr = xpath.compile(xpathExpression);
+            result = (String)expr.evaluate(doc, XPathConstants.STRING);
+            logger.info("result of xpath evaluation: " + result);
+            sensor.update(result);
+          } catch (Exception e)
+          {
+            logger.error("Could not perform xpath evaluation", e);
+            sensor.update("N/A");
+          }
+        } else {
+          sensor.update(readValue);
+        }
+        try {
+           Thread.sleep(pollingInterval); // We wait for the given pollingInterval before requesting URL again
+        } catch (InterruptedException e) {
+           doPoll = false;
+           pollingThread.interrupt();
+        }
+     }
+     logger.debug("*** Out of run method: " + sensor);
   }
 
-
-
-  private int resolveRangeMaximum(String max)
-  {
-    if (max == null || max.equals(""))
-    {
-      return Integer.MAX_VALUE;
-    }
-
-    try
-    {
-      BigInteger maximum = new BigInteger(max);
-      BigInteger integerMax = new BigInteger(Integer.toString(Integer.MAX_VALUE));
-
-      if (maximum.compareTo(integerMax) > 0)
-      {
-        return Integer.MAX_VALUE;
-      }
-
-      else
-      {
-        return maximum.intValue();
-      }
-    }
-
-    catch (NumberFormatException e)
-    {
-      // TODO : log
-
-      return Integer.MAX_VALUE;
-    }
-  }
-
-
-  private String resolveToLevelSensorValue(int result)
-  {
-    if (result > 100)
-    {
-      return "100";
-    }
-
-    else if (result < 0)
-    {
-      return "0";
-    }
-
-    else
-    {
-      return Integer.toString(result);
-    }
-  }
-
-
-
-  private int resolveResultAsInteger(String rawResult) throws ConversionException
-  {
-    try
-    {
-      BigInteger min = new BigInteger(Integer.toString(Integer.MIN_VALUE));
-      BigInteger max = new BigInteger(Integer.toString(Integer.MAX_VALUE));
-
-      BigInteger result = new BigInteger(rawResult);
-
-      if (result.compareTo(min) < 0)
-      {
-        return Integer.MIN_VALUE;
-      }
-
-      else if (result.compareTo(max) > 0)
-      {
-        return Integer.MAX_VALUE;
-      }
-
-      else
-      {
-        return result.intValue();
-      }
-    }
-
-    catch (NumberFormatException e)
-    {
-      throw new ConversionException(
-          "Cannot parse device return value to Java integer: " + e.getMessage(), e
-      );
-    }
-  }
-
-
-  private String resolveToRangeSensorValue(int result, int rangeMinimum, int rangeMaximum)
-  {
-    if (result < rangeMinimum)
-    {
-      return Integer.toString(rangeMinimum);
-    }
-
-    else if (result > rangeMaximum)
-    {
-      return Integer.toString(rangeMaximum);
-    }
-
-    else
-    {
-      return Integer.toString(result);
-    }
-
-  }
 }
