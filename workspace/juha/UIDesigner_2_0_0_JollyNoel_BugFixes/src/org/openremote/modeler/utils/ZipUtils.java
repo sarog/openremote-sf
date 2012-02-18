928af4690bf7006cb995a89dbba778df3b6d808f
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-02-18 07:32:02 +0000

    deprecate the class

--- workspace/juha/UIDesigner_2_0_0_JollyNoel_BugFixes/src/org/openremote/modeler/utils/ZipUtils.java
+++ workspace/juha/UIDesigner_2_0_0_JollyNoel_BugFixes/src/org/openremote/modeler/utils/ZipUtils.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.openremote.modeler.utils;
 
@@ -42,9 +43,8 @@ import org.openremote.modeler.exception.FileOperationException;
  *  
  * @author Allen, Handy
  */
-public class ZipUtils {
+@Deprecated public class ZipUtils {
    
-   /** The Constant LOGGER. */
    private static final Logger LOGGER = Logger.getLogger(ZipUtils.class);
 
    /**
@@ -60,8 +60,11 @@ public class ZipUtils {
     * @param files the files
     * 
     * @return the file
+    *
+    * @deprecated Currently only used by template related implementation -- this method should
+    *             go away with template functionality migrated to use ResourceCache interface
     */
-   public static File compress(String outputFilePath, List<File> files) {
+   @Deprecated public static File compress(String outputFilePath, List<File> files) {
       final int buffer = 2048;
       BufferedInputStream bufferedInputStream;
       File outputFile = new File(outputFilePath);
@@ -130,7 +133,7 @@ public class ZipUtils {
     * 
     * @return true, if success
     */
-   public static boolean unzip(InputStream inputStream, String targetDir){
+   private static boolean unzip(InputStream inputStream, String targetDir){
       ZipInputStream zipInputStream = new ZipInputStream(inputStream);
       ZipEntry zipEntry;
       FileOutputStream fileOutputStream = null;
@@ -164,8 +167,12 @@ public class ZipUtils {
       }
       return true;
    }
-   
-   public static boolean unzip(File zipFile, String targetDir) {
+
+  /**
+   * @deprecated Currently only used by template related implementation -- this method should
+   *             go away with template functionality migrated to use ResourceCache interface
+   */
+   @Deprecated public static boolean unzip(File zipFile, String targetDir) {
       try {
          InputStream inputStream = new FileInputStream(zipFile);
          return unzip(inputStream, targetDir);
