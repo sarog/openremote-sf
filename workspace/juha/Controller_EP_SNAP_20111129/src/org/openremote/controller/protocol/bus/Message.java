026469d5751466e00a27c3a969fba4204323f30e
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-02-25 11:49:40 +0000

    format according to our Java source code convention

--- workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/bus/Message.java
+++ workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/bus/Message.java
@@ -23,26 +23,29 @@ package org.openremote.controller.protocol.bus;
 /**
  * A message to send or receive over a physical bus.
  */
-public class Message {
+public class Message
+{
 
-   private byte[] content;
+  private byte[] content;
 
-   /**
-    * Constructor.
-    * 
-    * @param content
-    *           Message content.
-    */
-   public Message(byte[] content) {
-      this.content = content;
-   }
+  /**
+   * Constructor.
+   *
+   * @param content
+   *           Message content.
+   */
+  public Message(byte[] content)
+  {
+    this.content = content;
+  }
 
-   /**
-    * Get message content.
-    * 
-    * @return An array of byte representing message content.
-    */
-   public byte[] getContent() {
-      return this.content;
-   }
+  /**
+   * Get message content.
+   *
+   * @return An array of byte representing message content.
+   */
+  public byte[] getContent()
+  {
+    return this.content;
+  }
 }
