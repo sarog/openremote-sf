573e005d9a6f229283313267eccbf2bcf5fc2796
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-03-02 08:27:39 +0000

    format according to our Java source code conventions

--- workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/message/IpConnectReq.java
+++ workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/message/IpConnectReq.java
@@ -1,6 +1,6 @@
 /*
  * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
+ * Copyright 2008-2012, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
@@ -23,34 +23,57 @@ package org.openremote.controller.protocol.knx.ip.message;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class IpConnectReq extends IpMessage {
-   public static final int STI = 0x205;
-   // TODO check if 3rd byte should not be changed
-   private byte[] cri;
-   private Hpai controlEndpoint, dataEndpoint;
 
-   public IpConnectReq(Hpai controlEndpoint, Hpai dataEndpoint) {
-      super(STI, 0x14);
-      this.controlEndpoint = controlEndpoint;
-      this.dataEndpoint = dataEndpoint;
-      this.cri = new byte[] { 0x04, 0x04, 0x02, 0x00 };
-   }
+/**
+ * TODO
+ *
+ * @author Olivier Gandit
+ */
+public class IpConnectReq extends IpMessage
+{
 
-   @Override
-   public Primitive getPrimitive() {
-      return Primitive.REQ;
-   }
+  // Constants ------------------------------------------------------------------------------------
 
-   @Override
-   public int getSyncSendTimeout() {
-      return 10000;
-   }
+  public final static int STI = 0x205;
 
-   @Override
-   public void write(OutputStream os) throws IOException {
-      super.write(os);
-      this.controlEndpoint.write(os);
-      this.dataEndpoint.write(os);
-      os.write(this.cri);
-   }
+
+  // Instance Fields ------------------------------------------------------------------------------
+
+  // TODO check if 3rd byte should not be changed
+  private byte[] cri;
+  private Hpai controlEndpoint, dataEndpoint;
+
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  public IpConnectReq(Hpai controlEndpoint, Hpai dataEndpoint)
+  {
+    super(STI, 0x14);
+
+    this.controlEndpoint = controlEndpoint;
+    this.dataEndpoint = dataEndpoint;
+    this.cri = new byte[] { 0x04, 0x04, 0x02, 0x00 };
+  }
+
+
+  // IpMessage Overrides --------------------------------------------------------------------------
+
+  @Override public Primitive getPrimitive()
+  {
+    return Primitive.REQ;
+  }
+
+  @Override public int getSyncSendTimeout()
+  {
+    return 10000;
+  }
+
+  @Override public void write(OutputStream os) throws IOException
+  {
+    super.write(os);
+
+    this.controlEndpoint.write(os);
+    this.dataEndpoint.write(os);
+    os.write(this.cri);
+  }
 }
