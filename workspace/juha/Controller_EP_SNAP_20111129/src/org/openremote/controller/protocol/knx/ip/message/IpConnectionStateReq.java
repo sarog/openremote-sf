cbff93f355a2092927575114a83e6e032380986d
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-03-03 15:07:48 +0000

    format according to our Java source convenetions

--- workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/message/IpConnectionStateReq.java
+++ workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/message/IpConnectionStateReq.java
@@ -1,6 +1,6 @@
 /*
  * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
+ * Copyright 2008-2012, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
@@ -23,32 +23,57 @@ package org.openremote.controller.protocol.knx.ip.message;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class IpConnectionStateReq extends IpMessage {
-   public static final int STI = 0x207;
-   private int channelId;
-   private Hpai controlEndpoint;
+/**
+ * TODO
+ *
+ * @author Olivier Gandit
+ */
+public class IpConnectionStateReq extends IpMessage
+{
 
-   public IpConnectionStateReq(int channelId, Hpai controlEndpoint) {
-      super(STI, 0x0A);
-      this.controlEndpoint = controlEndpoint;
-      this.channelId = channelId;
-   }
+  // Constants ------------------------------------------------------------------------------------
 
-   @Override
-   public Primitive getPrimitive() {
-      return Primitive.REQ;
-   }
+  public final static int STI = 0x207;
 
-   @Override
-   public int getSyncSendTimeout() {
-      return 10000;
-   }
 
-   @Override
-   public void write(OutputStream os) throws IOException {
-      super.write(os);
-      os.write(this.channelId);
-      os.write(0);
-      this.controlEndpoint.write(os);
-   }
+  // Instance Fields ------------------------------------------------------------------------------
+
+  private int channelId;
+
+  private Hpai controlEndpoint;
+
+
+  // Constructors ---------------------------------------------------------------------------------
+  
+  public IpConnectionStateReq(int channelId, Hpai controlEndpoint)
+  {
+    super(STI, 0x0A);
+
+    this.controlEndpoint = controlEndpoint;
+    this.channelId = channelId;
+  }
+
+
+  // IpMessage Overrides --------------------------------------------------------------------------
+
+  @Override public Primitive getPrimitive()
+  {
+    return Primitive.REQ;
+  }
+
+  @Override public int getSyncSendTimeout()
+  {
+    return 10000;
+  }
+
+  @Override public void write(OutputStream os) throws IOException
+  {
+    super.write(os);
+
+    os.write(this.channelId);
+    os.write(0);
+
+    this.controlEndpoint.write(os);
+  }
+
 }
