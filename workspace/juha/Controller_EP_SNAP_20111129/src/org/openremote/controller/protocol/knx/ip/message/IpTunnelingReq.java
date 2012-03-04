01e8587be9dea23845fd43c40d06ce7628d1ec14
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-03-04 06:28:54 +0000

    add method to inspect the KNX frame header if this is a KNX telegram tunneling request

--- workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/message/IpTunnelingReq.java
+++ workspace/juha/Controller_EP_SNAP_20111129/src/org/openremote/controller/protocol/knx/ip/message/IpTunnelingReq.java
@@ -24,6 +24,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.openremote.controller.protocol.knx.ServiceTypeIdentifier;
+
 
 /**
  * TODO
@@ -32,11 +34,32 @@ import java.io.OutputStream;
  */
 public class IpTunnelingReq extends IpMessage
 {
+  
   // Constants ------------------------------------------------------------------------------------
 
-  public final static int STI = 0x420;
+  public final static int STI = ServiceTypeIdentifier.TUNNELING_REQUEST.getValue();
 
 
+
+  // Class Members --------------------------------------------------------------------------------
+
+  /**
+   * Indicates whether the given KNXnet/IP frame includes a
+   * {@link ServiceTypeIdentifier#TUNNELING_REQUEST} service type identifier.
+   *
+   * @param     knxFrame    KNXnet/IP frame as a byte array
+   *
+   * @return    true if the frame header includes a <tt>TUNNELING_REQUEST</tt> service type
+   *            identifier, false otherwise
+   */
+  public static boolean isTunnelingRequest(byte[] knxFrame)
+  {
+    return (knxFrame[KNXNET_IP_10_HEADER_SIZE_INDEX]       == KNXNET_IP_10_HEADER_SIZE &&
+            knxFrame[KNXNET_IP_10_HEADER_VERSION_INDEX]    == KNXNET_IP_10_VERSION &&
+            ServiceTypeIdentifier.TUNNELING_REQUEST.isIncluded(knxFrame));
+  }
+
+  
   // Instance Fields ------------------------------------------------------------------------------
 
   private int channelId;
