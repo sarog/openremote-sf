db4513d3c9c8bce462b245228f60a6fd7ba8c268
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-04-06 12:52:30 +0000

    Added OR license text and a bit of code formatting

--- workspace/ebariaux/Designer_2_0_0_SNAPSHOT-20120218_RemoveGilead/src/org/openremote/modeler/irfileparser/CodeSetInfo.java
+++ workspace/ebariaux/Designer_2_0_0_SNAPSHOT-20120218_RemoveGilead/src/org/openremote/modeler/irfileparser/CodeSetInfo.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.modeler.irfileparser;
 
 import org.openremote.ir.domain.DeviceInfo;
@@ -5,11 +24,10 @@ import org.openremote.ir.domain.DeviceInfo;
 import com.extjs.gxt.ui.client.data.BeanModelTag;
 
 /**
- * allows to share xcfFileParser.codeset necessary information with the
- * client side
+ * Adds BeanModelTag capability to CodeSetInfo for compatibility with GXT stores.
  * 
- * @author wbalcaen
- * 
+ * @author Eric Bariaux (eric@openremote.org)
+ *
  */
 public class CodeSetInfo extends org.openremote.ir.domain.CodeSetInfo implements BeanModelTag {
 
