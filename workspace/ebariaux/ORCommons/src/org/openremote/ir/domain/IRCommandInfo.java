68bb50fae6c870ff16b1a5eb02debf33c15476e2
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-04-06 12:55:50 +0000

    Added OR license text and a bit of code formatting

--- workspace/ebariaux/ORCommons/src/org/openremote/ir/domain/IRCommandInfo.java
+++ workspace/ebariaux/ORCommons/src/org/openremote/ir/domain/IRCommandInfo.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.ir.domain;
 
 import java.io.Serializable;
@@ -9,25 +28,24 @@ import java.io.Serializable;
  */
 public class IRCommandInfo implements Serializable {
 
-   private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-   private String name;
-   private String code;
-   private String originalCode;
-   private String comment;
-   private CodeSetInfo codeSet;
-   
-   public IRCommandInfo() {
-   }
+  private String name;
+  private String code;
+  private String originalCode;
+  private String comment;
+  private CodeSetInfo codeSet;
 
-   public IRCommandInfo(String name, String code, String originalCode,
-         String comment, CodeSetInfo codeSet) {
-      setName(name);
-      setCode(code);
-      setOriginalCode(originalCode);
-      setComment(comment);
-      setCodeSet(codeSet);
-   }
+  public IRCommandInfo() {
+  }
+
+  public IRCommandInfo(String name, String code, String originalCode, String comment, CodeSetInfo codeSet) {
+    setName(name);
+    setCode(code);
+    setOriginalCode(originalCode);
+    setComment(comment);
+    setCodeSet(codeSet);
+  }
 
   public String getName() {
     return name;
