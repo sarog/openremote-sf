e7ee96febb78b6e666cc1129a7ea087fe35edf83
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-04-06 12:55:50 +0000

    Added OR license text and a bit of code formatting

--- workspace/ebariaux/ORCommons/src/org/openremote/ir/domain/GlobalCache.java
+++ workspace/ebariaux/ORCommons/src/org/openremote/ir/domain/GlobalCache.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.ir.domain;
 
 import java.io.Serializable;
@@ -12,42 +31,42 @@ public class GlobalCache implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
-   private String IpAddress;
-   private String tcpPort;
-   private String connector;
+  private String IpAddress;
+  private String tcpPort;
+  private String connector;
 
-   public GlobalCache(String ipAddress, String tcpPort, String connector) {
-      super();
-      IpAddress = ipAddress;
-      this.tcpPort = tcpPort;
-      this.connector = connector;
-   }
+  public GlobalCache(String ipAddress, String tcpPort, String connector) {
+    super();
+    IpAddress = ipAddress;
+    this.tcpPort = tcpPort;
+    this.connector = connector;
+  }
 
-   public GlobalCache() {
-   }
+  public GlobalCache() {
+  }
 
-   public String getIpAddress() {
-      return this.IpAddress;
-   }
+  public String getIpAddress() {
+    return this.IpAddress;
+  }
 
-   public String getTcpPort() {
-      return this.tcpPort;
-   }
+  public String getTcpPort() {
+    return this.tcpPort;
+  }
 
-   public String getConnector() {
-      return this.connector;
-   }
+  public String getConnector() {
+    return this.connector;
+  }
 
-   public void setIpAddress(String ipAddress) {
-      IpAddress = ipAddress;
-   }
+  public void setIpAddress(String ipAddress) {
+    IpAddress = ipAddress;
+  }
 
-   public void setTcpPort(String tcpPort) {
-      this.tcpPort = tcpPort;
-   }
+  public void setTcpPort(String tcpPort) {
+    this.tcpPort = tcpPort;
+  }
 
-   public void setConnector(String connector) {
-      this.connector = connector;
-   }
+  public void setConnector(String connector) {
+    this.connector = connector;
+  }
 
 }
--- workspace/ebariaux/ORCommons/src/org/openremote/ir/domain/IRTrans.java
+++ workspace/ebariaux/ORCommons/src/org/openremote/ir/domain/IRTrans.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.ir.domain;
 
 import java.io.Serializable;
@@ -10,43 +29,43 @@ import java.io.Serializable;
 public class IRTrans implements Serializable {
 
   private static final long serialVersionUID = 1L;
-  
+
   private String ipAdress;
-   private String udpPort;
-   private String irLed;
+  private String udpPort;
+  private String irLed;
 
-   public IRTrans() {
-   }
+  public IRTrans() {
+  }
 
-   public IRTrans(String ipAdress, String udpPort, String irLed) {
-      super();
-      this.ipAdress = ipAdress;
-      this.udpPort = udpPort;
-      this.irLed = irLed;
-   }
+  public IRTrans(String ipAdress, String udpPort, String irLed) {
+    super();
+    this.ipAdress = ipAdress;
+    this.udpPort = udpPort;
+    this.irLed = irLed;
+  }
 
-   public String getIpAdress() {
-      return ipAdress;
-   }
+  public String getIpAdress() {
+    return ipAdress;
+  }
 
-   public void setIpAdress(String ipAdress) {
-      this.ipAdress = ipAdress;
-   }
+  public void setIpAdress(String ipAdress) {
+    this.ipAdress = ipAdress;
+  }
 
-   public String getUdpPort() {
-      return udpPort;
-   }
+  public String getUdpPort() {
+    return udpPort;
+  }
 
-   public void setUdpPort(String udpPort) {
-      this.udpPort = udpPort;
-   }
+  public void setUdpPort(String udpPort) {
+    this.udpPort = udpPort;
+  }
 
-   public String getIrLed() {
-      return irLed;
-   }
+  public String getIrLed() {
+    return irLed;
+  }
 
-   public void setIrLed(String irLed) {
-      this.irLed = irLed;
-   }
+  public void setIrLed(String irLed) {
+    this.irLed = irLed;
+  }
 
 }
