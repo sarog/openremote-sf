a59e5a45c3de4a1085c888ac2d457ce09c3c5172
rhitz <rhitz@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-04-11 08:26:24 +0000

    add COM port adapter and related tests

---
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/EspComPortAdapterBase.java
@@ -0,0 +1,318 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.enocean;
+
+import org.openremote.controller.protocol.port.Message;
+import org.openremote.controller.protocol.port.Port;
+import org.openremote.controller.protocol.port.PortException;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * A common superclass for EnOcean Serial Protocol (ESP) COM port adapter implementations to
+ * reuse code. <p>
+ *
+ * The class acts as an adapter to the {@link org.openremote.controller.protocol.port.Port}
+ * interface. The adapter translates exceptions and automates serial port configuration. <p>
+ *
+ * Subclasses should provide an implementation of {@link #createComPortConfiguration(EspPortConfiguration)}.
+ * The createComPortConfiguration() method should create a configuration which is a valid parameter
+ * for {@link org.openremote.controller.protocol.port.Port#configure(java.util.Map)}.
+ *
+ * @author Rainer Hitz
+ */
+public abstract class EspComPortAdapterBase implements EspPort
+{
+
+  // Class Members --------------------------------------------------------------------------------
+
+  /**
+   * TODO : Logger
+   */
+
+
+  // Private Instance Fields ----------------------------------------------------------------------
+
+  /**
+   * Wrapped serial port.
+   */
+  private Port port;
+
+  /**
+   * Configuration which is used to configure the wrapped serial port.
+   */
+  private EspPortConfiguration configuration;
+
+  /**
+   * Indicates if wrapped serial port has already been configured.
+   */
+  private boolean configured;
+
+  /**
+   * Indicated if wrapped serial port has already been started.
+   */
+  private boolean started;
+
+
+  // Constructors -------------------------------------------------------------------------------
+
+  /**
+   * Constructs a new EnOcean Serial Protocol (ESP) port adapter with a given serial port and
+   * an EnOcean Serial Protocol port configuration used to configure the wrapped serial port.
+   *
+   * @param port           serial port
+   * @param configuration  EnOcean Serial Protocol (ESP) port configuration
+   */
+  public EspComPortAdapterBase(Port port, EspPortConfiguration configuration)
+  {
+    if(port == null)
+    {
+      throw new IllegalArgumentException("Must include a reference to a port.");
+    }
+
+    if(configuration == null)
+    {
+      throw new IllegalArgumentException("Must include a reference to a configuration.");
+    }
+
+    this.started = false;
+    this.configured = false;
+
+    this.port = port;
+    this.configuration = configuration;
+
+  }
+
+
+  // Implements EspPort ---------------------------------------------------------------------------
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public void start() throws ConnectionException, ConfigurationException
+  {
+    if(started)
+      return;
+
+    configurePort();
+
+    try
+    {
+      port.start();
+    }
+
+    catch(PortException e)
+    {
+      throw new ConnectionException(
+          "Starting serial port failed : {0}", e, e.getMessage()
+      );
+    }
+
+    catch(IOException e)
+    {
+      throw new ConnectionException(
+          "Starting serial port failed : {0}", e, e.getMessage()
+      );
+    }
+
+    started = true;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public void stop() throws ConnectionException
+  {
+    if(!started)
+      return;
+
+    try
+    {
+      port.stop();
+    }
+
+    catch(PortException e)
+    {
+      throw new ConnectionException(
+          "Stopping serial port failed : {0}", e, e.getMessage()
+      );
+    }
+
+    catch(IOException e)
+    {
+      throw new ConnectionException(
+          "Stopping serial port failed : {0}", e, e.getMessage()
+      );
+    }
+
+    started = false;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public boolean isStarted()
+  {
+    return started;
+  }
+
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public void send(byte[] data) throws ConnectionException
+  {
+    if(!started)
+    {
+      throw new ConnectionException(
+          "Failed to send data because serial port has not been started."
+      );
+    }
+
+    Message msg = new Message(data);
+
+    try
+    {
+      port.send(msg);
+    }
+
+    catch(PortException e)
+    {
+      throw new ConnectionException(
+          "Failed to send data : {0}", e, e.getMessage()
+      );
+    }
+
+    catch(IOException e)
+    {
+      throw new ConnectionException(
+          "Failed to send data : {0}", e, e.getMessage()
+      );
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public byte[] receive() throws ConnectionException
+  {
+    if(!started)
+    {
+      throw new ConnectionException(
+          "Failed to receive data because serial port has not been started."
+      );
+    }
+
+    Message msg = null;
+
+    try
+    {
+      msg = port.receive();
+    }
+
+    catch(IOException e)
+    {
+      throw new ConnectionException(
+          "Failed to receive data : {0}", e, e.getMessage()
+      );
+    }
+
+    byte[] data = msg.getContent();
+
+    if(null == data)
+      data = new byte[] {};
+
+    return data;
+  }
+
+
+  // Protected Instance Methods -------------------------------------------------------------------
+
+  /**
+   * Subclasses should implement this to provide the COM port configuration which is used inside
+   * the {@link #configurePort()} method to configure the wrapped serial port.
+   *
+   * @param   configuration  EnOcean Serial Protocol (ESP) port configuration
+   *
+   * @return  serial port configuration. The returned configuration should be a valid parameter
+   *          for {@link org.openremote.controller.protocol.port.Port#configure(java.util.Map)}
+   *
+   * @throws  ConfigurationException
+   *            if the configuration provided by the input parameter is invalid
+   */
+  protected abstract Map<String, Object> createComPortConfiguration(EspPortConfiguration configuration)
+      throws ConfigurationException;
+
+
+  // Private Instance Methods ---------------------------------------------------------------------
+
+  /**
+   * Configures the wrapped {@link #port} after translating the configuration in the
+   * {@link #configuration} field to a configuration which can be passed as parameter to
+   * {@link org.openremote.controller.protocol.port.Port#configure(java.util.Map)}. <p>
+   *
+   * Subclasses have to provide an implementation of
+   * {@link #createComPortConfiguration(EspPortConfiguration)} which creates a configuration format
+   * suitable for {@link org.openremote.controller.protocol.port.Port#configure(java.util.Map)}.
+   *
+   * @throws ConnectionException
+   *           if port cannot be configured because of port connection errors
+   *
+   * @throws ConfigurationException
+   *           if port cannot be configured because of an invalid configuration
+   */
+  private void configurePort() throws ConnectionException, ConfigurationException
+  {
+    if(configured)
+      return;
+
+    Map<String, Object> comPortConfig = createComPortConfiguration(configuration);
+
+    try
+    {
+      port.configure(comPortConfig);
+    }
+
+    catch(PortException e)
+    {
+      if(e.getCode() == PortException.INVALID_CONFIGURATION)
+      {
+        throw new ConfigurationException(
+            "Configuring serial port failed : {0}",e , e.getMessage()
+        );
+      }
+
+      throw new ConnectionException(
+          "Configuring serial port failed : {0}", e, e.getMessage()
+      );
+    }
+
+    catch(IOException e)
+    {
+      throw new ConnectionException(
+          "Configuring serial port failed : {0}", e, e.getMessage()
+      );
+    }
+
+    configured = true;
+  }
+}
---
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/test/org/openremote/controller/protocol/enocean/MockPort.java
@@ -0,0 +1,212 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.enocean;
+
+import junit.framework.Assert;
+import org.openremote.controller.protocol.port.Message;
+import org.openremote.controller.protocol.port.Port;
+import org.openremote.controller.protocol.port.PortException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Configurable test double for mocking a {@link org.openremote.controller.protocol.port.Port}
+ * implementation.
+ *
+ * @author Rainer Hitz
+ */
+public class MockPort implements Port
+{
+
+  // Enums ----------------------------------------------------------------------------------------
+
+  public enum Method
+  {
+    START,
+    STOP,
+    CONFIGURE
+  }
+
+
+  // Private Instance Fields ----------------------------------------------------------------------
+
+  private List<Method> expectedMethodCalls = new ArrayList<Method>();
+  private List<Method> actualMethodCalls = new ArrayList<Method>();
+
+  private List<ArrayList<Byte>> expectedDataToSend = new ArrayList<ArrayList<Byte>>();
+  private List<ArrayList<Byte>> actualDataToSend = new ArrayList<ArrayList<Byte>>();
+  private LinkedList<Message> messagesToReturn = new LinkedList<Message>();
+
+  private Exception configureException = null;
+  private Exception startException = null;
+  private Exception stopException = null;
+  private Exception sendException = null;
+  private IOException receiveException = null;
+
+
+  // Public Instance Methods ----------------------------------------------------------------------
+
+  public void addExpectedMethodCall(Method method)
+  {
+    expectedMethodCalls.add(method);
+  }
+
+  public void addExpectedDataToSend(byte[] rawData)
+  {
+    addData(expectedDataToSend, rawData);
+  }
+
+  public void addDataToReturn(byte[] rawData)
+  {
+    messagesToReturn.addLast(new Message(rawData.clone()));
+  }
+
+
+  public void setupThrowExceptionOnConfigure(Exception exception)
+  {
+    configureException = exception;
+  }
+
+  public void setupThrowExceptionOnStart(Exception exception)
+  {
+    startException = exception;
+  }
+
+  public void setupThrowExceptionOnStop(Exception exception)
+  {
+    stopException = exception;
+  }
+
+  public void setupThrowExceptionOnSend(Exception exception)
+  {
+    sendException = exception;
+  }
+
+  public void setupThrowExceptionOnReceive(IOException exception)
+  {
+    receiveException = exception;
+  }
+
+
+  public void verifyMethodCalls()
+  {
+    Assert.assertEquals(expectedMethodCalls, actualMethodCalls);
+  }
+
+  public void verifyData()
+  {
+    Assert.assertEquals(expectedDataToSend, actualDataToSend);
+
+    Assert.assertEquals(messagesToReturn.size(), 0);
+  }
+
+
+  // Implements Port ------------------------------------------------------------------------------
+
+  @Override public void configure(Map<String, Object> configuration) throws IOException, PortException
+  {
+    actualMethodCalls.add(Method.CONFIGURE);
+
+    if(null != configureException)
+    {
+      triggerException(configureException);
+    }
+  }
+
+
+  @Override public void start() throws IOException, PortException
+  {
+    actualMethodCalls.add(Method.START);
+
+    if(null != startException)
+    {
+      triggerException(startException);
+    }
+  }
+
+
+  @Override public void stop() throws IOException, PortException
+  {
+    actualMethodCalls.add(Method.STOP);
+
+    if(null != stopException)
+    {
+      triggerException(stopException);
+    }
+  }
+
+
+  @Override public void send(Message message) throws IOException, PortException
+  {
+    addData(actualDataToSend, message.getContent());
+
+    if(null != sendException)
+    {
+      triggerException(sendException);
+    }
+  }
+
+
+  @Override public Message receive() throws IOException
+  {
+    Message msg = messagesToReturn.removeFirst();
+
+    if(null != receiveException)
+    {
+      throw receiveException;
+    }
+
+    return msg;
+  }
+
+
+  // Private Instance Methods ---------------------------------------------------------------------
+
+  private void addData(List<ArrayList<Byte>> list, byte[] rawData)
+  {
+    ArrayList<Byte> data = new ArrayList<Byte>(rawData.length);
+
+    for (byte dataByte : rawData)
+    {
+      data.add(dataByte);
+    }
+
+    list.add(data);
+  }
+
+
+  private void triggerException(Exception exception) throws IOException, PortException
+  {
+    if(exception instanceof IOException)
+    {
+      throw (IOException)exception;
+    }
+
+    else if(exception instanceof PortException)
+    {
+      throw (PortException)exception;
+    }
+  }
+}
