c2fca730586658c456e73b959cad7b527b69f4ce
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-05-02 14:55:20 +0000

    IPHONE-127: Factored out and rewritten URL validation on controller details page. Although correct per RFC, it accepts URLs that would never be controller URLs

---
+++ branches/project/iOS_Console_2_2_0_Betas/NSURLHelperTest.h
@@ -0,0 +1,13 @@
+//
+//  NSURLHelperTest.h
+//  openremote
+//
+//  Created by Eric Bariaux on 02/05/12.
+//  Copyright (c) 2012 OpenRemote, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSURLHelperTest : SenTestCase
+
+@end
