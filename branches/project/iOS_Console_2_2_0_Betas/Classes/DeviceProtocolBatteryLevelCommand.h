c32e7110cc16e3421804b6b8881a83df27cdc4c4
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-05-09 09:15:22 +0000

    IPHONE-142: Added implementation of command to report battery level on device

---
+++ branches/project/iOS_Console_2_2_0_Betas/Classes/DeviceProtocolBatteryLevelCommand.h
@@ -0,0 +1,14 @@
+//
+//  DeviceProtocolBatteryLevelCommand.h
+//  openremote
+//
+//  Created by Eric Bariaux on 09/05/12.
+//  Copyright (c) 2012 OpenRemote, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ClientSideProtocolReadCommand.h"
+
+@interface DeviceProtocolBatteryLevelCommand : NSObject <ClientSideProtocolReadCommand>
+
+@end
