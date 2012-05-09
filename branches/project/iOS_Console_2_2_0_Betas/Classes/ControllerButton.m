2503a96f65ae8b41f3af7688613bb2e759afeff1
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-05-09 11:17:17 +0000

    IPHONE-142: Some refactoring on the way the represent <ctrl:components> sub tags in the model

--- branches/project/iOS_Console_2_2_0_Betas/Classes/ControllerButton.m
+++ branches/project/iOS_Console_2_2_0_Betas/Classes/ControllerButton.m
@@ -11,43 +11,8 @@
 
 @interface ControllerButton()
 
-@property (nonatomic, retain) NSMutableDictionary *commandsPerActionRegistry;
-
 @end
 
 @implementation ControllerButton
-
-- (id)initWithId:(int)anId
-{
-    self = [super initWithId:anId];
-    if (self) {
-        self.commandsPerActionRegistry = [NSMutableDictionary dictionaryWithCapacity:1];
-    }
-    return self;
-}
-
-- (void)dealloc
-{
-    self.commandsPerActionRegistry = nil;
-    [super dealloc];
-}
-
-- (void)addCommand:(LocalCommand *)aCommand forAction:(NSString *)anAction
-{
-    NSMutableArray *commands = [self.commandsPerAction objectForKey:anAction];
-    if (!commands) {
-        commands = [NSMutableArray array];
-        [self.commandsPerActionRegistry setObject:commands forKey:anAction];
-    }
-    [commands addObject:aCommand];
-}
-
-// TODO: this is only valid for 2.0 API, must check for 2.1 (long button press support)
-- (NSDictionary *)commandsPerAction
-{
-    return [NSDictionary dictionaryWithDictionary:self.commandsPerActionRegistry];
-}
-
-@synthesize commandsPerActionRegistry;
 
 @end
