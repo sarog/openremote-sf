281b02f0088b7290c29493c4f9c7feca143352f2
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-05-17 10:33:06 +0000

    need a temporary work branch to resolve merge inconsistencies between DTO branch and LutronImport branch

---
+++ workspace/juha/ResolveMergeConflictDTOAndLutron/src/org/openremote/modeler/server/UserController.java
@@ -0,0 +1,68 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2010, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.modeler.server;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.openremote.modeler.client.rpc.UserRPCService;
+import org.openremote.modeler.domain.User;
+import org.openremote.modeler.exception.UserInvitationException;
+import org.openremote.modeler.service.UserService;
+
+/**
+ * The Class is for inviting user and managing invited user.
+ */
+public class UserController extends BaseGWTSpringControllerWithHibernateSupport implements UserRPCService {
+
+   private static final long serialVersionUID = -3486307399647834562L;
+
+   private UserService userService;
+   public User inviteUser(String email, String role) throws UserInvitationException{
+      if (StringUtils.isEmpty(email) || StringUtils.isEmpty(role)) {
+         throw new UserInvitationException("Failed to send invitation");
+      }
+      return userService.inviteUser(email, role, userService.getCurrentUser());
+   }
+
+   public void setUserService(UserService userService) {
+      this.userService = userService;
+   }
+
+   public List<User> getPendingInviteesByAccount() {
+      return userService.getPendingInviteesByAccount(userService.getCurrentUser());
+   }
+
+   public User updateUserRoles(long uid, String roles) {
+      return userService.updateUserRoles(uid, roles);
+   }
+
+   public void deleteUser(long uid) {
+      userService.deleteUser(uid);
+   }
+
+   public List<User> getAccountAccessUsers() {
+      return userService.getAccountAccessUsers(userService.getCurrentUser());
+   }
+
+   public Long getUserId() {
+      return userService.getCurrentUser().getOid();
+   }
+}
