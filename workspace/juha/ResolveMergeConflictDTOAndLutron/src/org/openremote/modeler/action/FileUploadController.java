7022bc757f6975acd47797aac095cec294dc3369
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-05-17 10:33:06 +0000

    need a temporary work branch to resolve merge inconsistencies between DTO branch and LutronImport branch

---
+++ workspace/juha/ResolveMergeConflictDTOAndLutron/src/org/openremote/modeler/action/FileUploadController.java
@@ -0,0 +1,254 @@
+/* OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openremote.modeler.action;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.zip.ZipFile;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
+import org.openremote.modeler.client.Constants;
+import org.openremote.modeler.domain.KnxGroupAddress;
+import org.openremote.modeler.lutron.ImportException;
+import org.openremote.modeler.lutron.LutronHomeworksImporter;
+import org.openremote.modeler.server.IRFileParserController;
+import org.openremote.modeler.server.lutron.importmodel.LutronImportResult;
+import org.openremote.modeler.server.lutron.importmodel.Project;
+import org.openremote.modeler.service.ResourceService;
+import org.openremote.modeler.utils.ImageRotateUtil;
+import org.openremote.modeler.utils.KnxImporter;
+import org.openremote.modeler.utils.MultipartFileUtil;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import com.tinsys.ir.representations.IRCodeRepresentationFactory;
+import com.tinsys.ir.representations.pronto.NecIRCodeRepresentationHandler;
+import com.tinsys.ir.representations.pronto.RC5IRCodeRepresentationHandler;
+import com.tinsys.ir.representations.pronto.RC5xIRCodeRepresentationHandler;
+import com.tinsys.ir.representations.pronto.RawIRCodeRepresentationHandler;
+import com.tinsys.pronto.irfiles.ProntoFileParser;
+
+import flexjson.JSONSerializer;
+
+/**
+ * The Class is used for uploading files.
+ * 
+ * @author handy.wang
+ */
+public class FileUploadController extends MultiActionController implements BeanFactoryAware{
+
+    private static final Logger LOGGER = Logger.getLogger(FileUploadController.class);
+   
+    /** The resource service. */
+    private ResourceService resourceService;
+
+    private BeanFactory beanFactory;
+
+    /** sets the bean factory
+     * @param beanFactory
+     */
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+    /**
+     * Import openremote.zip into application, but now is not use.
+     * 
+     * @param request
+     *            the request
+     * @param response
+     *            the response
+     * 
+     * @return the model and view
+     */
+    @SuppressWarnings("finally")
+    public ModelAndView importFile(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String importJson = resourceService.getDotImportFileForRender(request.getSession().getId(),
+                    MultipartFileUtil.getMultipartFileFromRequest(request, "file").getInputStream());
+            response.getWriter().write(importJson);
+        } catch (Exception e) {
+            LOGGER.error("Import file error.", e);
+            response.getWriter().write("");
+        } finally {
+            return null;
+        }
+    }
+
+    public void importETS4(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        MultipartFile multipartFile = MultipartFileUtil.getMultipartFileFromRequest(request, "knxproj");
+        String contentType = multipartFile.getContentType();
+        List<KnxGroupAddress> addresses = null;
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        
+        try
+        {
+          if ("text/csv".equalsIgnoreCase(contentType)) {
+            addresses = new KnxImporter().importETS3GroupAddressCsvExport(multipartFile.getInputStream());
+          } else {
+            addresses = new KnxImporter().importETS4Configuration(multipartFile.getInputStream());  
+          }
+          data.put("records", addresses);
+        } catch (Exception e)
+        {
+          LOGGER.error("Could not import ETS data", e);
+          data.put("exception", e.toString());
+        }
+        
+        JSONSerializer serializer = new JSONSerializer();
+        String jsonResult = serializer.exclude("*.class").deepSerialize(data);
+        logger.debug("Responding with string\n" + jsonResult);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().println(jsonResult);
+    }
+
+    public void importLutron(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      MultipartFile multipartFile = MultipartFileUtil.getMultipartFileFromRequest(request, "lutron");
+
+      LutronImportResult importResult = new LutronImportResult();
+
+      try {
+        Project project = LutronHomeworksImporter.importXMLConfiguration(multipartFile.getInputStream());
+        importResult.setProject(project);
+      } catch (ImportException e) {
+        LOGGER.error("Import file error.", e);
+        importResult.setErrorMessage(e.getMessage());        
+      }
+      JSONSerializer serializer = new JSONSerializer();
+      System.out.println("Generated JSON >" + serializer.exclude("*.class").deepSerialize(importResult) + "<");
+      response.setHeader("content-type", "text/html");
+      response.getWriter().println(serializer.exclude("*.class").deepSerialize(importResult));
+    }
+    
+    /**
+     * import commands from an IrFile
+     * 
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    public void importIRFile(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      MultipartFile multipartFile = MultipartFileUtil.getMultipartFileFromRequest(request, "importIRFile");
+      File f = new File(resourceService.getTempDirectory(request.getSession().getId()) + File.separator
+              + multipartFile.getOriginalFilename());
+      ZipFile zip;
+      ProntoFileParser prontoParser= new ProntoFileParser();
+      String responseContent;
+      try {
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(multipartFile.getBytes());
+        fos.flush();
+        fos.close();
+          try {
+            zip = new ZipFile(f);
+            IRCodeRepresentationFactory factory = new IRCodeRepresentationFactory();
+            new RC5IRCodeRepresentationHandler().registerWithFactory(factory);
+            new RC5xIRCodeRepresentationHandler().registerWithFactory(factory);
+            new RawIRCodeRepresentationHandler().registerWithFactory(factory);
+            new NecIRCodeRepresentationHandler().registerWithFactory(factory);
+            prontoParser.setFactory(factory);
+            // serialize the objects
+              try {
+                prontoParser.parseFile(zip);
+                JSONSerializer serializer = new JSONSerializer();
+                response.setCharacterEncoding("UTF-8");
+                  responseContent = serializer.exclude("*.class").include("brands").serialize(prontoParser);
+                } catch (Exception e) {
+                  responseContent = Constants.IRFILE_UPLOAD_ERROR + "Couldn't parse file : " + e.getMessage();
+                }
+          } catch (Exception e) {
+              responseContent = Constants.IRFILE_UPLOAD_ERROR + "Couldn't open temporary file on server :"
+                      + e.getMessage();
+            }
+      } catch (Exception e) {
+          responseContent = Constants.IRFILE_UPLOAD_ERROR + "Couldn't upload file to server :" + e.getMessage();
+      }
+      IRFileParserController iRFileParserController = (IRFileParserController) beanFactory
+            .getBean("irFileParserController");
+      iRFileParserController.setProntoFileParser(prontoParser);
+      response.getWriter().println(responseContent);
+    }
+    
+    
+    
+    
+    /**
+     * Sets the resource service.
+     * 
+     * @param resourceService
+     *            the new resource service
+     */
+    public void setResourceService(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
+
+    /**
+     * upload an image.<br /> your action should be :
+     * fileUploadController.htm?method=uploadImage&uploadFieldName=<b>your
+     * upload Field Name</b> .
+     * 
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    public void uploadImage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String uploadFieldName = request.getParameter("uploadFieldName");
+
+        if (uploadFieldName == null || uploadFieldName.trim().length() == 0) {
+            LOGGER.error("The action must have a parameter 'uploadFieldName'");
+            return;
+        }
+
+        long maxImageSize = 1024 * 1024 * 5;
+        MultipartFile multipartFile = MultipartFileUtil.getMultipartFileFromRequest(request, uploadFieldName);
+        if (multipartFile.getSize() == 0 || multipartFile.getSize() > maxImageSize) {
+            return;
+        }
+        File file = resourceService.uploadImage(multipartFile.getInputStream(), multipartFile.getOriginalFilename());
+
+        if (("panelImage".equals(uploadFieldName) || "tabbarImage".equals(uploadFieldName)) && file.exists()) {
+            rotateBackgroud(file);
+            BufferedImage buff = ImageIO.read(file);
+            response.getWriter().print(
+                    "{\"name\": \"" + resourceService.getRelativeResourcePathByCurrentAccount(file.getName())
+                            + "\",\"width\":" + buff.getWidth() + ",\"height\":" + buff.getHeight() + "}");
+        } else {
+            response.getWriter().print(resourceService.getRelativeResourcePathByCurrentAccount(file.getName()));
+        }
+    }
+
+    private void rotateBackgroud(File sourceFile) {
+        String targetImagePath = sourceFile.getParent() + File.separator + sourceFile.getName().replace(".", "_h.");
+        ImageRotateUtil.rotate(sourceFile, targetImagePath, -90);
+    }
+
+}
---
+++ workspace/juha/ResolveMergeConflictDTOAndLutron/src/org/openremote/modeler/service/ResourceService.java
@@ -0,0 +1,104 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2011, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.modeler.service;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.List;
+
+import org.openremote.modeler.client.utils.PanelsAndMaxOid;
+import org.openremote.modeler.domain.Panel;
+import org.openremote.modeler.domain.Template;
+import org.openremote.modeler.shared.GraphicalAssetDTO;
+
+/**
+ * The Interface ResourceService.
+ * 
+ * @author Allen, Handy
+ */
+
+public interface ResourceService {
+   
+   /**
+    * Download zip resource.
+    * 
+    * @param maxId the max id
+    * @param sessionId the session id
+    * @param activities the activities
+    * 
+    * @return the string
+    */
+   String downloadZipResource(long maxId, String sessionId, List<Panel> panels/*, List<Group> groups, List<Screen> screens*/);
+   
+   /**
+    * Gets the irb file from zip.
+    * 
+    * @param inputStream the input stream
+    * @param sessionId the session id
+    * 
+    * @return the irb file from zip
+    */
+   String getDotImportFileForRender(String sessionId, InputStream inputStream);
+   
+   /**
+    * Upload image.
+    * 
+    * @param inputStream the input stream
+    * @param fileName the file name
+    * @param sessionId the session id
+    * 
+    * @return the file
+    */
+   File uploadImage(InputStream inputStream, String fileName, String sessionId);
+   
+   File uploadImage(InputStream inputStream, String fileName);
+   
+   public List<GraphicalAssetDTO>getUserImagesURLs();
+
+   /**
+    * Gets the relative resource path.
+    * 
+    * @param sessionId the session id
+    * @param fileName the file name
+    * 
+    * @return the relative resource path
+    */
+   String getRelativeResourcePath(String sessionId, String fileName);
+   
+   String getRelativeResourcePathByCurrentAccount(String fileName);
+   
+   String getPanelsJson(Collection<Panel> panels);
+   
+   void initResources(Collection<Panel> panels,long maxOid);
+   
+   PanelsAndMaxOid restore();
+   
+   boolean canRestore();
+   
+   void saveResourcesToBeehive(Collection<Panel> panels);
+   void saveTemplateResourcesToBeehive(Template Template);
+   void downloadResourcesForTemplate(long templateOid); 
+   
+   File getTemplateResource(Template template); 
+   File getTempDirectory(String sessionId);
+   
+   void deleteImage(String imageName);
+}
