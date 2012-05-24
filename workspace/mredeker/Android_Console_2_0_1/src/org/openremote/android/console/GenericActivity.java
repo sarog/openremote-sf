a1de9e7f54bf67ee30d43424176f49548a84c0b2
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-05-24 12:02:51 +0000

    Create copy to start more development

---
+++ workspace/mredeker/Android_Console_2_0_1/src/org/openremote/android/console/GenericActivity.java
@@ -0,0 +1,60 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2011, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.android.console;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * The Class GenericActivity is the super activity in the application.
+ */
+public class GenericActivity extends Activity {
+
+   /** The is activity resumed. */
+   private boolean isActivityResumed;
+   
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+   }
+
+   @Override
+   protected void onPause() {
+      isActivityResumed = false;
+      super.onPause();
+   }
+
+   @Override
+   protected void onResume() {
+      isActivityResumed = true;
+      super.onResume();
+   }
+   
+   /**
+    * Checks if is activity resumed.
+    * If activity is resumed, can operate it's views;
+    * otherwise can't.
+    * 
+    * @return true, if is activity resumed
+    */
+   public boolean isActivityResumed() {
+      return isActivityResumed;
+   }
+}
