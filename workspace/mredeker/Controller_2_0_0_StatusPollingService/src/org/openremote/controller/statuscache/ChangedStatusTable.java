5e752e45f346f6389c02dd9ea4b334aad9b1a34d
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-05-29 21:42:57 +0000

    Fixed a bug which caused not all waiting panels to be updated

--- workspace/mredeker/Controller_2_0_0_StatusPollingService/src/org/openremote/controller/statuscache/ChangedStatusTable.java
+++ workspace/mredeker/Controller_2_0_0_StatusPollingService/src/org/openremote/controller/statuscache/ChangedStatusTable.java
@@ -88,7 +88,6 @@ public class ChangedStatusTable
         {
            record.getStatusChangedSensorIDs().add(statusChangedSensorID);
            record.notifyAll();
-           break;
         }
       }
     }
