d9cfbd92333b96fc2d126a353994a78f975fed5e
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-06-01 10:35:14 +0000

    add new release branch candidate based on /workspace/juha/Designer_2_13_0_Rosemary_WithInfrared

---
+++ workspace/juha/Designer_2_13_0_Rosemary/src/org/openremote/modeler/shared/dto/DTOReference.java
@@ -0,0 +1,55 @@
+package org.openremote.modeler.shared.dto;
+
+import java.io.Serializable;
+
+/**
+ * Used so that one DTO can reference another one (e.g. sensor referencing its command).
+ * Required so that reference can both exist for objects already persisted (that have an id)
+ * and newly created instances (that don't have an id).
+ * 
+ * @author Eric Bariaux (eric@openremote.org)
+ */
+public class DTOReference implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  
+  private DTO dto;
+  private Long id;
+
+  public DTOReference() {
+    super();
+  }
+
+  public DTOReference(DTO dto) {
+    super();
+    this.dto = dto;
+  }
+
+  public DTOReference(Long id) {
+    super();
+    this.id = id;
+  }
+
+  public DTOReference(DTO dto, Long id) {
+    super();
+    this.dto = dto;
+    this.id = id;
+  }
+
+  public DTO getDto() {
+    return dto;
+  }
+
+  public void setDto(DTO dto) {
+    this.dto = dto;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+  
+}
