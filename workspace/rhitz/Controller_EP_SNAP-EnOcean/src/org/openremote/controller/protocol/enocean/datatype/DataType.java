b86bb473b4abf33478b32b32da95b11dc69b60c5
rhitz <rhitz@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-07-02 18:33:11 +0000

    add javadoc and delete obsolete methods

--- workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/datatype/DataType.java
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/datatype/DataType.java
@@ -1,13 +1,39 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.protocol.enocean.datatype;
 
-
 import org.openremote.controller.model.sensor.Sensor;
-import org.openremote.controller.protocol.enocean.packet.radio.EspRadioTelegram;
 
+/**
+ * The data type interface for EnOcean equipment profile (EEP) data type
+ * implementations.
+ *
+ * @author Rainer Hitz
+ */
 public interface DataType
 {
-  void setRadioData(byte[] data);
-  byte[] getRadioData(int length);
-
+  /**
+   * Updates sensor with data type value.
+   *
+   * @param sensor  the sensor
+   */
   void updateSensor(Sensor sensor);
 }
