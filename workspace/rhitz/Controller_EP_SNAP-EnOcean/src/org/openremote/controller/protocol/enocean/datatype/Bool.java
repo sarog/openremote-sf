647b83a9f099638be4331b963386c897093b2363
rhitz <rhitz@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-07-03 07:19:13 +0000

    add javadoc and finalize implementation

--- workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/datatype/Bool.java
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/datatype/Bool.java
@@ -1,38 +1,103 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.protocol.enocean.datatype;
 
+import org.openremote.controller.component.RangeSensor;
 import org.openremote.controller.model.sensor.Sensor;
 import org.openremote.controller.model.sensor.StateSensor;
+import org.openremote.controller.protocol.enocean.EnOceanCommandBuilder;
+import org.openremote.controller.protocol.enocean.profile.EepData;
 import org.openremote.controller.protocol.enocean.profile.EepDataField;
+import org.openremote.controller.protocol.enocean.profile.EepDataListener;
+import org.openremote.controller.utils.Logger;
 
-public class Bool implements DataType
+/**
+ * Represents a boolean EnOcean equipment profile (EEP) data field type.
+ *
+ *
+ * @author Rainer Hitz
+ */
+public class Bool implements DataType, EepDataListener
 {
+
+  // Class Members --------------------------------------------------------------------------------
+
+  /**
+   * EnOcean logger. Uses a common category for all EnOcean related logging.
+   */
+  private final static Logger log = Logger.getLogger(EnOceanCommandBuilder.ENOCEAN_LOG_CATEGORY);
+
 
   // Private Instance Fields ----------------------------------------------------------------------
 
+  /**
+   * EnOcean equipment profile (EEP) data field.
+   */
   private EepDataField dataField;
-  private BoolScale scale = null;
-  private int rawValue = 0;
+
+  /**
+   * Scale for converting raw EnOcean equipment profile (EEP) data field values to
+   * a boolean value.
+   */
+  private BoolScale scale;
+
+  /**
+   * Raw EnOcean equipment profile (EEP) data field value.
+   */
+  private int rawValue;
 
 
   // Constructors ---------------------------------------------------------------------------------
 
   /**
-   * TODO
+   * Constructs a bool instance with given data field and scale.
    *
-   * @param dataField
-   * @param scale
+   * @param dataField  EnOcean equipment profile (EEP) data field
+   *
+   * @param scale      scale for converting raw EnOcean equipment profile (EEP) data field
+   *                   values to a boolean value
    */
   public Bool(EepDataField dataField, BoolScale scale)
   {
+    if(dataField == null)
+    {
+      throw new IllegalArgumentException("null data field");
+    }
+
+    if(scale == null)
+    {
+      throw new IllegalArgumentException("null scale");
+    }
+
     this.dataField = dataField;
     this.scale = scale;
+    this.rawValue = scale.getFalseCategory().getMinValue();
   }
 
   /**
-   * TODO
+   * Constructs a bool instance with given data field and primitive value.
    *
-   * @param dataField
-   * @param value
+   * @param dataField  EnOcean equipment profile (EEP) data field.
+   *
+   * @param value      initial primitive value
    */
   public Bool(EepDataField dataField, boolean value)
   {
@@ -43,52 +108,104 @@ public class Bool implements DataType
 
   // Public Instance Methods ----------------------------------------------------------------------
 
+  /**
+   * Returns the value of this <tt>Bool</tt> object as a boolean
+   * primitive.
+   *
+   * @return <tt>true</tt> if the object represents the boolean primitive value <tt>true</tt>,
+   *         otherwise <tt>false</tt>
+   */
   public boolean boolValue()
   {
-    int scaledValue = rawValue;
-
-    ScaleCategory category = getScaleCategory();
-
-    if(category != null)
+    if(scale != null)
     {
-      scaledValue = category.getSensorValue();
+      ScaleCategory category = scaleRawValue();
+
+      if(category != null)
+      {
+        return scale.isTrue(category);
+      }
+      else
+      {
+        log.error(
+            "Failed to scale value ''{0}'' for data field ''{1}'' " +
+            "because value is out of valid range.",
+            rawValue, dataField
+        );
+
+        return false;
+      }
     }
 
-    return (scaledValue == 0 ? false : true);
+    else
+    {
+      return (rawValue == 0 ? false : true);
+    }
+  }
+
+
+  // Implements EepDataListener --------------------------------------------------------------
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public void didUpdateData(EepData data)
+  {
+    rawValue = dataField.read(data);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public void updateData(EepData data)
+  {
+    try
+    {
+      dataField.write(rawValue, data);
+    }
+    catch (EepDataField.ValueOutOfRangeException e)
+    {
+      throw new RuntimeException(
+          "Could not write EEP data field value: " + e.getMessage(), e
+      );
+    }
   }
 
 
   // Implements DataType --------------------------------------------------------------------------
 
-  @Override public void setRadioData(byte[] data)
-  {
-    rawValue = dataField.read(data);
-  }
-
-  @Override public byte[] getRadioData(int length)
-  {
-    // TODO
-
-    return null;
-  }
-
+  /**
+   * {@inheritDoc}
+   */
   @Override public void updateSensor(Sensor sensor)
   {
-    ScaleCategory category = getScaleCategory();
+    ScaleCategory category = scaleRawValue();
 
     if(category == null)
     {
+      log.error(
+          "Failed to update sensor ''{0}'' because value ''{1}'' " +
+          "is out of valid range for data field ''{2}''",
+          sensor, rawValue, dataField
+      );
+
       return;
     }
 
+
     if(sensor instanceof StateSensor)
     {
-      sensor.update(category.getSensorStringValue());
+      sensor.update(category.getSensorStateValue());
+    }
+
+    else if(sensor instanceof RangeSensor)
+    {
+      sensor.update("" + category.getSensorValue());
     }
 
     else
     {
-      // TODO : log
+      throw new Error("Unrecognized sensor type: " + sensor.toString());
     }
   }
 
@@ -96,11 +213,13 @@ public class Bool implements DataType
   // Private Instance Methods ---------------------------------------------------------------------
 
   /**
-   * TODO
+   * Determines the scale category for the raw data field value and returns it.
    *
-   * @return
+   * @return scale category which represents a boolean primitive value or <tt>null</tt>
+   *         if there is no scale object set or there is no matching scale category for the
+   *         raw value
    */
-  private ScaleCategory getScaleCategory()
+  private ScaleCategory scaleRawValue()
   {
     ScaleCategory category = null;
 
