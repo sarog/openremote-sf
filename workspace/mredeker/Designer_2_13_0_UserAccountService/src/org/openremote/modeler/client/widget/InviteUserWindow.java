e0323d0cd2a5ebef484c4709ae4d7a75f209d305
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-07-27 13:55:04 +0000

    Code cleaning

--- workspace/mredeker/Designer_2_13_0_UserAccountService/src/org/openremote/modeler/client/widget/InviteUserWindow.java
+++ workspace/mredeker/Designer_2_13_0_UserAccountService/src/org/openremote/modeler/client/widget/InviteUserWindow.java
@@ -1,3 +1,22 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.openremote.modeler.client.widget;
 
 import org.openremote.modeler.client.Constants;
@@ -22,6 +41,11 @@ import com.sencha.gxt.widget.core.client.form.SimpleComboBox;
 import com.sencha.gxt.widget.core.client.form.TextField;
 import com.sencha.gxt.widget.core.client.form.validator.RegExValidator;
 
+/*
+ * Dailog for inviting a user to "share" a designer account.
+ * 
+ * @author <a href = "mailto:eric@openremote.org">Eric Bariaux</a>
+ */
 public class InviteUserWindow extends Window {
 
   private static InviteUserWindowUiBinder uiBinder = GWT.create(InviteUserWindowUiBinder.class);
