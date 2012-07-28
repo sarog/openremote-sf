a8bf6802e82fc1214ade9757c7d2d3c005d813ae
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-07-28 10:57:55 +0000

    Finish ControllerManagement window based on new Account management

--- workspace/mredeker/Designer_2_13_0_UserAccountService/src/org/openremote/modeler/client/widget/ControllerManageWindow.java
+++ workspace/mredeker/Designer_2_13_0_UserAccountService/src/org/openremote/modeler/client/widget/ControllerManageWindow.java
@@ -22,230 +22,163 @@ package org.openremote.modeler.client.widget;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openremote.modeler.client.event.SubmitEvent;
-import org.openremote.modeler.client.icon.Icons;
-import org.openremote.modeler.client.listener.FormSubmitListener;
-import org.openremote.modeler.client.listener.SubmitListener;
+import org.openremote.modeler.client.icon.IconResources;
 import org.openremote.modeler.client.rpc.AsyncServiceFactory;
 import org.openremote.modeler.client.rpc.AsyncSuccessCallback;
+import org.openremote.modeler.client.widget.AddControllerWindow.ControllerAddedEvent;
 import org.openremote.useraccount.domain.ControllerDTO;
 
-import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
-import com.extjs.gxt.ui.client.Style.IconAlign;
-import com.extjs.gxt.ui.client.data.BeanModel;
-import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.Events;
-import com.extjs.gxt.ui.client.event.FormEvent;
-import com.extjs.gxt.ui.client.event.Listener;
-import com.extjs.gxt.ui.client.event.SelectionListener;
-import com.extjs.gxt.ui.client.store.ListStore;
-import com.extjs.gxt.ui.client.widget.ContentPanel;
-import com.extjs.gxt.ui.client.widget.Dialog;
-import com.extjs.gxt.ui.client.widget.Info;
-import com.extjs.gxt.ui.client.widget.button.Button;
-import com.extjs.gxt.ui.client.widget.form.TextField;
-import com.extjs.gxt.ui.client.widget.form.FormPanel.LabelAlign;
-import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
-import com.extjs.gxt.ui.client.widget.grid.ColumnData;
-import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
-import com.extjs.gxt.ui.client.widget.grid.EditorGrid;
-import com.extjs.gxt.ui.client.widget.grid.Grid;
-import com.extjs.gxt.ui.client.widget.grid.GridCellRenderer;
-import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.editor.client.Editor.Path;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiFactory;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.cell.core.client.TextButtonCell;
+import com.sencha.gxt.core.client.ValueProvider;
+import com.sencha.gxt.data.shared.ListStore;
+import com.sencha.gxt.data.shared.ModelKeyProvider;
+import com.sencha.gxt.data.shared.PropertyAccess;
+import com.sencha.gxt.widget.core.client.Window;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
+import com.sencha.gxt.widget.core.client.event.SelectEvent;
+import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
+import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
+import com.sencha.gxt.widget.core.client.grid.ColumnModel;
+import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.info.Info;
 
 /**
- * This window is for managing controller, linked to the account
- * @author marcus
+ * This window is for managing controller that are linked to the account.
+ * 
+ * @author <a href = "mailto:marcus@openremote.org">Marcus Redeker</a>
  */
-public class ControllerManageWindow extends Dialog {
-   private Icons icons = GWT.create(Icons.class);
-   
-   /** 
-    * The linked controller grid.
-    */
-   private EditorGrid<BeanModel> linkedControllerGrid = null;
-   
-   public ControllerManageWindow() {
-      setHeading("Controller management");
-      setHideOnButtonClick(true);
-      setButtonAlign(HorizontalAlignment.CENTER);
-      setAutoHeight(true);
-      setButtons("");
-      setWidth(452);
-      setMinHeight(280);
-      addControllerButton();
-      createLinkedControllerGrid();
-      show();
-   }
-   
-   /**
-    * Adds a button, if click it, it would pop up a window to input a controller MAC address<br>
-    * After the MAC address is submitted, it's checked against unlinked controller MAC address and<br> 
-    * if a match is found, the controller is linked to this account and the link shown in the linked<br>
-    * controller grid. 
-    */
-   private void addControllerButton() {
-      Button inviteUserButton = new Button("Link controller");
-      inviteUserButton.setIcon(icons.controllerAddIcon());
-      inviteUserButton.setIconAlign(IconAlign.LEFT);
-      inviteUserButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
-         public void componentSelected(ButtonEvent ce) {
-            final LinkControllerWindow linkControllerWindow = new LinkControllerWindow();
-            linkControllerWindow.addListener(SubmitEvent.SUBMIT, new SubmitListener() {
-               public void afterSubmit(SubmitEvent be) {
-                 linkControllerWindow.hide();
-                  ControllerDTO controllerDTO = be.getData();
-                  if (controllerDTO != null) {
-                     if (linkedControllerGrid == null) {
-                       createLinkedControllerGrid();
-                     }
-                     linkedControllerGrid.stopEditing();
-//                     linkedControllerGrid.getStore().insert(DTOHelper.getBeanModel(controllerDTO), 0);
-                     linkedControllerGrid.startEditing(0, 1);
-                  }
-               }
-            });
-         }
-      });
-      add(inviteUserButton);
-   }
-   
-   
-   /**
-    * Creates the linkedControllerGrid, the grid stores the controller that are linked to the account.
-    * The grid is used for managing the linked controller 
-    * columns: MACAddress and delete.
-    */
-   private void createLinkedControllerGrid() {
-      List<ColumnConfig> linkedControllerConfigs = new ArrayList<ColumnConfig>();
-      linkedControllerConfigs.add(new ColumnConfig("macAddress", "MAC Address", 180));
+public class ControllerManageWindow extends Window {  
+  
+  private IconResources icons = GWT.create(IconResources.class);
+  
+  @UiField(provided = true)
+  BorderLayoutData northData = new BorderLayoutData(36);
+    
+  private static ControllerManageWindowUiBinder uiBinder = GWT.create(ControllerManageWindowUiBinder.class);
+
+  interface ControllerDTOProvider extends PropertyAccess<ControllerDTO> {    
+    @Path("oid")
+    ModelKeyProvider<ControllerDTO> key();
+    
+    ValueProvider<ControllerDTO, String> macAddress();
+    ValueProvider<ControllerDTO, Long> oid();
+  }
+  
+  private ControllerDTOProvider controllers = GWT.create(ControllerDTOProvider.class);
+
+  interface ControllerManageWindowUiBinder extends UiBinder<Widget, ControllerManageWindow> {
+  }
+  
+  @UiFactory
+  Window itself() {
+    return this;
+  }
+
+  @UiField
+  TextButton addControllerButton;
+     
+  @UiField(provided=true)
+  Grid<ControllerDTO> linkedControllerGrid;
+
+  private ColumnModel<ControllerDTO> linkedControllerColumnModel;
+  private ListStore<ControllerDTO> linkedControllerStore;
+
+  public ControllerManageWindow() {
+
+    createLinkedControllerGrid();
+
+    uiBinder.createAndBindUi(this);
       
-      GridCellRenderer<BeanModel> buttonRenderer = new GridCellRenderer<BeanModel>() {
-        public Object render(final BeanModel model, String property, ColumnData config, final int rowIndex,
-              final int colIndex, final ListStore<BeanModel> store, Grid<BeanModel> grid) {
-           Button deleteButton = createDeleteButton(model, store);
-           return deleteButton;
+    linkedControllerGrid.getView().setAutoExpandColumn(linkedControllerGrid.getColumnModel().getColumn(0));      
+    linkedControllerGrid.mask("Loading linked controller...");
+
+    show();
+    
+    AsyncServiceFactory.getLinkControllerRPCServiceAsync().getLinkedControllerDTOs(new AsyncSuccessCallback<ArrayList<ControllerDTO>>() {
+      @Override
+      public void onSuccess(ArrayList<ControllerDTO> linkedController) {
+        linkedControllerStore.addAll(linkedController);
+        linkedControllerGrid.unmask();
+            setPixelSize(453, 281);
+      }
+    });
+
+  }
+
+  /**
+   * Brings up a dialog to enter a MAC address for a new controller.
+   * On success, linked controller table is updated with newly linked controller.
+   */   
+  @UiHandler("addControllerButton")
+  void onAddControllerClick(SelectEvent e) {
+    final AddControllerWindow addControllerWindow = new AddControllerWindow();
+
+    addControllerWindow.addHandler(new AddControllerWindow.ControllerAddedHandler() {
+      @Override
+      public void controllerAdded(ControllerDTO controller) {
+        if (controller != null) {
+          linkedControllerStore.add(controller);
         }
-      };
-      ColumnConfig actionColumn = new ColumnConfig("delete", "Delete", 50);
-      actionColumn.setSortable(false);
-      actionColumn.setRenderer(buttonRenderer);
-      linkedControllerConfigs.add(actionColumn);
-      
-      final EditorGrid<BeanModel> linkedControllerGrid = new EditorGrid<BeanModel>(new ListStore<BeanModel>(), new ColumnModel(linkedControllerConfigs)) {
-         @Override
-         protected void afterRender() {
-            super.afterRender();
-            layout();
-            center();
-            this.mask("Loading linked controller...");
-         }
-      };
-      
-      ContentPanel accessUsersContainer = new ContentPanel();
-      accessUsersContainer.setBodyBorder(false);
-      accessUsersContainer.setHeading("Linked controller");
-      accessUsersContainer.setLayout(new FitLayout());
-      accessUsersContainer.setStyleAttribute("paddingTop", "5px");
-      accessUsersContainer.setSize(440, 150);
-      accessUsersContainer.add(linkedControllerGrid);
-      add(accessUsersContainer);
-      AsyncServiceFactory.getLinkControllerRPCServiceAsync().getLinkedControllerDTOs(new AsyncSuccessCallback<ArrayList<ControllerDTO>>() {
-         public void onSuccess(ArrayList<ControllerDTO> linkedControllers) {
-            if (linkedControllers.size() > 0) {
-              //linkedControllerGrid.getStore().add(DTOHelper.createModels(linkedControllers));
-              linkedControllerGrid.unmask();
-            }
-         }
-         public void onFailure(Throwable caught) {
-            super.onFailure(caught);
-            linkedControllerGrid.unmask();
-         }
-      });
-   }
+        addControllerWindow.hide();
+      }
+    }, ControllerAddedEvent.TYPE);
+    addControllerWindow.show();
+  }
    
+  private void createLinkedControllerGrid() {
+    linkedControllerStore = new ListStore<ControllerDTO>(controllers.key());
 
-   /**
-    * Creates the delete button to remove a linked controller from the account
-    * 
-    * @param model the model
-    * @param store the store
-    * 
-    * @return the button
-    */
-   private Button createDeleteButton(final BeanModel model, final ListStore<BeanModel> store) {
-      Button deleteButton = new Button();
-      deleteButton.setIcon(icons.controllerDeleteIcon());
-      deleteButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
-         public void componentSelected(ButtonEvent ce) {
-            AsyncServiceFactory.getLinkControllerRPCServiceAsync().deleteController(((ControllerDTO)model.getBean()).getOid(), new AsyncSuccessCallback<Void>() {
-               public void onSuccess(Void result) {
-                  store.remove(model);
-                  Info.display("Delete controller", "Delete controller '" + model.get("macAddress").toString() + "' success.");
-               }
-            });
-         }
-      });
-      return deleteButton;
-   }
+    ColumnConfig<ControllerDTO, Long> idColumn = new ColumnConfig<ControllerDTO, Long>(controllers.oid(), 20, "Id");
+    idColumn.setSortable(false);
+     
+    ColumnConfig<ControllerDTO, String> macAddressColumn = new ColumnConfig<ControllerDTO, String>(controllers.macAddress(), 210, "MAC Address");
+     
+    ColumnConfig<ControllerDTO, String> deleteColumn = createDeleteColumn(new TextButtonCell() {
+      public void render(Context context, String value, SafeHtmlBuilder sb) {       
+        super.render(context, "", sb);
+      }
+    }, linkedControllerStore);
+     
+    List<ColumnConfig<ControllerDTO, ?>> l = new ArrayList<ColumnConfig<ControllerDTO, ?>>();
+    l.add(idColumn);
+    l.add(macAddressColumn);
+    l.add(deleteColumn);
+    
+    linkedControllerColumnModel = new ColumnModel<ControllerDTO>(l);
+    linkedControllerGrid = new Grid<ControllerDTO>(linkedControllerStore, linkedControllerColumnModel);
+  }
 
-   /**
-    * The inner class is for linking a controller
-    */
-   private class LinkControllerWindow extends FormWindow {
-      public LinkControllerWindow() {
-         setSize(370, 150);
-         setHeading("Add controller");
-         form.setLabelAlign(LabelAlign.RIGHT);
-         createFields();
-         createButtons(this);
-         add(form);
-         show();
+   
+  private ColumnConfig<ControllerDTO, String> createDeleteColumn(TextButtonCell button, ListStore<ControllerDTO> store) {
+    ColumnConfig<ControllerDTO, String> deleteColumn = new ColumnConfig<ControllerDTO, String>(controllers.macAddress(), 45, "Delete");
+    deleteColumn.setSortable(false);
+    button.setIcon(icons.delete());
+    button.addSelectHandler(createDeleteSelectHandler(store));
+    deleteColumn.setCell(button);
+    return deleteColumn;
+  }
+   
+  private SelectHandler createDeleteSelectHandler(final ListStore<ControllerDTO> store) {
+    return new SelectHandler() { 
+      @Override
+      public void onSelect(SelectEvent event) {
+        final ControllerDTO controller = store.get(event.getContext().getIndex());
+        AsyncServiceFactory.getLinkControllerRPCServiceAsync().deleteController(controller.getOid(), new AsyncSuccessCallback<Void>() {
+          public void onSuccess(Void result) {
+            store.remove(controller);
+            Info.display("Delete controller", "Delete controller '" + controller.getOid() + "' success.");
+          }
+        });
       }
-      
-      /**
-       * Creates one fields: MAC address input
-       */
-      private void createFields() {
-         final TextField<String> macAddressField = new TextField<String>();
-         macAddressField.setFieldLabel("MAC address");
-         macAddressField.setAllowBlank(false);
-         form.add(macAddressField);
-         
-         form.addListener(Events.BeforeSubmit, new Listener<FormEvent>() {
-            public void handleEvent(FormEvent be) {
-               form.mask("Adding controller ...");
-               AsyncServiceFactory.getLinkControllerRPCServiceAsync().linkController(macAddressField.getValue(), new AsyncSuccessCallback<ControllerDTO>() {
-                    public void onSuccess(ControllerDTO controller) {
-                       form.unmask();
-                       fireEvent(SubmitEvent.SUBMIT, new SubmitEvent(controller));
-                    }
-                    public void onFailure(Throwable caught) {
-                       super.onFailure(caught);
-                       form.unmask();
-                    }
-                 });
-            }
-         });
-      }
-      
-      /**
-       * Creates two buttons to link controller or cancel.
-       * 
-       * @param window the window
-       */
-      private void createButtons(final LinkControllerWindow window) {
-         Button send = new Button("Add controller");
-         send.addSelectionListener(new FormSubmitListener(form, send));
-         Button cancel = new Button("Cancel");
-         cancel.addSelectionListener(new SelectionListener<ButtonEvent>() {
-            public void componentSelected(ButtonEvent ce) {
-               window.hide();
-            }
-         });
-         form.addButton(send);
-         form.addButton(cancel);
-      }
-   }
+    };
+  }
 }
