ebcb0cd0b046bab500b84eae00b09435473d505a
rhitz <rhitz@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-07-30 16:37:36 +0000

    add constant classes

---
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/profile/EepConstants.java
@@ -0,0 +1,99 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.enocean.profile;
+
+/**
+ * Constant class for EnOcean equipment profile (EEP) related constants.
+ *
+ * @author Rainer Hitz
+ */
+public final class EepConstants
+{
+
+  // Constants ------------------------------------------------------------------------------------
+
+  /**
+   * EnOcean equipment profile (EEP) temperature data field name.
+   */
+  public static final String EEP_TEMPERATURE_DATA_FIELD_NAME = "TMP";
+
+  /**
+   * EnOcean equipment profile (EEP) learn bit data field name.
+   */
+  public static final String EEP_LEARN_BIT_DATA_FIELD_NAME = "LRNB";
+
+  /**
+   * Bit offset of EnOcean equipment profile (EEP) learn bit data field for
+   * {@link org.openremote.controller.protocol.enocean.packet.radio.EspRadioTelegram.RORG#BS4 4BS}
+   * radio telegrams.
+   */
+  public static final int EEP_LEARN_BIT_DATA_FIELD_OFFSET_4BS = 28;
+
+  /**
+   * Bit size of EnOcean equipment profile (EEP) learn bit data field.
+   */
+  public static final int EEP_LEARN_BIT_DATA_FIELD_SIZE = 1;
+
+  /**
+   * Enocean equipment profile (EEP) learn bit data field value for indicating
+   * a teach in telegram.
+   *
+   * @see #EEP_LEARN_BIT_DATA_FIELD_NAME
+   * @see #EEP_LEARN_BIT_DATA_FIELD_TEACH_IN_TELEGRAM_DESC
+   */
+  public static final int EEP_LEARN_BIT_DATA_FIELD_TEACH_IN_TELEGRAM_VALUE = 0;
+
+  /**
+   * Enocean equipment profile (EEP) learn bit data field value for indicating
+   * a regular data telegram.
+   *
+   * @see #EEP_LEARN_BIT_DATA_FIELD_NAME
+   * @see #EEP_LEARN_BIT_DATA_FIELD_DATA_TELEGRAM_VALUE
+   */
+  public static final int EEP_LEARN_BIT_DATA_FIELD_DATA_TELEGRAM_VALUE = 1;
+
+  /**
+   * Description for an EnOcean teach in telegram indicated by the
+   * {@link #EEP_LEARN_BIT_DATA_FIELD_TEACH_IN_TELEGRAM_VALUE}.
+   *
+   * @see #EEP_LEARN_BIT_DATA_FIELD_NAME
+   */
+  public static final String EEP_LEARN_BIT_DATA_FIELD_TEACH_IN_TELEGRAM_DESC = "Teach-in telegram";
+
+  /**
+   * Description for an EnOcean data telegram indicated by the
+   * {@link #EEP_LEARN_BIT_DATA_FIELD_DATA_TELEGRAM_VALUE}.
+   *
+   * @see #EEP_LEARN_BIT_DATA_FIELD_NAME
+   */
+  public static final String EEP_LEARN_BIT_DATA_FIELD_DATA_TELEGRAM_DESC = "Data telegram";
+
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  /**
+   * Private constructor to prevent instantiation of constant class.
+   */
+  private EepConstants()
+  {
+
+  }
+}
