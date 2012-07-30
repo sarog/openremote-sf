796b72810499c007410f81ef68b7fe89a8388bb5
rhitz <rhitz@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-07-30 16:45:01 +0000

    add unit tests for EnOcean temperature sensor profiles

---
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/test/org/openremote/controller/protocol/enocean/profile/EepA5020BTest.java
@@ -0,0 +1,111 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.enocean.profile;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openremote.controller.protocol.enocean.Constants;
+import org.openremote.controller.protocol.enocean.DeviceID;
+
+
+/**
+ * Unit tests for {@link EepA5020B} class.
+ *
+ * @author Rainer Hitz
+ */
+public class EepA5020BTest extends EepA502XXTest
+{
+
+  // Tests ----------------------------------------------------------------------------------------
+
+  @Test public void testBasicConstruction() throws Exception
+  {
+    Eep eep = EepType.lookup("A5-02-0B").createEep(
+        deviceID, Constants.TEMPERATURE_STATUS_COMMAND
+    );
+
+    Assert.assertTrue(eep instanceof EepA5020B);
+    Assert.assertEquals(EepType.EEP_TYPE_A5020B, eep.getType());
+  }
+
+  @Test public void testUpdate() throws Exception
+  {
+    EepA5020B eep = (EepA5020B)EepType.lookup("A5-02-0B").createEep(
+        deviceID, Constants.TEMPERATURE_STATUS_COMMAND
+    );
+
+    int rawTemperatureValue = 255;
+    Boolean isUpdate = eep.update(createRadioTelegram(deviceID, rawTemperatureValue));
+
+    Assert.assertTrue(isUpdate);
+    Assert.assertEquals(Double.valueOf(60), eep.getTemperature(), 0.0);
+
+
+    rawTemperatureValue = 255;
+    isUpdate = eep.update(createRadioTelegram(deviceID, rawTemperatureValue));
+    Assert.assertFalse(isUpdate);
+
+
+    rawTemperatureValue = 0;
+    isUpdate = eep.update(createRadioTelegram(deviceID, rawTemperatureValue));
+
+    Assert.assertTrue(isUpdate);
+    Assert.assertEquals(Double.valueOf(100), eep.getTemperature(), 0.0);
+  }
+
+  @Test public void testUpdateWithTeachInTelegram() throws Exception
+  {
+    EepA5020B eep = (EepA5020B)EepType.lookup("A5-02-0B").createEep(
+        deviceID, Constants.TEMPERATURE_STATUS_COMMAND
+    );
+
+    // Regular update...
+
+    int rawTemperatureValue = 255;
+    Boolean isUpdate = eep.update(createRadioTelegram(deviceID, rawTemperatureValue));
+
+    Assert.assertTrue(isUpdate);
+    Assert.assertEquals(Double.valueOf(60), eep.getTemperature(), 0.0);
+
+
+    // Update with teach-in telegram...
+
+    rawTemperatureValue = 100;
+    isUpdate = eep.update(createTeachInTelegram(deviceID, rawTemperatureValue));
+
+    Assert.assertFalse(isUpdate);
+    Assert.assertEquals(Double.valueOf(60), eep.getTemperature(), 0.0);
+
+
+    // Regular update...
+
+    rawTemperatureValue = 0;
+    isUpdate = eep.update(createRadioTelegram(deviceID, rawTemperatureValue));
+
+    Assert.assertTrue(isUpdate);
+    Assert.assertEquals(Double.valueOf(100), eep.getTemperature(), 0.0);
+  }
+}
+
+
+
+
---
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/test/org/openremote/controller/protocol/enocean/profile/EepA502XXTest.java
@@ -0,0 +1,74 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.enocean.profile;
+
+import org.junit.Before;
+import org.openremote.controller.protocol.enocean.DeviceID;
+import org.openremote.controller.protocol.enocean.packet.radio.Esp34BSTelegram;
+
+/**
+ * Unit test base class for temperature sensor EnOcean equipment profile (EEP)
+ * implementations ('A5-02-01', 'A5-02-02', 'A5-02-03'...).
+ *
+ * @author Rainer Hitz
+ */
+public class EepA502XXTest
+{
+
+  // Protected Instance Fields ----------------------------------------------------------------------
+
+  protected DeviceID deviceID;
+
+
+  // Test Lifecycle -------------------------------------------------------------------------------
+
+  @Before public void setUp() throws Exception
+  {
+    deviceID = DeviceID.fromString("0xFF800001");
+  }
+
+
+  // Helpers --------------------------------------------------------------------------------------
+
+  protected Esp34BSTelegram createRadioTelegram(DeviceID deviceID, int rawTempValue)
+  {
+    byte[] payload = new byte[4];
+    payload[1] = (byte)((rawTempValue & 0xFF00) >> 8);
+    payload[2] = (byte)rawTempValue;
+    payload[3] = 0x08; // Regular telegram
+
+    Esp34BSTelegram telegram = new Esp34BSTelegram(deviceID, payload, (byte)0x00);
+
+    return telegram;
+  }
+
+  protected Esp34BSTelegram createTeachInTelegram(DeviceID deviceID, int rawTempValue)
+  {
+    byte[] payload = new byte[4];
+    payload[1] = (byte)((rawTempValue & 0xFF00) >> 8);
+    payload[2] = (byte)rawTempValue;
+    payload[3] = 0x00; // Teach-in telegram
+
+    Esp34BSTelegram telegram = new Esp34BSTelegram(deviceID, payload, (byte)0x00);
+
+    return telegram;
+  }
+}
