0ac7f61338773910d83eaacfacab0c50ce2ec19b
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-08-07 16:05:24 +0000

    Interpret command  as HEX value when prepended with "0x"

--- workspace/mredeker/Controller_2_0_0_Fixes/src/org/openremote/controller/protocol/socket/TCPSocketCommand.java
+++ workspace/mredeker/Controller_2_0_0_Fixes/src/org/openremote/controller/protocol/socket/TCPSocketCommand.java
@@ -147,7 +147,14 @@ public class TCPSocketCommand implements ExecutableCommand, StatusCommand {
          StringTokenizer st = new StringTokenizer(getCommand(), "|");
          while (st.hasMoreElements()) {
             String cmd = (String) st.nextElement();
-            out.write((cmd + "\r").getBytes());
+            byte[] bytes;
+            if (cmd.startsWith("0x")) {
+               String tmp = getCommand().substring(2);
+               bytes = hexStringToByteArray(tmp.replaceAll(" ", "").toLowerCase());
+            } else {
+               bytes = (cmd + "\r").getBytes();
+            }
+            out.write(bytes);
          }
 
          String result = readReply(socket);
@@ -179,6 +186,15 @@ public class TCPSocketCommand implements ExecutableCommand, StatusCommand {
       }
    }
 
+   
+   private byte[] hexStringToByteArray(String s) {
+      int len = s.length();
+      byte[] data = new byte[len / 2];
+      for (int i = 0; i < len; i += 2) {
+          data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+      }
+      return data;
+   }
 
 
   @Override public String read(EnumSensorType sensorType, Map<String, String> stateMap)
