447696df4204c9bd0cdee35ea5b1b504918e726a
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-10-02 23:37:35 +0000

    STEP 1: rewrite of tcp command to support keep-alive connections and waitForResponse flag, with Olivier's physical bus abstraction (non-shared implementation)

--- workspace/juha/Controller_2_0_0_TCP-KeepAlive/src/org/openremote/controller/protocol/socket/TCPSocketCommand.java
+++ workspace/juha/Controller_2_0_0_TCP-KeepAlive/src/org/openremote/controller/protocol/socket/TCPSocketCommand.java
@@ -1,6 +1,6 @@
 /*
  * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
+ * Copyright 2008-2012, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
@@ -20,258 +20,337 @@
  */
 package org.openremote.controller.protocol.socket;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.apache.log4j.Logger;
+import org.openremote.controller.Constants;
 import org.openremote.controller.command.ExecutableCommand;
-import org.openremote.controller.command.StatusCommand;
-import org.openremote.controller.component.EnumSensorType;
 import org.openremote.controller.model.sensor.Sensor;
+import org.openremote.controller.protocol.ReadCommand;
+import org.openremote.controller.protocol.bus.Message;
+import org.openremote.controller.utils.Logger;
 
 /**
  * TODO
  *
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  * @author Marcus Redeker 2009-4-26
  * @author Phillip Lavender
- * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  * @author Ivan Martinez
  */
-public class TCPSocketCommand implements ExecutableCommand, StatusCommand {
+public class TCPSocketCommand extends ReadCommand implements ExecutableCommand
+{
 
-   /** The logger. */
-   private static Logger logger = Logger.getLogger(TCPSocketCommand.class.getName());
+  // Class Members --------------------------------------------------------------------------------
 
-   /** A name to identify event in controller.xml. */
-   private String name;
-
-   /** A pipe separated list of command string that are sent over the socket */
-   private String command;
-
-   /** The IP to which the socket is opened */
-   private String ip;
-
-   /** The port that is opened */
-   private String port;
+  private static Logger log = Logger.getLogger(Constants.CONTROLLER_PROTOCOL_LOG_CATEGORY + "tcp");
 
 
-   /**
-    * Gets the command.
-    *
-    * @return the command
-    */
-   public String getCommand() {
-      return command;
-   }
 
-   /**
-    * Sets the command.
-    *
-    * @param command the new command
-    */
-   public void setCommand(String command) {
-      this.command = command;
-   }
+  // Instance Fields ------------------------------------------------------------------------------
 
-   /**
-    * Gets the name.
-    *
-    * @return the name
-    */
-   public String getName() {
-      return name;
-   }
-
-   /**
-    * Sets the name.
-    *
-    * @param name the new name
-    */
-   public void setName(String name) {
-      this.name = name;
-   }
+  private String name;
+  private TCPSocketCommandBuilder.TCPConnection connection;
+  private String payload;
+  private Boolean waitForResponse;
 
 
-   /**
-    * Gets the ip
-    * @return the ip
-    */
-   public String getIp() {
-      return ip;
-   }
+  // Constructors ---------------------------------------------------------------------------------
 
-   /**
-    * Sets the ip
-    * @param ip the new ip
-    */
-   public void setIp(String ip) {
-      this.ip = ip;
-   }
-
-   /**
-    * Gets the port
-    * @return the port
-    */
-   public String getPort() {
-      return port;
-   }
-
-	/**
-	 * Sets the port
-	 * @param port the new port
-	 */
-   public void setPort(String port) {
-      this.port = port;
-   }
-
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public void send() {
-      requestSocket();
-   }
-
-   private String requestSocket() {
-      Socket socket = null;
-      try {
-         socket = new Socket(getIp(), Integer.parseInt(getPort()));
-         OutputStream out = socket.getOutputStream();
-
-         StringTokenizer st = new StringTokenizer(getCommand(), "|");
-         while (st.hasMoreElements()) {
-            String cmd = (String) st.nextElement();
-            byte[] bytes;
-            if (cmd.startsWith("0x")) {
-               String tmp = getCommand().substring(2);
-               bytes = hexStringToByteArray(tmp.replaceAll(" ", "").toLowerCase());
-            } else {
-               bytes = (cmd + "\r").getBytes();
-            }
-            out.write(bytes);
-         }
-
-         String result = readReply(socket);
-         logger.info("received message: " + result);
-         return result;
-      } catch (Exception e) {
-         logger.error("Socket event could not execute", e);
-      } finally {
-         if (socket != null) {
-            try {
-               socket.close();
-            } catch (IOException e) {
-               logger.error("Socket could not be closed", e);
-            }
-         }
-      }
-      return "";
-   }
-
-   private String readReply(java.net.Socket socket) throws IOException {
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      char[] buffer = new char[200];
-      int readChars = bufferedReader.read(buffer, 0, 200); // blocks until message received
-      if (readChars > 0) {
-         String reply = new String(buffer, 0, readChars);
-         return reply;
-      } else {
-         return "";
-      }
-   }
-
-   
-   private byte[] hexStringToByteArray(String s) {
-      int len = s.length();
-      byte[] data = new byte[len / 2];
-      for (int i = 0; i < len; i += 2) {
-          data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
-      }
-      return data;
-   }
-
-
-  @Override public String read(EnumSensorType sensorType, Map<String, String> stateMap)
+  TCPSocketCommand(String name, String payload, TCPSocketCommandBuilder.TCPConnection connection,
+                   Boolean waitForResponse)
   {
-    String regexResult = null;
-    String strState = null;
+    log.info("Created TCP command ''{0}'' for connection {1} with unparsed payload ''{2}''",
+        name, connection, payload);
 
-    // Write the command to socket...
-    //
-    // TODO : rather poorly named method
+    this.connection = connection;
+    this.payload = payload;
+    this.name = name;
+    this.waitForResponse = waitForResponse;
+  }
 
-    String rawResult = requestSocket();
+
+  // Implements ExecutableCommand -----------------------------------------------------------------
+
+  @Override public void send()
+  {
+    if (waitForResponse)
+    {
+      sendAndReceive();
+    }
+
+    else
+    {
+      sendOnly();
+    }
+  }
+
+  // Implements ReadCommand -----------------------------------------------------------------------
+
+  @Override public String read(Sensor s)
+  {
+    String rawResult = sendAndReceive();
 
     // Strip response from control characters (non-ASCII)...
-    //
-    // Patch provided by Phillip Lavender
 
     Pattern p = Pattern.compile("\\p{Cntrl}");
     Matcher m = p.matcher(rawResult);
-    regexResult = m.replaceAll("");
-	   
+    String regexResult = m.replaceAll("");
+
     if ("".equals(regexResult))
     {
-       return UNKNOWN_STATUS;
+       return Sensor.UNKNOWN_STATUS;
     }
 
-    switch (sensorType)
+    return regexResult;
+  }
+
+
+  // Private Instance Methods ---------------------------------------------------------------------
+
+  private void sendOnly()
+  {
+    try
     {
-      case RANGE:
-         break;
+      Set<Message> messages = parseMessages(payload);
 
-      case LEVEL:
-         String min = stateMap.get(Sensor.RANGE_MIN_STATE);
-         String max = stateMap.get(Sensor.RANGE_MAX_STATE);
+      for (Message m : messages)
+      {
+        log.info("Sent " + m);
 
-         try
-         {
-            int val = Integer.valueOf(regexResult);
+        connection.send(m);
+      }
+    }
 
-            if (min != null && max != null)
-            {
-               int minVal = Integer.valueOf(min);
-               int maxVal = Integer.valueOf(max);
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
 
-               return String.valueOf(100 * (val - minVal)/ (maxVal - minVal));
-            } 
-         }
+  private String sendAndReceive()
+  {
+    StringBuffer sbuf = new StringBuffer();
 
-         catch (ArithmeticException e)
-         {
-            logger.warn("Level sensor values cannot be parsed: " + e.getMessage(), e);
-         }
+    try
+    {
+      Set<Message> messages = parseMessages(payload);
 
-         break;
+      for (Message m : messages)
+      {
+        log.info("Sent " + m);
 
-      default://NOTE: if sensor type is RANGE, this map only contains min/max states.
+        connection.send(m);
 
-        // If custom sensor type has been configured, map the 'raw' return value to configured
-        // 'wanted' return value
-        //
-        // TODO :
-        //   no reason to put this implementation burden on protocol implementations, the
-        //   calling code could do it instead
+        Message response = connection.receive();
 
-        for (String state : stateMap.keySet())
-        {
-          strState = stateMap.get(state);
+        sbuf.append(new String(response.getContent()));
+      }
+    }
 
-          if (regexResult.equals(strState))
-          {
-            return state;
-          }
-        }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+
+    return sbuf.toString();
+  }
+
+  private Set<Message> parseMessages(String payload)
+  {
+    StringTokenizer st = new StringTokenizer(payload, "|");
+
+    Set<Message> msgs = new HashSet<Message>();
+
+
+    while (st.hasMoreElements())
+    {
+      msgs.add(new ByteArrayMessage(st.nextToken()));
+    }
+
+    return msgs;
+  }
+
+
+  // Nested Classes -------------------------------------------------------------------------------
+
+  private static class ByteArrayMessage extends Message
+  {
+    private static byte[] parse(String unparsedPayload)
+    {
+      byte[] bytes;
+
+      if (unparsedPayload.startsWith("0x"))
+      {
+         String tmp = unparsedPayload.substring(2);
+         bytes = hexStringToByteArray(tmp.replaceAll(" ", "").toLowerCase());
       }
 
+      else
+      {
+         bytes = (unparsedPayload + "\r").getBytes();
+      }
 
-      return regexResult;
+      return bytes;
+    }
+
+    private static byte[] hexStringToByteArray(String s)
+    {
+      int len = s.length();
+      byte[] data = new byte[len / 2];
+
+      for (int i = 0; i < len; i += 2)
+      {
+        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+      }
+
+      return data;
+    }
+
+    private ByteArrayMessage(String unparsedPayload)
+    {
+      super(parse(unparsedPayload));
+    }
+
+    @Override public String toString()
+    {
+      return new String(super.getContent());
+    }
   }
+
+
+
+
+
+
+
+//   private String requestSocket() {
+//      Socket socket = null;
+//      try {
+//         socket = new Socket(getIp(), Integer.parseInt(getPort()));
+//         OutputStream out = socket.getOutputStream();
+//
+//         StringTokenizer st = new StringTokenizer(getCommand(), "|");
+//         while (st.hasMoreElements()) {
+//            String cmd = (String) st.nextElement();
+//+            byte[] bytes;
+//+            if (cmd.startsWith("0x")) {
+//+               String tmp = getCommand().substring(2);
+//+               bytes = hexStringToByteArray(tmp.replaceAll(" ", "").toLowerCase());
+//+            } else {
+//+               bytes = (cmd + "\r").getBytes();
+//+            }
+//+            out.write(bytes);
+//         }
+//
+//         String result = readReply(socket);
+//         logger.info("received message: " + result);
+//         return result;
+//      } catch (Exception e) {
+//         logger.error("Socket event could not execute", e);
+//      } finally {
+//         if (socket != null) {
+//            try {
+//               socket.close();
+//            } catch (IOException e) {
+//               logger.error("Socket could not be closed", e);
+//            }
+//         }
+//      }
+//      return "";
+//   }
+//
+//   private String readReply(java.net.Socket socket) throws IOException {
+//      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//      char[] buffer = new char[200];
+//      int readChars = bufferedReader.read(buffer, 0, 200); // blocks until message received
+//      if (readChars > 0) {
+//         String reply = new String(buffer, 0, readChars);
+//         return reply;
+//      } else {
+//         return "";
+//      }
+//   }
+//
+//  @Override public String read(EnumSensorType sensorType, Map<String, String> stateMap)
+//  {
+//    String regexResult = null;
+//    String strState = null;
+//
+//    // Write the command to socket...
+//    //
+//    // TODO : rather poorly named method
+//
+//    String rawResult = requestSocket();
+//
+//    // Strip response from control characters (non-ASCII)...
+//    //
+//    // Patch provided by Phillip Lavender
+//
+//    Pattern p = Pattern.compile("\\p{Cntrl}");
+//    Matcher m = p.matcher(rawResult);
+//    regexResult = m.replaceAll("");
+//
+//    if ("".equals(regexResult))
+//    {
+//       return UNKNOWN_STATUS;
+//    }
+//
+//    switch (sensorType)
+//    {
+//      case RANGE:
+//         break;
+//
+//      case LEVEL:
+//         String min = stateMap.get(Sensor.RANGE_MIN_STATE);
+//         String max = stateMap.get(Sensor.RANGE_MAX_STATE);
+//
+//         try
+//         {
+//            int val = Integer.valueOf(regexResult);
+//
+//            if (min != null && max != null)
+//            {
+//               int minVal = Integer.valueOf(min);
+//               int maxVal = Integer.valueOf(max);
+//
+//               return String.valueOf(100 * (val - minVal)/ (maxVal - minVal));
+//            }
+//         }
+//
+//         catch (ArithmeticException e)
+//         {
+//            logger.warn("Level sensor values cannot be parsed: " + e.getMessage(), e);
+//         }
+//
+//         break;
+//
+//      default://NOTE: if sensor type is RANGE, this map only contains min/max states.
+//
+//        // If custom sensor type has been configured, map the 'raw' return value to configured
+//        // 'wanted' return value
+//        //
+//        // TODO :
+//        //   no reason to put this implementation burden on protocol implementations, the
+//        //   calling code could do it instead
+//
+//        for (String state : stateMap.keySet())
+//        {
+//          strState = stateMap.get(state);
+//
+//          if (regexResult.equals(strState))
+//          {
+//            return state;
+//          }
+//        }
+//      }
+//
+//
+//      return regexResult;
+//  }
 
 }
