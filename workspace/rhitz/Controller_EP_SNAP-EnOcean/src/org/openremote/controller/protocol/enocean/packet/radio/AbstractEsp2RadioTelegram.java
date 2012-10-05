a758191c257d8ca66f949d5cf3bcefa2e74e79ab
rhitz <rhitz@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-10-05 09:53:24 +0000

    add ESP2 radio telegram types

---
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/packet/radio/AbstractEsp2RadioTelegram.java
@@ -0,0 +1,364 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.enocean.packet.radio;
+
+import org.openremote.controller.protocol.enocean.DeviceID;
+import org.openremote.controller.protocol.enocean.packet.AbstractEsp2RequestPacket;
+import org.openremote.controller.protocol.enocean.packet.Esp2Packet;
+import org.openremote.controller.protocol.enocean.packet.Esp2PacketHeader;
+
+/**
+ * A common superclass for ESP2 radio telegrams which represents a generic radio telegram. <p>
+ *
+ * The main purpose of the class is to manage the data group fields common to all radio
+ * telegram types. <p>
+ *
+ * According to the TCM120 and TCM300 user manuals the generic radio telegram
+ * structure is as follows:
+ *
+ * <pre>
+ *         |--------  Header  --------|------------------------------------------ Data ...
+ *         +--------+--------+--------+--------+--------+--------+--------+--------+-- ...
+ *         |  Sync  |  Sync  | H_SEQ/ |   ORG  | Data   | Data   | Data   | Data   |
+ *         |  Byte  |  Byte  | LENGTH |        | Byte3  | Byte2  | Byte1  | Byte0  |
+ *         +--------+--------+--------+--------+--------+--------+--------+--------+-- ...
+ *           1 byte   1 byte   1 byte   1 byte   1 byte   1 byte   1 byte   1 byte
+ *
+ *   ... Group -----------------------------------------|
+ *   ... --+--------+--------+--------+--------+--------+--------+
+ *         |SenderID|SenderID|SenderID|SenderID| Status |Checksum|
+ *         | Byte3  | Byte2  | Byte1  | Byte0  |        |        |
+ *   ... --+--------+--------+--------+--------+--------+--------+
+ *           1 byte   1 byte   1 byte   1 byte   1 byte   1 byte
+ * </pre>
+ *
+ * The ORG field is used to store the radio telegram type. The EnOcean
+ * Equipment Profiles (EEP) specification uses the alternative name RORG for this field which
+ * is an abbreviation for radio organization number. The value of this field is defined in
+ * {@link RORG}. <p>
+ *
+ * The sender ID field contains the unique transmitter {@link DeviceID device ID} value for
+ * addressing. <p>
+ *
+ * The status field is mainly used to store a repeater count value (least significant nibble).
+ *
+ * <pre>
+ *         |-------- Status field --------|
+ *         +--------------+---+---+---+---+
+ *         |              |RC3|RC2|RC1|RC0|
+ *         |              |   |   |   |   |
+ *         +--------------+---+---+---+---+
+ *                      1 byte
+ * </pre>
+ *
+ *
+ * @see Esp2PacketHeader
+ * @see DeviceID
+ *
+ *
+ * @author Rainer Hitz
+ */
+public class AbstractEsp2RadioTelegram extends AbstractEsp2RequestPacket implements EspRadioTelegram
+{
+
+  // Constants ------------------------------------------------------------------------------------
+
+  /**
+   * Length of ORG field: {@value}
+   */
+  public static final int ESP2_RADIO_ORG_LENGTH = 1;
+
+  /**
+   * Byte order index of ORG field relative to data group: {@value}
+   */
+  public static final int ESP2_RADIO_ORG_INDEX = 0x00;
+
+  /**
+   * Length of status field: {@value}
+   */
+  public static final int ESP2_RADIO_STATUS_LENGTH = 1;
+
+  /**
+   * Fixed size of the ESP2 radio telegram payload: {@value}
+   */
+  public static final int ESP2_RADIO_PAYLOAD_LENGTH = 4;
+
+  /**
+   * Fixed size of data group: {@value}
+   */
+  public static final int ESP2_RADIO_DATA_LENGTH = ESP2_RADIO_ORG_LENGTH +
+                                                   ESP2_RADIO_PAYLOAD_LENGTH +
+                                                   DeviceID.ENOCEAN_ESP_ID_LENGTH +
+                                                   ESP2_RADIO_STATUS_LENGTH;
+
+  /**
+   * Fixed size of a ESP2 radio telegram: {@value}
+   */
+  public static final int ESP2_RADIO_PACKET_LENGTH = Esp2PacketHeader.ESP2_HEADER_SIZE +
+                                                     ESP2_RADIO_DATA_LENGTH +
+                                                     Esp2Packet.ESP2_PACKET_CHECKSUM_LENGTH;
+
+
+  // Class Members --------------------------------------------------------------------------------
+
+  /**
+   * Retrieves ORG value from data group and returns it.
+   *
+   * @param  data data group
+   *
+   * @return ORG value
+   *
+   * @throws UnknownRorgException
+   *           if RORG type is unknown
+   */
+  public static RORG getORGFromDataGroup(byte[] data)
+      throws UnknownRorgException
+  {
+    if(data == null)
+    {
+      throw new IllegalArgumentException("null data group");
+    }
+
+    if(data.length != ESP2_RADIO_DATA_LENGTH)
+    {
+      throw new IllegalArgumentException("Invalid data group length.");
+    }
+
+    return RORG.resolve(data[ESP2_RADIO_ORG_INDEX] & 0xFF);
+  }
+
+  /**
+   * Creates a new data group and initializes it with given ORG value and sender ID
+   * and returns it.
+   *
+   * @param  org       telegram type
+   *
+   * @param  senderID  unique sender device ID
+   *
+   * @return returns new data group
+   */
+  protected static byte[] createDataGroup(RORG org, DeviceID senderID, byte[] payload, byte status)
+  {
+    if(senderID == null)
+    {
+      throw new IllegalArgumentException("null sender ID");
+    }
+
+    if(payload == null)
+    {
+      throw new IllegalArgumentException("null payload data");
+    }
+
+    if(payload.length != ESP2_RADIO_PAYLOAD_LENGTH)
+    {
+      throw new IllegalArgumentException("Invalid payload length.");
+    }
+
+    byte[] dataBytes = new byte[ESP2_RADIO_DATA_LENGTH];
+
+    // Radio telegram type...
+
+    dataBytes[ESP2_RADIO_ORG_INDEX] = org.getValue();
+
+    // Payload...
+
+    System.arraycopy(
+        payload, 0, dataBytes, ESP2_RADIO_ORG_LENGTH, ESP2_RADIO_PAYLOAD_LENGTH
+    );
+
+    // Sender ID...
+
+    System.arraycopy(
+        senderID.asByteArray(), 0 , dataBytes,
+        ESP2_RADIO_ORG_LENGTH + ESP2_RADIO_PAYLOAD_LENGTH, DeviceID.ENOCEAN_ESP_ID_LENGTH
+    );
+
+    // Status...
+
+    dataBytes[dataBytes.length - 1] = status;
+
+
+    return dataBytes;
+  }
+
+  /**
+   * Retrieves sender ID from data group and returns it.
+   *
+   * @param  data           data group
+   *
+   * @return sender ID
+   */
+  private static DeviceID getSenderIDFromDataGroup(byte[] data)
+  {
+    byte[] senderIDBytes = new byte[DeviceID.ENOCEAN_ESP_ID_LENGTH];
+
+    System.arraycopy(
+        data, ESP2_RADIO_ORG_LENGTH + ESP2_RADIO_PAYLOAD_LENGTH, senderIDBytes,
+        0 , DeviceID.ENOCEAN_ESP_ID_LENGTH
+    );
+
+    return DeviceID.fromByteArray(senderIDBytes);
+  }
+
+
+  // Private Instance Fields ----------------------------------------------------------------------
+
+  /**
+   * Radio telegram type.
+   */
+  private RORG org;
+
+  /**
+   * Sender ID.
+   */
+  private DeviceID senderID;
+
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  /**
+   * Constructs a new radio telegram instance.
+   *
+   * @param org   radio telegram type
+   *
+   * @param data  data group
+   */
+  public AbstractEsp2RadioTelegram(RORG org, byte[] data)
+  {
+    super(Esp2PacketHeader.PacketType.TRT, data);
+
+    if(org == null)
+    {
+      throw new IllegalArgumentException("null ORG type");
+    }
+
+    if(data == null)
+    {
+      throw new IllegalArgumentException("null data");
+    }
+
+    if(data.length != ESP2_RADIO_DATA_LENGTH)
+    {
+      throw new IllegalArgumentException("Invalid data length.");
+    }
+
+    if(data[ESP2_RADIO_ORG_INDEX] != org.getValue())
+    {
+      throw new IllegalArgumentException("Inconsistent ORG value.");
+    }
+
+    this.org = org;
+
+    this.senderID = AbstractEsp2RadioTelegram.getSenderIDFromDataGroup(data);
+  }
+
+
+  // Object Overrides -----------------------------------------------------------------------------
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public String toString()
+  {
+    StringBuilder builder = new StringBuilder();
+
+    String rorgString = "ORG=" + org + ", ";
+    String idString = "sender ID=" + senderID + ", ";
+    String payloadString = "payload=" + getPayloadAsString() + ", ";
+    String statusString = "status=" + String.format("0x%02X", getStatusByte());
+
+    builder
+        .append("[RADIO: ")
+        .append(rorgString)
+        .append(idString)
+        .append(payloadString)
+        .append(statusString)
+        .append("]");
+
+    return builder.toString();
+  }
+
+
+  // Implements EspRadioTelegram ------------------------------------------------------------------
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public RORG getRORG()
+  {
+    return org;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public DeviceID getSenderID()
+  {
+    return senderID;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public byte[] getPayload()
+  {
+    byte[] payload = new byte[ESP2_RADIO_PAYLOAD_LENGTH];
+
+    System.arraycopy(data, ESP2_RADIO_ORG_LENGTH, payload, 0, ESP2_RADIO_PAYLOAD_LENGTH);
+
+    return payload;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override public byte getStatusByte()
+  {
+    int statusIndex = ESP2_RADIO_ORG_LENGTH + ESP2_RADIO_PAYLOAD_LENGTH +
+                      DeviceID.ENOCEAN_ESP_ID_LENGTH;
+
+    return data[statusIndex];
+  }
+
+
+  // Private Instance Methods ---------------------------------------------------------------------
+
+  /**
+   * Returns payload as a string with hexadecimal encoding.
+   *
+   * @return payload string
+   */
+  private String getPayloadAsString()
+  {
+    StringBuilder builder = new StringBuilder();
+
+    byte[] payload = getPayload();
+
+    for(int i = 0; i < payload.length; i++)
+    {
+      builder.append(
+          String.format("0x%02X%s", payload[i], i == (payload.length - 1) ? "" : " ")
+      );
+    }
+
+    return builder.toString();
+  }
+}
---
+++ workspace/rhitz/Controller_EP_SNAP-EnOcean/test/org/openremote/controller/protocol/enocean/packet/radio/Esp21BSTelegramTest.java
@@ -0,0 +1,374 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.enocean.packet.radio;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openremote.controller.protocol.enocean.ConfigurationException;
+import org.openremote.controller.protocol.enocean.ConnectionException;
+import org.openremote.controller.protocol.enocean.DeviceID;
+import org.openremote.controller.protocol.enocean.EspException;
+import org.openremote.controller.protocol.enocean.packet.Esp2Packet;
+import org.openremote.controller.protocol.enocean.packet.Esp2PacketHeader;
+import org.openremote.controller.protocol.enocean.packet.Esp2ResponsePacket;
+import org.openremote.controller.protocol.enocean.packet.EspProcessor;
+
+
+/**
+ * Unit tests for {@link Esp21BSTelegram} class.
+ *
+ * @author Rainer Hitz
+ */
+public class Esp21BSTelegramTest
+{
+
+  // Private Instance Fields ----------------------------------------------------------------------
+
+  private DeviceID senderID;
+  private byte payload;
+  private byte status;
+
+
+  // Test Lifecycle -------------------------------------------------------------------------------
+
+  @Before public void setUp() throws Exception
+  {
+    senderID = DeviceID.fromString("0xFF800001");
+    payload = 0x22;
+    status = 0x11;
+  }
+
+  // Tests ----------------------------------------------------------------------------------------
+
+
+  @Test public void testBasicConstruction() throws Exception
+  {
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+    telegram = new Esp21BSTelegram(telegram.getData());
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+  }
+
+  @Test public void testSenderID() throws Exception
+  {
+    DeviceID senderID = DeviceID.fromString("0x44332211");
+
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+    telegram = new Esp21BSTelegram(telegram.getData());
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+
+    senderID = DeviceID.fromString("0xFFFFFFFF");
+
+    telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+    telegram = new Esp21BSTelegram(telegram.getData());
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+
+    senderID = DeviceID.fromString("0x00000000");
+
+    telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+    telegram = new Esp21BSTelegram(telegram.getData());
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+  }
+
+  @Test public void testPayload() throws Exception
+  {
+    payload = 0x21;
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+    payload = (byte)0xFF;
+    telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+    payload = (byte)0x00;
+    telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+  }
+
+  @Test public void testStatus() throws Exception
+  {
+    status = 0x12;
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+    status = (byte)0xFF;
+    telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+
+
+    status = (byte)0x00;
+    telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    assertTelegramAttributes(telegram, senderID, payload, status);
+  }
+
+  @Test (expected = IllegalArgumentException.class)
+  public void testNullArg1() throws Exception
+  {
+    Esp21BSTelegram telegram = new Esp21BSTelegram(null, payload, status);
+  }
+
+  @Test (expected = IllegalArgumentException.class)
+  public void testNullArg2() throws Exception
+  {
+    byte[] data = null;
+    Esp21BSTelegram telegram = new Esp21BSTelegram(data);
+  }
+
+  @Test (expected = IllegalArgumentException.class)
+  public void testInvalidRORG() throws Exception
+  {
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    byte[] dataBytes = telegram.getData();
+
+    dataBytes[0] = AbstractEsp2RadioTelegram.RORG.RPS_ESP2.getValue();
+
+    telegram = new Esp21BSTelegram(dataBytes);
+  }
+
+  @Test (expected = IllegalArgumentException.class)
+  public void testInvalidDataGroupLength() throws Exception
+  {
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    byte[] dataBytes = telegram.getData();
+    byte[] dataBytesTooLong = new byte[dataBytes.length + 1];
+
+    System.arraycopy(
+        dataBytes, 0, dataBytesTooLong, 0, dataBytes.length
+    );
+
+    telegram = new Esp21BSTelegram(dataBytesTooLong);
+  }
+
+  @Test public void testSend() throws Exception
+  {
+    Esp2ResponsePacket.ReturnCode returnCode = Esp2ResponsePacket.ReturnCode.OK;
+    Esp2ResponsePacket response = createResponse(returnCode);
+
+    TestProcessor processor = new TestProcessor(response);
+
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    telegram.send(processor);
+
+    Assert.assertEquals(returnCode, telegram.getReturnCode());
+  }
+
+  @Test public void testRepeatedSend() throws Exception
+  {
+    Esp2ResponsePacket.ReturnCode returnCode = Esp2ResponsePacket.ReturnCode.OK;
+    Esp2ResponsePacket response = createResponse(returnCode);
+
+    TestProcessor processor = new TestProcessor(response);
+
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    telegram.send(processor);
+
+
+    processor = new TestProcessor(null);
+    telegram.send(processor);
+
+    Assert.assertEquals(Esp2ResponsePacket.ReturnCode.RET_CODE_NOT_SET, telegram.getReturnCode());
+
+
+    processor = new TestProcessor(response);
+    telegram.send(processor);
+
+    Assert.assertEquals(returnCode, telegram.getReturnCode());
+  }
+
+  @Test (expected = IllegalArgumentException.class)
+  public void testSendNullArg() throws Exception
+  {
+    Esp21BSTelegram telegram = new Esp21BSTelegram(senderID, payload, status);
+
+    telegram.send(null);
+  }
+
+
+  // Helpers --------------------------------------------------------------------------------------
+
+  private void assertTelegramAttributes(Esp21BSTelegram telegram, DeviceID senderID,
+                                        byte payload, byte status)
+  {
+    Assert.assertEquals(Esp2PacketHeader.PacketType.TRT, telegram.getPacketType());
+    Assert.assertEquals(AbstractEsp2RadioTelegram.RORG.BS1_ESP2, telegram.getRORG());
+    Assert.assertEquals(senderID, telegram.getSenderID());
+
+    Assert.assertArrayEquals(createDataGroup(senderID, payload, status), telegram.getData());
+    Assert.assertArrayEquals(
+        createTelegramAsByteArray(senderID, payload, status), telegram.asByteArray()
+    );
+  }
+
+  private byte[] createTelegramAsByteArray(DeviceID deviceID, byte payload, byte status)
+  {
+    byte[] packetBytes = new byte[14];
+
+    // Header ...
+
+    Esp2PacketHeader header = new Esp2PacketHeader(
+        Esp2PacketHeader.PacketType.TRT, 11
+    );
+
+    int copyIndex = 0;
+
+    System.arraycopy(
+        header.asByteArray(), 0, packetBytes, copyIndex,
+        Esp2PacketHeader.ESP2_HEADER_SIZE
+    );
+
+    // Data group ...
+
+    copyIndex += Esp2PacketHeader.ESP2_HEADER_SIZE;
+
+    System.arraycopy(
+        createDataGroup(deviceID, payload, status), 0, packetBytes,
+        copyIndex, 10
+    );
+
+
+    // Checksum ...
+
+    int checksum = 0;
+
+    for(int index = 2; index < 13; index++)
+    {
+      checksum += packetBytes[index];
+    }
+
+    packetBytes[13] = (byte)checksum;
+
+    return packetBytes;
+  }
+
+  private byte[] createDataGroup(DeviceID deviceID, byte payload, byte status)
+  {
+    byte[] dataGroupBytes = new byte[10];
+
+    // ORG...
+
+    dataGroupBytes[0] = AbstractEsp3RadioTelegram.RORG.BS1_ESP2.getValue();
+
+    // Payload...
+
+    dataGroupBytes[1] = payload;
+
+    // Sender ID...
+
+    System.arraycopy(
+        deviceID.asByteArray(), 0, dataGroupBytes,
+        5, DeviceID.ENOCEAN_ESP_ID_LENGTH
+    );
+
+    // Status...
+
+    dataGroupBytes[dataGroupBytes.length - 1] = status;
+
+    return dataGroupBytes;
+  }
+
+  private Esp2ResponsePacket createResponse(Esp2ResponsePacket.ReturnCode returnCode)
+  {
+    byte[] responseDataBytes = new byte[10];
+    responseDataBytes[0] = returnCode.getValue();
+
+    Esp2ResponsePacket response = null;
+
+    try
+    {
+      response = new Esp2ResponsePacket(responseDataBytes);
+    }
+    catch (EspException e)
+    {
+
+    }
+
+    return response;
+  }
+
+  // Inner Classes --------------------------------------------------------------------------------
+
+  private static class TestProcessor implements EspProcessor<Esp2Packet>
+  {
+    private Esp2Packet responsePacket;
+
+    public TestProcessor(Esp2Packet responsePacket)
+    {
+      this.responsePacket = responsePacket;
+    }
+
+    @Override public void start() throws ConfigurationException, ConnectionException
+    {
+
+    }
+
+    @Override public void stop() throws ConnectionException
+    {
+
+    }
+
+    @Override public Esp2Packet sendRequest(Esp2Packet packet) throws ConnectionException, InterruptedException
+    {
+      return responsePacket;
+    }
+
+    @Override public void sendResponse(Esp2Packet packet) throws ConnectionException
+    {
+
+    }
+  }
+}
