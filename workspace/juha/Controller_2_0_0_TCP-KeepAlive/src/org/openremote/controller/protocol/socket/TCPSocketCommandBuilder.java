b2a53e7f5de5ba4e796c35af6d4d7fecc41fc20e
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-10-05 23:11:53 +0000

    Move TCPCommand implementation under the single inheritance tree (a step before pulling common implementation up in the tree)

--- workspace/juha/Controller_2_0_0_TCP-KeepAlive/src/org/openremote/controller/protocol/socket/TCPSocketCommandBuilder.java
+++ workspace/juha/Controller_2_0_0_TCP-KeepAlive/src/org/openremote/controller/protocol/socket/TCPSocketCommandBuilder.java
@@ -22,12 +22,23 @@ package org.openremote.controller.protocol.socket;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.SocketException;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.IOException;
 
 import org.openremote.controller.Constants;
+import org.openremote.controller.model.sensor.Sensor;
 import org.openremote.controller.command.Command;
+import org.openremote.controller.command.ExecutableCommand;
 import org.openremote.controller.protocol.DeviceProtocol;
+import org.openremote.controller.protocol.ReadCommand;
+import org.openremote.controller.protocol.EventListener;
 import org.openremote.controller.protocol.bus.Message;
 import org.openremote.controller.protocol.ip.IPConnection;
 import org.openremote.controller.protocol.ip.TCPIPConnection;
@@ -35,7 +46,9 @@ import org.openremote.controller.utils.Logger;
 
 
 /**
+ * TODO
  *
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  * @author Marcus 2009-4-26
  */
 public class TCPSocketCommandBuilder extends TCPIPConnection
@@ -81,11 +94,218 @@ public class TCPSocketCommandBuilder extends TCPIPConnection
 
     InetAddress ipAddress = properties.getMandatoryInetAddress("ipAddress");
 
+
+    Integer listenerPort = properties.getOptionalInteger("listenerPort");
+
+    Boolean pollingReader = properties.getMandatoryBoolean("pollingReader");
+
     InetSocketAddress socketAddress = new InetSocketAddress(ipAddress, socketPort);
 
     TCPIPConnection connection = getConnection(socketAddress);
 
-    return new TCPSocketCommand(commandName, messages, connection, responsePolicy);
+    if (listenerPort != null)
+    {
+      return new TCPListener(listenerPort);
+    }
+
+    if (pollingReader)
+    {
+      return new TCPReader(connection, messages);
+    }
+
+    else
+    {
+      return new TCPWriter(connection, messages, responsePolicy);
+    }
   }
 
+
+  // Nested Classes -------------------------------------------------------------------------------
+
+  private static class TCPWriter implements ExecutableCommand
+  {
+
+    private TCPIPConnection connection;
+    private Set<Message> messages;
+    private boolean waitForResponse = true;
+
+    private TCPWriter(TCPIPConnection connection, Set<Message> messages,
+                      IPConnection.Option responsePolicy)
+    {
+
+      this.connection = connection;
+      this.messages = messages;
+
+      waitForResponse = (responsePolicy != IPConnection.Option.RESPONSE_READ_NOTHING);
+    }
+
+    @Override public void send()
+    {
+      if (waitForResponse)
+      {
+        TCPReader.sendAndReceive(connection, messages);
+      }
+
+      else
+      {
+        try
+        {
+          for (Message m : messages)
+          {
+            connection.send(m);
+          }
+        }
+
+        catch (Exception e)
+        {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
+  private static class TCPReader extends ReadCommand
+  {
+
+    private static String sendAndReceive(TCPIPConnection connection, Set<Message> messages)
+    {
+      StringBuffer sbuf = new StringBuffer();
+
+      try
+      {
+        for (Message m : messages)
+        {
+          connection.send(m);
+
+          Message response = connection.receive();
+
+          sbuf.append(new String(response.getContent()));
+        }
+      }
+
+      catch (Exception e)
+      {
+        e.printStackTrace();
+      }
+
+      return sbuf.toString();
+    }
+
+    private TCPIPConnection connection;
+    private Set<Message> messages;
+
+    private TCPReader(TCPIPConnection connection, Set<Message> messages)
+    {
+      this.connection = connection;
+      this.messages = messages;
+    }
+
+    @Override public String read(Sensor s)
+    {
+      String rawResult = sendAndReceive(connection, messages);
+
+      // Strip response from control characters (non-ASCII)...
+
+      Pattern p = Pattern.compile("\\p{Cntrl}");
+      Matcher m = p.matcher(rawResult);
+      String regexResult = m.replaceAll("");
+
+      if ("".equals(regexResult))
+      {
+         return Sensor.UNKNOWN_STATUS;
+      }
+
+      return regexResult;
+    }
+  }
+
+  private static class TCPListener implements EventListener
+  {
+    private final static int SERVER_BACKLOG_QUEUE_SIZE = 50;
+
+    private ServerSocket listeningServer;
+    private InetSocketAddress bindAddress;
+
+    private TCPListener(int port, InetAddress address)
+    {
+      if (port < 0 || port > 65535)
+      {
+        throw new Error("Illegal port number : port");
+      }
+
+      bindAddress = new InetSocketAddress(address, port);
+    }
+
+    private TCPListener(int port)
+    {
+      this(port, null);
+    }
+
+    @Override public void setSensor(Sensor s)
+    {
+
+    }
+
+    @Override public void stop(Sensor s)
+    {
+
+    }
+
+    private class WorkerThread implements Runnable
+    {
+      private volatile boolean running = true;
+
+      @Override public void run()
+      {
+        try
+        {
+          listeningServer = new ServerSocket();
+          listeningServer.bind(bindAddress);
+        }
+
+        catch (IOException e)
+        {
+
+        }
+
+        catch (SecurityException e)
+        {
+
+        }
+
+        catch (Throwable t)
+        {
+
+        }
+
+        while (running)
+        {
+          Socket socket;
+
+          try
+          {
+            socket = listeningServer.accept();
+          }
+
+          catch (SocketException e)
+          {
+
+          }
+
+          catch (SocketTimeoutException e)
+          {
+
+          }
+          
+          catch (IOException e)
+          {
+
+          }
+
+
+
+        }
+      }
+    }
+  }
 }
