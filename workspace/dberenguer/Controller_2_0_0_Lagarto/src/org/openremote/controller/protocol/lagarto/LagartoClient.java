dfe5e804c24db9c90340205b641a3b8474e49a0c
dberenguer <dberenguer@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-10-10 10:28:09 +0000

    Switched to OpenRemote's AGPL license

--- workspace/dberenguer/Controller_2_0_0_Lagarto/src/org/openremote/controller/protocol/lagarto/LagartoClient.java
+++ workspace/dberenguer/Controller_2_0_0_Lagarto/src/org/openremote/controller/protocol/lagarto/LagartoClient.java
@@ -1,25 +1,22 @@
-/**
- * Copyright (c) 2012 Daniel Berenguer <dberenguer@usapiens.com>
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
  *
- * This file is part of the lagarto project.
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
  *
- * lagarto  is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * lagarto is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with lagarto; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- * USA
- *
- *  @author Daniel Berenguer
- *  @date   2012-09-15
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.openremote.controller.protocol.lagarto;
@@ -32,11 +29,8 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import org.openremote.controller.Configuration;
 import org.openremote.controller.utils.Logger;
-import org.openremote.controller.ControllerConfiguration;
 import org.openremote.controller.model.sensor.Sensor;
-import org.openremote.controller.service.ServiceContext;
 
 /**
  * Lagarto client thread
