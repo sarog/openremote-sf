84efaabe3200b36c596e132d416adf8477b059cf
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-10-15 11:59:38 +0000

    Merged MODELER-410 fix (r6978:6981)

--- workspace/ebariaux/Designer_2_14_0_Markab_work/src/org/openremote/modeler/client/widget/buildingmodeler/KNXImportWindow.java
+++ workspace/ebariaux/Designer_2_14_0_Markab_work/src/org/openremote/modeler/client/widget/buildingmodeler/KNXImportWindow.java
@@ -21,8 +21,6 @@ package org.openremote.modeler.client.widget.buildingmodeler;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import net.customware.gwt.dispatch.client.DispatchAsync;
 
