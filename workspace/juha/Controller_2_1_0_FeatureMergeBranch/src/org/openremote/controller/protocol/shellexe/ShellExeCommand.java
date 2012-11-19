ff18082022e123376dff14f27b04277eeba5525f
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-11-19 18:28:44 +0000

    Merge R6111, R6113, R6118, R6119 from /workspace/mredeker/Controller_2_0_0_Fixes -- adds polling interval and regexp filtering to shell command execution

--- workspace/juha/Controller_2_1_0_FeatureMergeBranch/src/org/openremote/controller/protocol/shellexe/ShellExeCommand.java
+++ workspace/juha/Controller_2_1_0_FeatureMergeBranch/src/org/openremote/controller/protocol/shellexe/ShellExeCommand.java
@@ -16,12 +16,18 @@
  */
 package org.openremote.controller.protocol.shellexe;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.openremote.controller.command.ExecutableCommand;
+import org.openremote.controller.model.sensor.Sensor;
+import org.openremote.controller.protocol.EventListener;
 
-public class ShellExeCommand implements ExecutableCommand {
+public class ShellExeCommand implements ExecutableCommand, EventListener, Runnable {
 
    /** The logger. */
    private static Logger logger = Logger.getLogger(ShellExeCommandBuilder.SHELLEXE_PROTOCOL_LOG_CATEGORY);
@@ -31,33 +37,112 @@ public class ShellExeCommand implements ExecutableCommand {
 
    /** The params that should be attached to the executable */
    private String commandParams;
+   
+   /** The regex which is used to extract sensor data from received result */
+   private String regex;
 
+   /** The polling interval which is used for the sensor update thread */
+   private Integer pollingInterval;
+
+   /** The thread that is used to peridically update the sensor */
+   private Thread pollingThread;
+   
+   /** The sensor which is updated */
+   private Sensor sensor;
+   
+   /** Boolean to indicate if polling thread should run */
+   boolean doPoll = false;
+   
    /**
     * ShellExeCommand is a protocol to start shell scripts on the controller
+    * 
     * @param commandPath
     * @param commandParams
     */
-   public ShellExeCommand(String commandPath, String commandParams) {
+   public ShellExeCommand(String commandPath, String commandParams, String regex, Integer pollingInterval) {
       this.commandPath = commandPath;
       this.commandParams = commandParams;
+      this.regex = regex;
+      this.pollingInterval = pollingInterval;
    }
-   
+
    /**
     * {@inheritDoc}
     */
    @Override
    public void send() {
+      executeCommand();
+   }
+
+   @Override
+   public void setSensor(Sensor sensor)
+   {
+     logger.debug("*** setSensor called as part of EventListener init *** sensor is: " + sensor);
+     if (pollingInterval == null) {
+       throw new RuntimeException("Could not set sensor because no polling interval was given");
+     }
+     this.sensor = sensor;
+     this.doPoll = true;
+     pollingThread = new Thread(this);
+     pollingThread.setName("Polling thread for sensor: " + sensor.getName());
+     pollingThread.start();
+   }
+
+   @Override
+   public void stop(Sensor sensor)
+   {
+     this.doPoll = false;
+   }
+   
+   private String executeCommand() {
       logger.debug("Will start shell command: " + commandPath + " and use params: " + commandParams);
-      try
-      {
-        if (commandParams == null) {
-          Runtime.getRuntime().exec(new String[]{commandPath});
-        } else {
-          Runtime.getRuntime().exec(new String[]{commandPath, commandParams});
-        }
-      } catch (IOException e)
-      {
-        logger.error("Could not execute shell command: "+commandPath, e);
+      String result = "";
+      try {
+         Process proc = null;
+         if (commandParams == null) {
+            proc = Runtime.getRuntime().exec(new String[] { commandPath });
+         } else {
+            proc = Runtime.getRuntime().exec(new String[] { commandPath, commandParams });
+         }
+         BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+         StringBuffer resultBuffer = new StringBuffer();
+         for (String tmp = reader.readLine(); tmp != null; tmp = reader.readLine()) {
+            resultBuffer.append(tmp);
+         }
+         result = resultBuffer.toString();
+      } catch (IOException e) {
+         logger.error("Could not execute shell command: " + commandPath, e);
       }
+      logger.debug("Shell command: " + commandPath + " returned: " + result);
+      return result;
+   }
+   
+   @Override
+   public void run() {
+      logger.debug("Sensor thread started for sensor: " + sensor);
+      while (doPoll) {
+         String readValue = this.executeCommand();
+         if (regex != null) {
+           Pattern regexPattern = Pattern.compile(regex);
+           Matcher matcher = regexPattern.matcher(readValue);
+           if (matcher.find()) {
+             String result = matcher.group();
+             logger.info("result of regex evaluation: " + result);
+             sensor.update(result);
+           } else {
+             logger.info("regex evaluation did not find a match");
+             sensor.update("N/A");
+           }
+         } else {
+           sensor.update(readValue);
+         }
+         try {
+            Thread.sleep(pollingInterval); // We wait for the given pollingInterval before requesting URL again
+         } catch (InterruptedException e) {
+            doPoll = false;
+            pollingThread.interrupt();
+         }
+      }
+      logger.debug("*** Out of run method: " + sensor);
    }
 }
