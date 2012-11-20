acf447be31ffc77c8b23bee6d643a139a2ffd058
rhitz <rhitz@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-11-20 19:36:45 +0000

    delete obsolete scale classes

--- workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/datatype/AbstractScale.java
+++
@@ -1,9 +0,0 @@
-
-package org.openremote.controller.protocol.enocean.datatype;
-
-import java.math.BigDecimal;
-
-public abstract class AbstractScale implements Scale
-{
-  @Override public abstract BigDecimal scale(byte[] rawData, int scale);
-}
--- workspace/rhitz/Controller_EP_SNAP-EnOcean/src/org/openremote/controller/protocol/enocean/datatype/Scale.java
+++
@@ -1,8 +0,0 @@
-package org.openremote.controller.protocol.enocean.datatype;
-
-import java.math.BigDecimal;
-
-public interface Scale
-{
-  BigDecimal scale(byte[] rawData, int scale);
-}
