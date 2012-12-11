936547513a4d814ef11d48d17096fea02b3cb754
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-12-11 12:40:11 +0000

    MODELER-390: Added extra utility method used when processing the graph reloaded from an export file

--- workspace/ebariaux/Designer_2_14_0_MODELER-390/src/org/openremote/modeler/shared/dto/DeviceDetailsWithChildrenDTO.java
+++ workspace/ebariaux/Designer_2_14_0_MODELER-390/src/org/openremote/modeler/shared/dto/DeviceDetailsWithChildrenDTO.java
@@ -1,6 +1,8 @@
 package org.openremote.modeler.shared.dto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DeviceDetailsWithChildrenDTO extends DeviceDetailsDTO {
 
@@ -49,6 +51,66 @@ public class DeviceDetailsWithChildrenDTO extends DeviceDetailsDTO {
 
   public void setSliders(ArrayList<SliderDetailsDTO> sliders) {
     this.sliders = sliders;
+  }
+  
+  /**
+   * SensorDetailsDTOs, SwitchDetailsDTOs and SliderDetailsDTOs use DTOReference objects
+   * to reference each other and the DeviceCommandDetailsDTOs.
+   * 
+   * Those DTOReference can either use an id or point to the DTO instance itself.
+   * This method will walk the hierarchy, ensuring that all references using ids
+   * are replaced with a reference to the DTO instance.
+   * 
+   * This is useful when this object comes from DB, where ids are used for relationship.
+   */
+  public void replaceIdWithDTOInReferences() {
+    Map<Long, DeviceCommandDetailsDTO> commandsPerId = new HashMap<Long, DeviceCommandDetailsDTO>();
+    if (getDeviceCommands() != null) {
+      for (DeviceCommandDetailsDTO dc : getDeviceCommands()) {
+        commandsPerId.put(dc.getOid(), dc);
+      }
+    }
+    
+    Map<Long, SensorDetailsDTO> sensorsPerId = new HashMap<Long, SensorDetailsDTO>();
+    if (getSensors() != null) {
+      for (SensorDetailsDTO s : getSensors()) {
+        DTOReference ref = s.getCommand();
+        if (ref != null) {
+          ref.resolveIdToDTO(commandsPerId);
+        }
+        sensorsPerId.put(s.getOid(), s);
+      }
+    }
+    
+    if (getSwitches() != null) {
+      for (SwitchDetailsDTO s : getSwitches()) {
+        DTOReference ref = s.getSensor();
+        if (ref != null) {
+          ref.resolveIdToDTO(sensorsPerId);
+        }
+        ref = s.getOnCommand();
+        if (ref != null) {
+          ref.resolveIdToDTO(commandsPerId);
+        }
+        ref = s.getOffCommand();
+        if (ref != null) {
+          ref.resolveIdToDTO(commandsPerId);
+        }
+      }
+    }
+    
+    if (getSliders() != null) {
+      for (SliderDetailsDTO s : getSliders()) {
+        DTOReference ref = s.getCommand();
+        if (ref != null) {
+          ref.resolveIdToDTO(commandsPerId);
+        }
+        ref = s.getSensor();
+        if (ref != null) {
+          ref.resolveIdToDTO(sensorsPerId);
+        }
+      }
+    }
   }
 
 }
