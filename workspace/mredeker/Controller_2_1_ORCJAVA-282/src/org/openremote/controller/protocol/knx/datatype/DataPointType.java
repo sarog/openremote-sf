3fd6127d7f074d3e945a2d5971391abecbebf137
mredeker <mredeker@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-12-28 21:18:47 +0000

    Add KNX DPT 232.600 for RGB values

--- workspace/mredeker/Controller_2_1_ORCJAVA-282/src/org/openremote/controller/protocol/knx/datatype/DataPointType.java
+++ workspace/mredeker/Controller_2_1_ORCJAVA-282/src/org/openremote/controller/protocol/knx/datatype/DataPointType.java
@@ -131,7 +131,9 @@ public abstract class DataPointType
   public final static Unsigned8BitValue SCENE_NUMBER = new Unsigned8BitValue(17, 1);
   public final static Unsigned8BitValue SCENE_CONTROL = new Unsigned8BitValue(18, 1);
 
-
+  // RGB value
+  public final static ThreeByteValue RGB_VALUE = new ThreeByteValue(232, 600);
+  
   //public final static Float2ByteValue VALUE_TEMP = new Float2ByteValue(9, 1);
 
   public static DataPointType lookup(String dptID)
@@ -313,4 +315,12 @@ public abstract class DataPointType
     }
   }
 
+  public static class ThreeByteValue extends DataPointType
+  {
+     
+    private ThreeByteValue(int main, int sub)
+    {
+      super(main, sub, false);
+    }
+  }
 }
