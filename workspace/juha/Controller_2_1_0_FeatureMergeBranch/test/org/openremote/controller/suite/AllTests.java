d2bdcc6fc925ddcc76736c88f7d3987036d35ffb
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2012-12-29 15:14:18 +0000

    Merge ORCJAVA-286

--- workspace/juha/Controller_2_1_0_FeatureMergeBranch/test/org/openremote/controller/suite/AllTests.java
+++ workspace/juha/Controller_2_1_0_FeatureMergeBranch/test/org/openremote/controller/suite/AllTests.java
@@ -36,6 +36,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.openremote.controller.deployer.Version20ModelBuilderTest;
 import org.openremote.controller.net.MulticastAutoDiscoveryTest;
 import org.openremote.controller.utils.MacrosIrDelayUtilTest;
 import org.openremote.controller.utils.PathUtil;
@@ -74,7 +75,8 @@ import org.jdom.input.DOMBuilder;
    StatusCacheTests.class,
    ProtocolTests.class,
    BusTests.class,
-   DSCIT100Tests.class
+   DSCIT100Tests.class,
+   Version20ModelBuilderTest.class
 })
 
 
