70d6fcecd823039b6ee2611ec41a0b1364f27503
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-02-01 14:06:02 +0000

    As submitted to AppStore for release

---
+++ tags/project/iOS_Console_2_1_1/Classes/SensorParser.m
@@ -0,0 +1,62 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#import "SensorParser.h"
+#import "LocalSensor.h"
+#import "XMLEntity.h"
+
+@interface SensorParser ()
+
+@property (nonatomic, retain, readwrite) LocalSensor *sensor;
+
+@end
+
+/**
+ * Stores model data about sensor parsed from "include" element in panel.xml.
+ * XML fragment example:
+ * <link type="sensor" ref="575">
+ * ......
+ * </link>
+ */
+@implementation SensorParser
+
+- (void)dealloc
+{
+    self.sensor = nil;
+    [super dealloc];
+}
+
+- (id)initWithRegister:(DefinitionElementParserRegister *)aRegister attributes:(NSDictionary *)attributeDict;
+{
+    self = [super initWithRegister:aRegister attributes:attributeDict];
+    if (self) {
+        LocalSensor *tmp = [[LocalSensor alloc] initWithId:[[attributeDict objectForKey:ID] intValue]
+                                       className:[attributeDict objectForKey:CLASS]
+                                      methodName:[attributeDict objectForKey:METHOD]
+                                     refreshRate:([attributeDict objectForKey:REFRESH_RATE]?[NSNumber numberWithInt:[[attributeDict objectForKey:REFRESH_RATE] intValue]]:nil)];
+        self.sensor = tmp;
+        [tmp release];
+    }
+    return self;
+}
+
+@synthesize sensor;
+
+@end
