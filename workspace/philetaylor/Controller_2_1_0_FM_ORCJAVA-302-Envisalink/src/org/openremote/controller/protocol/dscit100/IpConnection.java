41a0f9f4f7eb431e52210a5d102e2a76fb29ade2
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-02-11 13:49:27 +0000

    Code Review: 1. Use OR log facade 2. Remove specific references to IT-100 in Javadoc and talk in general about DSC since EnvisaLink gateway is also supported 3. Organize and format code according to current source code conventions 4. Switch from StringBuffer to StringBuilder to avoid unneeded synchronization (flagged by IDE code inspections) 5. Remove one potential null pointer exception (flagged by IDE code inspections)

--- workspace/philetaylor/Controller_2_1_0_FM_ORCJAVA-302-Envisalink/src/org/openremote/controller/protocol/dscit100/IpConnection.java
+++ workspace/philetaylor/Controller_2_1_0_FM_ORCJAVA-302-Envisalink/src/org/openremote/controller/protocol/dscit100/IpConnection.java
@@ -1,6 +1,6 @@
 /*
  * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
+ * Copyright 2008-2013, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
@@ -26,24 +26,28 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-import org.apache.log4j.Logger;
 import org.openremote.controller.protocol.dscit100.Packet.PacketCallback;
 import org.openremote.controller.protocol.dscit100.PanelState.State;
+import org.openremote.controller.utils.Logger;
 
 /**
- * Implements a DSCIT100Connection over IP
+ * Implements a DSC Connection over IP.
  *
+ * @author Greg Rapp
+ * @author Phil Taylor
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  */
 public class IpConnection implements DSCIT100Connection
 {
-  // Class Members
-  // --------------------------------------------------------------------------------
+  // Class Members --------------------------------------------------------------------------------
 
   /**
-   * DSCIT100 logger. Uses a common category for all DSCIT100 related logging.
+   * DSC logger. Uses a common category for all DSC related logging.
    */
-  private final Logger log = Logger
-      .getLogger(DSCIT100CommandBuilder.DSCIT100_LOG_CATEGORY);
+  private final Logger log = Logger.getLogger(DSCIT100CommandBuilder.DSC_LOG_CATEGORY);
+
+
+  // Instance Fields ------------------------------------------------------------------------------
 
   private Socket socket = null;
   private PrintWriter out = null;
@@ -51,10 +55,27 @@ public class IpConnection implements DSCIT100Connection
   protected PacketCallback packetCallback;
 
   /**
-   * @param socket An IP socket
+   * Connection credentials. Currently used by EnvisaLink gateway only. Set to null for IT-100.
    */
-  public IpConnection(Socket socket)
+  private String credentials;
+
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  /**
+   * Constructs a new DSC IP gateway connection with given connection credentials and Java socket.
+   * Note that credentials are needed for EnvisaLink gateway only, IT-100 should use a null
+   * credential.
+   *
+   * @param connectionCredentials
+   *          connection password for EnvisaLink gateway, null for IT-100 gateway
+   *
+   * @param socket
+   *          An IP socket
+   */
+  public IpConnection(String connectionCredentials, Socket socket)
   {
+    this.credentials = connectionCredentials;
     this.socket = socket;
 
     listener = new IpListener(socket);
@@ -63,99 +84,120 @@ public class IpConnection implements DSCIT100Connection
     {
       this.out = new PrintWriter(socket.getOutputStream(), true);
     }
+
     catch (IOException e)
     {
-      log.error("Error creating socket output stream for "
-          + socket.getInetAddress().getHostAddress(), e);
+      log.error(
+          "Error creating socket output stream for " + socket.getInetAddress().getHostAddress(), e
+      );
     }
   }
 
-  @Override
-  public void send(ExecuteCommand command)
+
+  // Implements DSCIT100 Connection ---------------------------------------------------------------
+
+  @Override public void send(ExecuteCommand command)
   {
     sendInternal(command.getPacket());
   }
 
-  @Override
-  public void send(Packet packet)
+  @Override public void send(Packet packet)
   {
     sendInternal(packet);
   }
+
+
+  @Override public boolean isConnected()
+  {
+    return socket.isConnected();
+  }
+
+  @Override public void close()
+  {
+    log.debug("Closing connection to " + socket.getInetAddress().getHostAddress());
+
+    try
+    {
+      socket.close();
+    }
+
+    catch (IOException e)
+    {
+      log.warn("Error closing connection to " + socket.getInetAddress().getHostAddress(), e);
+    }
+  }
+
+  @Override public String getAddress()
+  {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(socket.getInetAddress().getHostAddress());
+    sb.append(":");
+    sb.append(socket.getPort());
+
+    return sb.toString();
+  }
+
+  @Override public State getState(StateDefinition stateDefinition)
+  {
+    if (listener != null && listener.state != null)
+    {
+      return listener.state.getState(stateDefinition);
+    }
+
+    else
+    {
+      log.warn(
+          "Unable to get connection listener or listener state database is unavailable for " +
+          "connection to " + socket.getInetAddress().getHostAddress()
+      );
+
+      return null;
+    }
+  }
+
+
+
+  // Private Methods ------------------------------------------------------------------------------
 
   private void sendInternal(Packet packet)
   {
     if (isConnected())
     {
-      log.debug("Sending data to address "
-          + socket.getInetAddress().getHostAddress() + " : "
-          + packet.toPacket());
+      log.debug(
+          "Sending data to address " + socket.getInetAddress().getHostAddress() + " : " +
+          packet.toPacket()
+      );
+
       if (packet.getCallback() != null)
+      {
         this.packetCallback = packet.getCallback();
+      }
+
       out.println(packet.toPacket());
     }
+
     else
     {
-      log.warn("Could not send data to address "
-          + socket.getInetAddress().getHostAddress() + " : "
-          + packet.toPacket());
+      log.warn(
+          "Could not send data to address " + socket.getInetAddress().getHostAddress() + " : " +
+          packet.toPacket()
+      );
     }
   }
 
-  @Override
-  public boolean isConnected()
-  {
-    return socket.isConnected();
-  }
 
-  @Override
-  public void close()
-  {
-    log.debug("Closing connection to "
-        + socket.getInetAddress().getHostAddress());
-    try
-    {
-      socket.close();
-    }
-    catch (IOException e)
-    {
-      log.warn("Error closing connection to "
-          + socket.getInetAddress().getHostAddress(), e);
-    }
-  }
-
-  @Override
-  public String getAddress()
-  {
-    StringBuffer sb = new StringBuffer();
-    sb.append(socket.getInetAddress().getHostAddress());
-    sb.append(":");
-    sb.append(socket.getPort());
-    return sb.toString();
-  }
-
-  @Override
-  public State getState(StateDefinition stateDefinition)
-  {
-    if (listener != null && listener.state != null)
-      return listener.state.getState(stateDefinition);
-    else
-    {
-      log.warn("Unable to get connection listener or listener state database is unavailable for connection to "
-          + socket.getInetAddress().getHostAddress());
-      return null;
-    }
-  }
+  // Inner Classes --------------------------------------------------------------------------------
 
   /**
-   * A listener <code>Thread</code> for an <code>IpConnection</code>
-   *
+   * A listener Thread for an IpConnection
    */
   private class IpListener implements Runnable
   {
     private Thread thread;
     private Socket socket;
 
-    public PanelState state;
+    private PanelState state;
 
     public IpListener(Socket socket)
     {
@@ -166,50 +208,62 @@ public class IpConnection implements DSCIT100Connection
       thread.start();
     }
 
-    @Override
-    public void run()
+    @Override public void run()
     {
-      log.info("Starting connection listener thread for "
-          + socket.getInetAddress().getHostAddress());
+      log.info("Starting connection listener thread for " + socket.getInetAddress().getHostAddress());
+
       BufferedReader in = null;
+
       try
       {
-        in = new BufferedReader(
-            new InputStreamReader(socket.getInputStream()));
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
       }
+
       catch (IOException e)
       {
-        log.error("I/O error creating reader socket for "
-            + socket.getInetAddress().getHostAddress(), e);
+        log.error("I/O error creating reader socket for " + socket.getInetAddress().getHostAddress(), e);
+
+        return;
       }
 
-      log.info("Starting read loop for "
-          + socket.getInetAddress().getHostAddress());
+      log.info("Starting read loop for " + socket.getInetAddress().getHostAddress());
 
-      // Send IT100 state discovery packet to get current system state (not used for EnvisaLink)
-      if (DSCIT100CommandBuilder.dscPassword == null) {
+      // Send IT100 state discovery packet to get current system state (not used for EnvisaLink)...
+
+      if (credentials == null)
+      {
       		sendInternal(new Packet("001", ""));
-      		// Send IT100 labels request packet to get system labels
+
+      		// Send IT100 labels request packet to get system labels...
+
       		sendInternal(new Packet("002", ""));
-	  }
+	    }
+
       boolean isConnected = true;
 
       while (isConnected)
       {
         Packet packet = null;
+
         try
         {
           String rawData = in.readLine();
-          log.debug("Received data from "
-              + socket.getInetAddress().getHostAddress() + " : " + rawData);
+
+          log.debug(
+              "Received data from " + socket.getInetAddress().getHostAddress() + " : " + rawData
+          );
+
           packet = new Packet(rawData);
         }
+
         catch (IOException e)
         {
           log.warn("Error parsing packet", e);
+
           isConnected = false;
-          // Connection has failed, close the socket so it can be recreated
-          // later
+
+          // Connection has failed, close the socket so it can be recreated later...
+
           IpConnection.this.close();
         }
 
@@ -217,35 +271,55 @@ public class IpConnection implements DSCIT100Connection
         try
         {
           if (packet != null)
-            {
+          {
             /* 505 commands are sent by the EnvisaLink to request authentication
                As this isn't relevant to the panel state, process it here */
+
             if (packet.getCommand().equals("505"))
             { 
             	// Login interaction
-                String num = packet.getData().substring(0, 1);
-                if (num.equals("3")){
-                  log.info("EnvisaLink: Login required");
-                  sendInternal(new Packet("005", DSCIT100CommandBuilder.dscPassword));
-                } else if (num.equals("1")) {
-                  log.info("EnvisaLink: Login successful");
-                  sendInternal(new Packet("001", "")); // Ask for panel status.
-                } else if (num.equals("0")) {
-                  log.error("EnvisaLink: Invalid password");
-                  isConnected=false;
-                  IpConnection.this.close();
-                } else if (num.equals("2")) {
-                  log.error("EnvisaLink: connection timeout");
-                  isConnected=false;
-                  IpConnection.this.close();
-                }
-            } else {
-                  state.processPacket(packet);
+              String num = packet.getData().substring(0, 1);
+
+              if (num.equals("3"))
+              {
+                log.info("EnvisaLink: Login required");
+
+                sendInternal(new Packet("005", credentials));
+              }
+
+              else if (num.equals("1"))
+              {
+                log.info("EnvisaLink: Login successful");
+
+                sendInternal(new Packet("001", "")); // Ask for panel status.
+              }
+
+              else if (num.equals("0"))
+              {
+                log.error("EnvisaLink: Invalid password");
+
+                isConnected=false;
+                IpConnection.this.close();
+              }
+
+              else if (num.equals("2"))
+              {
+                log.error("EnvisaLink: connection timeout");
+
+                isConnected=false;
+                IpConnection.this.close();
+              }
+            }
+
+            else
+            {
+              state.processPacket(packet);
             }
 
             if (packetCallback != null)
             {
               log.debug("Executing callback method for packet " + packet);
+
               packetCallback.receive(IpConnection.this, packet);
             }
           }
@@ -253,12 +327,13 @@ public class IpConnection implements DSCIT100Connection
         catch (Exception e)
         {
           log.warn("Error in packet", e);
+
           isConnected = false;
-          // Connection has failed, close the socket so it can be recreated
-          // later
+
+          // Connection has failed, close the socket so it can be recreated later...
+
           IpConnection.this.close();
         }
-
       }
     }
   }
