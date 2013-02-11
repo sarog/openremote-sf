4c387ca1e1a44f08182af8d723122b64eb47a690
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-02-11 11:34:38 +0000

    code review: 1. switch to OR log facade 2. remove javadoc references to IT-100 and use DSC in general since also EnvisaLink is used/supported 3. reformat and reorganize code to match current source code conventions

--- workspace/philetaylor/Controller_2_1_0_FM_ORCJAVA-302-Envisalink/src/org/openremote/controller/protocol/dscit100/ExecuteCommand.java
+++ workspace/philetaylor/Controller_2_1_0_FM_ORCJAVA-302-Envisalink/src/org/openremote/controller/protocol/dscit100/ExecuteCommand.java
@@ -1,6 +1,6 @@
 /*
  * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
+ * Copyright 2008-2013, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
@@ -20,50 +20,66 @@
  */
 package org.openremote.controller.protocol.dscit100;
 
-import org.apache.log4j.Logger;
 import org.openremote.controller.command.ExecutableCommand;
 import org.openremote.controller.protocol.dscit100.Packet.PacketCallback;
+import org.openremote.controller.utils.Logger;
 
 /**
+ * Write command sent to DSC IP gateway (IT-100 or EnvisaLink)
+ *
  * @author Greg Rapp
- * 
+ * @author Phil Taylor
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  */
-public class ExecuteCommand extends DSCIT100Command implements
-    ExecutableCommand
+public class ExecuteCommand extends DSCIT100Command implements ExecutableCommand
 {
 
-  // Class Members
-  // --------------------------------------------------------------------------------
+  // TODO :
+  //   - candidate for rename since the implementation is also used for EnvisaLink gateways,
+  //     not only IT-100 anymore
+
+
+  // Class Members --------------------------------------------------------------------------------
 
   /**
-   * Factory method for creating DSCIT100 command instances
-   * {@link ExecuteCommand} and {@link ReadCommand} based on a human-readable
-   * configuration strings.
-   * <p>
+   * DSC logger. Uses a common category for all DSC related logging.
+   */
+  private static final Logger log = Logger.getLogger(DSCIT100CommandBuilder.DSC_LOG_CATEGORY);
+
+  /**
+   * Factory method for creating DSC command instances {@link ExecuteCommand} and
+   * {@link ReadCommand} based on a human-readable configuration strings. <p>
    * 
-   * Each DSCIT100 command instance is associated with a link to a connection
-   * manager and a destination address.
+   * Each DSC command instance is associated with a link to a connection manager and a destination
+   * address.
    * 
    * @param name
    *          Command lookup name. This is usually a human-readable string used
    *          in configuration and tools. Note that multiple lookup names can be
-   *          used to return Java equal() (but not same instance) commands.
+   *          used to return Java equal() (but not same instance) commands
+   *
    * @param address
-   *          DSCIT100 address.
+   *          DSC address
+   *
    * @param code
-   *          Security code.
+   *          Security code
+   *
    * @param target
-   *          Partition or zone that is the target of this command.
+   *          Partition or zone that is the target of this command
+   *
    * @param mgr
-   *          DSCIT100 connection manager used to transmit this command
+   *          DSC connection manager used to transmit this command
    * 
-   * @return new DSCIT100 command instance
+   * @return new DSC command instance, or null if lookup with the given name did not return
+   *         anything
    */
-  public static ExecuteCommand createCommand(String name, String address,
-      String code, String target, DSCIT100ConnectionManager mgr)
+  public static ExecuteCommand createCommand(String name, String address, String code,
+                                             String target, DSCIT100ConnectionManager mgr)
   {
     if (name == null || address == null || mgr == null)
+    {
       return null;
+    }
 
     name = name.trim().toUpperCase();
 
@@ -73,18 +89,15 @@ public class ExecuteCommand extends DSCIT100Command implements
     Packet packet = Lookup.get(name, code, target);
 
     if (packet == null)
+    {
       return null;
+    }
 
     return new ExecuteCommand(packet, address, target, mgr);
   }
 
-  // Private Instance Fields
-  // ----------------------------------------------------------------------
-  /**
-   * DSCIT100 logger. Uses a common category for all DSCIT100 related logging.
-   */
-  private static final Logger log = Logger
-      .getLogger(DSCIT100CommandBuilder.DSCIT100_LOG_CATEGORY);
+  // Private Instance Fields ----------------------------------------------------------------------
+
 
   /**
    * Command packet.
@@ -96,20 +109,21 @@ public class ExecuteCommand extends DSCIT100Command implements
    */
   // private String code;
 
-  // Constructors
-  // ---------------------------------------------------------------------------------
+
+  // Constructors ---------------------------------------------------------------------------------
 
   public ExecuteCommand(Packet packet, String address, String target,
-      DSCIT100ConnectionManager connectionManager)
+                        DSCIT100ConnectionManager connectionManager)
   {
     super(address, target, connectionManager);
+
     this.packet = packet;
     // this.code = code;
+
     log.info("Instantiating new ExecuteCommand instance - " + this.toString());
   }
 
-  // Object Overrides
-  // -----------------------------------------------------------------------------
+  // Object Overrides -----------------------------------------------------------------------------
 
   /**
    * Returns a string representation of this command. Expected output is:
@@ -124,10 +138,9 @@ public class ExecuteCommand extends DSCIT100Command implements
    * 
    * @return this command as string
    */
-  @Override
-  public String toString()
+  @Override public String toString()
   {
-    StringBuffer buffer = new StringBuffer();
+    StringBuilder buffer = new StringBuilder();
 
     String command = packet.getCommand();
     String data = packet.getData();
@@ -139,21 +152,19 @@ public class ExecuteCommand extends DSCIT100Command implements
     return buffer.toString();
   }
 
-  // Implements ExecutableCommand
-  // -----------------------------------------------------------------
+  // Implements ExecutableCommand -----------------------------------------------------------------
 
   /**
    * {@inheritDoc}
    */
-  public void send()
+  @Override public void send()
   {
     // delegate to super class...
 
     super.write(this);
   }
 
-  // Public Instance Methods
-  // -------------------------------------------------------------
+  // Public Instance Methods -------------------------------------------------------------
 
   /**
    * @return the command packet
@@ -169,8 +180,9 @@ public class ExecuteCommand extends DSCIT100Command implements
   /*
    * public String getCode() { return codeString; }
    */
-  // Nested Classes
-  // -------------------------------------------------------------------------------
+
+
+  // Nested Classes -------------------------------------------------------------------------------
 
   /**
    * Simple helper class to lookup user configured command names and match them
@@ -180,10 +192,11 @@ public class ExecuteCommand extends DSCIT100Command implements
   {
     /**
      * Lookup from user defined command strings in the designer (from which they
-     * end up into controller.xml) to type safe Packets for DSCIT100 packets.
+     * end up into controller.xml) to type safe Packets for DSC packets.
      * 
      * @param name
      *          lookup name
+     *
      * @param code
      *          security code
      * 
@@ -191,114 +204,136 @@ public class ExecuteCommand extends DSCIT100Command implements
      *         found by name
      */
 
-    private static Packet get(final String name, final String code,
-        final String target)
+    private static Packet get(final String name, final String code, final String target)
     {
       if (name.equals("POLL"))
       {
         return new Packet("000", "");
       }
+
       else if (name.equals("STATUS"))
       {
         return new Packet("001", "");
       }
+
       else if (name.equals("LOGIN"))
       {
         return new Packet("005", "user");
       }
+
       else if (name.equals("ZONE_TIMERS"))
       {
         return new Packet("008", "");
       }
+
       else if (name.equals("SET_DATETIME"))
       {
         return new Packet("010", code);
       }
+
       else if (name.equals("COMMAND_OUTPUT"))
       {
         return new Packet("020", target + code);
       }
+
       else if (name.equals("ARM_AWAY"))
       {
         return new Packet("030", target);
       }
+
       else if (name.equals("ARM_STAY"))
       {
         return new Packet("031", target, new PacketCallback()
         {
-          @Override
-          public void receive(DSCIT100Connection connection, Packet packet)
+          @Override public void receive(DSCIT100Connection connection, Packet packet)
           {
             if (packet.getCommand().equals("900") && code != null)
+            {
               connection.send(new Packet("200", target + code));
+            }
           }
         });
       }
+
       else if (name.equals("ARM_NO_ENTRY_DELAY"))
       {
         return new Packet("032", target, new PacketCallback()
         {
-          @Override
-          public void receive(DSCIT100Connection connection, Packet packet)
+          @Override public void receive(DSCIT100Connection connection, Packet packet)
           {
             if (packet.getCommand().equals("900") && code != null)
+            {
               connection.send(new Packet("200", target + code));
+            }
           }
-
         });
       }
+
       else if (name.equals("ARM"))
       {
         return new Packet("033", target + code);
       }
+
       else if (name.equals("DISARM"))
       {
         return new Packet("040", target + code);
       }
+
       else if (name.equals("TIMESTAMP"))
       {
         return new Packet("055", target);
       }
+
       else if (name.equals("TIME_BROADCAST"))
       {
         return new Packet("056", target);
       }
+
       else if (name.equals("TEMP_BROADCAST"))
       {
         return new Packet("057", target);
       }
+
       else if (name.equals("PANIC_FIRE"))
       {
         return new Packet("060", "1");
       }
+
       else if (name.equals("PANIC_AMBULANCE"))
       {
         return new Packet("060", "2");
       }
+
       else if (name.equals("PANIC"))
       {
         return new Packet("060", "3");
       }
+
       else if (name.equals("SINGLE_KEY"))
       {
         return new Packet("070", target);
       }
+
       else if (name.equals("KEYSTROKE"))
       {
         return new Packet("071", target + code);
       }
+
       else if (name.equals("USER_CODE_PROG"))
       {
         return new Packet("072", target);
       }
+
       else if (name.equals("USER_PROG"))
       {
         return new Packet("073", target);
       }
+
       else if (name.equals("KEEPALIVE"))
       {
         return new Packet("074", target);
       }
+
       else
       {
         return null;
