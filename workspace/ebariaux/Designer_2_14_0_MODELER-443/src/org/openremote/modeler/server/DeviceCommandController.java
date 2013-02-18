d9433e79173d5ce99a3dedd583b76e4567130f6b
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-02-18 16:25:23 +0000

    MODELER-443: Moved persistence/DTO logic from DeviceCommandController to DeviceCommandService

--- workspace/ebariaux/Designer_2_14_0_MODELER-443/src/org/openremote/modeler/server/DeviceCommandController.java
+++ workspace/ebariaux/Designer_2_14_0_MODELER-443/src/org/openremote/modeler/server/DeviceCommandController.java
@@ -30,7 +30,6 @@ import org.openremote.modeler.domain.DeviceCommand;
 import org.openremote.modeler.domain.Protocol;
 import org.openremote.modeler.domain.ProtocolAttr;
 import org.openremote.modeler.service.DeviceCommandService;
-import org.openremote.modeler.service.DeviceService;
 import org.openremote.modeler.shared.dto.DeviceCommandDTO;
 import org.openremote.modeler.shared.dto.DeviceCommandDetailsDTO;
 
@@ -45,7 +44,6 @@ public class DeviceCommandController extends BaseGWTSpringController implements
    
    /** The device command service. */
    private DeviceCommandService deviceCommandService;
-   private DeviceService deviceService;
 
     /**
      * Sets the device command service.
@@ -55,10 +53,6 @@ public class DeviceCommandController extends BaseGWTSpringController implements
     public void setDeviceCommandService(DeviceCommandService deviceCommandRPCService) {
       this.deviceCommandService = deviceCommandRPCService;
    }
-
-   public void setDeviceService(DeviceService deviceService) {
-      this.deviceService = deviceService;
-    }
 
    /**
     * {@inheritDoc}
@@ -76,8 +70,7 @@ public class DeviceCommandController extends BaseGWTSpringController implements
    
    @Override
    public DeviceCommandDetailsDTO loadCommandDetailsDTO(long id) {
-     DeviceCommand dc = deviceCommandService.loadById(id);
-     return (dc != null)?dc.getDeviceCommandDetailsDTO():null;
+	   return deviceCommandService.loadCommandDetailsDTO(id);
    }
 
    @Override
@@ -87,25 +80,6 @@ public class DeviceCommandController extends BaseGWTSpringController implements
    
    @Override
    public void saveNewDeviceCommand(DeviceCommandDetailsDTO dto, long deviceId) {
-     DeviceCommand dc = createDeviceCommandFromDTO(dto);
-
-     Device device = deviceService.loadById(deviceId);
-     dc.setDevice(device);
-
-     deviceCommandService.save(dc);
+     deviceCommandService.saveNewDeviceCommand(dto, deviceId);
    }
-
-   public static DeviceCommand createDeviceCommandFromDTO(DeviceCommandDetailsDTO dto) {
-     DeviceCommand dc = new DeviceCommand();     
-     dc.setName(dto.getName());
-     Protocol protocol = new Protocol();
-     protocol.setDeviceCommand(dc);
-     dc.setProtocol(protocol);
-     protocol.setType(dto.getProtocolType());
-     for (Map.Entry<String, String> e : dto.getProtocolAttributes().entrySet()) {
-       protocol.addProtocolAttribute(e.getKey(), e.getValue());
-     }
-     return dc;
-   }
-   
 }
