e017ec14bfcf37f282bf2276d33f6db4fde8c80c
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-02-19 10:29:33 +0000

    Branch to work on MODELER-390: export/import

---
+++ workspace/ebariaux/Designer_2_14_0_MODELER-390_Feb2013/src/org/openremote/modeler/server/SwitchController.java
@@ -0,0 +1,108 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.modeler.server;
+
+import java.util.ArrayList;
+
+import org.openremote.modeler.client.rpc.SwitchRPCService;
+import org.openremote.modeler.dao.GenericDAO;
+import org.openremote.modeler.domain.DeviceCommand;
+import org.openremote.modeler.domain.Sensor;
+import org.openremote.modeler.domain.Switch;
+import org.openremote.modeler.exception.PersistenceException;
+import org.openremote.modeler.service.DeviceCommandService;
+import org.openremote.modeler.service.SensorService;
+import org.openremote.modeler.service.SwitchService;
+import org.openremote.modeler.service.UserService;
+import org.openremote.modeler.shared.dto.DeviceCommandDTO;
+import org.openremote.modeler.shared.dto.SwitchDTO;
+import org.openremote.modeler.shared.dto.SwitchDetailsDTO;
+import org.openremote.modeler.shared.dto.SwitchWithInfoDTO;
+import org.openremote.modeler.utils.dtoconverter.SwitchDTOConverter;
+
+/**
+ * TODO :
+ *
+ *   The server side implementation of the RPC service <code>SwitchRPCService</code>.
+ *
+ *   Tasks to do :
+ *
+ *     - MODELER-313 -- return null reference in DTO if no sensor associated with switch
+ *     - DTO transformation should logically go into the domain object implementation
+ *     - This class should not access DB directly but to delegate to a REST API on Beehive
+ *       that is responsible for persistent switch operations.
+ *     - review the database load semantics, especially with regards to dependent objects
+ *       in Switch, requires unit testing
+ *
+ *
+ * @author <a href = "mailto:eric@openremote.org">Eric Bariaux</a>
+ * @author <a href = "mailto:juha@openremote.org">Juha Lindfors</a>
+ */
+public class SwitchController extends BaseGWTSpringController implements SwitchRPCService {
+
+   private SwitchService switchService;
+   
+   private UserService userService;
+   
+   @Override
+   public void delete(long id) {
+      switchService.delete(id);
+   }
+
+   public void setSwitchService(SwitchService switchService) {
+      this.switchService = switchService;
+   }
+
+  public void setUserService(UserService userService) {
+      this.userService = userService;
+   }
+
+  /**
+   * Loads persistent switch state including dependent object states (optional associated
+   * switch sensor, and mandatory switch 'on' and 'off' commands from the database and transforms
+   * them into data transfer object graph that can be serialized to the client. In effect, this
+   * method disconnects the persistent switch entities from client side processing.
+   *
+   * @param   id    the persistent switch entity identifier (primary key)
+   *
+   * @return  a data transfer object that contains the switch state including associated sensor
+   *          and command state in a serializable object graph
+   */
+  @Override public SwitchDetailsDTO loadSwitchDetails(long id) {
+    return switchService.loadSwitchDetailsDTO(id);
+  }
+
+  @Override
+   public ArrayList<SwitchWithInfoDTO> loadAllSwitchWithInfosDTO() {
+	   return new ArrayList<SwitchWithInfoDTO>(switchService.loadAllSwitchWithInfosDTO());
+   }
+
+  @Override
+   public void updateSwitchWithDTO(SwitchDetailsDTO switchDTO) {
+	  switchService.updateSwitchWithDTO(switchDTO);
+   }
+
+   @Override
+   public void saveNewSwitch(SwitchDetailsDTO switchDTO, long deviceId) {
+	   switchService.saveNewSwitch(switchDTO, deviceId);
+   }
+  
+}
---
+++ workspace/ebariaux/Designer_2_14_0_MODELER-390_Feb2013/src/org/openremote/modeler/service/DeviceService.java
@@ -0,0 +1,126 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.modeler.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openremote.modeler.domain.Account;
+import org.openremote.modeler.domain.Device;
+import org.openremote.modeler.shared.dto.DeviceCommandDetailsDTO;
+import org.openremote.modeler.shared.dto.DeviceDTO;
+import org.openremote.modeler.shared.dto.DeviceDetailsDTO;
+import org.openremote.modeler.shared.dto.DeviceWithChildrenDTO;
+import org.openremote.modeler.shared.dto.SensorDetailsDTO;
+import org.openremote.modeler.shared.dto.SliderDetailsDTO;
+import org.openremote.modeler.shared.dto.SwitchDetailsDTO;
+
+/**
+ * The Interface DeviceService.
+ */
+public interface DeviceService {
+   
+   /**
+    * Save device.
+    * 
+    * @param device the device
+    * 
+    * @return the device
+    */
+   Device saveDevice(Device device);
+   
+   /**
+    * Update device.
+    * 
+    * @param device the device
+    */
+   void updateDevice(Device device);
+   
+   /**
+    * Delete device.
+    * 
+    * @param id the id
+    */
+   void deleteDevice(long id);
+   
+   /**
+    * Load by id.
+    * 
+    * @param id the id
+    * 
+    * @return the device
+    */
+   Device loadById(long id);
+   
+
+   /**
+    * Load all.
+    * 
+    * @return the list< device>
+    */
+   List<Device> loadAll();
+   
+   /**
+    * Load all.
+    * 
+    * @param account the account
+    * 
+    * @return the list< device>
+    */
+   List<Device> loadAll(Account account);
+   
+   List<Device> loadSameDevices(Device device);
+   
+   /**
+    * Loads a device by id and creates a DTO with all information on the device and its related objects (commands, sensors, switches and sliders).
+    * 
+    * @param oid id of the device to load
+    * @return a DTO with information about the device and its related objects
+    */
+   DeviceWithChildrenDTO loadDeviceWithChildrenDTOById(long oid);
+
+   /**
+    * Loads a device by id and creates a DTO with all information on the device and its associated commands.
+    * 
+    * @param oid id of the device to load
+    * @return a DTO with information about the device and its commands
+    */
+   DeviceWithChildrenDTO loadDeviceWithCommandChildrenDTOById(long oid);
+
+   /**
+    * Loads all devices for a given account and creates DTOs with all information on the devices and their related objects (commands, sensors, switches and sliders).
+    * 
+    * @param account the account owning the devices to load
+    * @return a list of DTOs with information about the devices and their commands
+    */
+   ArrayList<DeviceWithChildrenDTO> loadAllDeviceWithChildrenDTOs(Account account);
+
+   List<DeviceDTO> loadAllDTOs(Account account);
+
+   DeviceDetailsDTO loadDeviceDetailsDTOById(long id);
+   
+   DeviceDTO saveNewDevice(DeviceDetailsDTO deviceDTO);
+
+   DeviceDTO saveNewDeviceWithChildren(DeviceDetailsDTO device, List<DeviceCommandDetailsDTO> commands, List<SensorDetailsDTO> sensors,
+           List<SwitchDetailsDTO> switches, List<SliderDetailsDTO> sliders);
+
+   void updateDeviceWithDTO(DeviceDetailsDTO deviceDTO);
+
+}
