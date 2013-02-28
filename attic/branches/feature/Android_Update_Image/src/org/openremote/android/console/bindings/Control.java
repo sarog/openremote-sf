7bfe2c6eaa9cd7e07b7514c1b633f2f07634e00b
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-02-28 11:54:26 +0000

    moved to attic

---
+++ attic/branches/feature/Android_Update_Image/src/org/openremote/android/console/bindings/Control.java
@@ -0,0 +1,58 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2011, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.android.console.bindings;
+
+import org.w3c.dom.Node;
+
+import android.util.Log;
+
+
+/**
+ * The super class of control component, which include button, switch and slider.
+ */
+@SuppressWarnings("serial")
+public class Control extends Component {
+
+   /**
+    * Builds the control component by parse component node, 
+    * which include button, switch and slider.
+    * 
+    * @param node the node
+    * 
+    * @return the control component
+    */
+   public static Component buildWithXML(Node node) {
+      Component component = null;
+      if (node == null) {
+         Log.e("OpenRemote-COMPONENT", "The node is null in buildWithXML.");
+         return null;
+      }
+      if (BUTTON.equals(node.getNodeName())) {
+         component =  new ORButton(node);
+      } else if (SWITCH.equals(node.getNodeName())) {
+         component = new Switch(node);
+      } else if (SLIDER.equalsIgnoreCase(node.getNodeName())) {
+         component = new Slider(node);
+      } else if (COLORPICKER.equals(node.getNodeName())) {
+         component = new ColorPicker(node);
+      }
+      return component;
+   }
+}
---
+++ attic/branches/feature/Android_Update_Image/tests/src/org/openremote/android/test/console/util/IpUtilTest.java
@@ -0,0 +1,62 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openremote.android.test.console.util;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.openremote.android.console.util.IpUtil;
+
+/**
+ * Tests for {@link org.openremote.android.console.util.IpUtil} class.
+ * 
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
+ * @author handy 2010-04-28
+ *
+ */
+public class IpUtilTest extends TestCase
+{
+
+  public void testIpStringToLong()
+  {
+    long ipLong = IpUtil.ipStringToLong("80.35.83.10");
+    Assert.assertEquals(1344492298, ipLong);
+  }
+
+  public void testIpLongToString()
+  {
+    String ipString = IpUtil.ipLongToString(1344492298);
+    Assert.assertEquals("80.35.83.10", ipString);
+  }
+
+  public void testSplitIpFromURL()
+  {
+    String ip1 = IpUtil.splitIpFromURL("http://192.168.100.34:8080/controller");
+    Assert.assertEquals("192.168.100.34", ip1);
+    String ip2 = IpUtil.splitIpFromURL("http://192.168.100.34/controller");
+    Assert.assertEquals("192.168.100.34", ip2);
+    String ip3 = IpUtil.splitIpFromURL("http://192.168.100.34/");
+    Assert.assertEquals(null, ip3);
+    String ip4 = IpUtil.splitIpFromURL("http://192.168.100.34/");
+    Assert.assertEquals(null, ip4);
+  }
+}
