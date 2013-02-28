b4c5eb3518bd3ab037fcac7021754a2f21d5ead1
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-02-28 11:24:36 +0000

    moved to attic

--- branches/feature/Android_Update_Image/src/org/openremote/android/console/bindings/OROrientation.java
+++
@@ -1,5 +0,0 @@
-package org.openremote.android.console.bindings;
-
-public enum OROrientation {
-   UIInterfaceOrientationPortrait,UIInterfaceOrientationLandscapeLeft,UIInterfaceOrientationLandscapeRight,UIInterfaceOrientationPortraitUpsideDown
-}
