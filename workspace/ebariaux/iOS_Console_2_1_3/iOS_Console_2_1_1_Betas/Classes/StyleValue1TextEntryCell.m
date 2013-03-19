e8cb8f5b0cc0fa2e0914a358939284a7727be51e
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-03-19 11:04:03 +0000

    Branch to work on iOS console 2.1.3 version

---
+++ workspace/ebariaux/iOS_Console_2_1_3/iOS_Console_2_1_1_Betas/Classes/StyleValue1TextEntryCell.m
@@ -0,0 +1,51 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2011, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#import "StyleValue1TextEntryCell.h"
+
+@implementation StyleValue1TextEntryCell
+
+- (id)initWithReuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier];
+    if (self) {
+        textField = [[UITextField alloc] initWithFrame:CGRectZero];
+        textField.autocapitalizationType = UITextAutocapitalizationTypeNone;// no auto capitalization support
+        textField.autocorrectionType = UITextAutocorrectionTypeNo;// no auto correction support
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+        [self.contentView addSubview:textField];
+        [textField release];
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    textField.font = self.detailTextLabel.font;
+    textField.textColor = self.detailTextLabel.textColor;
+    int offset = 300;
+    textField.frame = CGRectMake(self.textLabel.frame.origin.x + offset, self.textLabel.frame.origin.y, self.contentView.bounds.size.width - offset - self.textLabel.frame.origin.x, self.textLabel.frame.size.height);
+}
+
+@synthesize textField;
+
+@end
