f645cb32f1ce94694795cce53b929b91bfd61714
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-05-06 13:27:12 +0000

    Merged MODELER-452 branch (r7555:7561)

--- workspace/ebariaux/Designer_2_14_0_Preview-453-413-470__Refactoring_MODELER-437-287-452/src/org/openremote/modeler/service/impl/DesignerState.java
+++ workspace/ebariaux/Designer_2_14_0_Preview-453-413-470__Refactoring_MODELER-437-287-452/src/org/openremote/modeler/service/impl/DesignerState.java
@@ -38,6 +38,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
+import org.hibernate.ObjectNotFoundException;
 import org.openremote.modeler.beehive.Beehive30API;
 import org.openremote.modeler.beehive.BeehiveService;
 import org.openremote.modeler.cache.CacheOperationException;
@@ -46,13 +47,41 @@ import org.openremote.modeler.client.Configuration;
 import org.openremote.modeler.client.utils.PanelsAndMaxOid;
 import org.openremote.modeler.configuration.PathConfig;
 import org.openremote.modeler.domain.Account;
+import org.openremote.modeler.domain.DeviceCommand;
+import org.openremote.modeler.domain.DeviceCommandRef;
+import org.openremote.modeler.domain.DeviceMacro;
+import org.openremote.modeler.domain.DeviceMacroRef;
 import org.openremote.modeler.domain.Panel;
+import org.openremote.modeler.domain.Sensor;
+import org.openremote.modeler.domain.Slider;
+import org.openremote.modeler.domain.Switch;
+import org.openremote.modeler.domain.UICommand;
 import org.openremote.modeler.domain.User;
+import org.openremote.modeler.domain.Panel.UIComponentOperation;
+import org.openremote.modeler.domain.component.ColorPicker;
+import org.openremote.modeler.domain.component.Gesture;
+import org.openremote.modeler.domain.component.SensorLinkOwner;
+import org.openremote.modeler.domain.component.SensorOwner;
+import org.openremote.modeler.domain.component.UIButton;
+import org.openremote.modeler.domain.component.UIComponent;
+import org.openremote.modeler.domain.component.UISlider;
+import org.openremote.modeler.domain.component.UISwitch;
 import org.openremote.modeler.exception.ConfigurationException;
 import org.openremote.modeler.exception.NetworkException;
 import org.openremote.modeler.exception.UIRestoreException;
 import org.openremote.modeler.logging.AdministratorAlert;
 import org.openremote.modeler.logging.LogFacade;
+import org.openremote.modeler.server.SensorController;
+import org.openremote.modeler.server.SliderController;
+import org.openremote.modeler.server.SwitchController;
+import org.openremote.modeler.service.DeviceCommandService;
+import org.openremote.modeler.service.DeviceMacroService;
+import org.openremote.modeler.service.SensorService;
+import org.openremote.modeler.service.SliderService;
+import org.openremote.modeler.service.SwitchService;
+import org.openremote.modeler.shared.dto.DeviceCommandDTO;
+import org.openremote.modeler.shared.dto.MacroDTO;
+import org.openremote.modeler.shared.dto.UICommandDTO;
 
 
 /**
@@ -161,9 +190,14 @@ class DesignerState
    */
   private final static Set<Long> haltAccountSave = new CopyOnWriteArraySet<Long>();
 
+  // Services required to access DB building elements during configuration restore
 
+  private DeviceCommandService deviceCommandService;
+  private DeviceMacroService deviceMacroService;
 
-
+  private SensorService sensorService;
+  private SliderService sliderService;
+  private SwitchService switchService;
 
   /**
    * TODO : should come through resource cache interface
@@ -416,6 +450,45 @@ class DesignerState
           restoreLegacyDesignerUIState(cache.getLegacyPanelObjFile());
 
           restoreLog.info("Restored UI state : {0}", this);
+          
+          // TODO :
+          //    this should be pushed deeper into the call stack, either into the designer state
+          //    implementation (which can implement a translation to DTOs, similar to the current
+          //    hack of transformToPanelsAndMaxOid() as seen above) or better yet into the domain
+          //    package as was done in the case of Panel.getAllImageNames() method implementation --
+          //    same comment applies, the processing is part of the domain object which should also
+          //    help reduce the very brittle instanceof semantics seen in the populate and resolve
+          //    DTO references here.
+          //
+          //    Also the error handling needs to be pushed to new DesignerState implementation
+          //    so that errors in the implementation below are correctly handled and potentially
+          //    preventing user data corruption.
+          //                                                                            [JPL]
+          //
+          //    UPDATE 2012-09-13: Have not accomplished the task above yet (pushing call down
+          //    to DesignerState implementation which would have more robust error handling
+          //    *and* better error reporting due to user and account references that are carried
+          //    in it. Duplicating some error handling here until have time to reorganize the
+          //    code better, at which point the duplicate error handling can probably be removed. [JPL]
+          //
+          //    UPDATE 2013-02-11: Already pushed down to this level. Don't seem to change much with
+          //    regards to how errors are reported.
+
+          try
+          {
+            populateDTOReferences(this.panels);
+          }
+
+          catch (Throwable t)
+          {
+            // This exception type and message will propagate to the user...
+
+            throw new UIRestoreException(
+                "Restoring your account data has failed. Please contact an administrator for " +
+                "assistance. Avoid making further changes to your account and design to prevent " +
+                "any potential data corruption issues: " + t.getMessage(), t
+            );
+          }
 
           return;
         }
@@ -515,7 +588,130 @@ class DesignerState
     }
   }
 
+  /**
+   * Prepares the objects to be sent to client side by replacing references to entity beans with references to DTOs.
+   * The inverse operation is performed by the resolveDTOReferences method before using the objects on the server side.
+   *
+   * @param panels
+   */
+  private void populateDTOReferences(Collection<Panel> panels) {
+    Panel.walkAllUIComponents(panels, new UIComponentOperation() {
 
+      @Override
+      public void execute(UIComponent component) {
+        if (component instanceof SensorOwner) {
+          SensorOwner owner = (SensorOwner) component;
+          if (owner.getSensorDTO() == null && owner.getSensor() != null) {
+            Sensor sensor = sensorService.loadById(owner.getSensor().getOid());
+
+            if (sensor != null)
+            {
+              owner.setSensorDTO(SensorController.createSensorWithInfoDTO(sensor));
+            }
+
+            owner.setSensor(null);
+
+            if (owner instanceof SensorLinkOwner) {
+              ((SensorLinkOwner) owner).getSensorLink().setSensor(null);
+              ((SensorLinkOwner) owner).getSensorLink().setSensorDTO(owner.getSensorDTO());
+            }
+          }
+        }
+        if (component instanceof UISlider) {
+          UISlider uiSlider = (UISlider)component;
+          if (uiSlider.getSliderDTO() == null && uiSlider.getSlider() != null) {
+            // We must load slider because referenced sensor / command are not serialized, this reloads from DB
+            Slider slider = sliderService.loadById(uiSlider.getSlider().getOid());
+            if (slider != null) { // Just in case we have a dangling pointer
+              uiSlider.setSliderDTO(SliderController.createSliderWithInfoDTO(slider));
+            }
+            uiSlider.setSlider(null);
+          }
+        }
+        if (component instanceof UISwitch) {
+          UISwitch uiSwitch = (UISwitch)component;
+          if (uiSwitch.getSwitchDTO() == null && uiSwitch.getSwitchCommand() != null) {
+            Switch switchBean = switchService.loadById(uiSwitch.getSwitchCommand().getOid());
+            if (switchBean != null) { // Just in case we have a dangling pointer
+              uiSwitch.setSwitchDTO(SwitchController.createSwitchWithInfoDTO(switchBean));
+            }
+            uiSwitch.setSwitchCommand(null);
+          }
+        }
+        if (component instanceof UIButton) {
+          UIButton uiButton = (UIButton)component;
+          if (uiButton.getUiCommandDTO() == null && uiButton.getUiCommand() != null) {
+            uiButton.setUiCommandDTO(createUiCommandDTO(uiButton.getUiCommand()));
+            uiButton.setUiCommand(null);
+          }
+        }
+        if (component instanceof ColorPicker) {
+          ColorPicker colorPicker = (ColorPicker)component;
+          if (colorPicker.getUiCommandDTO() == null && colorPicker.getUiCommand() != null) {
+            colorPicker.setUiCommandDTO(createUiCommandDTO(colorPicker.getUiCommand()));
+            colorPicker.setUiCommand(null);
+          }
+        }
+        if (component instanceof Gesture) {
+          Gesture gesture = (Gesture)component;
+          if (gesture.getUiCommandDTO() == null && gesture.getUiCommand() != null) {
+            gesture.setUiCommandDTO(createUiCommandDTO(gesture.getUiCommand()));
+            gesture.setUiCommand(null);
+          }
+        }
+      }
+
+      private UICommandDTO createUiCommandDTO(UICommand uiCommand)
+      {
+        if (uiCommand instanceof DeviceCommandRef)
+        {
+          try
+          {
+            DeviceCommand dc = deviceCommandService.loadById(((DeviceCommandRef)uiCommand).getDeviceCommand().getOid());
+            return (dc != null)?new DeviceCommandDTO(dc.getOid(), dc.getDisplayName(), dc.getProtocol().getType()):null;
+          }
+          catch (ObjectNotFoundException e)
+          {
+            restoreLog.warn("Button is referencing inexistent command with id " + ((DeviceCommandRef)uiCommand).getDeviceCommand().getOid(), e);
+            return null;
+          }
+        }
+
+        else if (uiCommand instanceof DeviceMacroRef)
+        {
+          try
+          {
+            DeviceMacro targetMacro = ((DeviceMacroRef)uiCommand).getTargetDeviceMacro();
+
+            if (targetMacro != null)
+            {
+              long oid = targetMacro.getOid();
+
+              DeviceMacro dm = deviceMacroService.loadById(oid);
+
+              return (dm != null) ? new MacroDTO(dm.getOid(), dm.getDisplayName()) : null;
+            }
+
+            else
+            {
+              restoreLog.error("DeviceMacroRef had a null target device macro reference");
+
+              return null;
+            }
+          }
+
+          catch (ObjectNotFoundException e)
+          {
+            restoreLog.warn("Button is referencing inexistent macro with id " + ((DeviceMacroRef)uiCommand).getTargetDeviceMacro().getOid(), e);
+            return null;
+          }
+        }
+
+        throw new RuntimeException("We don't expect any other type of UICommand"); // TODO : review that exception type
+      }
+
+    });
+  }
 
   /**
    * Saves the account artifacts from the current in-memory domain model to Beehive server.
@@ -1146,5 +1342,25 @@ class DesignerState
       super(msg, cause);
     }
   }
-}
+  
+  public void setDeviceCommandService(DeviceCommandService deviceCommandService) {
+    this.deviceCommandService = deviceCommandService;
+  }
 
+  public void setDeviceMacroService(DeviceMacroService deviceMacroService) {
+    this.deviceMacroService = deviceMacroService;
+  }
+
+  public void setSensorService(SensorService sensorService) {
+    this.sensorService = sensorService;
+  }
+
+  public void setSliderService(SliderService sliderService) {
+    this.sliderService = sliderService;
+  }
+
+  public void setSwitchService(SwitchService switchService) {
+    this.switchService = switchService;
+  }
+
+}
