15e17339613cfc8949e9fed88b6ba1e345900df1
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-05-07 11:02:54 +0000

    MODELER-480: Extracted fix from MODELER-390 branch (r8210:8212)

--- workspace/ebariaux/Designer_2_14_0_Preview-453-413-470-437-287-452-450-405__MODELER-480/src/org/openremote/modeler/domain/component/ColorPicker.java
+++ workspace/ebariaux/Designer_2_14_0_Preview-453-413-470-437-287-452-450-405__MODELER-480/src/org/openremote/modeler/domain/component/ColorPicker.java
@@ -123,5 +123,36 @@ public class ColorPicker extends UIControl implements ImageSourceOwner {
       }
       return imageSources;
    }
+   
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + ((image == null) ? 0 : image.hashCode());
+    result = prime * result + ((uiCommandDTO == null) ? 0 : uiCommandDTO.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (!super.equals(obj))
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ColorPicker other = (ColorPicker) obj;
+    if (image == null) {
+      if (other.image != null)
+        return false;
+    } else if (!image.equals(other.image))
+      return false;
+    if (uiCommandDTO == null) {
+      if (other.uiCommandDTO != null)
+        return false;
+    } else if (!uiCommandDTO.equals(other.uiCommandDTO))
+      return false;
+    return true;
+  }
 
 }
