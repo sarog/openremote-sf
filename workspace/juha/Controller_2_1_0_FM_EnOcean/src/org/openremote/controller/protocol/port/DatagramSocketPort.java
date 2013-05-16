b7f0ac78038dc67d2a2f642ad8f2bdde363fb53d
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-05-16 19:13:38 +0000

    sync with changes in /workspace/juha/Controller_2_1_0_FM_ORCJAVA-314

--- workspace/juha/Controller_2_1_0_FM_EnOcean/src/org/openremote/controller/protocol/port/DatagramSocketPort.java
+++ workspace/juha/Controller_2_1_0_FM_EnOcean/src/org/openremote/controller/protocol/port/DatagramSocketPort.java
@@ -1,3 +1,23 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2013, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.protocol.port;
 
 import java.io.IOException;
@@ -7,9 +27,11 @@ import java.net.InetSocketAddress;
 import java.util.Map;
 
 /**
- * A physical bus using datagram sockets to send and receive messages.
+ * A physical port (bus) using datagram sockets to send and receive messages.
  * 
  * @see Port
+ *
+ * @author Olivier Gandit
  */
 public class DatagramSocketPort implements Port {
    private DatagramSocket inSocket, outSocket;
