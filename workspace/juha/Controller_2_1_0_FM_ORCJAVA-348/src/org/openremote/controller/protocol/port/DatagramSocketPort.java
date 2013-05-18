639a568a07e7f5eaeccb4909534b97789e230db2
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-05-18 09:41:11 +0000

    Apply ORCJAVA-314 (add PAD sources to project) -- mandatory dependency for ORCJAVA-348

---
+++ workspace/juha/Controller_2_1_0_FM_ORCJAVA-348/src/org/openremote/controller/protocol/port/DatagramSocketPort.java
@@ -0,0 +1,68 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2013, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.port;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+/**
+ * A physical port (bus) using datagram sockets to send and receive messages.
+ * 
+ * @see Port
+ *
+ * @author Olivier Gandit
+ */
+public class DatagramSocketPort implements Port {
+   private DatagramSocket inSocket, outSocket;
+
+   @Override
+   public void configure(Map<String, Object> configuration) {
+      this.inSocket = (DatagramSocket) configuration.get("inSocket");
+      this.outSocket = (DatagramSocket) configuration.get("outSocket");
+   }
+
+   @Override
+   public void start() {
+   }
+
+   @Override
+   public void stop() {
+      if (this.inSocket != null) this.inSocket.close();
+      if (this.outSocket != null) this.outSocket.close();
+   }
+
+   @Override
+   public void send(Message message) throws IOException {
+      DatagramSocketMessage m = (DatagramSocketMessage) message;
+      this.outSocket.send(new DatagramPacket(m.getContent(), m.getContent().length, m.getDestAddr()));
+   }
+
+   @Override
+   public Message receive() throws IOException {
+      byte[] buffer = new byte[1024];
+      DatagramPacket p = new DatagramPacket(buffer, buffer.length);
+      this.inSocket.receive(p);
+      return new DatagramSocketMessage((InetSocketAddress) this.inSocket.getLocalSocketAddress(), p.getData());
+   }
+}
