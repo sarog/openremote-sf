3dde53e05520c82083c912ecc46f3d2edd4143ba
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-05-21 11:59:23 +0000

    No work done on this feature yet, will restart from appropriate branch when starting real work on this

--- workspace/ebariaux/Designer_2_14_0_MODELER-439/src/org/openremote/modeler/client/event/DevicesCreatedEventHandler.java
+++
@@ -1,9 +0,0 @@
-package org.openremote.modeler.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface DevicesCreatedEventHandler extends EventHandler {
-
-  void onDevicesCreated(DevicesCreatedEvent event);
-  
-}
