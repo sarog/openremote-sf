a211fdc7f16272c337d0a35f7742991acbdb6abb
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-05-27 09:34:36 +0000

    Apply ORCJAVA-298 from /workspace/juha/Controller_2_1_0_FM_ORCJAVA-298 : TCP polling interval and regexp added, implementation based on event listener interface

--- workspace/juha/Controller_2_1_0_FeatureMergeBranch/src/org/openremote/controller/protocol/socket/TCPSocketCommandBuilder.java
+++ workspace/juha/Controller_2_1_0_FeatureMergeBranch/src/org/openremote/controller/protocol/socket/TCPSocketCommandBuilder.java
@@ -22,37 +22,98 @@ package org.openremote.controller.protocol.socket;
 import java.util.List;
 
 import org.jdom.Element;
+import org.openremote.controller.Constants;
 import org.openremote.controller.command.Command;
 import org.openremote.controller.command.CommandBuilder;
+import org.openremote.controller.exception.NoSuchCommandException;
 import org.openremote.controller.utils.CommandUtil;
+import org.openremote.controller.utils.Logger;
+import org.openremote.controller.utils.Strings;
 
 
 /**
- * The Class SocketEventBuilder.
+ * The Class TCPSocketCommandBuilder.
  *
  * @author Marcus 2009-4-26
  */
 public class TCPSocketCommandBuilder implements CommandBuilder {
 
+   // Constants
+   // ------------------------------------------------------------------------------------
+
+   /**
+    * Common log category name for all HTTP protocol related logging.
+    */
+   public final static String TCP_PROTOCOL_LOG_CATEGORY = Constants.CONTROLLER_PROTOCOL_LOG_CATEGORY + "tcp";
+
+   private final static String STR_ATTRIBUTE_NAME_PORT = "port";
+   private final static String STR_ATTRIBUTE_NAME_IP = "ipAddress";
+   private final static String STR_ATTRIBUTE_NAME_COMMAND = "command";
+   private final static String STR_ATTRIBUTE_NAME_REGEX = "regex";
+   private final static String STR_ATTRIBUTE_NAME_POLLINGINTERVAL = "pollingInterval";
+   
+   // Class Members
+   // --------------------------------------------------------------------------------
+
+   /**
+    * Logger for this HTTP protocol implementation.
+    */
+   private final static Logger logger = Logger.getLogger(TCP_PROTOCOL_LOG_CATEGORY);
+   
    /**
     * {@inheritDoc}
     */
    @SuppressWarnings("unchecked")
    public Command build(Element element) {
-      TCPSocketCommand tcpEvent = new TCPSocketCommand();
+      logger.debug("Building HttGetCommand");
       List<Element> propertyEles = element.getChildren("property", element.getNamespace());
-      for(Element ele : propertyEles){
-         if("name".equals(ele.getAttributeValue("name"))){
-            tcpEvent.setName(ele.getAttributeValue("value"));
-         } else if("port".equals(ele.getAttributeValue("name"))){
-            tcpEvent.setPort(ele.getAttributeValue("value"));
-         } else if("ipAddress".equals(ele.getAttributeValue("name"))){
-            tcpEvent.setIp(ele.getAttributeValue("value"));
-         } else if("command".equals(ele.getAttributeValue("name"))){
-            tcpEvent.setCommand(CommandUtil.parseStringWithParam(element, ele.getAttributeValue("value")));
-         }
+      String port = null;
+      String ipAddress = null;
+      String command = null;
+      String regex = null;
+      String interval = null;
+      Integer intervalInMillis = null;
+
+      // read values from config xml
+
+      for (Element ele : propertyEles)
+      {
+        String elementName = ele.getAttributeValue(CommandBuilder.XML_ATTRIBUTENAME_NAME);
+        String elementValue = ele.getAttributeValue(CommandBuilder.XML_ATTRIBUTENAME_VALUE);
+
+        if (STR_ATTRIBUTE_NAME_PORT.equals(elementName))
+        {
+          port = elementValue;
+          logger.debug("TCPSocketCommand: port = " + port);
+        } else if (STR_ATTRIBUTE_NAME_IP.equals(elementName))
+        {
+          ipAddress = elementValue;
+          logger.debug("TCPSocketCommand: ipAddress = " + ipAddress);
+        } else if (STR_ATTRIBUTE_NAME_COMMAND.equals(elementName))
+        {
+          command = CommandUtil.parseStringWithParam(element, elementValue);
+          logger.debug("TCPSocketCommand: command = " + command);
+        } else if (STR_ATTRIBUTE_NAME_POLLINGINTERVAL.equals(elementName))
+        {
+          interval = elementValue;
+          logger.debug("TCPSocketCommand: pollingInterval = " + interval);
+        } else if (STR_ATTRIBUTE_NAME_REGEX.equals(elementName))
+        {
+          regex = elementValue;
+          logger.debug("TCPSocketCommand: regex = " + regex);
+        }
       }
-      return tcpEvent;
+      try
+      {
+        if (null != interval) {
+          intervalInMillis = Integer.valueOf(Strings.convertPollingIntervalString(interval));
+        }
+      } catch (Exception e1)
+      {
+        throw new NoSuchCommandException("Unable to create TCPSocketCommand, pollingInterval could not be converted into milliseconds");
+      }
+
+      return new TCPSocketCommand(ipAddress, port, command, regex, intervalInMillis);
    }
 
 }
