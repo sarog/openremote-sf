531102f22cbb4c0123dea0d00387d3d65b25a2f1
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-05-27 17:30:37 +0000

    Bring KNX code refactoring/documentation/javadoc/tests from branch /workspace/juha/Controller_EP_SNAP_20111129 to 2.1 line.
     R5843 - format according to our Java code conventions
     R5844 - format according to our Java code conventions
     R5845 - delete what looks like a redundant override
     R5857 - use standard naming convention for enum constants
     R5858 - javadoc fix
     R5859 - format according to our Java source code conventions
     R5865 - add javadoc, get rid of magic bytes
     R5872 - add some javadoc and get rid of magic bytes
     R5873 - add class level javadoc with frame structure
     R5874 - add write method override to write the complete frame, 
             complete the stream-based constructor to include CRD 
             creation, add a new constructor to create connect 
             response frame from in-memory object structures, 
             and abstract CRD into its own object
     R5875 - add resolve() method to ConnectionType enum
     R5876 - use explicit status enum (also allows more precise error 
             resolution -- such as no more connections)
     R5878 - format according to our Java source convenetions
     R5879 - add some javadoc, remove magic bytes
     R5880 - add method to inspect the KNX frame header if this is a 
             connect request
     R5881 - add method to inspect the KNX frame header if this is a 
             connection state request
     R5882 - add class level javadoc with KNX frame details
     R5883 - format according to our Java source conventions
     R5884 - add some javadoc, a typesafe status enum that contains 
             all possible values from a connection state response
     R5885 - add class level javadoc with frame structure details
     R5886 - javadoc tweak
     R5887 - use explicit status enum to determine connection state 
             (also allows for detecting more fine grained error conditions)
     R5888 - add constructor to create connection state response using 
             the object model, complete write() override to write a 
             complete connection state response frame
     R5889 - format according to our Java source conventions
     R5890 - add method to inspect the KNX frame header if this is a 
             KNX telegram tunneling request

--- workspace/juha/Controller_2_1_0_FM_KNXUpdates/src/org/openremote/controller/protocol/knx/ip/message/IpTunnelingAck.java
+++ workspace/juha/Controller_2_1_0_FM_KNXUpdates/src/org/openremote/controller/protocol/knx/ip/message/IpTunnelingAck.java
@@ -1,6 +1,6 @@
 /*
  * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
+ * Copyright 2008-2012, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
@@ -24,51 +24,83 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class IpTunnelingAck extends IpMessage {
-  public static final int STI = 0x421;
-  private int             channelId;
-  private int             seqCounter;
-  private int             status;
 
-  public IpTunnelingAck(InputStream is, int length) throws IOException {
+/**
+ * TODO
+ *
+ * @author Olivier Gandit
+ */
+public class IpTunnelingAck extends IpMessage
+{
+
+  // Constants ------------------------------------------------------------------------------------
+
+  public static final int STI = 0x421;
+
+
+  // Instance Fields ------------------------------------------------------------------------------
+
+  private int channelId;
+  private int seqCounter;
+  private int status;
+
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  public IpTunnelingAck(InputStream is, int length) throws IOException
+  {
     super(STI, length);
 
     // TODO check structure length
     is.skip(1);
+
     this.channelId = is.read();
     this.seqCounter = is.read();
     this.status = is.read();
   }
 
-  public IpTunnelingAck(int channelId, int seqCounter, int status) {
+  public IpTunnelingAck(int channelId, int seqCounter, int status)
+  {
     super(STI, 4);
+
     this.channelId = channelId;
     this.seqCounter = seqCounter;
     this.status = status;
   }
 
-  public void write(OutputStream os) throws IOException {
+
+  // IpMessage Overrides --------------------------------------------------------------------------
+
+  @Override public void write(OutputStream os) throws IOException
+  {
     super.write(os);
+
     os.write(4);
     os.write(this.channelId);
     os.write(this.seqCounter);
     os.write(this.status);
   }
 
-  @Override
-  public Primitive getPrimitive() {
+  @Override public Primitive getPrimitive()
+  {
     return Primitive.RESP;
   }
 
-  public int getChannelId() {
+
+  // Instance Methods -----------------------------------------------------------------------------
+
+  public int getChannelId()
+  {
     return channelId;
   }
 
-  public int getSeqCounter() {
+  public int getSeqCounter()
+  {
     return seqCounter;
   }
 
-  public int getStatus() {
+  public int getStatus()
+  {
     return status;
   }
 }
