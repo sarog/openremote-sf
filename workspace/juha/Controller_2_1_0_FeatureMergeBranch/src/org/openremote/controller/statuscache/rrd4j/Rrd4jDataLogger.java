aba201a0b4e351a2b1fa9f8a2c3ccfcd089d310d
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-06-01 17:32:05 +0000

    Apply ORCJAVA-356 from /workspace/juha/Controller_2_1_0_FM_ORCJAVA-356 : null guard for RRD start/stop lifecycle

--- workspace/juha/Controller_2_1_0_FeatureMergeBranch/src/org/openremote/controller/statuscache/rrd4j/Rrd4jDataLogger.java
+++ workspace/juha/Controller_2_1_0_FeatureMergeBranch/src/org/openremote/controller/statuscache/rrd4j/Rrd4jDataLogger.java
@@ -22,6 +22,7 @@ import java.io.StringWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -59,9 +60,11 @@ import org.w3c.dom.NodeList;
  * @author marcus
  *
  */
-public class Rrd4jDataLogger extends EventProcessor {
+public class Rrd4jDataLogger extends EventProcessor
+{
 
-   private List<RrdDb> rrdDbList;
+   private List<RrdDb> rrdDbList = Collections.emptyList();
+
    private Map<String,String> graphDefMap;
    
    @Override
