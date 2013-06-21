ede1306ce9cf512492363e347d9357fff8662ce5
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-06-21 09:59:56 +0000

    Megred ORCJAVA-234, r7192:7193

--- workspace/ebariaux/Controller_2_0_2_ORCJAVA-234-244-310/src/org/openremote/controller/protocol/MessageQueueWithPriorityAndTTL.java
+++ workspace/ebariaux/Controller_2_0_2_ORCJAVA-234-244-310/src/org/openremote/controller/protocol/MessageQueueWithPriorityAndTTL.java
@@ -1,6 +1,6 @@
 /*
  * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
+ * Copyright 2008-2012, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.openremote.controller.protocol.lutron;
+package org.openremote.controller.protocol;
 
 import java.util.Date;
 import java.util.LinkedList;
