97223b12804db9e0093ea634ae2bcb939ffcfa4c
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-06-24 17:26:50 +0000

    Merge independent changes in R7506 (with modifications) from /workspace/mredeker/Controller_2_1_ORCJAVA-301

--- workspace/juha/Controller_2_1_0_FM_KNXMerge-ORCJAVA-294-301-361-362/src/org/openremote/controller/protocol/knx/ApplicationProtocolDataUnit.java
+++ workspace/juha/Controller_2_1_0_FM_KNXMerge-ORCJAVA-294-301-361-362/src/org/openremote/controller/protocol/knx/ApplicationProtocolDataUnit.java
@@ -30,6 +30,7 @@ import org.openremote.controller.protocol.knx.datatype.Controlled3Bit;
 import org.openremote.controller.protocol.knx.datatype.Unsigned8Bit;
 import org.openremote.controller.protocol.knx.datatype.Float2Byte;
 import org.openremote.controller.protocol.knx.datatype.TwoOctetFloat;
+import org.openremote.controller.protocol.knx.datatype.KNXString;
 import org.openremote.controller.exception.ConversionException;
 import org.openremote.controller.command.CommandParameter;
 
@@ -413,6 +414,15 @@ class ApplicationProtocolDataUnit
   }
 
 
+  public static ApplicationProtocolDataUnit createText(CommandParameter parameter) 
+  {
+    return new ApplicationProtocolDataUnit(
+        ApplicationLayer.Service.GROUPVALUE_WRITE,
+        new KNXString(DataPointType.STRING_ASCII, parameter.getRawValue())
+    );
+  }
+  
+
   // Private Instance Fields ----------------------------------------------------------------------
 
   /**
@@ -744,15 +754,10 @@ class ApplicationProtocolDataUnit
      */
     static ResponseAPDU createStringResponse(final byte[] apdu)
     {
-      int len = apdu.length;
-      byte[] stringData = new byte[len - 2];
-
-      System.arraycopy(apdu, 2, stringData, 0, stringData.length);
-
       return new ResponseAPDU(
           ApplicationLayer.Service.GROUPVALUE_RESPONSE,
-          stringData.length + 1, /* Data length */
-          stringData
+          15, /* Data length */
+          apdu
       );
     }
 
@@ -829,7 +834,14 @@ class ApplicationProtocolDataUnit
             getApplicationLayerService(),
             resolveToFloat2ByteValue(value, getDataType().getData()));
       }
+      else if (dpt instanceof DataPointType.KNXString)
+      {
+        DataPointType.KNXString value = (DataPointType.KNXString) dpt;
 
+        return new ApplicationProtocolDataUnit(
+            getApplicationLayerService(),
+            resolveToKNXString(value, getDataType().getData()));
+      }
 
       else
       {
@@ -866,6 +878,11 @@ class ApplicationProtocolDataUnit
     private TwoOctetFloat resolveToTwoOctetFloat(DataPointType.TwoOctetFloat dpt, byte[] value)
     {
       return new TwoOctetFloat(dpt, value);
+    }
+    
+    private KNXString resolveToKNXString(DataPointType.KNXString dpt, byte[] value)
+    {
+      return new KNXString(dpt, value);
     }
 
     /**
@@ -925,6 +942,5 @@ class ApplicationProtocolDataUnit
     }
 
   }
-
 
 }
