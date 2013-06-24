f6d8a296b4a61e37529d9d43b2f33b9bb359abc9
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-06-24 12:35:28 +0000

    Merge R4700:4702 from /workspace/mredeker/Controller_2_1_ORCJAVA-294 with minor review changes

--- workspace/juha/Controller_2_1_0_FM_ORCJAVA-294/src/org/openremote/controller/protocol/knx/KNXCommand.java
+++ workspace/juha/Controller_2_1_0_FM_ORCJAVA-294/src/org/openremote/controller/protocol/knx/KNXCommand.java
@@ -220,11 +220,14 @@ abstract class KNXCommand implements Command
    * Destination address for this command.
    */
   private GroupAddress address;
+  
+
+  // Protected Instance Fields ----------------------------------------------------------------------
 
   /**
    * Connection manager to be used to transmit this command.
    */
-  private KNXIpConnectionManager connectionManager;
+  protected KNXIpConnectionManager connectionManager;
 
 
 
@@ -327,32 +330,22 @@ abstract class KNXCommand implements Command
 
 
   /**
-   * Relay a read command to an open KNX/IP connection.
-   *
-   * TODO : call semantics on return value 
-   *
-   * @param command   KNX read command
-   *
-   * @return  Returns the application protocol data unit (APDU) for a Group Value Read Response
-   *          frame. This frame contains the response value from the device. <P>
-   *
-   *          NOTE: may return <code>null</code> in case there's a connection exception or the
-   *          read response is not available from the device yet.
+   * Relay a read command to an open KNX/IP connection. 
+   * The value which is received after this read is handled by the KNXConnection which updates the sensor
+   * 
+   * @param command KNX read command
    */
-  ApplicationProtocolDataUnit read(GroupValueRead command)
+  void triggerRead(GroupValueRead command)
   {
     KNXConnection connection = connectionManager.getCurrentConnection();
 
     if (connection == null)
     {
        log.info("KNX connection not available.");
-
-       return null;
     }
-
     else
     {
-      return connection.read(command);
+      connection.read(command);
     }
   }
   
