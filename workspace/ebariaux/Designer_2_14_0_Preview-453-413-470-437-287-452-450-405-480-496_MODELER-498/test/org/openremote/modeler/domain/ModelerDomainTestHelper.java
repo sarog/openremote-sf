1f9646c308eb958b68b4b8ab7c45e8f2812baf12
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-06-25 07:56:45 +0000

    MODELER-498: Refactored common code to setup test in helper class

---
+++ workspace/ebariaux/Designer_2_14_0_Preview-453-413-470-437-287-452-450-405-480-496_MODELER-498/test/org/openremote/modeler/domain/ModelerDomainTestHelper.java
@@ -0,0 +1,44 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2013, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.modeler.domain;
+
+import org.openremote.modeler.client.utils.IDUtil;
+import org.openremote.modeler.domain.component.UIComponent;
+
+/**
+ * When setting up object model (and validating results) for tests, same code
+ * comes over and over again.
+ * It is centralized in this helper class to avoid duplication in individual test classes.
+ * 
+ * @author <a href = "mailto:eric@openremote.org">Eric Bariaux</a>
+ */
+public class ModelerDomainTestHelper {
+
+  public static Absolute createAbsolute(int baseValue, UIComponent uiComponent) {
+    Absolute abs = new Absolute(IDUtil.nextID());
+    abs.setPosition(baseValue, baseValue + 1);
+    abs.setSize(baseValue + 2, baseValue + 3);
+    abs.setUiComponent(uiComponent);
+    return abs;
+  }
+
+
+}
