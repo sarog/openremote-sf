6ff1fb10b01043a68e665822acfc3c39896c1078
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-07-22 11:38:08 +0000

    reformat to match the current coding style convention

--- workspace/juha/Controller_2_1_0_FM_ORCJAVA-380/src/org/openremote/controller/protocol/infrared/IRCommandBuilder.java
+++ workspace/juha/Controller_2_1_0_FM_ORCJAVA-380/src/org/openremote/controller/protocol/infrared/IRCommandBuilder.java
@@ -1,22 +1,23 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2013, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.protocol.infrared;
 
 import java.util.List;
@@ -33,31 +34,49 @@ import org.openremote.controller.utils.CommandUtil;
  * 
  * @author Dan 2009-4-3
  */
-public class IRCommandBuilder implements CommandBuilder {
+public class IRCommandBuilder implements CommandBuilder
+{
 
-   /**
-    * {@inheritDoc}
-    */
-   @SuppressWarnings("unchecked")
-   public Command build(Element element) {
-      IRCommand irCommand = new IRCommand();
-      String command = "";
-      List<Element> propertyEles = element.getChildren("property", element.getNamespace());
-      String remotename = "";
-      for(Element ele : propertyEles){
-         if("remotename".equals(ele.getAttributeValue("name"))){
-            remotename = ele.getAttributeValue("value");
-         } else if ("command".equals(ele.getAttributeValue("name"))) {
-            command = ele.getAttributeValue("value");
-         }
+  /**
+   * {@inheritDoc}
+   */
+  @SuppressWarnings("unchecked")
+  public Command build(Element element)
+  {
+    IRCommand irCommand = new IRCommand();
+    String command = "";
+    List<Element> propertyEles = element.getChildren("property", element.getNamespace());
+    String remotename = "";
+
+    for(Element ele : propertyEles)
+    {
+      if("remotename".equals(ele.getAttributeValue("name")))
+      {
+        remotename = ele.getAttributeValue("value");
       }
-      if ("".equals(command.trim()) || "".equals(remotename.trim())) {
-         throw new CommandBuildException("Cannot build a IREvent with empty property : command=" + command + ",remotename=" + remotename);
-      } else {
-         irCommand.setCommand(CommandUtil.parseStringWithParam(element, command));
-         irCommand.setRemotename(remotename);
+
+      else if ("command".equals(ele.getAttributeValue("name")))
+      {
+        command = ele.getAttributeValue("value");
       }
-      return irCommand;
-   }
+    }
+
+    if ("".equals(command.trim()) || "".equals(remotename.trim()))
+    {
+        throw new CommandBuildException(
+            "Cannot build a IREvent with empty property : command=" +  command +
+            ",remotename=" + remotename
+        );
+    }
+
+    else
+    {
+       irCommand.setCommand(CommandUtil.parseStringWithParam(element, command));
+       irCommand.setRemotename(remotename);
+    }
+
+    return irCommand;
+  }
 
 }
+
