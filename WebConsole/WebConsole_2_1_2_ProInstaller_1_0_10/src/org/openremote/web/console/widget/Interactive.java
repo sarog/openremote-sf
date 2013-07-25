c96b29cb1379b8f06706fa2c777fec4a43d3dcc8
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-07-25 12:48:39 +0000

    ORCJAVA-378: tag for inclusion in Pro release

---
+++ WebConsole/WebConsole_2_1_2_ProInstaller_1_0_10/src/org/openremote/web/console/widget/Interactive.java
@@ -0,0 +1,41 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.web.console.widget;
+
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
+import com.google.gwt.event.dom.client.TouchEndEvent;
+import com.google.gwt.event.dom.client.TouchEndHandler;
+import com.google.gwt.event.dom.client.TouchStartEvent;
+import com.google.gwt.event.dom.client.TouchStartHandler;
+/**
+ * 
+ *  
+ * @author <a href="mailto:richard@openremote.org">Richard Turner</a>
+ */
+public interface Interactive extends MouseDownHandler, TouchStartHandler, MouseUpHandler, MouseOutHandler, TouchEndHandler {
+	public void onTouchStart(TouchStartEvent event);
+	public void onMouseDown(MouseDownEvent event);
+	public void onTouchEnd(TouchEndEvent event);
+	public void onMouseUp(MouseUpEvent event);
+}
