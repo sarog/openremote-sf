eedb521a4165279e90632ad4915f0671f585c6b9
digitaldan <digitaldan@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-10-03 02:11:34 +0000

    Areas and Security now implemented, zones now have status strings

--- workspace/digitaldan/Controller_2_1_0_FM_20130617_HAI/src/org/openremote/controller/protocol/omnilink/model/Area.java
+++ workspace/digitaldan/Controller_2_1_0_FM_20130617_HAI/src/org/openremote/controller/protocol/omnilink/model/Area.java
@@ -5,11 +5,19 @@ import org.openremote.controller.protocol.omnilink.OmniLinkCmd;
 import com.digitaldan.jomnilinkII.MessageTypes.properties.AreaProperties;
 
 public class Area extends OmnilinkDevice {
-
-   private AreaProperties properties;
+   public static final String[] omniText = {"Off", "Day", "Night", "away", 
+      "Vacation", "Day Instant", "Night Delayed"};
+   public static final String[] luminaText = {"Off", "Home", "Sleep", "Away", 
+      "Vacation","Party", "Special"};
+   public static final String[] alarms = {"Burglary", "Fire", "Gas", 
+      "Auxiliary","Freeze","Water", "Duress","Temperature"};
    
-   public Area(AreaProperties properties) {
+   private AreaProperties properties;
+   private boolean omni;
+   
+   public Area(AreaProperties properties, boolean omni) {
       this.properties = properties;
+      this.omni = omni;
    }
 
    public AreaProperties getProperties() {
@@ -19,11 +27,28 @@ public class Area extends OmnilinkDevice {
    public void setProperties(AreaProperties properties) {
       this.properties = properties;
    }
+   
+
+   public boolean isOmni() {
+      return omni;
+   }
+
+   public void setOmni(boolean omni) {
+      this.omni = omni;
+   }
 
    @Override
    public void updateSensors() {
-      if (sensors.get(OmniLinkCmd.SENSOR_AREA_STATUS) != null) {
-         sensors.get(OmniLinkCmd.SENSOR_AREA_STATUS).update(properties.getMode() + "");
+      
+      if (sensors.get(OmniLinkCmd.SENSOR_AREA_STATUS_MODE) != null) {
+         String txtArray[] = omni ? omniText : luminaText;
+         String text = properties.getMode() < txtArray.length ? 
+               txtArray[properties.getMode()] : "Unknown";
+         sensors.get(OmniLinkCmd.SENSOR_AREA_STATUS_MODE).update(text);
+      }
+      if (sensors.get(OmniLinkCmd.SENSOR_AREA_STATUS_ALARM) != null) {
+         String text = properties.getAlarms() < alarms.length ? alarms[properties.getAlarms()] : "Unknown";
+         sensors.get(OmniLinkCmd.SENSOR_AREA_STATUS_ALARM).update(text);
       }
    }
 
