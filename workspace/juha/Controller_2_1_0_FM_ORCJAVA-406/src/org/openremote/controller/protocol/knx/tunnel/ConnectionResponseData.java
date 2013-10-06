49ed49456d5bbec5b14456cc445a5cbbcca19c0c
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-10-06 10:32:00 +0000

    Merge R9031-R9043 from /workspace/juha/Controller_2_1_0_FM_KNXRollback:
      R9031: A *sub-tree* merge of /test/
      
             Bringing in test suite updates from /workspace/juha/Controller_2_1_0_FM_KNXUpdates branch
     
               R9025 -- complete the test for first exponent negative range value conversions 
                      between Java float -> KNX float byte format -> Java decimal
               R9026 -- javadoc for the test
               R9027 -- clean up and finalize the current tests
    
    
      R9032: Merge updates to IpMessage.java class only (sub-tree merge) from 
             /worspace/juha/Controller_2_1_0_FM_KNXUpdates/src/org/openremote/controller/
             protocol/knx/ip/message/IpMessage.java -- revisions R8598, R8600 and R8619
    
      R9033: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/ip/message/Hpai.java
             and /test/org/openremote/controller/protocol/knx/ip/message/IpDiscoverReq.java
             (R8598 and R8600)
    
      R9034: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/IndividualAddress.java
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch (R8598 and R8600)
    
      R9035: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/ip/message/IpDiscoverResp.java
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch (R8598, R8600 and R8602)
    
      R9036: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/ip/message/IpConnectReq.java
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch (R8598 and R8602)
    
      R9037: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/ip/message/IpConnectResp.java
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch (R8598, R8600 and R8602)
    
      R9038: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/ip/message/IpConnectionStateReq.java
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch (R8598 and R8602)
    
      R9039: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/ip/message/IpConnectionStateResp.java
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch (R8598 and R8602)
    
      R9040: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/ip/message/IpTunnelingReq.java
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch (R8598, R8602 and R8619)
    
      R9041: A *sub-tree* merge of /test/org/openremote/controller/protocol/knx/GroupAddress.java
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch (R8598 and R8619)
    
      R9042: A *sub-tree* merge of /src/org/openremote/controller/protocol/knx/dib
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch -- adds
             DeviceInformationBlocks (DIBs)
    
      R9043: A *sub-tree* merge of /src/org/openremote/controller/protocol/knx/tunnel
             from /workspace/juha/Controller_2_1_0_FM_KNXUpdated branch -- KNX IP 
             tunneling related data classes

---
+++ workspace/juha/Controller_2_1_0_FM_ORCJAVA-406/src/org/openremote/controller/protocol/knx/tunnel/ConnectionResponseData.java
@@ -0,0 +1,104 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.knx.tunnel;
+
+import org.openremote.controller.protocol.knx.IndividualAddress;
+import org.openremote.controller.protocol.knx.ip.message.IpConnectReq;
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
+ */
+public class ConnectionResponseData
+{
+
+  // Constants ------------------------------------------------------------------------------------
+
+  public final static int KNXNET_IP_10_TUNNELING_CRD_SIZE = 0x04;
+
+
+  // Instance Fields ------------------------------------------------------------------------------
+
+  private IndividualAddress individualAddress;
+
+
+  // Constructors ---------------------------------------------------------------------------------
+
+  public ConnectionResponseData(IndividualAddress address)
+  {
+    this.individualAddress = address;
+  }
+
+  public ConnectionResponseData(byte[] crd)
+  {
+    if (crd[0] != KNXNET_IP_10_TUNNELING_CRD_SIZE)
+    {
+      throw new IllegalArgumentException(
+          "Expected Connection Response Data (CRD) size " + KNXNET_IP_10_TUNNELING_CRD_SIZE +
+          ", got " + crd[0]
+      );
+    }
+
+    if (crd[1] != IpConnectReq.ConnectionType.TUNNEL_CONNECTION.getValue())
+    {
+      try
+      {
+        IpConnectReq.ConnectionType connType = IpConnectReq.ConnectionType.resolve(crd[1]);
+
+        throw new IllegalArgumentException(
+            "Attempted to construct tunneling connection response data structure with " +
+            "tunnel connection type " + connType
+        );
+      }
+
+      catch (IpConnectReq.UnknownConnectionTypeException e)
+      {
+        throw new IllegalArgumentException("Unknown connection type : " + e.getMessage());
+      }
+    }
+
+    int addrHiByte = crd[2];
+    int addrLoByte = crd[3];
+    int address = (addrHiByte & 0xFF) << 8;
+    address += addrLoByte & 0xFF;
+
+    this.individualAddress = new IndividualAddress(address);
+  }
+
+  
+  // Instance Methods -----------------------------------------------------------------------------
+
+  public byte[] getFrameStructure()
+  {
+    byte[] struct = new byte[KNXNET_IP_10_TUNNELING_CRD_SIZE];
+    byte[] addr = individualAddress.getAddress();
+
+    struct[0] = KNXNET_IP_10_TUNNELING_CRD_SIZE;
+    struct[1] = IpConnectReq.ConnectionType.TUNNEL_CONNECTION.getValue();
+    struct[2] = addr[0];
+    struct[3] = addr[1];
+
+    return struct;
+  }
+
+}
+
