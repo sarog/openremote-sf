16e2f755c930b962f664a806ce6b9e9ed9859f2c
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-10-07 15:03:40 +0000

    Merged ORCJAVA-381, r9090:9133

--- workspace/ebariaux/Controller_Pro1_0_11/src/org/openremote/controller/protocol/samsungtv/SamsungTVRemoteCommandBuilder.java
+++ workspace/ebariaux/Controller_Pro1_0_11/src/org/openremote/controller/protocol/samsungtv/SamsungTVRemoteCommandBuilder.java
@@ -1,117 +1,67 @@
-/* OpenRemote, the Home of the Digital Home.
-* Copyright 2008-2011, OpenRemote Inc.
-*
-* See the contributors.txt file in the distribution for a
-* full listing of individual contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2013, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.protocol.samsungtv;
 
-import java.util.Hashtable;
-import java.util.List;
-
 import org.jdom.Element;
-import org.openremote.controller.Constants;
 import org.openremote.controller.command.Command;
 import org.openremote.controller.command.CommandBuilder;
-import org.openremote.controller.exception.NoSuchCommandException;
-import org.openremote.controller.utils.Logger;
+import org.openremote.controller.service.Deployer;
 
 /**
- * Builds a SamsungTVRemoteCommand which can be used to control Samsung TV's through LAN
- * 
- * @author Marcus Redeker
- * @author Ivan Martinez
+ * This is a dummy placeholder for a Samsung TV implementation.
+ *
+ * @author <a href="mailto:eric@openremote.org">Eric Bariaux</a>
  */
-public class SamsungTVRemoteCommandBuilder implements CommandBuilder {
+public class SamsungTVRemoteCommandBuilder implements CommandBuilder
+{
 
-   // Constants ------------------------------------------------------------------------------------
+  private static boolean hasAnnounced = false;
 
-   public final static String SAMSUNG_TV_PROTOCOL_LOG_CATEGORY = Constants.CONTROLLER_PROTOCOL_LOG_CATEGORY + "SamsungTV";
+  public SamsungTVRemoteCommandBuilder(Deployer d)
+  {
 
-   private final static String STR_ATTRIBUTE_NAME_KEY_CODE = "keyCode";
-   private final static String STR_ATTRIBUTE_NAME_IP_ADDRESS = "ipAddress";
-   
-   private final static int SAMSUNG_TV_REMOTE_CONTROL_PORT = 55000;
-   private final static String SAMSUNG_TV_REMOTE_APPLICATION_NAME = "OpenRemote";
+    if (!hasAnnounced && System.getProperty("samsungTV.silent") == null)
+    {
+      System.out.println(
+          "\n\n" +
+          "******************************************************************************\n\n" +
+          "  The license of some of the source code used to support \n" +
+          "  Samsung TV IP control prevents its distribution in OpenRemote Professional. \n" +
+          "" +
+          "  To use Samsung TV control, download a freely available binary from \n" +
+          "  http://sourceforge.net/projects/openremote/files/samsung and follow the \n" +
+          "  installation instructions at http://www.openremote.org/x/-4YoAQ \n\n" +
+          "" +
+          "******************************************************************************\n\n"
+      );
 
-   // Class Members --------------------------------------------------------------------------------
+      hasAnnounced = true;
+    }
+  }
 
-   private final static Logger logger = Logger.getLogger(SAMSUNG_TV_PROTOCOL_LOG_CATEGORY);
+  @Override public Command build(Element el)
+  {
+    // no-op
 
-   // Instance Fields ------------------------------------------------------------------------------
-   
-   private String samsungTVIp;
-   // cached sessions for reusing session objects for different commands...
-   private Hashtable<String, SamsungTVSession> sessions = new Hashtable<String, SamsungTVSession>();
-   
-   // Constructors ---------------------------------------------------------------------------------
-   
+    return null;
+  }
 
-   public SamsungTVRemoteCommandBuilder(String samsungTVIp) {
-      this.samsungTVIp = samsungTVIp;
-   }
-   
-   
-   // Implements CommandBuilder --------------------------------------------------------------------
-   
-   /**
-    * {@inheritDoc}
-    */
-   @SuppressWarnings("unchecked")
-   public Command build(Element element) {
-      logger.debug("Building Samsung TV command");
-      List<Element> propertyEles = element.getChildren("property", element.getNamespace());
-      String keyCode = null;
-      String ip = samsungTVIp;
-
-      // read values from config xml
-
-      for (Element ele : propertyEles) {
-         String elementName = ele.getAttributeValue(CommandBuilder.XML_ATTRIBUTENAME_NAME);
-         String elementValue = ele.getAttributeValue(CommandBuilder.XML_ATTRIBUTENAME_VALUE);
-
-         if (STR_ATTRIBUTE_NAME_KEY_CODE.equals(elementName)) {
-            keyCode = elementValue;
-            logger.debug("Samsung TV Command: keyCode = " + keyCode);
-         } else if (STR_ATTRIBUTE_NAME_IP_ADDRESS.equals(elementName)) {
-            ip = elementValue;
-            logger.debug("Samsung TV Command: ipAddress = " + ip);
-         }
-      }
-
-      if (null == keyCode) {
-         throw new NoSuchCommandException("Samsung TV command must have a 'keyCode' property.");
-      }
-
-      try {
-         Key key = Key.valueOf(keyCode);
-         logger.debug("Samsung TV Command created successfully");
-         return new SamsungTVCommand(getSession(ip), key);
-
-      } catch (Exception e) {
-         throw new NoSuchCommandException("Unable to create SamsungTVCommand keyCode: " + keyCode, e);
-      }
-      
-   }
-   
-   private SamsungTVSession getSession(String ip) {
-      if (!sessions.contains(ip)) {
-         sessions.put(ip, new SamsungTVSession(ip, SAMSUNG_TV_REMOTE_CONTROL_PORT, SAMSUNG_TV_REMOTE_APPLICATION_NAME));
-      }
-      return sessions.get(ip);
-   }
-   
 }
