1435ec8192257057859c970a949c9c04eafb14a1
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-10-16 14:45:53 +0000

    work branch for 1-wire updates

---
+++ workspace/tkucharski/Controller_2_1_0_FM_20130617_OneWire/src/org/openremote/controller/protocol/enocean/EspVersion.java
@@ -0,0 +1,20 @@
+
+package org.openremote.controller.protocol.enocean;
+
+/**
+ * EnOcean serial protocol (ESP) version type.
+ *
+ * @author <a href="mailto:rainer@openremote.org">Rainer Hitz</a>
+ */
+public enum EspVersion
+{
+  /**
+   * EnOcean serial protocol version 2.0
+   */
+  ESP2,
+
+  /**
+   * EnOcean serial protocol version 3.0
+   */
+  ESP3
+}
