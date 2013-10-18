98b0e11b4e7132bf123a067ede98c333e22df93c
tom-kucharski <tom-kucharski@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-10-18 20:31:35 +0000

    License info at the top of every source code file

--- workspace/tkucharski/Controller_2_1_0_FM_20130617_OneWire/src/org/openremote/controller/protocol/onewire/command/OneWireSwitchSensor.java
+++ workspace/tkucharski/Controller_2_1_0_FM_20130617_OneWire/src/org/openremote/controller/protocol/onewire/command/OneWireSwitchSensor.java
@@ -1,3 +1,23 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.protocol.onewire.command;
 
 import java.util.Map;
@@ -8,8 +28,7 @@ import org.openremote.controller.utils.Logger;
 import org.springframework.security.util.FieldUtils;
 
 /**
- * @author Tomasz Kucharski <kucharski.tom@gmail.com>
- * @since 11.04.13 23:33
+ * @author Tom Kucharski <kucharski.tom@gmail.com>
  */
 public class OneWireSwitchSensor {
 	private final static Logger logger = OneWireLoggerFactory.getLogger();
