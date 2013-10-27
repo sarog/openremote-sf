0eece1f7ef06ed2d7daa033ee8fe240a6b30f813
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-10-27 23:30:55 +0000

    add utility methods to parse application layer service types from CEMI frames (ORCJAVA-406)

--- workspace/juha/Controller_2_1_0_FM_KNXRollback/src/org/openremote/controller/protocol/knx/ApplicationLayer.java
+++ workspace/juha/Controller_2_1_0_FM_KNXRollback/src/org/openremote/controller/protocol/knx/ApplicationLayer.java
@@ -1,6 +1,6 @@
 /*
  * OpenRemote, the Home of the Digital Home.
- * Copyright 2008-2011, OpenRemote Inc.
+ * Copyright 2008-2013, OpenRemote Inc.
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
@@ -20,13 +20,30 @@
  */
 package org.openremote.controller.protocol.knx;
 
+
 /**
- * Represents Data Link layer in KNX communication stack.
+ * Represents application layer in KNX communication stack.
  *
  * @author <a href="mailto:juha@openremote.org">Juha Lindfors</a>
  */
 class ApplicationLayer 
 {
+
+  // Constants ------------------------------------------------------------------------------------
+
+  /**
+   * Bitmask to extract APCI bits from the CEMI TPCI/APCI control information field.
+   */
+  private final static int CEMI_TPCI_APCI_FIELD_BITMASK = 0x03;
+
+  /**
+   * Bitmask to extract APCI bits from the CEMI APCI/Data field.
+   */
+  private final static int CEMI_APCI_DATA_FIELD_BITMASK = 0xC0;
+
+
+
+  // Enums ----------------------------------------------------------------------------------------
 
   /**
    * Transport layer and application layer control information (TPCI & APCI) for application
@@ -199,6 +216,203 @@ class ApplicationLayer
       this.apciLoBits = apciLoBits;
     }
 
+
+    // Enum Class Methods -------------------------------------------------------------------------
+
+    /**
+     * Resolves the application layer {@link Service} from the TPCI/APCI and APCI/Data bytes
+     * in a common EMI frame.
+     *
+     * @param dataLength      the data length of the CEMI frame -- should be value of 1 for
+     *                        six bit payloads, two or higher for 8-bit and above payloads.
+     *
+     * @param cemiTpciApci    the first byte in the CEMI frame after the data length field
+     *                        which is the TPCI/APCI control information field
+     *
+     * @param cemiApciData    the second byte in the CEMI frame after the data length field
+     *                        which is the APCI (if 8-bit or above data payload) or
+     *                        APCI + six bit data in case of six bit data payloads
+     *
+     * @return                the service type identifier
+     *
+     * @throws CommonEMI.InvalidFrameException  if the CEMI bytes cannot be parsed
+     */
+    protected static Service resolveCEMIServiceType(int dataLength,
+                                                    int cemiTpciApci,
+                                                    int cemiApciData)
+        throws CommonEMI.InvalidFrameException
+    {
+      // IMPLEMENTATION NOTE:
+      //
+      //   We do need the data length to determine the difference between 6-bit and above
+      //   six bit payloads -- otherwise the difference between a six bit value with data
+      //   of 0x00 is not distinguishable from 8-bit or higher data payloads from the TPCI/APCI
+      //   and APCI/Data bytes alone.
+
+      if (dataLength <= 0)
+      {
+        throw new CommonEMI.InvalidFrameException("CEMI data length is less than 1: {0}", dataLength);
+      }
+
+      if (isSixBitGroupValueResponse(dataLength, cemiTpciApci, cemiApciData))
+      {
+        return GROUPVALUE_RESPONSE_6BIT;
+      }
+
+      else if (isGroupValueResponse(dataLength, cemiTpciApci, cemiApciData))
+      {
+        return GROUPVALUE_RESPONSE;
+      }
+
+      else if (isGroupValueRead(dataLength, cemiTpciApci, cemiApciData))
+      {
+        return GROUPVALUE_READ;
+      }
+
+      else if (isSixBitGroupValueWrite(dataLength, cemiTpciApci, cemiApciData))
+      {
+        return GROUPVALUE_WRITE_6BIT;
+      }
+
+      else if (isGroupValueWrite(dataLength, cemiTpciApci, cemiApciData))
+      {
+        return GROUPVALUE_WRITE;
+      }
+
+      else
+      {
+        throw new CommonEMI.InvalidFrameException(
+            "Common EMI frame structure error. Could not resolve the CEMI " +
+            "frame to any known application layer service. "
+        );
+      }
+    }
+
+    /**
+     * Parses the CEMI data length, TPCI/APCI and APCI/Data fields to determine whether the
+     * given frame byte values represent a six bit group value response service.
+     *
+     * @param dataLength    the data length byte in CEMI frame
+     * @param tpciApci      the TPCI/APCI byte in CEMI frame
+     * @param apciData      the APCI/Data byte in CEMI frame
+     *
+     * @return    true if the CEMI frame values represent a six bit group value response service;
+     *            false otherwise
+     */
+    protected static boolean isSixBitGroupValueResponse(int dataLength, int tpciApci, int apciData)
+    {
+      // IMPLEMENTATION NOTE:
+      //
+      //   We do need the data length to determine the difference between 6-bit vs larger
+      //   payloads -- otherwise the difference between a six bit value with data of 0x00 is
+      //   not distinguishable from 8-bit or higher data payloads from the TPCI/APCI
+      //   and APCI/Data bytes alone.
+
+      int cemiAPCIHiBits = tpciApci & CEMI_TPCI_APCI_FIELD_BITMASK;
+      int cemiAPCILoBits = apciData & CEMI_APCI_DATA_FIELD_BITMASK;
+
+      return
+          dataLength == 1 &&
+          cemiAPCIHiBits == GROUPVALUE_RESPONSE_6BIT.getTPCIAPCI() &&
+          cemiAPCILoBits == GROUPVALUE_RESPONSE_6BIT.getAPCIData();
+    }
+
+    /**
+     * Parses the CEMI data length, TPCI/APCI and APCI/Data fields to determine whether the
+     * given frame byte values represent a six bit group value write service.
+     *
+     * @param dataLength    the data length byte in CEMI frame
+     * @param tpciApci      the TPCI/APCI byte in CEMI frame
+     * @param apciData      the APCI/Data byte in CEMI frame
+     *
+     * @return    true if the CEMI frame values represent a six bit group value write service;
+     *            false otherwise
+     */
+    protected static boolean isSixBitGroupValueWrite(int dataLength, int tpciApci, int apciData)
+    {
+      // IMPLEMENTATION NOTE:
+      //
+      //   We do need the data length to determine the difference between 6-bit vs larger
+      //   payloads -- otherwise the difference between a six bit value with data of 0x00 is
+      //   not distinguishable from 8-bit or higher data payloads from the TPCI/APCI
+      //   and APCI/Data bytes alone.
+
+      int cemiAPCIHiBits = tpciApci & CEMI_TPCI_APCI_FIELD_BITMASK;
+      int cemiAPCILoBits = apciData & CEMI_APCI_DATA_FIELD_BITMASK;
+
+      return
+          dataLength == 1 &&
+          cemiAPCIHiBits == GROUPVALUE_WRITE_6BIT.getTPCIAPCI() &&
+          cemiAPCILoBits == GROUPVALUE_WRITE.getAPCIData();
+    }
+
+    /**
+     * Parses the CEMI data length, TPCI/APCI and APCI/Data fields to determine whether the
+     * given frame byte values represent a group value read service.
+     *
+     * @param dataLength    the data length byte in CEMI frame
+     * @param tpciApci      the TPCI/APCI byte in CEMI frame
+     * @param apciData      the APCI/Data byte in CEMI frame
+     *
+     * @return    true if the CEMI frame values represent a six bit group value read service;
+     *            false otherwise
+     */
+    protected static boolean isGroupValueRead(int dataLength, int tpciApci, int apciData)
+    {
+      int cemiAPCIHiBits = tpciApci & CEMI_TPCI_APCI_FIELD_BITMASK;
+      int cemiAPCILoBits = apciData & CEMI_APCI_DATA_FIELD_BITMASK;
+
+      return
+          dataLength == 1 &&
+          cemiAPCIHiBits == GROUPVALUE_READ.getTPCIAPCI() &&
+          cemiAPCILoBits == GROUPVALUE_READ.getAPCIData();
+    }
+
+    /**
+     * Parses the CEMI data length, TPCI/APCI and APCI/Data fields to determine whether the
+     * given frame byte values represent a group value response (not 6-bit) service.
+     *
+     * @param dataLength    the data length byte in CEMI frame
+     * @param tpciApci      the TPCI/APCI byte in CEMI frame
+     * @param apciData      the APCI/Data byte in CEMI frame
+     *
+     * @return    true if the CEMI frame values represent a group value response service;
+     *            false otherwise
+     */
+    protected static boolean isGroupValueResponse(int dataLength, int tpciApci, int apciData)
+    {
+      int cemiAPCIHiBits = tpciApci & CEMI_TPCI_APCI_FIELD_BITMASK;
+      int cemiAPCILoBits = apciData & CEMI_APCI_DATA_FIELD_BITMASK;
+
+      return
+          dataLength > 1 &&
+          cemiAPCIHiBits == GROUPVALUE_RESPONSE.getTPCIAPCI() &&
+          cemiAPCILoBits == GROUPVALUE_RESPONSE.getAPCIData();
+    }
+
+    /**
+     * Parses the CEMI data length, TPCI/APCI and APCI/Data fields to determine whether the
+     * given frame byte values represent a six bit group value write (not 6-bit) service.
+     *
+     * @param dataLength    the data length byte in CEMI frame
+     * @param tpciApci      the TPCI/APCI byte in CEMI frame
+     * @param apciData      the APCI/Data byte in CEMI frame
+     *
+     * @return    true if the CEMI frame values represent a group value write service;
+     *            false otherwise
+     */
+    protected static boolean isGroupValueWrite(int dataLength, int tpciApci, int apciData)
+    {
+      int cemiAPCIHiBits = tpciApci & CEMI_TPCI_APCI_FIELD_BITMASK;
+      int cemiAPCILoBits = apciData & CEMI_APCI_DATA_FIELD_BITMASK;
+
+      return
+          dataLength > 1 &&
+          cemiAPCIHiBits == GROUPVALUE_WRITE.getTPCIAPCI() &&
+          cemiAPCILoBits == GROUPVALUE_WRITE.getAPCIData();
+    }
+
+
     // Enum Instance Methods ----------------------------------------------------------------------
 
     /**
@@ -215,7 +429,7 @@ class ApplicationLayer
      *
      * @return TPCI/APCI byte (8 bits) in Common EMI frame
      */
-    int getTPCIAPCI()
+    protected int getTPCIAPCI()
     {
       return apciHiBits;
     }
@@ -238,7 +452,7 @@ class ApplicationLayer
      *
      * @return APCI/Data byte (8 bits) in Common EMI frame
      */
-    int getAPCIData()
+    protected int getAPCIData()
     {
       return apciLoBits;
     }
