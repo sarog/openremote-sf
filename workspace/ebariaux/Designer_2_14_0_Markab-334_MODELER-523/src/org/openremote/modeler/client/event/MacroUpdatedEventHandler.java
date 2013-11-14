66effb47ec8efb081f994a99caa0c436c2d0c70f
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-11-14 15:31:51 +0000

    Did not start from correct branch

--- workspace/ebariaux/Designer_2_14_0_Markab-334_MODELER-523/src/org/openremote/modeler/client/event/MacroUpdatedEventHandler.java
+++
@@ -1,9 +0,0 @@
-package org.openremote.modeler.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface MacroUpdatedEventHandler extends EventHandler {
-
-  void onMacroUpdated(MacroUpdatedEvent event);
-  
-}
