6df60de1d2478c998e6cd96843ebc95f2faf44d3
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-11-20 15:19:30 +0000

    ORCJAVA-410: Organized imports

--- workspace/ebariaux/Controller_Pro1_0_11_ORCJAVA-410/src/org/openremote/controller/protocol/marantz_avr/MarantzAVRGateway.java
+++ workspace/ebariaux/Controller_Pro1_0_11_ORCJAVA-410/src/org/openremote/controller/protocol/marantz_avr/MarantzAVRGateway.java
@@ -21,10 +21,8 @@
 package org.openremote.controller.protocol.marantz_avr;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
