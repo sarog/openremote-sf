8ed61f2818895284aa4aebc0fa73fcf93d222311
melchoir55 <melchoir55@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-12-03 22:36:57 +0000

    Made doorlock methods and enums specific to doorlock, rather than coopting those used by switch.

--- workspace/melchoir55/Pro_Controller_1_0_11_VeraUpdates/src/org/openremote/controller/protocol/vera/model/DoorLock.java
+++ workspace/melchoir55/Pro_Controller_1_0_11_VeraUpdates/src/org/openremote/controller/protocol/vera/model/DoorLock.java
@@ -7,43 +7,43 @@ import org.openremote.controller.protocol.vera.VeraCmd;
 
 public class DoorLock extends VeraDevice {
 
-   protected Boolean status;
+   protected Boolean lockStatus;
    
    public DoorLock(VeraCategory category, int id, String name, VeraClient client) {
       super(category, id, name, client);
    }
 
-   public void turnOn() {
+   public void lock() {
       StringBuffer cmdUrl = new StringBuffer();
       cmdUrl.append("http://");
       cmdUrl.append(client.getAddress());
       cmdUrl.append(":3480/data_request?id=lu_action&output_format=xml&DeviceNum=");
       cmdUrl.append(id);
-      cmdUrl.append("&serviceId=urn:micasaverde-com:serviceId:DoorLock1&action=SetTarget&newTargetValue=1");
+      cmdUrl.append("&serviceId=urn:micasaverde-com:serviceId:DoorLock1&action=SetTarget&Target=1");
       getClient().sendCommand(cmdUrl.toString());
    }
 
-   public void turnOff() {
+   public void unlock() {
       StringBuffer cmdUrl = new StringBuffer();
       cmdUrl.append("http://");
       cmdUrl.append(client.getAddress());
       cmdUrl.append(":3480/data_request?id=lu_action&output_format=xml&DeviceNum=");
       cmdUrl.append(id);
-      cmdUrl.append("&serviceId=urn:micasaverde-com:serviceId:DoorLock1&action=SetTarget&newTargetValue=0");
+      cmdUrl.append("&serviceId=urn:micasaverde-com:serviceId:DoorLock1&action=SetTarget&Target=0");
       getClient().sendCommand(cmdUrl.toString());
    }
 
    @Override
    protected void updateDeviceSpecificSensors() {
-      if ((attachedSensors.get(VeraCmd.GET_STATUS) != null) && (status != null)) {
-         attachedSensors.get(VeraCmd.GET_STATUS).update(status?"on":"off");
+      if ((attachedSensors.get(VeraCmd.GET_LOCK_STATUS) != null) && (lockStatus != null)) {
+         attachedSensors.get(VeraCmd.GET_LOCK_STATUS).update(lockStatus?"locked":"unlocked");
       }
    }
 
    @Override
    protected void updateDeviceSpecificStatus(Element element) {
       if (element.getAttributeValue("status") != null) {
-         this.status = (element.getAttributeValue("status").equals("1"))?true:false;
+         this.lockStatus = (element.getAttributeValue("status").equals("1"))?true:false;
       }
    }
 
