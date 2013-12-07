dad945bf581b4aa58a150d953d17a19c7f3fe26f
digitaldan <digitaldan@9621b191-8951-0410-821f-b8f8a45b0b95>
2013-12-07 15:21:34 +0000

    Better thermostat support

--- workspace/digitaldan/Controller_2_1_0_FM_20130617_HAI/src/org/openremote/controller/protocol/omnilink/model/Thermostat.java
+++ workspace/digitaldan/Controller_2_1_0_FM_20130617_HAI/src/org/openremote/controller/protocol/omnilink/model/Thermostat.java
@@ -1,5 +1,7 @@
 package org.openremote.controller.protocol.omnilink.model;
 
+import org.openremote.controller.component.RangeSensor;
+import org.openremote.controller.model.sensor.Sensor;
 import org.openremote.controller.protocol.omnilink.OmniLinkCmd;
 
 import com.digitaldan.jomnilinkII.MessageUtils;
@@ -7,7 +9,7 @@ import com.digitaldan.jomnilinkII.MessageTypes.properties.ThermostatProperties;
 
 public class Thermostat extends OmnilinkDevice {
    private ThermostatProperties properties;
-   
+   private static String[] MODES = {"Off", "Heat", "Cool", "Auto", "Emergency Heat"};
    public Thermostat(ThermostatProperties properties) {
       this.properties = properties;
    }
@@ -47,7 +49,12 @@ public class Thermostat extends OmnilinkDevice {
          sensors.get(OmniLinkCmd.SENSOR_THERMO_HOLD_MODE).update(properties.isHold() ? "on" : "off");
       }
       if (sensors.get(OmniLinkCmd.SENSOR_THERMO_SYSTEM_MODE) != null) {
-         sensors.get(OmniLinkCmd.SENSOR_THERMO_SYSTEM_MODE).update(properties.getMode() + "");
+         Sensor sensor = sensors.get(OmniLinkCmd.SENSOR_THERMO_SYSTEM_MODE);
+         if(sensor instanceof RangeSensor){
+            sensor.update(properties.getMode() + "");
+         } else {
+            sensor.update(MODES[properties.getMode()]);
+         }
       }
    }
 
