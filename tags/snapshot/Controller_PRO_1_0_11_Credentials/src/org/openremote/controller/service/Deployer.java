ecf029083101b604a2aba8e38202cc5168711e44
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2014-01-29 20:41:31 +0000

    apply additional modifications as part of ORCJAVA-422 (via ORCJAVA-295) from /workspace/juha/Controller_2_1_0_FM_ORCJAVA-422 branch

--- tags/snapshot/Controller_PRO_1_0_11_Credentials/src/org/openremote/controller/service/Deployer.java
+++ tags/snapshot/Controller_PRO_1_0_11_Credentials/src/org/openremote/controller/service/Deployer.java
@@ -1901,12 +1901,14 @@ public class Deployer
         {
           synchronized (discoveredDevicesToAnnounce)
           {
-            ClientResource cr = new ClientResource(
-                controllerConfig.getBeehiveDeviceDiscoveryServiceRESTRootUrl() + "discoveredDevices"
-            );
+            ClientResource cr = null;
 
             try
             {
+              cr = new ClientResource(
+                  controllerConfig.getBeehiveDeviceDiscoveryServiceRESTRootUrl() + "discoveredDevices"
+              );
+
               String username = getUserName();
 
               if (username == null || username.equals(""))
@@ -1923,7 +1925,6 @@ public class Deployer
                 );
 
                 Representation result = cr.post(rep);
-                cr.release();
 
                 GenericResourceResultWithErrorMessage res = new JSONDeserializer<GenericResourceResultWithErrorMessage>()
                     .use(null, GenericResourceResultWithErrorMessage.class)
@@ -1943,6 +1944,14 @@ public class Deployer
             catch (Exception e)
             {
               log.error("Could not announce discovered devices", e);
+            }
+
+            finally
+            {
+              if (cr != null)
+              {
+                cr.release();
+              }
             }
           }
         }
