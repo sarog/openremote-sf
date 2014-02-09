c16a5fcc46f2e67a9109c91aa5e4c58316ef99c1
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2014-02-09 14:12:01 +0000

    add license and remove unused (and strange) imports

--- workspace/juha/Controller_2_1_0_FM_ORCJAVA-411/src/org/openremote/controller/service/BeehiveCommandCheckService.java
+++ workspace/juha/Controller_2_1_0_FM_ORCJAVA-411/src/org/openremote/controller/service/BeehiveCommandCheckService.java
@@ -1,13 +1,30 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2014, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.openremote.controller.service;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.Socket;
-import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.drools.lang.DRLParser.calendars_key_return;
 import org.openremote.controller.Constants;
 import org.openremote.controller.ControllerConfiguration;
 import org.openremote.controller.proxy.ControllerProxy;
@@ -25,6 +42,11 @@ import org.restlet.resource.ClientResource;
 
 import flexjson.JSONDeserializer;
 
+/**
+ * TODO
+ *
+ * @author Marcus Redeker
+ */
 public class BeehiveCommandCheckService {
 
    /**
