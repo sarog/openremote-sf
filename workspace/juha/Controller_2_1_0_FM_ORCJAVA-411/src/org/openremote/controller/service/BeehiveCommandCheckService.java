793ac8dccd71aea52e818ea4275fb61299d80716
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2014-02-10 06:52:59 +0000

    format according to our source code conventions

--- workspace/juha/Controller_2_1_0_FM_ORCJAVA-411/src/org/openremote/controller/service/BeehiveCommandCheckService.java
+++ workspace/juha/Controller_2_1_0_FM_ORCJAVA-411/src/org/openremote/controller/service/BeehiveCommandCheckService.java
@@ -46,182 +46,259 @@ import flexjson.JSONDeserializer;
  *
  * @author Marcus Redeker
  */
-public class BeehiveCommandCheckService {
+public class BeehiveCommandCheckService
+{
 
-   /**
-    * Common log category for startup logging, with a specific sub-category for the BeehiveCommandCheckService
-    */
-   private final static Logger log = Logger.getLogger(Constants.BEEHIVE_COMMAND_CHECKER_LOG_CATEGORY);
+  /**
+   * Common log category for startup logging, with a specific sub-category for the BeehiveCommandCheckService
+   */
+  private final static Logger log = Logger.getLogger(Constants.BEEHIVE_COMMAND_CHECKER_LOG_CATEGORY);
 
    
-   private ControllerConfiguration controllerConfig;
-   private ControllerDTO controllerDTO;
-   private BeehiveCommandChecker commandCheckerThread;
+  private ControllerConfiguration controllerConfig;
+  private ControllerDTO controllerDTO;
+  private BeehiveCommandChecker commandCheckerThread;
 
 
-   private Deployer deployer;
+  private Deployer deployer;
 
-   public BeehiveCommandCheckService(ControllerConfiguration controllerConfig) {
-      this.controllerConfig = controllerConfig;
-      this.commandCheckerThread = new BeehiveCommandChecker();
-   }
+  public BeehiveCommandCheckService(ControllerConfiguration controllerConfig)
+  {
+    this.controllerConfig = controllerConfig;
+    this.commandCheckerThread = new BeehiveCommandChecker();
+  }
 
-   public void setDeployer(Deployer deployer) {
-      this.deployer = deployer;
-   }
+  public void setDeployer(Deployer deployer)
+  {
+    this.deployer = deployer;
+  }
    
-   public void start(ControllerDTO controllerDTO) {
-      this.controllerDTO = controllerDTO;
-      commandCheckerThread.setRunning(true);
-      commandCheckerThread.start();
-   }
+  public void start(ControllerDTO controllerDTO)
+  {
+    this.controllerDTO = controllerDTO;
+    commandCheckerThread.setRunning(true);
+    commandCheckerThread.start();
+  }
    
-   public void stop() {
-      commandCheckerThread.setRunning(false);
-   }
+  public void stop()
+  {
+    commandCheckerThread.setRunning(false);
+  }
    
    
-   protected void initiateProxy(ControllerCommandDTO command)  {
-      Long id = command.getOid();
-      String url = command.getCommandParameter().get("url");
-      String token = command.getCommandParameter().get("token");
+  protected void initiateProxy(ControllerCommandDTO command)
+  {
+    Long id = command.getOid();
+    String url = command.getCommandParameter().get("url");
+    String token = command.getCommandParameter().get("token");
       
-      Socket beehiveSocket = null;
-      boolean needsAck = true;
-      try {
-         log.info("Connecting to beehive at "+url+" for proxy");
-         beehiveSocket = ControllerProxy.makeClientSocket(url, token, controllerConfig.getProxyTimeout());
-         // at this point the command should already have been marked as ack by the listening end at beehive
-         log.info("Connected to beehive");
-         needsAck = false;
-         // try to connect to it, see if it's still valid
-         String ip = controllerConfig.getWebappIp();
-         int port = controllerConfig.getWebappPort();
-         if (ip==null || ip.trim().length()==0) {
-            ip = "localhost";
-         }
-         if (port == 0) {
-            port = 8080;
-         }
-         ControllerProxy proxy = new ControllerProxy(beehiveSocket, ip, port, controllerConfig.getProxyTimeout());
-         log.info("Starting proxy");
-         proxy.start();
-      } catch (IOException e) {
-         log.info("Got exception while connecting to beehive", e);
-         if(beehiveSocket != null){
-            try {
-               beehiveSocket.close();
-            } catch (IOException e1) {
-               // ignore
-            }
-         }
-         // the server should have closed it, but let's help him to make sure
-         if(needsAck)
-            ackCommand(id);
-      }
-   }
+    Socket beehiveSocket = null;
 
-   private void ackCommand(Long id)  {
-      log.info("Acking command "+id);
-      ClientResource cr = null;
-      try {
-         cr = new ClientResource( controllerConfig.getBeehiveControllerCommandServiceRESTRootUrl() + "command/" + id);
+    boolean needsAck = true;
 
-         String username = deployer.getUserName();
+    try
+    {
+      log.info("Connecting to beehive at "+url+" for proxy");
+      beehiveSocket = ControllerProxy.makeClientSocket(url, token, controllerConfig.getProxyTimeout());
 
-         cr.setChallengeResponse(ChallengeScheme.HTTP_BASIC, username, deployer.getPassword(username));
+      // at this point the command should already have been marked as ack by the listening end at beehive
 
-         Representation r = cr.delete();
-         String str;
-         str = r.getText();
-         GenericResourceResultWithErrorMessage res = new JSONDeserializer<GenericResourceResultWithErrorMessage>().use(null, GenericResourceResultWithErrorMessage.class).use("result", String.class).deserialize(str);
-         if (res.getErrorMessage() != null) {
-            throw new RuntimeException(res.getErrorMessage());
-         }
-      } catch (Exception e) {
-         log.error("!!! Unable to ACK controller command with id: " + id, e);
-      } finally {
-         if (cr != null) {
-            cr.release();
-         }
-      }
-   }
-   
-   
-   private void unlinkController(ControllerCommandDTO controllerCommandDTO) {
-      this.stop();
-      this.controllerDTO = null;
-      this.deployer.unlinkController();
-   }   
-   
-   private void executeCommand(ControllerCommandDTO controllerCommandDTO) {
-      switch (controllerCommandDTO.getCommandTypeEnum()) {
-         case INITIATE_PROXY:
-            initiateProxy(controllerCommandDTO);
-            break;
-         case UNLINK_CONTROLLER:
-            unlinkController(controllerCommandDTO);
-            break;
-         default:
-            log.error("ControllerCommand not implemented yet: " + controllerCommandDTO.getCommandType());
-      }
-      
-   }
-   
-   private class BeehiveCommandChecker extends Thread {
-      
-      private boolean running;
-      
-      BeehiveCommandChecker() {
-         super("BeehiveCommandChecker");
+      log.info("Connected to beehive");
+      needsAck = false;
+
+      // try to connect to it, see if it's still valid
+
+      String ip = controllerConfig.getWebappIp();
+      int port = controllerConfig.getWebappPort();
+
+      if (ip==null || ip.trim().length()==0)
+      {
+        ip = "localhost";
       }
 
-      @SuppressWarnings("unchecked")
-      public void run() {
-         //As long as we are not linked to an account we periodically try to receive account info
-         int sleepTime = controllerConfig.getBeehiveCommandServiceCheckInterval();
-         Client c = new Client(new Context(), Protocol.HTTP);
-         while (isRunning()) {
-            ClientResource cr = null;
-            try {
-               cr = new ClientResource( controllerConfig.getBeehiveControllerCommandServiceRESTRootUrl() + "commands/" + controllerDTO.getOid());
-               cr.setNext(c);
-
-               String username = deployer.getUserName();
-
-               cr.setChallengeResponse(ChallengeScheme.HTTP_BASIC, username, deployer.getPassword(username));
-               Representation r = cr.get();
-               String str;
-               str = r.getText();
-               GenericResourceResultWithErrorMessage res = new JSONDeserializer<GenericResourceResultWithErrorMessage>().use(null, GenericResourceResultWithErrorMessage.class).use("result", ArrayList.class).use("result.values", ControllerCommandDTO.class).deserialize(str);
-               if (res.getErrorMessage() != null) {
-                  throw new RuntimeException(res.getErrorMessage());
-               }
-               List<ControllerCommandDTO> commands = (ArrayList<ControllerCommandDTO>)res.getResult();
-               if (!commands.isEmpty()) {
-                  executeCommand(commands.get(0));
-               }
-            } catch (Exception e) {
-               log.error("!!! Unable to check for new controller command from Beehive", e);
-            } finally {
-               if (cr != null) {
-                  cr.release();
-               }
-            }
-            try { Thread.sleep(sleepTime); } catch (InterruptedException e) {} //Let's wait 30 seconds
-         }
+      if (port == 0)
+      {
+        port = 8080;
       }
 
-      public boolean isRunning() {
-         return running;
-      }
-
-      public void setRunning(boolean running) {
-         this.running = running;
-      }
-      
-      
+      ControllerProxy proxy = new ControllerProxy(beehiveSocket, ip, port, controllerConfig.getProxyTimeout());
+      log.info("Starting proxy");
+      proxy.start();
     }
 
+    catch (IOException e)
+    {
+      log.info("Got exception while connecting to beehive", e);
 
+      if(beehiveSocket != null)
+      {
+        try
+        {
+          beehiveSocket.close();
+        }
+
+        catch (IOException e1)
+        {
+          // ignore
+        }
+      }
+
+      // the server should have closed it, but let's help him to make sure
+
+      if(needsAck)
+      {
+        ackCommand(id);
+      }
+    }
+  }
+
+
+  private void ackCommand(Long id)
+  {
+    log.info("Acking command "+id);
+    ClientResource cr = null;
+
+    try
+    {
+      cr = new ClientResource( controllerConfig.getBeehiveControllerCommandServiceRESTRootUrl() + "command/" + id);
+
+      String username = deployer.getUserName();
+
+      cr.setChallengeResponse(ChallengeScheme.HTTP_BASIC, username, deployer.getPassword(username));
+
+      Representation r = cr.delete();
+      String str;
+      str = r.getText();
+
+      GenericResourceResultWithErrorMessage res =
+          new JSONDeserializer<GenericResourceResultWithErrorMessage>()
+              .use(null, GenericResourceResultWithErrorMessage.class)
+              .use("result", String.class).deserialize(str);
+
+      if (res.getErrorMessage() != null)
+      {
+            throw new RuntimeException(res.getErrorMessage());
+      }
+    }
+
+    catch (Exception e)
+    {
+      log.error("!!! Unable to ACK controller command with id: " + id, e);
+    }
+
+    finally
+    {
+      if (cr != null)
+      {
+        cr.release();
+      }
+    }
+  }
+   
+   
+  private void unlinkController(ControllerCommandDTO controllerCommandDTO)
+  {
+    this.stop();
+    this.controllerDTO = null;
+    this.deployer.unlinkController();
+  }
+   
+  private void executeCommand(ControllerCommandDTO controllerCommandDTO)
+  {
+    switch (controllerCommandDTO.getCommandTypeEnum())
+    {
+      case INITIATE_PROXY:
+          initiateProxy(controllerCommandDTO);
+          break;
+
+      case UNLINK_CONTROLLER:
+          unlinkController(controllerCommandDTO);
+          break;
+
+      default:
+          log.error("ControllerCommand not implemented yet: " + controllerCommandDTO.getCommandType());
+    }
+  }
+   
+  private class BeehiveCommandChecker extends Thread
+  {
+      
+    private boolean running;
+      
+    BeehiveCommandChecker()
+    {
+      super("BeehiveCommandChecker");
+    }
+
+    @SuppressWarnings("unchecked")
+    public void run()
+    {
+      //As long as we are not linked to an account we periodically try to receive account info
+      int sleepTime = controllerConfig.getBeehiveCommandServiceCheckInterval();
+
+      Client c = new Client(new Context(), Protocol.HTTP);
+
+      while (isRunning())
+      {
+        ClientResource cr = null;
+
+        try
+        {
+          cr = new ClientResource( controllerConfig.getBeehiveControllerCommandServiceRESTRootUrl() + "commands/" + controllerDTO.getOid());
+          cr.setNext(c);
+
+          String username = deployer.getUserName();
+
+          cr.setChallengeResponse(ChallengeScheme.HTTP_BASIC, username, deployer.getPassword(username));
+          Representation r = cr.get();
+          String str;
+          str = r.getText();
+
+          GenericResourceResultWithErrorMessage res =
+              new JSONDeserializer<GenericResourceResultWithErrorMessage>()
+                  .use(null, GenericResourceResultWithErrorMessage.class)
+                  .use("result", ArrayList.class)
+                  .use("result.values", ControllerCommandDTO.class).deserialize(str);
+
+          if (res.getErrorMessage() != null)
+          {
+            throw new RuntimeException(res.getErrorMessage());
+          }
+
+          List<ControllerCommandDTO> commands = (ArrayList<ControllerCommandDTO>)res.getResult();
+
+          if (!commands.isEmpty())
+          {
+            executeCommand(commands.get(0));
+          }
+        }
+
+        catch (Exception e)
+        {
+          log.error("!!! Unable to check for new controller command from Beehive", e);
+        }
+
+        finally
+        {
+          if (cr != null)
+          {
+            cr.release();
+          }
+        }
+
+        try { Thread.sleep(sleepTime); } catch (InterruptedException e) {} //Let's wait 30 seconds
+      }
+    }
+
+    public boolean isRunning()
+    {
+         return running;
+    }
+
+    public void setRunning(boolean running)
+    {
+         this.running = running;
+    }
+  }
 }
+
