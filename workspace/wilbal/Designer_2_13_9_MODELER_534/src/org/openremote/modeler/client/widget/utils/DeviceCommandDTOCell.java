a007ddd765d01556e3822358ca738522c537fb70
wilbal <wilbal@9621b191-8951-0410-821f-b8f8a45b0b95>
2014-06-02 16:16:08 +0000

    MODELER-534 : drag and drop implementation in macro window( gquery dnd)

--- workspace/wilbal/Designer_2_13_9_MODELER_534/src/org/openremote/modeler/client/widget/utils/DeviceCommandDTOCell.java
+++ workspace/wilbal/Designer_2_13_9_MODELER_534/src/org/openremote/modeler/client/widget/utils/DeviceCommandDTOCell.java
@@ -1,17 +1,13 @@
 package org.openremote.modeler.client.widget.utils;
 
 import org.openremote.modeler.client.icon.Icons;
-import org.openremote.modeler.shared.dto.DeviceCommandDTO;
-import org.openremote.modeler.shared.dto.DeviceDTO;
+import org.openremote.modeler.shared.dto.MacroItemDetailsDTO;
 
 import com.google.gwt.cell.client.AbstractCell;
-import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 
-public class DeviceCommandDTOCell extends AbstractCell<DeviceCommandDTO> {
+public class DeviceCommandDTOCell extends AbstractCell<MacroItemDetailsDTO> {
 
   private static final Icons ICON = GWT.create(Icons.class);
   private final String imageHtml;
@@ -22,7 +18,7 @@ public class DeviceCommandDTOCell extends AbstractCell<DeviceCommandDTO> {
 
   @Override
   public void render(com.google.gwt.cell.client.Cell.Context context,
-      DeviceCommandDTO value, SafeHtmlBuilder sb) {
+      MacroItemDetailsDTO value, SafeHtmlBuilder sb) {
     if (value != null) {
        sb.appendHtmlConstant("<div style='margin:-1px -5px -5px -5px;' >");
        sb.appendHtmlConstant(imageHtml).appendEscaped(" ");
