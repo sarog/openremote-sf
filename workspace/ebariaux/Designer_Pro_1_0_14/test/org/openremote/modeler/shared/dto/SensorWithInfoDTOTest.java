5c212a841b4901d0cd9efefec617c403bea77735
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2014-08-11 14:02:58 +0000

    Merged MODELER-501, r8836:8837. All tests are now passing.

--- workspace/ebariaux/Designer_Pro_1_0_14/test/org/openremote/modeler/shared/dto/SensorWithInfoDTOTest.java
+++ workspace/ebariaux/Designer_Pro_1_0_14/test/org/openremote/modeler/shared/dto/SensorWithInfoDTOTest.java
@@ -37,37 +37,37 @@ public class SensorWithInfoDTOTest {
     SensorWithInfoDTO sensor1 = new SensorWithInfoDTO();
     SensorWithInfoDTO sensor2 = new SensorWithInfoDTO();
     
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
     
     sensor1.setOid(IDUtil.nextID());
     sensor2.setOid(sensor1.getOid());
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
 
     sensor1.setDisplayName("Name");
     sensor2.setDisplayName("Name");
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
 
     sensor1.setType(SensorType.SWITCH);
     sensor2.setType(SensorType.SWITCH);
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
 
     sensor1.setCommandName("Command name");
     sensor2.setCommandName("Command name");
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
 
     sensor1.setMinValue("0");
     sensor2.setMinValue("0");
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
 
     sensor1.setMaxValue("100");
     sensor2.setMaxValue("100");
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
 
     ArrayList<String> states = new ArrayList<String>();
     states.add("On");
@@ -76,7 +76,7 @@ public class SensorWithInfoDTOTest {
     sensor1.setStateNames(states);
     sensor2.setStateNames(states);
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
   }
   
   @Test
@@ -103,73 +103,73 @@ public class SensorWithInfoDTOTest {
     sensor2.setMaxValue("100");
     sensor2.setStateNames(states);
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
     
     sensor2.setOid(null);
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, second id is not set");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, second id is not set");
 
     sensor2.setOid(IDUtil.nextID());
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, id is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, id is different");
 
     sensor2.setOid(sensor1.getOid());
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
     
     sensor2.setDisplayName(null);
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, second displayName is not set");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, second displayName is not set");
     
     sensor2.setDisplayName("Name 2");
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, displayName is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, displayName is different");
 
     sensor2.setDisplayName("Name");
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
     
     sensor2.setType(null);
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, second type is not set");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, second type is not set");
 
     sensor2.setType(SensorType.CUSTOM);
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, type is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, type is different");
 
     sensor2.setType(SensorType.SWITCH);
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
     
     sensor2.setMinValue(null);
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, second minValue is not set");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, second minValue is not set");
 
     sensor2.setMinValue("1");
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, minValue is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, minValue is different");
 
     sensor2.setMinValue("0");
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
     
     sensor2.setMaxValue(null);
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, second maxValue is not set");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, second maxValue is not set");
 
     sensor2.setMaxValue("1");
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, maxValue is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, maxValue is different");
 
     sensor2.setMaxValue("100");
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
     
     sensor2.setStateNames(null);
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, second stateNames is not set");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, second stateNames is not set");
 
     sensor2.setStateNames(new ArrayList<String>());
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, stateNames is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, stateNames is different");
     
     ArrayList<String> states2 = new ArrayList<String>();
     states2.add("Foo");
 
     sensor2.setStateNames(states2);
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, stateNames is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, stateNames is different");
     
     states2.add("On");
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, stateNames is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, stateNames is different");
 
     states2.add("Off");
-    Assert.assertFalse(sensor1.equals(sensor2), "Expected the SensorWithInfoDTO to be different, stateNames is different");
+    Assert.assertFalse(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be different, stateNames is different");
 
     states2.remove("Foo");
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
   }
   
   @Test
@@ -189,7 +189,7 @@ public class SensorWithInfoDTOTest {
 
     SensorWithInfoDTO sensor2 = new SensorWithInfoDTO(sensor1.getOid(), "Name", SensorType.SWITCH, "Command name", "0", "100", states);
 
-    Assert.assertEquals(sensor1, sensor2, "Expected the SensorWithInfoDTO to be equal");
+    Assert.assertTrue(sensor1.equalityEquals(sensor2), "Expected the SensorWithInfoDTO to be equal");
   }
 
 }
