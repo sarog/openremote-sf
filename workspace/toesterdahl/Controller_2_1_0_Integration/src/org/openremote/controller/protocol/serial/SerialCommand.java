7bc80781c0a92a37385b62ccb79e2dd4086ae770
tosterdahl <tosterdahl@9621b191-8951-0410-821f-b8f8a45b0b95>
2014-08-26 21:10:33 +0000

    Added SerialCommand implementation based on Port framework. Added serial and port-dev log categories.

--- workspace/toesterdahl/Controller_2_1_0_Integration/src/org/openremote/controller/protocol/serial/SerialCommand.java
+++ workspace/toesterdahl/Controller_2_1_0_Integration/src/org/openremote/controller/protocol/serial/SerialCommand.java
@@ -1,117 +1,95 @@
-/*
- * OpenRemote, the Home of the Digital Home. Copyright 2008-2011, OpenRemote Inc.
- * 
- * See the contributors.txt file in the distribution for a full listing of individual contributors.
- * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
- * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Affero General Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
- */
 package org.openremote.controller.protocol.serial;
 
-import gnu.io.SerialPort;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Map;
+import java.nio.charset.Charset;
 import java.util.StringTokenizer;
-import java.util.regex.*;
-import org.apache.log4j.Logger;
-import org.openremote.controller.Constants;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.CharSet;
 import org.openremote.controller.command.ExecutableCommand;
-import org.openremote.controller.command.StatusCommand;
-import org.openremote.controller.component.EnumSensorType;
-import org.openremote.controller.model.sensor.Sensor;
+import org.openremote.controller.protocol.port.Message;
+import org.openremote.controller.protocol.port.Port;
+import org.openremote.controller.protocol.port.PortException;
+import org.openremote.controller.utils.Logger;
 
 /**
- * 
- * Based on TCP/IP Socket protocol
- * 
- * @author Lawrie Griffiths
+ * @author toesterdahl - Torbjörn Österdahl
  */
-public class SerialCommand implements ExecutableCommand, StatusCommand {
-   // Constants ------------------------------------------------------------------------------------
-   public final static String SERIAL_PROTOCOL_LOG_CATEGORY = Constants.CONTROLLER_PROTOCOL_LOG_CATEGORY + "serial";
-   // Class Members --------------------------------------------------------------------------------
-   private final static Logger logger = Logger.getLogger(SERIAL_PROTOCOL_LOG_CATEGORY);
-   // Instance Fields
-   // ----------------------------------------------------------
-   /** A name to identify event in controller.xml. */
-   private String name;
-   /** A pipe separated list of command string that are sent over the socket */
-   private String command;
-   /** The name of the com port that is opened */
-   private String port;
-   /** The data rate for the serial port */
-   private int rate;
-   // The open serial port
-   private SerialPort serialPort;
+public class SerialCommand implements ExecutableCommand {
 
-   public SerialCommand(String name, String port, int rate, String command, SerialPort serialPort) {
-      this.name = name;
+    // Class Members --------------------------------------------------------------------------------
+   private final static Logger logger = Logger.getLogger(SerialCommandBuilder.SERIAL_PROTOCOL_LOG_CATEGORY);
+   
+   private static final Pattern PTN_CR = Pattern.compile("\\\\r");
+
+   private static final Pattern PTN_LN = Pattern.compile("\\\\n");
+
+   private static final Charset US_ASCII = Charset.forName("US-ASCII");
+   
+   private Port port = null;
+   
+   private String name = null;
+   
+   private String command = null;
+
+   private SerialCommand(Port port, String name, String command) {
       this.port = port;
-      this.rate = rate;
+      this.name = name;
       this.command = command;
-      this.serialPort = serialPort;
    }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
+   public static SerialCommand createSerialCommand(Port port, String name, String value) throws PortException, IOException {
+      SerialCommand command  = new SerialCommand(port, name, value);
+      
+      return command;
+   }
+   
    public void send() {
-      requestData();
+      logger.info("SerialCommand command: " + command);
+      try {
+         // TODO (TOE) Consider if ports should be held in some kind of pool. Would it be possible to allow a protocol to hold a port of a limited time and automatically return it if it is not used again within a certain time-out?
+         port.start();
+      } catch (Exception e) {
+         logger.error("Error opening port ", e);
+      }
+
+      try {
+         requestData();
+      } catch (Exception e) {
+         logger.error("Error opening port ", e);
+      } 
+
+      try {
+         port.stop();
+      } catch (Exception e) {
+         logger.error("Error opening port ", e);
+      } 
    }
 
    private String requestData() {
       String reply;
       try {
-         serialPort.setSerialPortParams(rate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-         OutputStream out = serialPort.getOutputStream();
-         InputStream in = serialPort.getInputStream();
          StringTokenizer st = new StringTokenizer(command, "|");
          while (st.hasMoreElements()) {
             String cmd = (String) st.nextElement();
-            byte[] bytes;
+            byte[] bytesIn;
             if (cmd.startsWith("0x")) {
                String tmp = command.substring(2);
-               bytes = hexStringToByteArray(tmp.replaceAll(" ", "").toLowerCase());
+               bytesIn = hexStringToByteArray(tmp.replaceAll(" ", "").toLowerCase());
             } else {
-               bytes = (cmd).getBytes();
+               String unescapedCmd = unescapeCmd(cmd);
+               bytesIn = unescapedCmd.getBytes(US_ASCII);
             }
-            out.write(bytes);
+            port.send(new Message(bytesIn));
          }
-         reply = readReply(in);
-         out.close();
-         in.close();
+         Message out = port.receive();
+         byte[] bytesOut = out.getContent();
+         reply = new String(bytesOut, US_ASCII);
          return reply;
       } catch (Exception e) {
          logger.error("Serial: Error sending command to port: " + port, e);
          return null;
-      }
-   }
-
-   private String readReply(InputStream inputStream) throws IOException {
-      StringBuilder b = new StringBuilder();
-      while (true) {
-         int c = readByte(inputStream);
-         if (c == '\r') return b.toString();
-         else
-            b.append((char) c);
-      }
-   }
-
-   public int readByte(InputStream input) throws IOException {
-      for (;;) {
-         int i = input.read();
-         if (i >= 0) return (i & 0xFF);
       }
    }
 
@@ -123,53 +101,23 @@ public class SerialCommand implements ExecutableCommand, StatusCommand {
       }
       return data;
    }
-
-   @Override
-   public String read(EnumSensorType sensorType, Map<String, String> stateMap) {
-      String regexResult = null;
-      String strState = null;
-      // Request data from the serial device
-      String rawResult = requestData();
-      // Strip response from control characters (non-ASCII)...
-      //
-      // Patch provided by Phillip Lavender
-      Pattern p = Pattern.compile("\\p{Cntrl}");
-      Matcher m = p.matcher(rawResult);
-      regexResult = m.replaceAll("");
-      if ("".equals(regexResult)) {
-         return UNKNOWN_STATUS;
+   
+   private String unescapeCmd(String cmd) {
+//      String s1 = cmd.replaceAll("\\\\r", "\r");
+//      String s2 = s1.replaceAll("\\\\n", "\n");
+      Matcher matchCR = PTN_CR.matcher(cmd);
+      String s1 = matchCR.replaceAll("\r");
+      Matcher matchLN = PTN_LN.matcher(s1);
+      String s2 =matchLN.replaceAll("\n");
+      return s2;
+   }
+   
+   public static void main(String[] args) {
+      SerialCommand app = new SerialCommand(null, "/dev/ttyUSB0", "A1\\r");
+      String unescaped = app.unescapeCmd(app.command);
+      byte[] bytes = unescaped.getBytes(US_ASCII);
+      for (byte b:bytes) {
+         System.out.println("" + b);
       }
-      switch (sensorType) {
-      case RANGE:
-         break;
-      case LEVEL:
-         String min = stateMap.get(Sensor.RANGE_MIN_STATE);
-         String max = stateMap.get(Sensor.RANGE_MAX_STATE);
-         try {
-            int val = Integer.valueOf(regexResult);
-            if (min != null && max != null) {
-               int minVal = Integer.valueOf(min);
-               int maxVal = Integer.valueOf(max);
-               return String.valueOf(100 * (val - minVal) / (maxVal - minVal));
-            }
-         } catch (ArithmeticException e) {
-            logger.warn("Level sensor values cannot be parsed: " + e.getMessage(), e);
-         }
-         break;
-      default:// NOTE: if sensor type is RANGE, this map only contains min/max states.
-         // If custom sensor type has been configured, map the 'raw' return value to configured
-         // 'wanted' return value
-         //
-         // TODO :
-         // no reason to put this implementation burden on protocol implementations, the
-         // calling code could do it instead
-         for (String state : stateMap.keySet()) {
-            strState = stateMap.get(state);
-            if (regexResult.equals(strState)) {
-               return state;
-            }
-         }
-      }
-      return regexResult;
    }
 }
