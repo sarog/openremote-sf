06c4987c97ecd653f67ebbc95028bcacc5000077
kurrazyman <kurrazyman@9621b191-8951-0410-821f-b8f8a45b0b95>
2014-10-03 10:49:00 +0000

    Fixed ANDROID-118

--- workspace/richturner/Android_Console_2_0_0_Betas/src/org/openremote/android/console/util/AsyncResourceLoader.java
+++ workspace/richturner/Android_Console_2_0_0_Betas/src/org/openremote/android/console/util/AsyncResourceLoader.java
@@ -17,19 +17,20 @@
 * You should have received a copy of the GNU Affero General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-
 package org.openremote.android.console.util;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.openremote.android.console.AppSettingsActivity;
+import org.openremote.android.console.ControllerListActivity;
 import org.openremote.android.console.GenericActivity;
 import org.openremote.android.console.R;
 import org.openremote.android.console.Constants;
 import org.openremote.android.console.GroupActivity;
 import org.openremote.android.console.LoginViewActivity;
 import org.openremote.android.console.Main;
+import org.openremote.android.console.exceptions.AppInitializationException;
 import org.openremote.android.console.exceptions.ControllerAuthenticationFailureException;
 import org.openremote.android.console.exceptions.ORConnectionException;
 import org.openremote.android.console.model.AppSettingsModel;
@@ -42,7 +43,6 @@ import org.xml.sax.SAXException;
 import com.google.inject.Inject;
 
 import roboguice.util.RoboAsyncTask;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -61,6 +61,10 @@ import android.widget.TextView;
  * @author handy 2010-05-10
  * @author Dan Cong
  * @author Andrew D. Ball <aball@osintegrators.com>
+ * 
+ * RT: ANDROID-118: Prevented call() method from throwing exception as this causes the entire app to freeze 
+ * 
+ * @author Rich Turner <richard@openremote.org>
  */
 public class AsyncResourceLoader extends RoboAsyncTask<AsyncResourceLoaderResult>
 {
@@ -68,6 +72,7 @@ public class AsyncResourceLoader extends RoboAsyncTask<AsyncResourceLoaderResult
 
   private static final int TO_LOGIN = 1;
   private static final int TO_GROUP = 2;
+  private static final int TO_CONTROLLER_LIST = 3;
 
   private ControllerService controllerService;
   private Context context;
@@ -108,7 +113,8 @@ public class AsyncResourceLoader extends RoboAsyncTask<AsyncResourceLoaderResult
   /**
    * Download panel.xml and images in the background.
    */
-  public AsyncResourceLoaderResult call() throws Exception
+  @Override
+  public AsyncResourceLoaderResult call()
   {
     final String logPrefix = "call(): ";
 
@@ -141,7 +147,10 @@ public class AsyncResourceLoader extends RoboAsyncTask<AsyncResourceLoaderResult
         {
           Log.i(LOG_CATEGORY, logPrefix + "downloading new image " + imageName);
           InputStream imageStream = controllerService.getResource(imageName);
-          FileUtil.writeStreamToFile(context, imageStream, imageName);
+          if (imageStream != null)
+          {
+            FileUtil.writeStreamToFile(context, imageStream, imageName);
+          }
         }
       }
     }
@@ -158,23 +167,28 @@ public class AsyncResourceLoader extends RoboAsyncTask<AsyncResourceLoaderResult
 
       try
       {
-        FileUtil.parsePanelXML(context);
-        result.setAction(TO_GROUP);
-        result.setCanUseLocalCache(true);
+          FileUtil.parsePanelXML(context);
+          result.setAction(TO_GROUP);
+          result.setCanUseLocalCache(true);
+      }
+      catch (AppInitializationException e1) {
+        Log.e(LOG_CATEGORY, logPrefix + "could not use local cache", e1);
+        result.setCanUseLocalCache(false);
+        result.setAction(TO_CONTROLLER_LIST);
       }
       catch (SAXException se)
       {
         Log.e(LOG_CATEGORY, logPrefix + "parse error on cached panel.xml.  " +
             "not using local cache", e);
         result.setCanUseLocalCache(false);
-        throw se;
+        result.setAction(TO_CONTROLLER_LIST);
       }
       catch (IOException ie)
       {
         Log.e(LOG_CATEGORY, logPrefix + "IO error when trying to parse panel.xml.  " +
             "not using local cache", e);
         result.setCanUseLocalCache(false);
-       throw ie;//throws exception but nothing happens when ok is clicked. Get back to Settings menu if controller is no longer avail
+        result.setAction(TO_CONTROLLER_LIST);
         /**
          * Forward to settings view.
          */
@@ -188,6 +202,10 @@ public class AsyncResourceLoader extends RoboAsyncTask<AsyncResourceLoaderResult
 
  
       }
+    }
+    catch (Exception ex) {
+      Log.e(LOG_CATEGORY, logPrefix + "Error while retrieving resources", ex);
+      result.setAction(TO_CONTROLLER_LIST);
     }
 
     return result;
@@ -257,19 +275,20 @@ public class AsyncResourceLoader extends RoboAsyncTask<AsyncResourceLoaderResult
 
     switch (result.getAction())
     {
-    case TO_GROUP:
-      intent.setClass(context, GroupActivity.class);
-      break;
-
-    case TO_LOGIN:
-      intent.setClass(context, LoginViewActivity.class);
-      intent.setData(Uri.parse(Main.LOAD_RESOURCE));
-      break;
-
-    default:
-      ViewHelper.showAlertViewWithTitle(context, "Send Request Error",
-              ControllerException.exceptionMessageOfCode(result.getStatusCode()));
-      return;
+      case TO_GROUP:
+        intent.setClass(context, GroupActivity.class);
+        break;  
+      case TO_LOGIN:
+        intent.setClass(context, LoginViewActivity.class);
+        intent.setData(Uri.parse(Main.LOAD_RESOURCE));
+        break;
+      case TO_CONTROLLER_LIST:
+        intent.setClass(context, ControllerListActivity.class);
+        break;        
+      default:
+        ViewHelper.showAlertViewWithTitle(context, "Send Request Error",
+                ControllerException.exceptionMessageOfCode(result.getStatusCode()));
+        return;
     }
 
     if (activity != null)
