ed5a779941d52b9a72d634091aadd022f31fc1ce
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2014-10-14 13:05:12 +0000

    Messed up the branch by doing 2 copy in a row, re-doing from scratch

--- workspace/ebariaux/Designer_2_13_9_MODELER-540/src/org/openremote/modeler/client/event/MacrosCreatedEventHandler.java
+++
@@ -1,9 +0,0 @@
-package org.openremote.modeler.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface MacrosCreatedEventHandler extends EventHandler {
-
-  void onMacrosCreated(MacrosCreatedEvent event);
-  
-}
