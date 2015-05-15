8ae34991fab6bc8c461368954c380028da9e43e0
juhalindfors <juhalindfors@9621b191-8951-0410-821f-b8f8a45b0b95>
2015-05-15 23:15:46 +0000

    branch for ORCJAVA-478: migrate device discovery to object model api

---
+++ workspace/juha/Controller_2_1_1_ORCJAVA-478/test/org/openremote/controller/protocol/ictprotege/network/ProtegeTCPReaderTest.java
@@ -0,0 +1,79 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2012, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.ictprotege.network;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Adam Mcnabb
+ */
+public class ProtegeTCPReaderTest {
+    
+    public ProtegeTCPReaderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of run method, of class ProtegeTCPReader.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        ProtegeTCPReader instance = null;
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of requestStop method, of class ProtegeTCPReader.
+     */
+    @Test
+    public void testRequestStop() {
+        System.out.println("requestStop");
+        ProtegeTCPReader instance = null;
+        instance.requestStop();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
