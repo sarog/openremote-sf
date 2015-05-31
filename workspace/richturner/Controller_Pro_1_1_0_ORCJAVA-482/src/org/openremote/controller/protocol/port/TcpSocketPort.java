b50171504e48feead6c4e4aaed62db8645b07330
kurrazyman <kurrazyman@9621b191-8951-0410-821f-b8f8a45b0b95>
2015-05-31 16:01:02 +0000

    Implemented ORCJAVA-482

---
+++ workspace/richturner/Controller_Pro_1_1_0_ORCJAVA-482/src/org/openremote/controller/protocol/port/TcpSocketPort.java
@@ -0,0 +1,196 @@
+/*
+ * OpenRemote, the Home of the Digital Home.
+ * Copyright 2008-2013, OpenRemote Inc.
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openremote.controller.protocol.port;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Map;
+
+import org.openremote.controller.Constants;
+import org.openremote.controller.utils.Logger;
+
+/**
+ * A physical port (bus) using a tcp socket to send and receive messages.
+ * 
+ * Can optionally specify the start and end bytes of a message frame, the packet size
+ * or a packet processor implementation for customised packet processing
+ * 
+ * @see Port
+ *
+ * @author Richard Turner
+ */
+public class TcpSocketPort implements Port
+{
+  /**
+   * A Packet Processor delegate that builds the packet and indicates
+   * when a packet is completely assembled
+   * @author <a href="mailto:richard@openremote.org">Richard Turner</a>
+   *
+   */
+  public interface PacketProcessor {
+    /**
+     * Process the new byte read from the socket and optionally
+     * add it to the packet being built; return true if the
+     * packet is completely assembled and ready to be returned
+     * otherwise return false
+     * @param packet
+     * @param newByte
+     * @return
+     */
+    boolean processByte(ByteArrayOutputStream packet, byte newByte);
+    
+    /**
+     * Called just before a packet is returned to the caller; allows
+     * the packet to be validated; return true if valid otherwise false
+     * @param packet
+     * @return
+     */
+    boolean packetIsValid(ByteArrayOutputStream packet);
+  }
+
+  public final static String TCP_PORT_LOG_CATEGORY = Constants.CONTROLLER_PROTOCOL_LOG_CATEGORY + "port.tcp";
+  private final static Logger log = Logger.getLogger(TCP_PORT_LOG_CATEGORY);
+  private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+  private Byte startByte;
+  private Byte endByte;
+  private Integer packetSize;
+  private PacketProcessor processor;
+  
+  private Socket socket;
+
+  @Override
+  public void configure(Map<String, Object> configuration)
+  {
+    this.socket = (Socket) configuration.get("socket");
+    this.processor = (PacketProcessor) configuration.get("processor");
+    this.startByte = (Byte)configuration.get("startByte");
+    this.endByte = (Byte)configuration.get("endByte");
+    this.packetSize = (Integer)configuration.get("packetSize");
+  }
+
+   @Override
+   public void start() {
+   }
+
+   @Override
+   public void stop() {
+    if (this.socket != null)
+    try {
+      this.socket.close();
+    } catch (IOException e) {
+      log.error(e.getMessage(), e);
+    }
+   }
+
+   @Override
+   public void send(Message message) throws IOException {
+     //System.out.println("Packet Send: " + bytesToHex(message.getContent()));
+     OutputStream out = socket.getOutputStream();
+     out.write(message.getContent());
+     out.flush();
+   }
+
+
+  @Override public Message receive() throws IOException
+  {
+    if (socket == null) {
+      throw new IOException("Configuration error, listening socket is null and cannot receive!");
+    }
+    
+//    if (packetSize == null && startByte == null && endByte == null || (packetSize == null && (startByte == null || endByte == null)) {
+//      throw new IOException("Configuration error, either packet size or start and end bytes must be set!");
+//    }
+    
+//    if (packetSize != null) {
+//      InputStream is = socket.getInputStream();
+//      byte[] buffer = new byte[packetSize];
+//      int offset = 0;
+//      while (offset < packetSize) {
+//          int bytesRead = is.read(buffer, offset, packetSize - offset);
+//          if (bytesRead == -1) { 
+//              // Incomplete packet - handle however you want to
+//          }
+//          offset += bytesRead;
+//      }
+//    }
+
+    InputStream is = socket.getInputStream();
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    byte[] buffer = new byte[1];
+    
+    while (is.read(buffer,0, 1)  != -1) {
+      // If packet processor defined then delegate packet logic to it
+      if (processor != null) {
+        if (processor.processByte(baos, buffer[0])) {
+
+          // Validate the packet
+          if (!processor.packetIsValid(baos)) {
+            baos.reset();
+            System.out.println("Processor deemed packet as invalid");
+          } else {
+            break;
+          }
+        }
+        continue;
+      }      
+      
+      if (baos.size() == 0 && startByte != null) {
+        // If start byte is defined check that it has been found
+        if (buffer[0] != startByte.byteValue()) {
+          // Ignore and continue
+          continue;
+        }
+      }
+
+      // Add read byte to the packet
+      baos.write(buffer, 0, 1);
+      
+      // If packet size defined check if that has been reached
+      if (packetSize != null && baos.size() == packetSize.intValue()) {
+        break;
+      }
+      
+      // If end byte defined check if it has been found
+      if (baos.size() > 0 && endByte != null) {
+        if (buffer[0] == endByte.byteValue()) {
+          break;
+        }
+      }
+    }
+    
+    byte[] packetBytes = baos.toByteArray();
+    //System.out.println("Packet Received: " + bytesToHex(packetBytes));
+    return new Message(packetBytes);
+  }
+  
+  public static String bytesToHex(byte[] bytes) {
+      char[] hexChars = new char[bytes.length * 2];
+      for ( int j = 0; j < bytes.length; j++ ) {
+          int v = bytes[j] & 0xFF;
+          hexChars[j * 2] = hexArray[v >>> 4];
+          hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+      }
+      return new String(hexChars);
+  }
+}
