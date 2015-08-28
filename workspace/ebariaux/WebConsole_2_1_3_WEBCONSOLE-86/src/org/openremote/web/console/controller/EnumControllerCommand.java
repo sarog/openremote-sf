7cbe8bc9d06981bf3f8378a188849ae3dbc46cbc
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2015-08-28 18:21:18 +0000

    Branch to isolate work on WEBCONSOLE-86

---
+++ workspace/ebariaux/WebConsole_2_1_3_WEBCONSOLE-86/src/org/openremote/web/console/controller/EnumControllerCommand.java
@@ -0,0 +1,45 @@
+/* OpenRemote, the Home of the Digital Home.
+* Copyright 2008-2012, OpenRemote Inc.
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openremote.web.console.controller;
+
+public enum EnumControllerCommand {
+	GET_PANEL_LIST,
+	GET_PANEL_LAYOUT,
+	SEND_COMMAND,
+	GET_SENSOR_STATUS,
+	DO_SENSOR_POLLING,
+	GET_ROUND_ROBIN_LIST,
+	IS_ALIVE,
+	IS_SECURE,
+	LOGOUT;
+	
+   @Override
+   public String toString() {
+      return super.toString().toLowerCase();
+   }
+   
+   public static EnumControllerCommand enumValueOf(String commandActionTypeValue) {
+   	EnumControllerCommand result = null;
+      try {
+         result = Enum.valueOf(EnumControllerCommand.class, commandActionTypeValue.toUpperCase());
+      } catch (Exception e) {}
+      return result;
+   }
+}
