b3e01e014b378b2a6aa9da6a53221120fe2b8aed
ebariaux <ebariaux@9621b191-8951-0410-821f-b8f8a45b0b95>
2016-02-05 13:32:03 +0000

    Freeze for ProInstaller v1.4.1 build 20160205

---
+++ Designer/Designer_ProInstaller_1_4_1_20160205/src/org/openremote/modeler/shared/dto/MacroItemDTO.java
@@ -0,0 +1,62 @@
+package org.openremote.modeler.shared.dto;
+
+public class MacroItemDTO implements DTO {
+
+  private static final long serialVersionUID = 1L;
+
+  private String displayName;
+  private MacroItemType type;
+  
+  public MacroItemDTO() {
+    super();
+  }
+  
+  public MacroItemDTO(String displayName, MacroItemType type) {
+    super();
+    this.displayName = displayName;
+    this.type = type;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+  
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+  
+  public MacroItemType getType() {
+    return type;
+  }
+  
+  public void setType(MacroItemType type) {
+    this.type = type;
+  }
+
+  public int equalityHashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((displayName == null) ? 0 : displayName.hashCode());
+    result = prime * result + ((type == null) ? 0 : type.hashCode());
+    return result;
+  }
+
+  public boolean equalityEquals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    MacroItemDTO other = (MacroItemDTO) obj;
+    if (displayName == null) {
+      if (other.displayName != null)
+        return false;
+    } else if (!displayName.equals(other.displayName))
+      return false;
+    if (type != other.type)
+      return false;
+    return true;
+  }
+  
+}
